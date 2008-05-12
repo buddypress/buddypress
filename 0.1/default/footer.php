b51846766d2c7da6c7cc076488f6a8e08f93a0d2
Andy Peatling <apeatling@gmail.com>
2008-05-12 22:36:55 +0000

    tagging default theme v0.1
    git-svn-id: https://buddypress.svn.wordpress.org/tags/buddypress-theme@99 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

---
+++ 0.1/default/footer.php
@@ -0,0 +1,12 @@
+
+<?php $current_site = get_current_site(); ?>
+<hr />
+</div>
+
+
+<!-- Gorgeous design by Michael Heilemann - http://binarybonsai.com/kubrick/ -->
+<?php /* "Just what do you think you're doing Dave?" */ ?>
+
+		<?php wp_footer(); ?>
+</body>
+</html>
