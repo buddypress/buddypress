817e38b2ac68c54f9bf1e7f90596e53b35926755
jbasdf <nobody@localhost>
2008-05-15 15:50:01 +0000

    updated bp_friends template tags
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@117 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-friends/bp-friends-templatetags.php
+++ bp-friends/bp-friends-templatetags.php
@@ -2,8 +2,6 @@
 
 Class BP_Friends_Template {
 	
-	var $user_id;
-	
 	function bp_friends_template() {
 		global $authordata;
 		$this->user_id = $authordata->ID;
@@ -12,13 +10,35 @@ Class BP_Friends_Template {
 }
 
 function bp_list_friends() { 
-	global $friends_template;
-	return $friends_template->list_friends();
+	$bp_friends = new BP_Friends();
+	$friends = $bp_friends->get_friends();
 }
 
-function bp_find_friends() { 
-	global $friends_template;
-	return $friends_template->find_friends();
+function bp_validate_friend_search_terms() {
+	$message = null;
+	if(isset($_POST['searchterm']) && isset($_POST['search']))
+	{
+		if($_POST['searchterm'] == "")
+		{
+			$message = __("Please make sure you enter something to search for.");
+		}
+		else if(strlen($_POST['searchterm']) < 3)
+		{
+			$message = __("Your search term must be longer than 3 letters.");
+		}
+	}
+	return $message;
 }
 
+function bp_output_find_friends_message($message) {
+	if ( isset($message) ) {
+		if($type == 'error') { $type = "error"; } else { $type = "updated"; } ?>
+			<div id="message" class="<?php echo $type; ?>">
+				<p><?php echo $message; ?></p>
+			</div> <?php
+		return true;
+	} else {
+		return false;
+	}
+}
 ?>
