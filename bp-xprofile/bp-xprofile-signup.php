7121f93e364c3da34f76ea5d2ad7a95f04b4e1e5
jbasdf <nobody@localhost>
2008-05-15 23:24:16 +0000

    Removed cellpadding and spacing from table
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@120 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-xprofile/bp-xprofile-signup.php
+++ bp-xprofile/bp-xprofile-signup.php
@@ -14,7 +14,7 @@ function xprofile_add_signup_fields() {
 	
 	if ( $fields ) {
 	?>
-	<table width="100%" cellspacing="4" cellpadding="9" border="0" id="extraFields">
+	<table width="100%" border="0" id="extraFields">
 		<tbody>
 			<tr>
 				<td>
