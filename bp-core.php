b95302a9ed4c7c821ab6b2b377ef7d2080a39a4d
Andy Peatling <apeatling@gmail.com>
2008-05-21 01:33:03 +0000

    Removed paginations functions from main core file. New pagination file now holds all pagination functions.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@129 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-core.php
+++ bp-core.php
@@ -1,7 +1,8 @@
 <?php
 
-include_once( ABSPATH . 'wp-content/mu-plugins/bp-core/bp-core-thirdlevel.php' );
-include_once( ABSPATH . 'wp-content/mu-plugins/bp-core/bp-core-settingstab.php' );
+require_once( ABSPATH . 'wp-content/mu-plugins/bp-core/bp-core-thirdlevel.php' );
+require_once( ABSPATH . 'wp-content/mu-plugins/bp-core/bp-core-settingstab.php' );
+require_once( ABSPATH . 'wp-content/mu-plugins/bp-core/bp-core-pagination.php' );
 
 
 if ( !get_site_option('bp_disable_blog_tab') ) {
@@ -248,6 +249,17 @@ function bp_core_get_username( $uid ) {
 	return $ud->user_login;	
 }
 
+function bp_core_get_blogdetails( $domain ) {
+	global $wpdb;
+	
+	$sql = $wpdb->prepare("SELECT * FROM $wpdb->site WHERE domain = %s", $domain);
+	
+	if ( !$blog = $wpdb->get_row($sql) )
+		return false;
+	
+	return $blog;
+}
+
 function bp_core_get_userlink( $uid ) {
 	global $userdata;
 	
@@ -379,143 +391,6 @@ function bp_int( $var, $nullToOne=false ) {
 	}
 }
 
-// get the start number for pagination
-function bp_get_page_start( $p, $num ) {
-	$p   = bp_int($p);
-	$num = bp_int($num);
-	
-	if ( $p == "" ) {
-		return 0;
-	} else {
-		return ( $p * $num ) - $num;
-	}
-}
-
-// get the page number from the $_GET["p"] variable
-function bp_get_page() {
-	if ( isset( $_GET["p"] ) ) {
-		return (int) $_GET["p"]; 		
-	}
-	else {
-		return 1;
-	}		
-}
-
-// generate page links
-function bp_generate_pages_links( $totalRows, $maxPerPage = 25, $linktext = "", $var, $attributes = "" ) {
-    // loop all the pages in the result set
-    for ( $i = 1; $i <= ceil( $totalRows / $maxPerPage ); $i++ ) {
-		// if the current page is different to this link, create the querystring
-		$page = bp_int( @$var, true );
-		if ($i != $page)
-		{
-			if ( $linktext == "" ) {
-				$link = "?p=" . $i;
-			} else {
-				$link = str_replace( "%%", $i, $linktext );
-			}
-			$links["link"][] = $link;
-			$links["text"][] = $i;
-			$links["attributes"][] = str_replace( "%%", $i, $attributes );
-		// otherwise make the link empty
-		} else {
-			$links["link"][] = "";
-			$links["text"][] = $i;
-			$links["attributes"][] = str_replace( "%%", $i, $attributes );
-		}
-    }
-    // return the links
-    return $links;
-}
-
-// generate page link list
-function bp_paginate( $links, $currentPage = 1, $firstItem = "", $listclass = "" ) {
-	$return = "";
-	// check the parameter is an array with more than 1 items in
-	if ( is_array($links) && count($links["text"]) > 1 ) {
-		// get the total number of links
-		$totalPages = count($links["text"]);
-		
-		// set showstart and showend to false
-		$showStart = false;
-		$showEnd   = false;
-		
-		// if the total number of pages is greater than 10
-		if ( $totalPages > 10 ) {
-			
-			// if the current page is less than 5 from the start
-			if ( $currentPage <= 5 ) {
-				// set the minimum and maximum pages to show
-				$minimum = 0;
-				$maximum = 9;
-				$showEnd = true;
-			}
-			
-			// if the current page is less than 5 from the end
-			if ( $currentPage >= ( $totalPages - 5 ) ) {
-				// set the minumum and maximum pages to show
-				$minimum   = $totalPages - 9;
-				$maximum   = $totalPages;
-				$showStart = true;
-			}
-			
-			// if the current page is somewhere in the middle
-			if ( $currentPage > 5 && $currentPage < ( $totalPages - 5 ) )
-			{
-				$showEnd   = true;
-				$showStart = true;
-				$minimum   = $currentPage - 4;
-				$maximum   = $currentPage + 4;
-			}
-			
-		} else {
-			$minimum = 0;
-			$maximum = $totalPages;
-		}
-		
-		// print the start of the list
-		$return .= "\n\n<ul class=\"pagelinks";
-		
-		if ( $listclass != "" )
-			$return .= " ".$listclass;
-			
-		$return .= "\">\n";
-		
-		// print the first item, it if is set
-		if ( $firstItem != "" ) {
-			$return .= "<li>" . $firstItem . "</li>\n";
-		}
-		
-		// print the page text
-		$return .= "<li>Pages:</li>\n";
-		
-		// if set, show the start
-		if ( $showStart )
-			$return .= "<li><a href=\"" . str_replace( "&", "&amp;", $links["link"][0] ) . "\">" . $links["text"][0] . "...</a></li>\n";
-
-		// loop the links
-		for ( $i = $minimum; $i < $maximum; $i++ ) {
-			if ( $i == ( $currentPage - 1 ) ) {
-				$url = "<li class=\"current\">" . $links["text"][$i] . "</li>\n";
-			} else {
-				if ($links["attributes"][$i] != "")
-					$attributes = " " . $links["attributes"][$i];
-				else
-					$attributes = "";
-					
-				$url = "<li><a href=\"" . str_replace( "&", "&amp;", $links["link"][$i] ) . "\"" . $attributes . ">" . $links["text"][$i] . "</a></li>\n";
-			}
-			$return .= $url;
-		}
-		// if set, show the end
-		if ( $showEnd ) {
-			$return .= "<li><a href=\"" . str_replace( "&", "&amp;", $links["link"][$totalPages - 1]) . "\">..." . $links["text"][$totalPages-1] . "</a></li>\n";
-		}
-		$return .= "</ul>\n\n";
-	}
-	
-	return $return;
-}
 
 // show a friendly date
 function bp_friendly_date($timestamp) {
