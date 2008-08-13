d9b159bd37d20d7406d67734d1ce390b77509281
Andy Peatling <apeatling@gmail.com>
2008-08-13 22:38:24 +0000

    tagging buddypress default theme v0.2.2
    git-svn-id: https://buddypress.svn.wordpress.org/tags/buddypress-theme@272 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

---
+++ 0.2.2/buddypress-theme/home/index.php
@@ -0,0 +1,46 @@
+<?php get_header(); ?>
+
+	<div id="content">
+	
+		<?php if ( has_profile() ) : ?>
+			<div id="profile-name">
+				<h2><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h2>
+				<div class="description"><?php bloginfo('description'); ?></div>
+			</div>		
+			
+			<?php while ( profile_groups() ) : the_profile_group(); ?>
+
+				<?php if ( group_has_fields() ) : ?>
+					<div class="profile-group">
+						<h3><?php the_profile_group_name() ?></h3>
+						
+						<table class="profile-fields">
+						<?php while ( profile_fields() ) : the_profile_field(); ?>
+							
+							<?php if ( field_has_data() ) : ?>
+							<tr>
+								<td class="label">
+									<?php the_profile_field_name() ?>
+								</td>
+								<td class="data">
+									<?php the_profile_field_value() ?>
+								</td>
+							</tr>
+							<?php endif; ?>
+							
+						<?php endwhile; ?>
+						</table>
+					</div>
+				<?php endif; ?>	
+				
+			<?php endwhile; ?>
+			
+		<?php else: ?>
+			
+			<p><?php _e('Sorry, this person does not have a profile.'); ?></p>
+			
+		<?php endif;?>
+	
+	</div>
+	
+<?php get_footer(); ?>
