df5ce43281b5e86803f99599c6dea302cfd3870e
Andy Peatling <apeatling@gmail.com>
2008-08-14 05:39:16 +0000

    Tagging default theme 0.2.3
    git-svn-id: https://buddypress.svn.wordpress.org/tags/buddypress-theme@291 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

---
+++ 0.2.3/buddypress-theme/default/groups/forum.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+	<div class="content-header">
+		
+	</div>
+
+	<div id="content">	
+		<?php if ( bp_has_groups() ) : while ( bp_groups() ) : bp_the_group(); ?>
+			
+		<h2><a href="<?php bp_group_permalink() ?>"><?php bp_group_name() ?></a> &raquo; Forum</h2>
+		
+		<h3>A bbPress forum for the group will go here in the next version.</h3>
+		
+		<?php endwhile; endif; ?>
+		
+		</div>
+	</div>
+
+<?php get_footer() ?>
