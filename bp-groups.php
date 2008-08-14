3581b6bef03c5d6c528fc1583edf7b258ae2718c
Andy Peatling <apeatling@gmail.com>
2008-08-14 05:37:34 +0000

    Updating internal group version number
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@287 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-groups.php
+++ bp-groups.php
@@ -2,7 +2,7 @@
 require_once( 'bp-core.php' );
 
 define ( 'BP_GROUPS_IS_INSTALLED', 1 );
-define ( 'BP_GROUPS_VERSION', '0.1' );
+define ( 'BP_GROUPS_VERSION', '0.1.1' );
 
 include_once( 'bp-groups/bp-groups-classes.php' );
 include_once( 'bp-groups/bp-groups-ajax.php' );
