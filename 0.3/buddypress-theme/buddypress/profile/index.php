3fd3282b19ce28ab000f6b2f7983b1e78be09102
Andy Peatling <apeatling@gmail.com>
2008-09-11 19:57:38 +0000

    Tagging BuddyPress themes v0.3.
    git-svn-id: https://buddypress.svn.wordpress.org/tags/buddypress-theme@316 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

---
+++ 0.3/buddypress-theme/buddypress/profile/index.php
@@ -0,0 +1,58 @@
+<div class="content-header">
+	Header Content
+</div>
+
+<div id="content">
+
+	<div class="left-menu">
+		<?php bp_the_avatar() ?>
+		
+		<?php if ( bp_exists('friends') ) : ?>
+			<?php bp_add_friend_button() ?>
+		<?php endif; ?>
+		
+		<?php //bp_user_groups() ?>
+	</div>
+
+	<div class="main-column">
+	<?php if ( bp_has_profile() ) : ?>
+		<div id="profile-name">
+			<h1><a href="<?php bp_user_link() ?>"><?php bp_user_fullname() ?></a></h1>
+			<p class="status"><?php bp_user_status() ?></p>
+		</div>
+		
+		<?php while ( bp_profile_groups() ) : bp_the_profile_group(); ?>
+
+			<?php if ( bp_group_has_fields() ) : ?>
+				<div class="info-group">
+					<h4><?php bp_the_profile_group_name() ?></h4>
+					
+					<table class="profile-fields">
+					<?php while ( bp_profile_fields() ) : bp_the_profile_field(); ?>
+
+						<?php if ( bp_field_has_data() ) : ?>
+						<tr>
+							<td class="label">
+								<?php bp_the_profile_field_name() ?>
+							</td>
+							<td class="data">
+								<?php bp_the_profile_field_value() ?>
+							</td>
+						</tr>
+						<?php endif; ?>
+
+					<?php endwhile; ?>
+					</table>
+				</div>
+			<?php endif; ?>	
+			
+		<?php endwhile; ?>
+		
+	<?php else: ?>
+		
+		<p><?php _e('Sorry, this person does not have a public profile.'); ?></p>
+		
+	<?php endif;?>
+	</div>
+
+</div>
---
+++ 0.3/buddypress-theme/buddypress/search.php
@@ -0,0 +1,33 @@
+<?php if (have_posts()) : ?>
+
+	<h2>Search Results</h2>
+
+	<div class="navigation">
+		<div><?php next_posts_link('&laquo; Older Entries') ?></div>
+		<div><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+	</div>
+
+
+	<?php while (have_posts()) : the_post(); ?>
+
+		<div class="post">
+			<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+			<p><?php the_time('l, F jS, Y') ?></p>
+			<p class="postmetadata"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
+		</div>
+
+	<?php endwhile; ?>
+
+	<div class="navigation">
+		<div><?php next_posts_link('&laquo; Older Entries') ?></div>
+		<div><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+	</div>
+
+<?php else : ?>
+
+	<h2>No posts found. Try a different search?</h2>
+	<?php include (TEMPLATEPATH . '/searchform.php'); ?>
+
+<?php endif; ?>
+
+<?php get_sidebar(); ?>
