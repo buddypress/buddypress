33a47abdbd7899fc3cbd2d9541f2e9f6a3a97096
Andy Peatling <apeatling@gmail.com>
2008-10-23 14:39:25 +0000

    Tagging buddypress-home theme 0.4
    git-svn-id: https://buddypress.svn.wordpress.org/tags/buddypress-theme@424 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

---
+++ 0.4/buddypress-home/plugin-template.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+	<div id="content" class="narrowcolumn">
+	
+		<?php do_action('bp_template_content') ?>
+
+	</div>
+
+<?php bp_get_plugin_sidebar(); ?>
+
+<?php get_footer(); ?>
---
+++ 0.4/buddypress-home/sidebar.php
@@ -0,0 +1,7 @@
+<div id="sidebar">
+	<?php if ( !function_exists('dynamic_sidebar')
+	        || !dynamic_sidebar('blog-sidebar') ) : ?>
+	
+	
+	<?php endif; ?>
+</div>
