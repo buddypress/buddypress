ba37fc826ea08a419addf00d1b6048ec80f9804a
Andy Peatling <apeatling@gmail.com>
2008-10-29 15:45:48 +0000

    Modifying bp_profile_wire_can_post() so members can post on their own profile again.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@448 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-xprofile/bp-xprofile-templatetags.php
+++ bp-xprofile/bp-xprofile-templatetags.php
@@ -296,6 +296,9 @@ function bp_profile_last_updated() {
 function bp_profile_wire_can_post() {
 	global $bp;
 	
+	if ( bp_is_home() )
+		return true;
+	
 	if ( function_exists('friends_install') ) {
 		if ( friends_check_friendship( $bp['loggedin_userid'], $bp['current_userid'] ) )
 			return true;
