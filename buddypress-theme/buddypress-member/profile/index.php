bab386a822d683c9ad95b50b35942674b59e8170
Andy Peatling <apeatling@gmail.com>
2008-10-29 15:37:47 +0000

    Theme updates to support r446
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@447 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- buddypress-theme/buddypress-member/profile/index.php
+++ buddypress-theme/buddypress-member/profile/index.php
@@ -11,8 +11,6 @@
 		<?php if ( function_exists('bp_add_friend_button') ) : ?>
 			<?php bp_add_friend_button() ?>
 		<?php endif; ?>
-		
-		<?php //bp_user_groups() ?>
 	</div>
 
 	<div class="main-column">
@@ -25,7 +23,7 @@
 		<?php endif; ?>
 		
 		<?php if ( function_exists('bp_activity_get_list') ) : ?>
-			<?php bp_activity_get_list( bp_current_user_id(), bp_word_or_name( __( "My Activity", 'buddypress' ), __( "%s's Activity", 'buddypress' ), true, false ), 5 ) ?>
+			<?php bp_activity_get_list( bp_current_user_id(), bp_word_or_name( __( "My Activity", 'buddypress' ), __( "%s's Activity", 'buddypress' ), true, false ), bp_word_or_name( __( "You haven't done anything recently.", 'buddypress' ), __( "%s has not done anything recently.", 'buddypress' ), true, false ), 5 ) ?>
 		<?php endif; ?>
 		
 		<?php if ( function_exists('bp_groups_random_groups') ) : ?>
@@ -37,7 +35,7 @@
 		<?php endif; ?>
 
 		<?php if ( function_exists('bp_wire_get_post_list') ) : ?>
-			<?php bp_wire_get_post_list( bp_current_user_id(), bp_word_or_name( __( "My Wire", 'buddypress' ), __( "%s's Wire", 'buddypress' ), true, false ), bp_word_or_name( __( "No one has posted to your wire yet.", 'buddypress' ), __( "No one has posted to %s's wire yet.", 'buddypress' ), true, false ) ) ?>
+			<?php bp_wire_get_post_list( bp_current_user_id(), bp_word_or_name( __( "My Wire", 'buddypress' ), __( "%s's Wire", 'buddypress' ), true, false ), bp_word_or_name( __( "No one has posted to your wire yet.", 'buddypress' ), __( "No one has posted to %s's wire yet.", 'buddypress' ), true, false), bp_profile_wire_can_post() ) ?>
 		<?php endif; ?>
 	</div>
 
--- buddypress-theme/buddypress-member/wire/latest.php
+++ buddypress-theme/buddypress-member/wire/latest.php
@@ -19,7 +19,7 @@
 		<?php bp_get_profile_header() ?>
 		
 		<?php if ( function_exists('bp_wire_get_post_list') ) : ?>
-			<?php bp_wire_get_post_list( bp_current_user_id(), bp_word_or_name( __( "Your Wire", 'buddypress' ), __( "%s's Wire", 'buddypress' ), true, false ), bp_word_or_name( __( "No one has posted to your wire yet.", 'buddypress' ), __( "No one has posted to %s's wire yet.", 'buddypress' ), false, false ) ) ?>
+			<?php bp_wire_get_post_list( bp_current_user_id(), bp_word_or_name( __( "Your Wire", 'buddypress' ), __( "%s's Wire", 'buddypress' ), true, false ), bp_word_or_name( __( "No one has posted to your wire yet.", 'buddypress' ), __( "No one has posted to %s's wire yet.", 'buddypress' ), true, false ), bp_profile_wire_can_post() ) ?>
 		<?php endif; ?>
 		
 	</div>
