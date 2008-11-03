75f1bdb90efa4f55864baed168404379ec7c1403
Andy Peatling <apeatling@gmail.com>
2008-11-03 13:25:21 +0000

    Fixing display name caching as get_usermeta returns an empty text string on no match rather than false.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@478 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-core/bp-core-templatetags.php
+++ bp-core/bp-core-templatetags.php
@@ -215,7 +215,9 @@ function bp_fetch_user_fullname( $user_id = false, $echo = true ) {
 	
 	if ( function_exists('xprofile_install') ) {
 		// First check the usermeta table for a easily fetchable value
-		if ( !$data = get_usermeta( $user_id, 'bp_display_name' ) ) {
+		$data = get_usermeta( $user_id, 'bp_display_name' );
+
+		if ( $data == '' ) {
 			$data = bp_get_field_data( array( 'First Name', 'Last Name' ), $user_id );
 
 			if ( empty($data['First Name']) && empty($data['Last Name']) ) {
