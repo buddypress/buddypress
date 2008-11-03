0b8d7c8c655e091d054ee36fc2a5c729395555d3
Andy Peatling <apeatling@gmail.com>
2008-11-03 11:35:36 +0000

    Adding support in member theme for wire post list pagination.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@477 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- buddypress-theme/buddypress-member/wire/post-list.php
+++ buddypress-theme/buddypress-member/wire/post-list.php
@@ -1,8 +1,16 @@
 <div class="info-group">
 	<h4><?php bp_wire_title() ?> <a href="<?php bp_wire_see_all_link() ?>">See All &raquo;</a></h4>
 
+	<form name="wire-post-list-form" id="wire-post-list-form" action="" method="post">
 	<?php if ( bp_has_wire_posts( bp_wire_item_id(), bp_wire_can_post() ) ) : ?>
-	
+		<div id="wire-count" class="pag-count">
+			<?php bp_wire_pagination_count() ?>
+		</div>
+		
+		<div id="wire-pagination" class="pagination-links">
+			<?php bp_wire_pagination() ?>
+		</div>
+		
 		<ul id="wire-post-list">
 		<?php while ( bp_wire_posts() ) : bp_the_wire_post(); ?>
 			<li>
@@ -27,6 +35,9 @@
 		</div>
 
 	<?php endif;?>
+	
+	<input type="hidden" name="bp_wire_item_id" id="bp_wire_item_id" value="<?php bp_wire_item_id(true) ?>" />
+	</form>
 
 	<?php bp_wire_get_post_form() ?>		
 </div>
