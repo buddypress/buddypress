bfdc5b13e9a22ce2ce8f4631bc4bd93db9d0840e
Andy Peatling <apeatling@gmail.com>
2008-11-07 18:01:39 +0000

    Theme updates to support previous revision
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@512 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- buddypress-theme/buddypress-member/messages/compose.php
+++ buddypress-theme/buddypress-member/messages/compose.php
@@ -1,5 +1,5 @@
 <div class="content-header">
-	Compose Message
+
 </div>
 
 <div id="content">
@@ -7,5 +7,41 @@
 	
 	<?php do_action( 'template_notices' ) ?>
 
-	<?php bp_compose_message_form() ?>
-</div>
+	<form action="<?php bp_messages_form_action('compose') ?>" method="post" id="send_message_form" class="standard-form">
+	<div id="poststuff">
+		<p>			
+		<div id="titlediv">
+			<h3><?php _e("Send To", 'buddypress') ?> <small>(Use username - autocomplete coming soon)</small></h3>
+			<div id="titlewrap">
+				<input type="text" name="send_to" id="send_to" value="<?php bp_messages_username_value() ?>" />
+				<?php if ( is_site_admin() ) : ?><br /><input type="checkbox" id="send-notice" name="send-notice" value="1" /> This is a notice to all users.<?php endif; ?>
+			</div>
+		</div>
+		</p>
+
+		<p>
+		<div id="titlediv">
+			<h3><?php _e("Subject", 'buddypress') ?></h3>
+			<div id="titlewrap">
+				<input type="text" name="subject" id="subject" value="<?php bp_messages_subject_value() ?>" />
+			</div>
+		</div>
+		</p>
+		
+		<p>
+			<div id="postdivrich" class="postarea">
+				<h3><?php _e("Message", 'buddypress') ?></h3>
+				<div id="editorcontainer">
+					<textarea name="content" id="message_content" rows="15" cols="40"><?php bp_messages_content_value() ?></textarea>
+				</div>
+			</div>
+		</p>
+		
+		<p class="submit">
+				<input type="submit" value="<?php _e("Send", 'buddypress') ?> &raquo;" name="send" id="send" style="font-weight: bold" />
+		</p>
+	</div>
+	</form>
+	<script type="text/javascript">
+		document.getElementById("send_to").focus();
+	</script></div>
