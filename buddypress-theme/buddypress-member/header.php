5ab167a0d75f11bf58857bb711ea8c49f1ff59a9
Andy Peatling <apeatling@gmail.com>
2008-11-17 13:43:22 +0000

    Theme updates to support previous revision
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@551 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- buddypress-theme/buddypress-member/header.php
+++ buddypress-theme/buddypress-member/header.php
@@ -4,7 +4,7 @@
 <head profile="http://gmpg.org/xfn/11">
 
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-	<title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; Blog Archive <?php } ?> <?php wp_title(); ?></title>
+	<title><?php bp_page_title() ?></title>
 	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats -->
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
