eb96100dd9e3b86917a483ec6d736e2632f47d86
Andy Peatling <apeatling@gmail.com>
2008-12-06 01:49:30 +0000

    Fixing #168
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@626 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-activity/bp-activity-templatetags.php
+++ bp-activity/bp-activity-templatetags.php
@@ -182,7 +182,7 @@ function bp_activity_insert_time_since( $content, $date ) {
 	if ( !$content || !$date )
 		return false;
 		
-	return @sprintf( $content, '&nbsp; ' . bp_core_time_since( strtotime( $date ) ) . ' ' . __('ago', 'buddypress') );
+	return @sprintf( $content, @sprintf( __( '&nbsp; %s ago', 'buddypress' ), bp_core_time_since( strtotime( $date ) ) ) );
 }
 
 function bp_activity_css_class() {
