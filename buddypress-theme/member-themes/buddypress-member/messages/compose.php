2515933d4f2ee0a84f7de855fc6fc2852bc135a6
Andy Peatling <apeatling@gmail.com>
2008-12-13 19:12:26 +0000

    Member theme updates to support previous revision.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@661 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- buddypress-theme/member-themes/buddypress-member/messages/compose.php
+++ buddypress-theme/member-themes/buddypress-member/messages/compose.php
@@ -10,34 +10,24 @@
 	<?php do_action( 'template_notices' ) ?>
 
 	<form action="<?php bp_messages_form_action('compose') ?>" method="post" id="send_message_form" class="standard-form">
-	<div id="poststuff">
-		<p>			
-		<div id="titlediv">
-			<h3><?php _e("Send To", 'buddypress') ?> <small><?php _e("(Use username - autocomplete coming soon)", "buddypress"); ?></small></h3>
-			<div id="titlewrap">
-				<input type="text" name="send_to" id="send_to" value="<?php bp_messages_username_value() ?>" />
-				<?php if ( is_site_admin() ) : ?><br /><input type="checkbox" id="send-notice" name="send-notice" value="1" /> <?php _e("This is a notice to all users.", "buddypress"); ?><?php endif; ?>
-			</div>
-		</div>
-		</p>
+	
+		<label for="send-to-input"><?php _e("Send To", 'buddypress') ?> &nbsp;<img src="<?php bp_message_loading_image_src() ?>" alt="Loading" id="send-to-loading" style="display: none" height="7" /></label>
+		<ul class="first acfb-holder">
+			<li>
+				<?php bp_message_get_recipient_tabs() ?>
+				<input type="text" name="send-to-input" class="send-to-input" id="send-to-input" />
+			</li>
+		</ul>
+		<?php if ( is_site_admin() ) : ?><input type="checkbox" id="send-notice" name="send-notice" value="1" /> <?php _e("This is a notice to all users.", "buddypress"); ?><?php endif; ?>
 
-		<p>
-		<div id="titlediv">
-			<h3><?php _e("Subject", 'buddypress') ?></h3>
-			<div id="titlewrap">
-				<input type="text" name="subject" id="subject" value="<?php bp_messages_subject_value() ?>" />
-			</div>
-		</div>
-		</p>
-		
-		<p>
-			<div id="postdivrich" class="postarea">
-				<h3><?php _e("Message", 'buddypress') ?></h3>
-				<div id="editorcontainer">
-					<textarea name="content" id="message_content" rows="15" cols="40"><?php bp_messages_content_value() ?></textarea>
-				</div>
-			</div>
-		</p>
+
+		<label for="subject"><?php _e("Subject", 'buddypress') ?></label>
+		<input type="text" name="subject" id="subject" value="<?php bp_messages_subject_value() ?>" />
+
+		<label for="content"><?php _e("Message", 'buddypress') ?></label>
+		<textarea name="content" id="message_content" rows="15" cols="40"><?php bp_messages_content_value() ?></textarea>
+
+		<input type="hidden" name="send_to_usernames" id="send-to-usernames" value="" class="<?php bp_message_get_recipient_usernames() ?>" />
 		
 		<p class="submit">
 				<input type="submit" value="<?php _e("Send", 'buddypress') ?> &raquo;" name="send" id="send" style="font-weight: bold" />
@@ -45,7 +35,7 @@
 	</div>
 	</form>
 	<script type="text/javascript">
-		document.getElementById("send_to").focus();
+		document.getElementById("send-to-input").focus();
 	</script>
 </div>
 
