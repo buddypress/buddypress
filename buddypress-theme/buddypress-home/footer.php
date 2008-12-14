dfaba357093184a051809d59c7bf417c1c89c68a
Andy Peatling <apeatling@gmail.com>
2008-12-14 05:40:12 +0000

    Adding footer to home theme.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@673 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- buddypress-theme/buddypress-home/footer.php
+++ buddypress-theme/buddypress-home/footer.php
@@ -1,6 +1,8 @@
 
+<div class="clear"></div>
+
 <div id="footer">
-	
+	<p><?php printf( __( '%s is proudly powered by <a href="http://mu.wordpress.org">WordPress MU</a> and <a href="http://buddypress.org">BuddyPress</a>' ), bloginfo('name') ); ?></p>
 </div>
 
 <?php wp_footer(); ?>
