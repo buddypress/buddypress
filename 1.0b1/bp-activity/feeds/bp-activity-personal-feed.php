1704b2c6a633bc521723cfb0a77d9b62b4335cbe
Andy Peatling <apeatling@gmail.com>
2008-12-15 23:19:59 +0000

    Tagging 1.0b1 activity streams
    git-svn-id: https://buddypress.svn.wordpress.org/tags/activity-streams@761 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

---
+++ 1.0b1/bp-activity/feeds/bp-activity-personal-feed.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * RSS2 Feed Template for displaying the site wide activity stream.
+ *
+ * @package BuddyPress
+ */
+header('Content-Type: text/xml; charset=' . get_option('blog_charset'), true);
+header('Status: 200 OK');
+?>
+<?php echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
+
+<rss version="2.0"
+	xmlns:content="http://purl.org/rss/1.0/modules/content/"
+	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+	xmlns:dc="http://purl.org/dc/elements/1.1/"
+	xmlns:atom="http://www.w3.org/2005/Atom"
+	<?php do_action('bp_activity_personal_feed'); ?>
+>
+
+<channel>
+	<title><?php echo $bp['current_fullname']; ?> - <?php _e( 'Activity', 'buddypress' ) ?></title>
+	<atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
+	<link><?php echo $bp['current_domain'] . $bp['activity']['slug'] . '/feed' ?></link>
+	<description><?php _e( sprintf( '%s - Activity Feed', $bp['current_fullname'] ), 'buddypress' ) ?></description>
+	<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', bp_activity_get_last_updated(), false); ?></pubDate>
+	<generator>http://buddypress.org/?bp-activity-version=<?php echo BP_ACTIVITY_VERSION ?></generator>
+	<language><?php echo get_option('rss_language'); ?></language>
+	<?php do_action('bp_activity_personal_feed_head'); ?>
+	
+	<?php if ( bp_has_activities() ) : ?>
+		<?php while ( bp_activities() ) : bp_the_activity(); ?>
+			<item>
+				<title><![CDATA[<?php bp_activity_feed_item_title() ?>]]></title>
+				<link><?php echo bp_activity_feed_item_link() ?></link>
+				<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', bp_activity_feed_item_date(), false); ?></pubDate>
+
+				<description><![CDATA[<?php bp_activity_feed_item_description() ?>]]></description>
+			<?php do_action('bp_activity_personal_feed_item'); ?>
+			</item>
+		<?php endwhile; ?>
+
+	<?php endif; ?>
+</channel>
+</rss>
