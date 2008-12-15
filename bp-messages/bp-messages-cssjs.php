6d85a30b7b6133e9a13e896142ac7fd92e5d2c9e
Andy Peatling <apeatling@gmail.com>
2008-12-15 17:15:07 +0000

    Removing TinyMCE from messaging for the beta, as it is not integrating correctly in WPMU 2.7.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@722 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-messages/bp-messages-cssjs.php
+++ bp-messages/bp-messages-cssjs.php
@@ -1,36 +1,36 @@
 <?php
 
-function messages_add_tinymce() {
-	global $bp;
-	
-	if ( ( $bp['current_component'] == $bp['messages']['slug'] && $bp['current_action'] == 'compose' ) || ( $bp['current_component'] == $bp['messages']['slug'] && $bp['current_action'] == 'view' ) ) {
-		echo '
-			<script type="text/javascript" src="' . site_url() . '/wp-includes/js/tinymce/tiny_mce.js"></script>
-				<script type="text/javascript">
-					<!--
-					tinyMCE.init({
-					theme : "advanced",
-					skin : "wp_theme",
-					language : "en",
-					theme_advanced_toolbar_location : "top",
-					theme_advanced_toolbar_align : "left",
-					theme_advanced_path_location : "bottom",
-					theme_advanced_resizing : true,
-					theme_advanced_resize_horizontal : false,
-					theme_advanced_buttons1:"bold,italic,strikethrough,|,bullist,numlist,blockquote,|,justifyleft,justifycenter,justifyright,|,link,unlink,image,wp_more,|,fullscreen,wp_adv",theme_advanced_buttons2:"formatselect,underline,justifyfull,forecolor,|,pastetext,pasteword,removeformat,|,media,charmap,|,outdent,indent,|,undo,redo,wp_help",theme_advanced_buttons3:"",
-					content_css : "' . site_url() . '/wp-includes/js/tinymce/plugins/wordpress/wordpress.css",
-					mode : "exact",
-					elements : "message_content",
-					width : "100%",
-					height : "250",
-					plugins:"safari,inlinepopups,autosave,paste,wordpress,media,fullscreen"
-					});
-					-->
-				</script>';
-	}
-	
-}
-add_action( 'wp_head', 'messages_add_tinymce' );
+// function messages_add_tinymce() {
+// 	global $bp;
+// 	
+// 	if ( ( $bp['current_component'] == $bp['messages']['slug'] && $bp['current_action'] == 'compose' ) || ( $bp['current_component'] == $bp['messages']['slug'] && $bp['current_action'] == 'view' ) ) {
+// 		echo '
+// 			<script type="text/javascript" src="' . site_url() . '/wp-includes/js/tinymce/tiny_mce.js"></script>
+// 				<script type="text/javascript">
+// 					<!--
+// 					tinyMCE.init({
+// 					theme : "advanced",
+// 					skin : "wp_theme",
+// 					language : "en",
+// 					theme_advanced_toolbar_location : "top",
+// 					theme_advanced_toolbar_align : "left",
+// 					theme_advanced_path_location : "bottom",
+// 					theme_advanced_resizing : true,
+// 					theme_advanced_resize_horizontal : false,
+// 					theme_advanced_buttons1:"bold,italic,strikethrough,|,bullist,numlist,blockquote,|,justifyleft,justifycenter,justifyright,|,link,unlink,image,wp_more,|,fullscreen,wp_adv",theme_advanced_buttons2:"formatselect,underline,justifyfull,forecolor,|,pastetext,pasteword,removeformat,|,media,charmap,|,outdent,indent,|,undo,redo,wp_help",theme_advanced_buttons3:"",
+// 					content_css : "' . site_url() . '/wp-includes/js/tinymce/plugins/wordpress/wordpress.css",
+// 					mode : "exact",
+// 					elements : "message_content",
+// 					width : "100%",
+// 					height : "250",
+// 					plugins:"safari,inlinepopups,autosave,paste,wordpress,media,fullscreen"
+// 					});
+// 					-->
+// 				</script>';
+// 	}
+// 	
+// }
+// add_action( 'wp_head', 'messages_add_tinymce' );
 
 function messages_add_js() {
 	global $bp;
