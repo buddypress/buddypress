fee21638199494147a8e71881637ccb28e7fb2ce
Andy Peatling <apeatling@gmail.com>
2008-12-15 03:31:44 +0000

    Updating 404 page.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@698 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- buddypress-theme/buddypress-home/404.php
+++ buddypress-theme/buddypress-home/404.php
@@ -1,11 +1,40 @@
 <?php get_header(); ?>
 
-	<div id="content" class="narrowcolumn">
+<div id="content" class="widecolumn">
+	
+	<div id="right-column">
+		<?php if ( !function_exists('dynamic_sidebar')
+		        || !dynamic_sidebar('right-column') ) : ?>
 
-		<h2 class="center">Error 404 - Not Found</h2>
-
+		<div class="widget-error">
+			Please log in and <a href="<?php echo get_option('siteurl') ?>/wp-admin/widgets.php?s=&amp;show=&amp;sidebar=sidebar-3">add widgets to this column</a>.
+		</div>		
+		
+		<?php endif; ?>
 	</div>
 
-<?php get_sidebar(); ?>
+	<div id="center-column">
+		<?php if ( !function_exists('dynamic_sidebar')
+		        || !dynamic_sidebar('center-column') ) : ?>
+		
+		<div class="widget-error">
+			Please log in and <a href="<?php echo get_option('siteurl') ?>/wp-admin/widgets.php?s=&amp;show=&amp;sidebar=sidebar-2">add widgets to this column</a>.
+		</div>
+		
+		<?php endif; ?>
+	</div>
+
+	<div id="left-column">
+		<div class="404 bp_core_widget_welcome">
+			<h2 class="widgettitle"><?php _e( 'Sorry, that page was not found', 'buddypress' ) ?></h2>
+			
+			<div id="message" class="info">
+				<p><?php _e( 'The page you were looking for was not found.', 'buddypress' ) ?>
+			</div>
+		</div>
+	</div>
+	
+
+</div>
 
 <?php get_footer(); ?>
