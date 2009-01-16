e21299e2d62bb13643ede031208d2d3eed527808
Andy Peatling <apeatling@gmail.com>
2009-01-16 21:45:11 +0000

    Removing incorrect filter.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@860 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-groups/bp-groups-filters.php
+++ bp-groups/bp-groups-filters.php
@@ -6,7 +6,6 @@ add_filter( 'bp_group_description_excerpt', 'wptexturize' );
 add_filter( 'bp_group_news', 'wptexturize' );
 add_filter( 'bp_group_name', 'wptexturize' );
 
-add_filter( 'bp_the_topic_post_content', 'make_clickable' );
 add_filter( 'bp_group_description', 'convert_smilies' );
 add_filter( 'bp_group_description_excerpt', 'convert_smilies' );
 add_filter( 'bp_group_news', 'convert_smilies' );
