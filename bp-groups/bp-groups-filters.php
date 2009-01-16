9c5e06060f4da484a10ae6e96f62b412af8a2947
Andy Peatling <apeatling@gmail.com>
2009-01-16 21:44:37 +0000

    Added make_clickable filter to the groups component.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@859 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-groups/bp-groups-filters.php
+++ bp-groups/bp-groups-filters.php
@@ -6,6 +6,7 @@ add_filter( 'bp_group_description_excerpt', 'wptexturize' );
 add_filter( 'bp_group_news', 'wptexturize' );
 add_filter( 'bp_group_name', 'wptexturize' );
 
+add_filter( 'bp_the_topic_post_content', 'make_clickable' );
 add_filter( 'bp_group_description', 'convert_smilies' );
 add_filter( 'bp_group_description_excerpt', 'convert_smilies' );
 add_filter( 'bp_group_news', 'convert_smilies' );
@@ -17,5 +18,9 @@ add_filter( 'bp_group_name', 'convert_chars' );
 
 add_filter( 'bp_group_description', 'wpautop' );
 add_filter( 'bp_group_news', 'wpautop' );
+
+add_filter( 'bp_group_description', 'make_clickable' );
+add_filter( 'bp_group_description_excerpt', 'make_clickable' );
+add_filter( 'bp_group_news', 'make_clickable' );
 
 ?>
