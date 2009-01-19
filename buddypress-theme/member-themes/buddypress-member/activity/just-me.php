0c6f78b8974aed0ccca3db5dffa44a915c7cd9e7
Andy Peatling <apeatling@gmail.com>
2009-01-19 05:08:44 +0000

    Fixing incorrect use of "You" when viewing a person's empty activity stream.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@870 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- buddypress-theme/member-themes/buddypress-member/activity/just-me.php
+++ buddypress-theme/member-themes/buddypress-member/activity/just-me.php
@@ -21,7 +21,7 @@
 		<?php bp_get_profile_header() ?>
 
 		<?php if ( function_exists('bp_activity_get_list') ) : ?>
-			<?php bp_activity_get_list( bp_current_user_id(), bp_word_or_name( __( "My Activity", 'buddypress' ), __( "%s's Activity", 'buddypress' ), true, false ), __( "You haven't done anything yet.", 'buddypress' )  ) ?>
+			<?php bp_activity_get_list( bp_current_user_id(), bp_word_or_name( __( "My Activity", 'buddypress' ), __( "%s's Activity", 'buddypress' ), true, false ), bp_word_or_name( __( "You haven't done anything yet.", 'buddypress' ), __( "%s hasn't done anything yet.", 'buddypress' ), true, false )  ) ?>
 		<?php endif; ?>
 		
 	</div>
