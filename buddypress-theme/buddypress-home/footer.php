1e1df55e7d5c14c36af642913ef2c50aba27cc04
Andy Peatling <apeatling@gmail.com>
2009-01-20 19:47:12 +0000

    Fixes #369
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@889 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- buddypress-theme/buddypress-home/footer.php
+++ buddypress-theme/buddypress-home/footer.php
@@ -2,7 +2,7 @@
 <div class="clear"></div>
 
 <div id="footer">
-	<p><?php printf( __( '%s is proudly powered by <a href="http://mu.wordpress.org">WordPress MU</a> and <a href="http://buddypress.org">BuddyPress</a>' ), bloginfo('name') ); ?></p>
+    <p><?php printf( __( '%s is proudly powered by <a href="http://mu.wordpress.org">WordPress MU</a> and <a href="http://buddypress.org">BuddyPress</a>', 'buddypress' ), bloginfo('name') ); ?></p>
 </div>
 
 <?php wp_footer(); ?>
