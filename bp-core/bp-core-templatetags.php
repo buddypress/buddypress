9c8e1aefbc43d8ca39f9cf33fd51f0ef0e7dbf7b
Andy Peatling <apeatling@gmail.com>
2009-01-29 03:04:28 +0000

    Fixed navigation highlighting.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@958 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-core/bp-core-templatetags.php
+++ bp-core/bp-core-templatetags.php
@@ -540,15 +540,15 @@ function bp_nav_items() {
 	global $bp;
 ?>
 	<li<?php if(bp_is_page('home')) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>" title="<?php _e( 'Home', 'buddypress' ) ?>"><?php _e( 'Home', 'buddypress' ) ?></a></li>
-	<li<?php if(bp_is_page('news')) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>/<?php echo HOME_BLOG_SLUG ?>" title="<?php _e( 'Blog', 'buddypress' ) ?>"><?php _e( 'Blog', 'buddypress' ) ?></a></li>
+	<li<?php if(bp_is_page(HOME_BLOG_SLUG)) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>/<?php echo HOME_BLOG_SLUG ?>" title="<?php _e( 'Blog', 'buddypress' ) ?>"><?php _e( 'Blog', 'buddypress' ) ?></a></li>
 	<li<?php if(bp_is_page(MEMBERS_SLUG)) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>/<?php echo MEMBERS_SLUG ?>" title="<?php _e( 'Members', 'buddypress' ) ?>"><?php _e( 'Members', 'buddypress' ) ?></a></li>
 	
 	<?php if ( function_exists('groups_install') ) { ?>
-		<li<?php if(bp_is_page('groups')) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>/<?php echo $bp['groups']['slug'] ?>" title="<?php _e( 'Groups', 'buddypress' ) ?>"><?php _e( 'Groups', 'buddypress' ) ?></a></li>
+		<li<?php if(bp_is_page($bp['groups']['slug'])) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>/<?php echo $bp['groups']['slug'] ?>" title="<?php _e( 'Groups', 'buddypress' ) ?>"><?php _e( 'Groups', 'buddypress' ) ?></a></li>
 	<?php } ?>
 	
 	<?php if ( function_exists('bp_blogs_install') ) { ?>
-		<li<?php if(bp_is_page('blogs')) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>/<?php echo $bp['blogs']['slug'] ?>" title="<?php _e( 'Blogs', 'buddypress' ) ?>"><?php _e( 'Blogs', 'buddypress' ) ?></a></li>
+		<li<?php if(bp_is_page($bp['blogs']['slug'])) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>/<?php echo $bp['blogs']['slug'] ?>" title="<?php _e( 'Blogs', 'buddypress' ) ?>"><?php _e( 'Blogs', 'buddypress' ) ?></a></li>
 	<?php } ?>
 <?php
 	do_action( 'bp_nav_items' );
