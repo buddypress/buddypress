a924fafa44447c2eb71b4d0e06d5724551c80323
Andy Peatling <apeatling@gmail.com>
2009-01-29 22:18:18 +0000

    Tagging messaging component 1.0b2 file+folder
    git-svn-id: https://buddypress.svn.wordpress.org/tags/private-messaging@1004 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

---
+++ 1.0b2/bp-messages/bp-messages/bp-messages-filters.php
@@ -0,0 +1,36 @@
+<?php
+
+/* Apply WordPress defined filters */
+add_filter( 'bp_message_notice_subject', 'wptexturize' );
+add_filter( 'bp_message_notice_text', 'wptexturize' );
+add_filter( 'bp_message_thread_subject', 'wptexturize' );
+add_filter( 'bp_message_thread_excerpt', 'wptexturize' );
+add_filter( 'bp_message_content', 'wptexturize' );
+
+add_filter( 'bp_message_notice_subject', 'convert_smilies' );
+add_filter( 'bp_message_notice_text', 'convert_smilies' );
+add_filter( 'bp_message_thread_subject', 'convert_smilies' );
+add_filter( 'bp_message_thread_excerpt', 'convert_smilies' );
+add_filter( 'bp_message_content', 'convert_smilies' );
+
+add_filter( 'bp_message_notice_subject', 'convert_chars' );
+add_filter( 'bp_message_notice_text', 'convert_chars' );
+add_filter( 'bp_message_thread_subject', 'convert_chars' );
+add_filter( 'bp_message_thread_excerpt', 'convert_chars' );
+add_filter( 'bp_message_content', 'convert_chars' );
+
+add_filter( 'bp_message_notice_subject', 'wpautop' );
+add_filter( 'bp_message_notice_text', 'wpautop' );
+add_filter( 'bp_message_thread_subject', 'wpautop' );
+add_filter( 'bp_message_thread_excerpt', 'wpautop' );
+add_filter( 'bp_message_content', 'wpautop' );
+
+add_filter( 'bp_message_notice_subject', 'stripslashes_deep' );
+add_filter( 'bp_message_notice_text', 'stripslashes_deep' );
+add_filter( 'bp_message_thread_subject', 'stripslashes_deep' );
+add_filter( 'bp_message_thread_excerpt', 'stripslashes_deep' );
+add_filter( 'bp_messages_subject_value', 'stripslashes_deep' );
+add_filter( 'bp_messages_content_value', 'stripslashes_deep' );
+add_filter( 'bp_message_content', 'stripslashes_deep' );
+
+?>
