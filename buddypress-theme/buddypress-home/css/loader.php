07d6f391725b37a22124a3986468c5a880d2e075
Andy Peatling <apeatling@gmail.com>
2009-02-07 06:45:12 +0000

    Changed site-wide.css file to side-wide-sample.css so that custom css is not overwritten by updates.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@1028 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- buddypress-theme/buddypress-home/css/loader.php
+++ buddypress-theme/buddypress-home/css/loader.php
@@ -1,4 +1,7 @@
 <?php
+/* Load the WP environment */
+require_once( preg_replace('%(.*)[/\\\\]wp-content[/\\\\].*%', '\1', $_SERVER['SCRIPT_FILENAME'] ) . '/wp-load.php' ); 
+
 /* Set the content type to CSS */
 header('Content-type: text/css'); 
 
@@ -9,4 +12,6 @@ if ( file_exists('base.css') )
 /* Load the custom css if there is any. */
 if ( file_exists('custom.css') )
 	echo "@import url(custom.css);\n";
+
+do_action( 'bp_custom_home_styles' );
 ?>
