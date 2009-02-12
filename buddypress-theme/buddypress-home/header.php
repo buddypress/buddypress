fd483313a5046ed2ce2251f48aa2b987d9ab81a9
Andy Peatling <apeatling@gmail.com>
2009-02-12 18:59:57 +0000

    Added navigation items back into the template, it's far easier for custom edits that way.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@1075 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- buddypress-theme/buddypress-home/header.php
+++ buddypress-theme/buddypress-home/header.php
@@ -2,48 +2,57 @@
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 
 <head profile="http://gmpg.org/xfn/11">
-<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
-<title><?php bloginfo('name'); ?></title>
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<title><?php bp_page_title() ?></title>
+	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats -->
 
-<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats -->
-
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-
-<?php if ( function_exists('bp_sitewide_activity_feed_link') ) : ?>
-	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> <?php _e('Site Wide Activity RSS Feed', 'buddypress' ) ?>" href="<?php bp_sitewide_activity_feed_link() ?>" />
-<?php endif; ?>
-
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
-<?php wp_head(); ?>
-
-<!--[if IE 6]>
-<link rel="stylesheet" href="<?php echo bloginfo('template_url') . '/css/ie/ie6.css' ?>" type="text/css" media="screen" />	
-<![endif]-->
-
-<!--[if IE 7]>
-<link rel="stylesheet" href="<?php echo bloginfo('template_url') . '/css/ie/ie7.css' ?>" type="text/css" media="screen" />	
-<![endif]-->
-
-</head>
-<body>
+	<?php bp_styles(); ?>
 	
-	<div id="search-login-bar">
-		<?php bp_search_form() ?>		
-		<?php bp_login_bar() ?>
-		
-		<div class="clear"></div>
-	</div>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	
+	<?php wp_head(); ?>
+	
+	<!--[if IE 6]>
+	<link rel="stylesheet" href="<?php echo bloginfo('template_url') . '/css/ie/ie6.css' ?>" type="text/css" media="screen" />	
+	<![endif]-->
+	
+	<!--[if IE 7]>
+	<link rel="stylesheet" href="<?php echo bloginfo('template_url') . '/css/ie/ie7.css' ?>" type="text/css" media="screen" />	
+	<![endif]-->
+</head>
 
-	<div id="header">		
-		<h1 id="logo"><a href="<?php echo get_option('home') ?>" title="<?php _e( 'Home', 'buddypress' ) ?>"><?php bp_site_name() ?></a></h1>
-		
-		<ul id="nav">
-			<?php bp_nav_items() ?>
-		</ul>
-		
-		<div class="clear"></div>
-	</div>
+<body>
+
+<div id="search-login-bar">
+	<?php bp_search_form() ?>
+	<?php bp_login_bar() ?>
 	
 	<div class="clear"></div>
+</div>
+
+<div id="header">
+	<h1 id="logo"><a href="<?php echo get_option('home') ?>" title="<?php _e( 'Home', 'buddypress' ) ?>"><?php bp_site_name() ?></a></h1>
+	
+	<ul id="nav">
+		<li<?php if ( bp_is_page( 'home' ) ) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>" title="<?php _e( 'Home', 'buddypress' ) ?>"><?php _e( 'Home', 'buddypress' ) ?></a></li>
+		<li<?php if ( bp_is_page( MEMBERS_SLUG ) ) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>/<?php echo MEMBERS_SLUG ?>" title="<?php _e( 'Members', 'buddypress' ) ?>"><?php _e( 'Members', 'buddypress' ) ?></a></li>
+
+		<?php if ( function_exists( 'groups_install' ) ) { ?>
+			<li<?php if ( bp_is_page( BP_GROUPS_SLUG ) ) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>/<?php echo BP_GROUPS_SLUG ?>" title="<?php _e( 'Groups', 'buddypress' ) ?>"><?php _e( 'Groups', 'buddypress' ) ?></a></li>
+		<?php } ?>
+
+		<?php if ( function_exists( 'bp_blogs_install' ) ) { ?>
+			<li<?php if ( bp_is_page( BP_BLOGS_SLUG ) ) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>/<?php echo BP_BLOGS_SLUG ?>" title="<?php _e( 'Blogs', 'buddypress' ) ?>"><?php _e( 'Blogs', 'buddypress' ) ?></a></li>
+		<?php } ?>
+
+		<?php do_action( 'bp_nav_items' ); ?>
+	</ul>
+</div>
+
+<?php include_once( TEMPLATEPATH . '/userbar.php' ) ?>
+<?php include_once( TEMPLATEPATH . '/optionsbar.php' ) ?>
+
+<div id="main">
--- buddypress-theme/member-themes/buddypress-member/header.php
+++ buddypress-theme/member-themes/buddypress-member/header.php
@@ -37,7 +37,18 @@
 	<h1 id="logo"><a href="<?php echo get_option('home') ?>" title="<?php _e( 'Home', 'buddypress' ) ?>"><?php bp_site_name() ?></a></h1>
 	
 	<ul id="nav">
-		<?php bp_nav_items() ?>
+		<li<?php if ( bp_is_page( 'home' ) ) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>" title="<?php _e( 'Home', 'buddypress' ) ?>"><?php _e( 'Home', 'buddypress' ) ?></a></li>
+		<li<?php if ( bp_is_page( MEMBERS_SLUG ) ) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>/<?php echo MEMBERS_SLUG ?>" title="<?php _e( 'Members', 'buddypress' ) ?>"><?php _e( 'Members', 'buddypress' ) ?></a></li>
+
+		<?php if ( function_exists( 'groups_install' ) ) { ?>
+			<li<?php if ( bp_is_page( BP_GROUPS_SLUG ) ) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>/<?php echo BP_GROUPS_SLUG ?>" title="<?php _e( 'Groups', 'buddypress' ) ?>"><?php _e( 'Groups', 'buddypress' ) ?></a></li>
+		<?php } ?>
+
+		<?php if ( function_exists( 'bp_blogs_install' ) ) { ?>
+			<li<?php if ( bp_is_page( BP_BLOGS_SLUG ) ) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>/<?php echo BP_BLOGS_SLUG ?>" title="<?php _e( 'Blogs', 'buddypress' ) ?>"><?php _e( 'Blogs', 'buddypress' ) ?></a></li>
+		<?php } ?>
+
+		<?php do_action( 'bp_nav_items' ); ?>
 	</ul>
 </div>
 
