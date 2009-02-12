24ca318642d2659065d130710d697f6d476f7d69
Andy Peatling <apeatling@gmail.com>
2009-02-12 19:17:22 +0000

    Reinstating home blog nav item in member theme.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@1078 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- buddypress-theme/member-themes/buddypress-member/header.php
+++ buddypress-theme/member-themes/buddypress-member/header.php
@@ -38,6 +38,7 @@
 	
 	<ul id="nav">
 		<li<?php if ( bp_is_page( 'home' ) ) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>" title="<?php _e( 'Home', 'buddypress' ) ?>"><?php _e( 'Home', 'buddypress' ) ?></a></li>
+		<li<?php if ( bp_is_page( HOME_BLOG_SLUG ) ) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>/<?php echo HOME_BLOG_SLUG ?>" title="<?php _e( 'Blog', 'buddypress' ) ?>"><?php _e( 'Blog', 'buddypress' ) ?></a></li>
 		<li<?php if ( bp_is_page( MEMBERS_SLUG ) ) {?> class="selected"<?php } ?>><a href="<?php echo get_option('home') ?>/<?php echo MEMBERS_SLUG ?>" title="<?php _e( 'Members', 'buddypress' ) ?>"><?php _e( 'Members', 'buddypress' ) ?></a></li>
 
 		<?php if ( function_exists( 'groups_install' ) ) { ?>
