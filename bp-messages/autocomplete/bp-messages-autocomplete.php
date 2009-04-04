92d1ff2ed35d85d8ac1a8d5ca5a3212ffefde8f6
Andy Peatling <apeatling@gmail.com>
2009-04-04 19:41:16 +0000

    Fixes #655 props nicolagreco
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@1312 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-messages/autocomplete/bp-messages-autocomplete.php
+++ bp-messages/autocomplete/bp-messages-autocomplete.php
@@ -1,8 +1,8 @@
 <?php
 /* Load the WP environment */
 require_once( preg_replace('%(.*)[/\\\\]wp-content[/\\\\].*%', '\1', dirname( __FILE__ ) ) . '/wp-load.php' ); 
-require_once( WP_CONTENT_DIR . '/mu-plugins/bp-core.php' );
-require_once( WP_CONTENT_DIR . '/mu-plugins/bp-friends.php' );
+require_once( BP_PLUGIN_DIR . '/bp-core.php' );
+require_once( BP_PLUGIN_DIR . '/bp-friends.php' );
 
 // Setup the $bp global array as it's not auto set outside of the normal WP enviro.
 bp_core_setup_globals();
@@ -15,8 +15,7 @@ if ( $friends['friends'] ) {
 	foreach ( $friends['friends'] as $key => $friend ) {
 		$ud = get_userdata($friend['user_id']);
 		$username = $ud->user_login;
-		echo bp_core_get_avatar( $friend['user_id'], 1, 15, 15 ) . ' ' . bp_fetch_user_fullname( $friend['user_id'], false ) . ' (' . $username . ')
-		';
-	}		
+		echo bp_core_get_avatar( $friend['user_id'], 1, 15, 15 ) . ' ' . bp_fetch_user_fullname( $friend['user_id'], false ) . ' (' . $username . ')';
+	}
 }
 ?>
