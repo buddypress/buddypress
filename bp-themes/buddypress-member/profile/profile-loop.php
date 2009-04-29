78fedf794f4008d746adb41898b0186d38c3fded
Andy Peatling <apeatling@gmail.com>
2009-04-29 13:56:07 +0000

    Deprecating bp_group_has_fields() and taking its place is bp_profile_group_has_fields() so to not make it easy to confuse this template tag with site groups.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@1442 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/buddypress-member/profile/profile-loop.php
+++ bp-themes/buddypress-member/profile/profile-loop.php
@@ -1,7 +1,7 @@
 <?php if ( bp_has_profile() ) : ?>	
 	<?php while ( bp_profile_groups() ) : bp_the_profile_group(); ?>
 
-		<?php if ( bp_group_has_fields() ) : ?>
+		<?php if ( bp_profile_group_has_fields() ) : ?>
 			<div class="info-group">
 				<h4><?php bp_the_profile_group_name() ?></h4>
 				
