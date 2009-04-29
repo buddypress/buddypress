69d2683bf4168ab0c1f367febe75dea2fedd16d2
Andy Peatling <apeatling@gmail.com>
2009-04-29 07:46:55 +0000

    Updating blogs directory to 10 per page.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@1429 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/buddypress-member/directories/blogs/blogs-loop.php
+++ bp-themes/buddypress-member/directories/blogs/blogs-loop.php
@@ -1,27 +1,51 @@
-<?php if ( bp_has_site_blogs( 'type=active&per_page=5' ) ) : ?>
+<?php if ( bp_has_site_blogs( 'type=active&per_page=10' ) ) : ?>
+	
+	<div class="pag-count" id="blog-dir-count">
+		<?php bp_site_blogs_pagination_count() ?>
+	</div>
 
-<div class="pag-count" id="blog-dir-count">
-<?php bp_site_blogs_pagination_count() ?>
-</div>
+	<div class="pagination-links" id="blog-dir-pag">
+		<?php bp_site_blogs_pagination_links() ?>
+	</div>
 
-<div class="pagination-links" id="blog-dir-pag">
-<?php bp_site_blogs_pagination_links() ?>
-</div>
+	<ul id="blogs-list" class="item-list">
+	<?php while ( bp_site_blogs() ) : bp_the_site_blog(); ?>
 
-<ul id="blogs-list" class="item-list">
-<?php while ( bp_site_blogs() ) : bp_the_site_blog(); ?>
+		<li>
+			<div class="item-avatar">
+				<a href="<?php bp_the_site_blog_link() ?>"><?php bp_the_site_blog_avatar_thumb() ?></a>
+			</div>
 
-<li>
-</li>
+			<div class="item">
+				<div class="item-title"><a href="<?php bp_the_site_blog_link() ?>"><?php bp_the_site_blog_name() ?></a></div>
+				<div class="item-meta"><span class="activity"><?php bp_the_site_blog_last_active() ?></span></div>
 
-<?php endwhile; ?>
-</ul>		
+				<?php do_action( 'bp_core_directory_blogs_content' ) ?>
+			</div>
 
+			<div class="action">
+				<div class="blog-button visit">
+					<a href="<?php bp_the_site_blog_link() ?>" class="visit" title="<?php _e( 'Visit Blog', 'buddypress' ) ?>"><?php _e( 'Visit Blog', 'buddypress' ) ?></a>
+				</div>
+				
+				<div class="meta">
+					<?php bp_the_site_blog_latest_post() ?>
+				</div>
+				
+				<?php do_action( 'bp_core_directory_blogs_actions' ) ?>
+			</div>
+
+			<div class="clear"></div>
+		</li>
+
+	<?php endwhile; ?>
+	</ul>		
+			
 <?php else: ?>
 
-<div id="message" class="info">
-<p><?php _e( 'There were no blogs found.', 'buddypress' ) ?></p>
-</div>
+	<div id="message" class="info">
+		<p><?php _e( 'No blogs found. Blogs must fill in at least one piece of profile data to show in blog lists.', 'buddypress' ) ?></p>
+	</div>
 
 <?php endif; ?>
 
