b7bb45da04f7f7724d4ae73feefc8ed6515bb66e
Andy Peatling <apeatling@gmail.com>
2009-06-01 21:43:41 +0000

    Moved the autocomplete messaging JS to a javascript inline block to avoid having to load WP again.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@1494 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-messages/bp-messages-cssjs.php
+++ bp-messages/bp-messages-cssjs.php
@@ -8,11 +8,12 @@ function messages_add_js() {
 
 	// Include the autocomplete JS for composing a message.
 	if ( $bp->current_component == $bp->messages->slug && $bp->current_action == 'compose') {
+		add_action( 'wp_head', 'messaegs_autocomplete_init_jsblock' );
+		
 		wp_enqueue_script( 'bp-jquery-autocomplete', BP_PLUGIN_URL . '/bp-messages/js/autocomplete/jquery.autocomplete.js', 'jquery' );
 		wp_enqueue_script( 'bp-jquery-autocomplete-fb', BP_PLUGIN_URL . '/bp-messages/js/autocomplete/jquery.autocompletefb.js', 'jquery' );
 		wp_enqueue_script( 'bp-jquery-bgiframe', BP_PLUGIN_URL . '/bp-messages/js/autocomplete/jquery.bgiframe.min.js', 'jquery' );
 		wp_enqueue_script( 'bp-jquery-dimensions', BP_PLUGIN_URL . '/bp-messages/js/autocomplete/jquery.dimensions.js', 'jquery' );	
-		wp_enqueue_script( 'bp-autocomplete-init', BP_PLUGIN_URL . '/bp-messages/js/autocomplete/init.php', 'jquery' );	
 	}
 
 }
@@ -28,12 +29,24 @@ function messages_add_css() {
 }
 add_action( 'wp_head', 'messages_add_css' );
 
-
 function messages_add_structure_css() {
 	/* Enqueue the structure CSS file to give basic positional formatting for components */
 	wp_enqueue_style( 'bp-messages-structure', BP_PLUGIN_URL . '/bp-messages/css/structure.css' );	
 }
 add_action( 'bp_styles', 'messages_add_structure_css' );
 
-
+function messaegs_autocomplete_init_jsblock() {
 ?>
+	<script type="text/javascript">
+		jQuery(document).ready(function() {
+			var acfb = 
+			jQuery("ul.first").autoCompletefb({urlLookup:'<?php echo BP_PLUGIN_URL . '/bp-messages/autocomplete/bp-messages-autocomplete.php' ?>'});
+
+			jQuery('#send_message_form').submit( function() {
+				var users = document.getElementById('send-to-usernames').className;
+				document.getElementById('send-to-usernames').value = String(users);
+			});
+		});
+	</script>
+<?php
+}
