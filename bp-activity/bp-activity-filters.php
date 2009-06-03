08237976224ce4f5b2d48dabf2e75ed999bc7b28
Andy Peatling <apeatling@gmail.com>
2009-06-03 23:01:09 +0000

    Added kses filter to activity content with a span tag exception.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@1504 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-activity/bp-activity-filters.php
+++ bp-activity/bp-activity-filters.php
@@ -1,6 +1,8 @@
 <?php
 
 /* Apply WordPress defined filters */
+add_filter( 'bp_get_activity_content', 'wp_filter_kses', 1 );
+
 add_filter( 'bp_get_activity_content', 'wptexturize' );
 
 add_filter( 'bp_get_activity_content', 'convert_smilies' );
@@ -9,8 +11,15 @@ add_filter( 'bp_get_activity_content', 'convert_chars' );
 
 add_filter( 'bp_get_activity_content', 'wpautop' );
 
+add_filter( 'bp_get_activity_content', 'make_clickable' );
+
 add_filter( 'bp_get_activity_content', 'stripslashes_deep' );
 
-add_filter( 'bp_get_activity_content', 'make_clickable' );
+function bp_activity_add_allowed_tags( $allowedtags ) {
+	$allowedtags['span'] = array();
+	$allowedtags['span']['class'] = array();
+	return $allowedtags;
+}
+add_filter( 'edit_allowedtags', 'bp_activity_add_allowed_tags', 1 );
 
 ?>
