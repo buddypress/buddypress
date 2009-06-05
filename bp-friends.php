743ff901a6ac3ffc313185397f33568811d6d412
Andy Peatling <apeatling@gmail.com>
2009-06-05 19:19:37 +0000

    Fixes #744
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@1517 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-friends.php
+++ bp-friends.php
@@ -181,6 +181,85 @@ function friends_screen_notification_settings() {
 add_action( 'bp_notification_settings', 'friends_screen_notification_settings' );
 
 
+/***** Actions **********/
+
+function friends_action_add_friend() {
+	global $bp;
+
+	if ( $bp->current_component != $bp->friends->slug || $bp->current_action != 'add-friend' )
+		return false;
+	
+	$potential_friend_id = $bp->action_variables[0];
+
+	if ( !is_numeric( $potential_friend_id ) || !isset( $potential_friend_id ) )
+		return false;
+
+	if ( $potential_friend_id == $bp->loggedin_user->id )
+		return false;
+	
+	$friendship_status = BP_Friends_Friendship::check_is_friend( $bp->loggedin_user->id, $potential_friend_id );
+
+	if ( 'not_friends' == $friendship_status ) {
+		
+		if ( !check_admin_referer( 'friends_add_friend' ) )
+			return false;
+			
+		if ( !friends_add_friend( $bp->loggedin_user->id, $potential_friend_id ) ) {
+			bp_core_add_message( __( 'Friendship could not be requested.', 'buddypress' ), 'error' );
+		} else {
+			bp_core_add_message( __( 'Friendship requested', 'buddypress' ) );
+		}
+	} else if ( 'is_friend' == $friendship_status ) {
+		bp_core_add_message( __( 'You are already friends with this user', 'buddypress' ), 'error' );		
+	} else {
+		bp_core_add_message( __( 'You already have a pending friendship request with this user', 'buddypress' ), 'error' );		
+	}
+	
+	bp_core_redirect( $_SERVER['HTTP_REFERER'] );
+	
+	return false;
+}
+add_action( 'init', 'friends_action_add_friend' );
+
+function friends_action_remove_friend() {
+	global $bp;
+	
+	if ( $bp->current_component != $bp->friends->slug || $bp->current_action != 'remove-friend' )
+		return false;
+	
+	$potential_friend_id = $bp->action_variables[0];
+
+	if ( !is_numeric( $potential_friend_id ) || !isset( $potential_friend_id ) )
+		return false;
+
+	if ( $potential_friend_id == $bp->loggedin_user->id )
+		return false;
+		
+	$friendship_status = BP_Friends_Friendship::check_is_friend( $bp->loggedin_user->id, $potential_friend_id );
+	
+	if ( 'is_friend' == $friendship_status ) {
+		
+		if ( !check_admin_referer( 'friends_remove_friend' ) )
+			return false;
+			
+		if ( !friends_remove_friend( $bp->loggedin_user->id, $potential_friend_id ) ) {
+			bp_core_add_message( __( 'Friendship could not be canceled.', 'buddypress' ), 'error' );
+		} else {
+			bp_core_add_message( __( 'Friendship canceled', 'buddypress' ) );
+		}
+	} else if ( 'is_friends' == $friendship_status ) {
+		bp_core_add_message( __( 'You are not yet friends with this user', 'buddypress' ), 'error' );		
+	} else {
+		bp_core_add_message( __( 'You have a pending friendship request with this user', 'buddypress' ), 'error' );		
+	}
+	
+	bp_core_redirect( $_SERVER['HTTP_REFERER'] );
+	
+	return false;
+}
+add_action( 'init', 'friends_action_remove_friend' );
+
+
 /**************************************************************************
  friends_record_activity()
  
