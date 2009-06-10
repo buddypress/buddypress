40d750d6068a079e6a330b4dd0004baf13362f5c
Andy Peatling <apeatling@gmail.com>
2009-06-10 03:19:42 +0000

    Fixed activity stream template loop per page support
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@1532 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-activity/bp-activity-templatetags.php
+++ bp-activity/bp-activity-templatetags.php
@@ -28,10 +28,10 @@ class BP_Activity_Template {
 			$this->activities = bp_activity_get_sitewide_activity( $max, $this->pag_num, $this->pag_page );
 		
 		if ( $type == 'personal' )
-			$this->activities = bp_activity_get_user_activity( $user_id, $timeframe, $this->page_num, $this->pag_page );
+			$this->activities = bp_activity_get_user_activity( $user_id, $max, $timeframe, false, $this->page_num, $this->pag_page );
 
 		if ( $type == 'friends' && ( bp_is_home() || is_site_admin() || $bp->loggedin_user->id == $user_id ) )
-			$this->activities = bp_activity_get_friends_activity( $user_id, $timeframe, $this->pag_num, $this->pag_page );
+			$this->activities = bp_activity_get_friends_activity( $user_id, $max, $timeframe, false, $this->pag_num, $this->pag_page );
 		
 		if ( !$max )
 			$this->total_activity_count = (int)$this->activities['total'];
