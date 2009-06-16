40994eb983706152bdd7ad2f5e503141c0d7a2f8
Andy Peatling <apeatling@gmail.com>
2009-06-16 19:16:38 +0000

    Removing dash from wp, as this could be added after this point.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@1552 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-groups.php
+++ bp-groups.php
@@ -1598,8 +1598,8 @@ function groups_create_group( $step, $group_id ) {
 function groups_check_slug( $slug ) {
 	global $bp;
 
-	if ( 'wp-' == substr( $slug, 0, 3 ) )
-		$slug = substr( $slug, 3, strlen( $slug ) - 3 );
+	if ( 'wp' == substr( $slug, 0, 2 ) )
+		$slug = substr( $slug, 2, strlen( $slug ) - 2 );
 			
 	if ( in_array( $slug, $bp->groups->forbidden_names ) ) {
 		$slug = $slug . '-' . rand();
