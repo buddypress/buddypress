e4771abbfe561e98862520c5a765ea7c54ca7db3
Andy Peatling <apeatling@gmail.com>
2009-07-15 19:50:00 +0000

    Added filters to welcome widget.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@1611 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-core/bp-core-widgets.php
+++ bp-core/bp-core-widgets.php
@@ -25,7 +25,7 @@ class BP_Core_Welcome_Widget extends WP_Widget {
 			. $after_title; ?>
 
 		<?php if ( $instance['title'] ) : ?><h3><?php echo attribute_escape( stripslashes( $instance['title'] ) ) ?></h3><?php endif; ?>
-		<?php if ( $instance['text'] ) : ?><p><?php echo attribute_escape( stripslashes( $instance['text'] ) ) ?></p><?php endif; ?>
+		<?php if ( $instance['text'] ) : ?><p><?php echo apply_filters( 'bp_core_welcome_widget_text', $instance['text'] ) ?></p><?php endif; ?>
 
 		<?php if ( !is_user_logged_in() ) { ?>
 		<div class="create-account"><div class="visit generic-button"><a href="<?php bp_signup_page() ?>" title="<?php _e('Create Account', 'buddypress') ?>"><?php _e('Create Account', 'buddypress') ?></a></div></div>
@@ -48,15 +48,23 @@ class BP_Core_Welcome_Widget extends WP_Widget {
 		$title = strip_tags( $instance['title'] );
 		$text = strip_tags( $instance['text'] );
 		?>
-			<p><label for="bp-widget-welcome-title"><?php _e('Title:', 'buddypress'); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo attribute_escape( $title ); ?>" /></label></p>
+			<p><label for="bp-widget-welcome-title"><?php _e('Title:', 'buddypress'); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo attribute_escape( stripslashes( $title ) ); ?>" /></label></p>
 			<p>
 				<label for="bp-widget-welcome-text"><?php _e( 'Welcome Text:' , 'buddypress'); ?>
-					<textarea id="<?php echo $this->get_field_id( 'text' ); ?>" name="<?php echo $this->get_field_name( 'text' ); ?>" class="widefat" style="height: 100px"><?php echo attribute_escape( $text ); ?></textarea>
+					<textarea id="<?php echo $this->get_field_id( 'text' ); ?>" name="<?php echo $this->get_field_name( 'text' ); ?>" class="widefat" style="height: 100px"><?php echo attribute_escape( stripslashes( $text ) ); ?></textarea>
 				</label>
 			</p>
 	<?php
 	}
 }
+add_filter( 'bp_core_welcome_widget_text', 'attribute_escape' );
+add_filter( 'bp_core_welcome_widget_text', 'wptexturize' );
+add_filter( 'bp_core_welcome_widget_text', 'convert_smilies' );
+add_filter( 'bp_core_welcome_widget_text', 'convert_chars' );
+add_filter( 'bp_core_welcome_widget_text', 'stripslashes' );
+add_filter( 'bp_core_welcome_widget_text', 'wpautop' );
+add_filter( 'bp_core_welcome_widget_text', 'force_balance_tags' );
+
 
 /*** MEMBERS WIDGET *****************/
 
