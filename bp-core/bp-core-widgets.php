02b908c6468023f8b521a2dc05eef6f6285fbc65
Andy Peatling <apeatling@gmail.com>
2009-07-15 19:42:30 +0000

    Adding stripslashes to welcome widget output.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@1610 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-core/bp-core-widgets.php
+++ bp-core/bp-core-widgets.php
@@ -24,8 +24,8 @@ class BP_Core_Welcome_Widget extends WP_Widget {
 			. $instance['title']
 			. $after_title; ?>
 
-		<?php if ( $instance['title'] ) : ?><h3><?php echo attribute_escape( $instance['title'] ) ?></h3><?php endif; ?>
-		<?php if ( $instance['text'] ) : ?><p><?php echo attribute_escape( $instance['text'] ) ?></p><?php endif; ?>
+		<?php if ( $instance['title'] ) : ?><h3><?php echo attribute_escape( stripslashes( $instance['title'] ) ) ?></h3><?php endif; ?>
+		<?php if ( $instance['text'] ) : ?><p><?php echo attribute_escape( stripslashes( $instance['text'] ) ) ?></p><?php endif; ?>
 
 		<?php if ( !is_user_logged_in() ) { ?>
 		<div class="create-account"><div class="visit generic-button"><a href="<?php bp_signup_page() ?>" title="<?php _e('Create Account', 'buddypress') ?>"><?php _e('Create Account', 'buddypress') ?></a></div></div>
