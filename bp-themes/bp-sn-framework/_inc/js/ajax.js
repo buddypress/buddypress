b6877638c52d301e5db06743ea8cd96a04fbd27a
Andy Peatling <apeatling@gmail.com>
2009-08-31 19:42:08 +0000

    Fixes #958
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@1744 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-sn-framework/_inc/js/ajax.js
+++ bp-themes/bp-sn-framework/_inc/js/ajax.js
@@ -874,46 +874,6 @@ jQuery(document).ready( function() {
 		}
 	);
 
-	jQuery("div#user-status p, a#status-new-status").livequery('click', 
-		function() {
-			jQuery.post( ajaxurl, {
-				action: 'status_show_form',
-				'cookie': encodeURIComponent(document.cookie)
-			},
-			function(response) {				
-				jQuery("div#user-status").slideUp(400, 
-					function() {
-						jQuery("div#user-status").html(response);
-						jQuery("div#user-status").slideDown(400, function() {
-							jQuery("#status-update-input").focus();
-						});
-					}
-				);
-				
-				jQuery(window).bind('click', function(ev) {
-					if ( !jQuery(ev.target).is('div#user-status') && !jQuery(ev.target).parents('div#user-status').length ) {
-						jQuery.post( ajaxurl, {
-							action: 'status_show_status',
-							'cookie': encodeURIComponent(document.cookie)
-						},
-						function(response) {				
-							jQuery("div#user-status").slideUp(400, 
-								function() {
-									jQuery("div#user-status").html(response);
-									jQuery("div#user-status").slideDown(400);
-								}
-							);
-							
-							jQuery(window).unbind('click');
-						});
-					}
-				});
-			});
-
-			return false;
-		}
-	);
-
 	jQuery("form#status-update-form").livequery('submit', 
 		function() {
 			jQuery.post( ajaxurl, {
