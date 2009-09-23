fa7c4f062e805cd8e4728b52355104a2d3e7f34e
Andy Peatling <apeatling@gmail.com>
2009-09-23 02:54:38 +0000

    Fixes #793
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@1938 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-sn-parent/userbar.php
+++ bp-themes/bp-sn-parent/userbar.php
@@ -23,18 +23,18 @@
 		
 		<p id="login-text"><?php _e( 'You must log in to access your account.', 'buddypress' ) ?></p>
 	
-		<form name="loginform" id="loginform" action="<?php echo site_url( 'wp-login.php', 'login' ) ?>" method="post">
+		<form name="userbar_loginform" id="userbar_loginform" action="<?php echo site_url( 'wp-login.php', 'login' ) ?>" method="post">
 			<p>
 				<label><?php _e( 'Username', 'buddypress' ) ?><br />
-				<input type="text" name="log" id="user_login" class="input" value="<?php echo attribute_escape(stripslashes($user_login)); ?>" /></label>
+				<input type="text" name="log" id="userbar_user_login" class="input" value="<?php echo attribute_escape(stripslashes($user_login)); ?>" /></label>
 			</p>
 			<p>
 				<label><?php _e( 'Password', 'buddypress' ) ?><br />
-				<input type="password" name="pwd" id="user_pass" class="input" value="" /></label>
+				<input type="password" name="pwd" id="userbar_user_pass" class="input" value="" /></label>
 			</p>
-			<p class="forgetmenot"><label><input name="rememberme" type="checkbox" id="rememberme" value="forever" /> <?php _e( 'Remember Me', 'buddypress' ) ?></label></p>
+			<p class="forgetmenot"><label><input name="rememberme" type="checkbox" id="userbar_rememberme" value="forever" /> <?php _e( 'Remember Me', 'buddypress' ) ?></label></p>
 			<p class="submit">
-				<input type="submit" name="wp-submit" id="wp-submit" value="<?php _e('Log In'); ?>" tabindex="100" />
+				<input type="submit" name="wp-submit" id="userbar_wp-submit" value="<?php _e('Log In'); ?>" tabindex="100" />
 				<input type="hidden" name="redirect_to" value="http://<?php echo $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] ?>?nr" />
 				<input type="hidden" name="testcookie" value="1" />
 			</p>
