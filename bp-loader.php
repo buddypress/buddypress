691a8f8e2341b55fd16c8a02ce781b13ea1ed881
Andy Peatling <apeatling@gmail.com>
2009-10-05 20:36:20 +0000

    Updating trunk version numbers to 1.2-bleeding.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@2042 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-loader.php
+++ bp-loader.php
@@ -4,12 +4,12 @@ Plugin Name: BuddyPress
 Plugin URI: http://buddypress.org/download/
 Description: BuddyPress will add social networking features to a new or existing WordPress MU installation.
 Author: The BuddyPress Community
-Version: 1.1.1
+Version: 1.2-bleeding
 Author URI: http://buddypress.org/developers/
 Site Wide Only: true
 */
 
-define( 'BP_VERSION', '1.1.1' );
+define( 'BP_VERSION', '1.2-bleeding' );
 
 /***
  * This file will load in each BuddyPress component based on which
