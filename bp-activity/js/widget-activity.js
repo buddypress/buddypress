eaeaf71c95f371ff0ca7a03bb92e740bec78bc07
Andy Peatling <apeatling@gmail.com>
2009-11-27 16:00:31 +0000

    Adding missed widget JS
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@2148 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

---
+++ bp-activity/js/widget-activity.js
@@ -0,0 +1,84 @@
+jQuery(document).ready( function() {
+	var j = jQuery;
+
+	if ( j('div.widget_bp_activity_widget').length )
+		bp_activity_widget_post( j.cookie('bp_atype'), j.cookie('bp_afilter') );
+
+	/* List tabs event delegation */
+	j('div.item-list-tabs').click( function(event) {
+		var target = j(event.target).parent();
+
+		/* Activity Stream Tabs */
+		if ( target.attr('id') == 'activity-all' ||
+		 	 target.attr('id') == 'activity-friends' ||
+			 target.attr('id') == 'activity-groups' ) {
+
+			var type = target.attr('id').substr( 9, target.attr('id').length );
+			var filter = j("#activity-filter-select select").val();
+
+			bp_activity_widget_post(type, filter);
+
+			return false;
+		}
+	});
+
+	j('#activity-filter-select select').change( function() {
+		var selected_tab = j( '.' + j(this).parent().parent().parent().attr('class') + ' li.selected');
+		var type = selected_tab.attr('id').substr( 9, selected_tab.attr('id').length );
+		var filter = j(this).val();
+
+		bp_activity_widget_post(type, filter);
+
+		return false;
+	});
+
+	function bp_activity_widget_post(type, filter) {
+		if ( null == type )
+			var type = 'all';
+
+		if ( null == filter )
+			var filter = '-1';
+
+		/* Save the type and filter to a session cookie */
+		j.cookie( 'bp_atype', type, null );
+		j.cookie( 'bp_afilter', filter, null );
+
+		/* Set the correct selected nav and filter */
+		j('.widget_bp_activity_widget div.item-list-tabs li').each( function() {
+			j(this).removeClass('selected');
+		});
+		j('li#activity-' + type).addClass('selected');
+		j('#activity-filter-select select option[value=' + filter + ']').attr( 'selected', 'selected' );
+
+		/* Reload the activity stream based on the selection */
+		j('.widget_bp_activity_widget h2 span.ajax-loader').show();
+
+		j.post( ajaxurl, {
+			action: 'activity_widget_filter',
+			'cookie': encodeURIComponent(document.cookie),
+			'_wpnonce_activity_filter': j("input#_wpnonce_activity_filter").val(),
+			'type': type,
+			'filter': filter
+		},
+		function(response)
+		{
+			j('.widget_bp_activity_widget h2 span.ajax-loader').hide();
+
+			/* Check for errors and append if found. */
+			if ( response[0] + response[1] == '-1' ) {
+				j('div.astream').fadeOut( 100, function() {
+					j(this).html( response.substr( 2, response.length ) ).hide().fadeIn( 200 );
+					j(this).fadeIn(100);
+				});
+			} else {
+				j('div.astream').fadeOut( 100, function() {
+					j(this).html(response);
+					j(this).fadeIn(100);
+				});
+			}
+		});
+	}
+});
+
+/* jQuery Cookie plugin */
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('o.5=B(9,b,2){6(h b!=\'E\'){2=2||{};6(b===n){b=\'\';2.3=-1}4 3=\'\';6(2.3&&(h 2.3==\'j\'||2.3.k)){4 7;6(h 2.3==\'j\'){7=w u();7.t(7.q()+(2.3*r*l*l*x))}m{7=2.3}3=\'; 3=\'+7.k()}4 8=2.8?\'; 8=\'+(2.8):\'\';4 a=2.a?\'; a=\'+(2.a):\'\';4 c=2.c?\'; c\':\'\';d.5=[9,\'=\',C(b),3,8,a,c].y(\'\')}m{4 e=n;6(d.5&&d.5!=\'\'){4 g=d.5.A(\';\');s(4 i=0;i<g.f;i++){4 5=o.z(g[i]);6(5.p(0,9.f+1)==(9+\'=\')){e=D(5.p(9.f+1));v}}}F e}};',42,42,'||options|expires|var|cookie|if|date|path|name|domain|value|secure|document|cookieValue|length|cookies|typeof||number|toUTCString|60|else|null|jQuery|substring|getTime|24|for|setTime|Date|break|new|1000|join|trim|split|function|encodeURIComponent|decodeURIComponent|undefined|return'.split('|'),0,{}))
