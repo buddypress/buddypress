c699da47e9aea76189360c3b0358f259e5c5be66
Andy Peatling <apeatling@gmail.com>
2009-11-30 00:25:30 +0000

    Fixing incorrect activation email link.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@2157 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-core/deprecated/bp-core-deprecated.php
+++ bp-core/deprecated/bp-core-deprecated.php
@@ -539,68 +539,6 @@ function bp_core_activation_do_activation() {
 	}
 }
 
-// Notify user of signup success.
-function bp_core_activation_signup_blog_notification( $domain, $path, $title, $user, $user_email, $key, $meta ) {
-	global $current_site;
-
-	// Send email with activation link.
-	if ( 'no' == constant( "VHOST" ) ) {
-		$activate_url = bp_activation_page( false ) . "?key=$key";
-	} else {
-		$activate_url = bp_activation_page( false ) ."?key=$key";
-	}
-
-	$activate_url = clean_url($activate_url);
-	$admin_email = get_site_option( "admin_email" );
-
-	if ( empty( $admin_email ) )
-		$admin_email = 'support@' . $_SERVER['SERVER_NAME'];
-
-	$from_name = ( '' == get_site_option( "site_name" ) ) ? 'WordPress' : wp_specialchars( get_site_option( "site_name" ) );
-	$message_headers = "MIME-Version: 1.0\n" . "From: \"{$from_name}\" <{$admin_email}>\n" . "Content-Type: text/plain; charset=\"" . get_option('blog_charset') . "\"\n";
-	$message = sprintf(__("To activate your blog, please click the following link:\n\n%s\n\nAfter you activate, you will receive *another email* with your login.\n\nAfter you activate, you can visit your blog here:\n\n%s", 'buddypress' ), $activate_url, clean_url("http://{$domain}{$path}" ) );
-	$subject = '[' . $from_name . '] ' . sprintf(__('Activate %s', 'buddypress' ), clean_url('http://' . $domain . $path));
-
-	wp_mail($user_email, $subject, $message, $message_headers);
-
-	// Return false to stop the original WPMU function from continuing
-	return false;
-}
-
-function bp_core_activation_signup_user_notification( $user, $user_email, $key, $meta ) {
-	global $current_site;
-
-	if( !apply_filters('wpmu_signup_user_notification', $user, $user_email, $key, $meta) )
-		return false;
-
-	// Send email with activation link.
-	$admin_email = get_site_option( "admin_email" );
-
-	if ( $admin_email == '' )
-		$admin_email = 'support@' . $_SERVER['SERVER_NAME'];
-
-	$from_name = get_site_option( "site_name" ) == '' ? 'WordPress' : wp_specialchars( get_site_option( "site_name" ) );
-	$message_headers = "MIME-Version: 1.0\n" . "From: \"{$from_name}\" <{$admin_email}>\n" . "Content-Type: text/plain; charset=\"" . get_option('blog_charset') . "\"\n";
-	$message = sprintf( apply_filters( 'wpmu_signup_user_notification_email', __( "To activate your user, please click the following link:\n\n%s\n\nAfter you activate, you will receive *another email* with your login.\n\n" ) ), site_url( "wp-activate.php?key=$key" ), $key );
-
-	// TODO: Don't hard code activation link.
-	$subject = sprintf(__( apply_filters( 'wpmu_signup_user_notification_subject', 'Activate %s' )), $user);
-	wp_mail($user_email, $subject, $message, $message_headers);
-
-	// Return false to stop the original WPMU function from continuing
-	return false;
-}
-
-function bp_core_load_deprecated_signup_email_functions() {
-	/* If we are using a BuddyPress 1.1+ theme ignore this. */
-	if ( !file_exists( WP_CONTENT_DIR . '/bp-themes' ) )
-		return false;
-
-	add_filter( 'wpmu_signup_user_notification', 'bp_core_activation_signup_user_notification', 1, 4 );
-	add_filter( 'wpmu_signup_blog_notification', 'bp_core_activation_signup_blog_notification', 1, 7 );
-}
-add_action( 'plugins_loaded', 'bp_core_load_deprecated_signup_email_functions' );
-
 /*** END DEPRECATED SIGNUP FUNCTIONS **********/
 
 
