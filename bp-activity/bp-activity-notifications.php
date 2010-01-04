4e4cf489ad8e71e114abb99fea9809de122932c1
Andy Peatling <apeatling@gmail.com>
2010-01-04 16:08:57 +0000

    Stop comment email notifications for comments on a user's own activity.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@2261 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-activity/bp-activity-notifications.php
+++ bp-activity/bp-activity-notifications.php
@@ -6,6 +6,10 @@ function bp_activity_new_comment_notification( $comment_id, $params ) {
 	extract( $params );
 	$original_activity = new BP_Activity_Activity( $activity_id );
 
+	/* Don't email comments on a member's own activity */
+	if ( $original_activity->user_id == $user_id )
+		return false;
+
 	if ( !get_usermeta( $user_id, 'notification_activity_new_reply' ) || 'yes' == get_usermeta( $user_id, 'notification_activity_new_reply' ) ) {
 		$poster_name = bp_core_get_user_displayname( $user_id );
 		$thread_link = bp_activity_get_permalink( $activity_id );
