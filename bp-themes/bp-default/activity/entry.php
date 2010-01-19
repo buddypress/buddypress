30cdd14bb5a7dfb737bad55024c03d3f07babe27
Andy Peatling <apeatling@gmail.com>
2010-01-19 16:07:30 +0000

    Fixes #1627 props hempsworth
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@2363 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/activity/entry.php
+++ bp-themes/bp-default/activity/entry.php
@@ -31,7 +31,7 @@
 
 	<?php if ( 'activity_comment' == bp_get_activity_action_name() ) : ?>
 		<div class="activity-inreplyto">
-			<strong><?php _e( 'In reply to', 'buddypress' ) ?></strong> - <?php bp_activity_parent_content() ?>
+			<strong><?php _e( 'In reply to', 'buddypress' ) ?></strong> - <?php bp_activity_parent_content() ?> &middot;
 			<a href="<?php bp_activity_thread_permalink() ?>" class="view" title="<?php _e( 'View Thread / Permalink', 'buddypress' ) ?>">#</a>
 		</div>
 	<?php endif; ?>
