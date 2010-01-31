938ea48178853d0869a0c0f33e1b38744201eac6
Andy Peatling <apeatling@gmail.com>
2010-01-31 14:39:44 +0000

    Fixes #1653
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@2499 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-activity/bp-activity-templatetags.php
+++ bp-activity/bp-activity-templatetags.php
@@ -573,7 +573,7 @@ function bp_activity_comments( $args = '' ) {
 
 				/* Delete link */
 				if ( is_site_admin() || $bp->loggedin_user->id == $comment->user_id )
-					$content .= ' &middot; <a href="' . wp_nonce_url( $bp->activity->id . '/delete/?cid=' . $comment->id, 'bp_activity_delete_link' ) . '" class="delete acomment-delete">' . __( 'Delete', 'buddypress' ) . '</a>';
+					$content .= ' &middot; <a href="' . wp_nonce_url( $bp->root_domain . '/' . $bp->activity->slug . '/delete/?cid=' . $comment->id, 'bp_activity_delete_link' ) . '" class="delete acomment-delete">' . __( 'Delete', 'buddypress' ) . '</a>';
 
 				$content .= '</div>';
 				$content .= '<div class="acomment-content">' . apply_filters( 'bp_get_activity_content', $comment->content ) . '</div>';
