56e5c9f1d695a182a49f26f798005f0e1b0873fa
Andy Peatling <apeatling@gmail.com>
2010-01-31 13:21:13 +0000

    Fixes #1737
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@2490 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/members/single/activity.php
+++ bp-themes/bp-default/members/single/activity.php
@@ -22,7 +22,7 @@
 
 <?php do_action( 'bp_before_member_activity_post_form' ) ?>
 
-<?php if ( is_user_logged_in() && bp_is_my_profile() ) : ?>
+<?php if ( is_user_logged_in() && bp_is_my_profile() && ( '' == bp_current_action() || 'just-me' == bp_current_action() ) ) : ?>
 	<?php locate_template( array( 'activity/post-form.php'), true ) ?>
 <?php endif; ?>
 
