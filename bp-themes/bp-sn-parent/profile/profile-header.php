f69c7a5bcb994e5ec8daea5a65cdaaab68666088
Andy Peatling <apeatling@gmail.com>
2010-02-04 16:45:26 +0000

    Removing the classic theme from the core, this will be provided instead in the bp-backwards-compatibility plugin.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@2597 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-sn-parent/profile/profile-header.php
+++
@@ -1,9 +0,0 @@
-<?php do_action( 'bp_before_profile_header_content' ) ?>
-
-<div id="profile-name">
-	<h1 class="fn"><a href="<?php bp_user_link() ?>"><?php bp_user_fullname() ?></a></h1>
-
-	<?php do_action( 'bp_profile_header_content' ) ?>
-</div>
-
-<?php do_action( 'bp_after_profile_header_content' ) ?>
