7f6ba1f430a6078c0c0dcb9833207af05d98d616
John James Jacoby <johnjamesjacoby@me.com>
2010-02-05 06:20:14 +0000

    Fixes #1815
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@2603 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-blogs.php
+++ bp-blogs.php
@@ -657,8 +657,8 @@ function bp_blogs_get_all_blogs( $limit = null, $page = null ) {
 	return BP_Blogs_Blog::get_all( $limit, $page );
 }
 
-function bp_blogs_get_random_blog( $limit = null, $page = null ) {
-	return BP_Blogs_Blog::get_random( $limit, $page );
+function bp_blogs_get_random_blogs( $limit = null, $page = null ) {
+	return BP_Blogs_Blog::get( 'random', $limit, $page );
 }
 
 function bp_blogs_get_all_posts( $limit = null, $page = null ) {
@@ -681,7 +681,7 @@ function bp_blogs_redirect_to_random_blog() {
 	global $bp, $wpdb;
 
 	if ( $bp->current_component == $bp->blogs->slug && isset( $_GET['random-blog'] ) ) {
-		$blog = bp_blogs_get_random_blog();
+		$blog = bp_blogs_get_random_blogs( 1, 1 );
 
 		bp_core_redirect( get_blog_option( $blog['blogs'][0]->blog_id, 'siteurl') );
 	}
