a0f5e4a15a2c6848c45e75fb90ad929ab99e38d1
Andy Peatling <apeatling@gmail.com>
2010-02-12 14:28:15 +0000

    Using get_site_option() on first install of xprofile so values are set.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@2704 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-xprofile.php
+++ bp-xprofile.php
@@ -73,13 +73,13 @@ function xprofile_install() {
 
 	if ( empty( $bp->option['bp-xprofile-db-version'] ) ) {
 		if ( !$wpdb->get_var( "SELECT id FROM {$bp->profile->table_name_groups} WHERE id = 1" ) )
-			$sql[] = "INSERT INTO {$bp->profile->table_name_groups} VALUES ( 1, '" . $bp->option['bp-xprofile-base-group-name'] . "', '', 0 );";
+			$sql[] = "INSERT INTO {$bp->profile->table_name_groups} VALUES ( 1, '" . get_site_option( 'bp-xprofile-base-group-name' ) . "', '', 0 );";
 
 		if ( !$wpdb->get_var( "SELECT id FROM {$bp->profile->table_name_fields} WHERE id = 1" ) ) {
 			$sql[] = "INSERT INTO {$bp->profile->table_name_fields} (
 						id, group_id, parent_id, type, name, is_required, can_delete
 					  ) VALUES (
-						1, 1, 0, 'textbox', '" . $bp->option['bp-xprofile-fullname-field-name'] . "', 1, 0
+						1, 1, 0, 'textbox', '" . get_site_option( 'bp-xprofile-fullname-field-name' ) . "', 1, 0
 					  );";
 		}
 	}
