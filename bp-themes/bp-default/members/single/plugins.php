bcb050ec170c1ea3975c656949c98bf60bf6fdf1
Andy Peatling <apeatling@gmail.com>
2010-02-24 20:35:18 +0000

    Removing invalid hooks in the members plugin template.
    git-svn-id: https://buddypress.svn.wordpress.org/branches/1.2@2787 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/members/single/plugins.php
+++ bp-themes/bp-default/members/single/plugins.php
@@ -13,8 +13,6 @@
 				<div class="item-list-tabs no-ajax" id="object-nav">
 					<ul>
 						<?php bp_get_displayed_user_nav() ?>
-
-						<?php do_action( 'bp_members_directory_member_types' ) ?>
 					</ul>
 				</div>
 			</div>
@@ -24,12 +22,12 @@
 				<div class="item-list-tabs no-ajax" id="subnav">
 					<ul>
 						<?php bp_get_options_nav() ?>
+
+						<?php do_action( 'bp_member_plugin_options_nav' ) ?>
 					</ul>
 				</div>
 
 				<?php do_action( 'bp_template_content' ) ?>
-
-				<?php do_action( 'bp_directory_members_content' ) ?>
 
 			</div><!-- #item-body -->
 
