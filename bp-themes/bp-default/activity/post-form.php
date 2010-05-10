d5bf786768e2ed51eb51d5b49b6eb92eb7caebb7
John James Jacoby <johnjamesjacoby@me.com>
2010-05-10 08:39:26 +0000

    Fix invalid XHTML on activity post-form.
    git-svn-id: https://buddypress.svn.wordpress.org/branches/1.2@2993 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/activity/post-form.php
+++ bp-themes/bp-default/activity/post-form.php
@@ -24,7 +24,7 @@
 
 	<div id="whats-new-content">
 		<div id="whats-new-textarea">
-			<textarea name="whats-new" id="whats-new" value="" /><?php if ( isset( $_GET['r'] ) ) : ?>@<?php echo esc_attr( $_GET['r'] ) ?> <?php endif; ?></textarea>
+			<textarea name="whats-new" id="whats-new" cols="50" rows="10"><?php if ( isset( $_GET['r'] ) ) : ?>@<?php echo esc_attr( $_GET['r'] ) ?> <?php endif; ?></textarea>
 		</div>
 
 		<div id="whats-new-options">
