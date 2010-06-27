77d018ead5790801bd588a4b9e87e5a50aa604ce
John James Jacoby <johnjamesjacoby@me.com>
2010-06-27 01:17:24 +0000

    Fixes #2458
    git-svn-id: https://buddypress.svn.wordpress.org/branches/1.2@3075 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/_inc/global.js
+++ bp-themes/bp-default/_inc/global.js
@@ -228,7 +228,7 @@ jq(document).ready( function() {
 
 		/* Load more updates at the end of the page */
 		if ( target.parent().attr('class') == 'load-more' ) {
-			jq("li.load-more").addClass('loading');
+			jq("#content li.load-more").addClass('loading');
 
 			if ( null == jq.cookie('bp-activity-oldestpage') )
 				jq.cookie('bp-activity-oldestpage', 1, {path: '/'} );
@@ -242,9 +242,9 @@ jq(document).ready( function() {
 			},
 			function(response)
 			{
-				jq("li.load-more").removeClass('loading');
+				jq("#content li.load-more").removeClass('loading');
 				jq.cookie( 'bp-activity-oldestpage', oldest_page, {path: '/'} );
-				jq("ul.activity-list").append(response.contents);
+				jq("#content ul.activity-list").append(response.contents);
 
 				target.parent().hide();
 			}, 'json' );
