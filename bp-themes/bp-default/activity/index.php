379ea04460fac29c978c73e6a24b91e81e1f7862
John James Jacoby <johnjamesjacoby@me.com>
2010-08-25 02:43:59 +0000

    Fixes #2596 props Paul Gibbs
    git-svn-id: https://buddypress.svn.wordpress.org/branches/1.2@3213 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/activity/index.php
+++ bp-themes/bp-default/activity/index.php
@@ -57,7 +57,7 @@
 
 			<div class="item-list-tabs no-ajax" id="subnav">
 				<ul>
-					<li class="feed"><a href="<?php bp_sitewide_activity_feed_link() ?>" title="RSS Feed"><?php _e( 'RSS', 'buddypress' ) ?></a></li>
+					<li class="feed"><a href="<?php bp_sitewide_activity_feed_link() ?>" title="<?php _e( 'RSS Feed', 'buddypress' ); ?>"><?php _e( 'RSS', 'buddypress' ) ?></a></li>
 
 					<?php do_action( 'bp_activity_syndication_options' ) ?>
 
