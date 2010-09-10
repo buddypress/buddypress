34b545ed7fdc2adccabf02dc730d40db2fc00c0a
John James Jacoby <johnjamesjacoby@me.com>
2010-09-10 23:10:55 +0000

    xProfile filter clean-up(1.2 branch). Fixes #2603. Props Paul Gibbs, mariochampion.
    git-svn-id: https://buddypress.svn.wordpress.org/branches/1.2@3248 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-xprofile/bp-xprofile-filters.php
+++ bp-xprofile/bp-xprofile-filters.php
@@ -1,57 +1,54 @@
 <?php
 
 /* Apply WordPress defined filters */
-add_filter( 'bp_get_the_profile_field_value', 'wp_filter_kses', 1 );
-add_filter( 'bp_get_the_profile_field_name', 'wp_filter_kses', 1 );
 
-add_filter( 'bp_get_the_site_member_profile_data', 'wp_filter_kses', 1 );
-add_filter( 'xprofile_get_field_data', 'wp_filter_kses', 1 );
-add_filter( 'xprofile_field_name_before_save', 'wp_filter_kses', 1 );
+add_filter( 'bp_get_the_profile_field_value',         'wp_filter_kses', 1 );
+add_filter( 'bp_get_the_profile_field_name',          'wp_filter_kses', 1 );
+add_filter( 'bp_get_the_site_member_profile_data',    'wp_filter_kses', 1 );
+add_filter( 'bp_get_the_profile_field_edit_value',    'wp_filter_kses', 1 );
+add_filter( 'bp_get_the_profile_field_description',   'wp_filter_kses', 1 );
+
+add_filter( 'bp_get_the_profile_field_value',         'wptexturize'        );
+add_filter( 'bp_get_the_profile_field_value',         'convert_smilies', 2 );
+add_filter( 'bp_get_the_profile_field_value',         'convert_chars'      );
+add_filter( 'bp_get_the_profile_field_value',         'wpautop'            );
+add_filter( 'bp_get_the_profile_field_value',         'make_clickable'     );
+add_filter( 'bp_get_the_profile_field_value',         'force_balance_tags' );
+
+add_filter( 'bp_get_the_profile_field_value',         'stripslashes' );
+add_filter( 'bp_get_the_profile_field_edit_value',    'stripslashes' );
+add_filter( 'bp_get_the_profile_field_name',          'stripslashes' );
+add_filter( 'bp_get_the_profile_field_description',   'stripslashes' );
+add_filter( 'bp_get_the_site_member_profile_data',    'stripslashes' );
+
+add_filter( 'xprofile_get_field_data',                'wp_filter_kses', 1 );
+add_filter( 'xprofile_field_name_before_save',        'wp_filter_kses', 1 );
 add_filter( 'xprofile_field_description_before_save', 'wp_filter_kses', 1 );
+add_filter( 'xprofile_data_value_before_save',        'wp_filter_kses', 1 );
 
-add_filter( 'bp_get_the_profile_field_edit_value', 'wp_filter_kses', 1 );
-add_filter( 'bp_get_the_profile_field_description', 'wp_filter_kses', 1 );
-
-add_filter( 'xprofile_field_name_before_save', 'force_balance_tags' );
+add_filter( 'xprofile_get_field_data',                'force_balance_tags' );
+add_filter( 'xprofile_field_name_before_save',        'force_balance_tags' );
 add_filter( 'xprofile_field_description_before_save', 'force_balance_tags' );
+add_filter( 'xprofile_data_value_before_save',        'force_balance_tags' );
 
-add_filter( 'bp_get_the_profile_field_value', 'wptexturize' );
-add_filter( 'bp_get_the_profile_field_value', 'convert_smilies', 2 );
-add_filter( 'bp_get_the_profile_field_value', 'convert_chars' );
-add_filter( 'bp_get_the_profile_field_value', 'wpautop' );
-add_filter( 'bp_get_the_profile_field_value', 'make_clickable' );
-add_filter( 'bp_get_the_profile_field_value', 'force_balance_tags' );
-
-add_filter( 'bp_get_the_site_member_profile_data', 'wptexturize' );
-add_filter( 'bp_get_the_site_member_profile_data', 'convert_smilies', 2 );
-add_filter( 'bp_get_the_site_member_profile_data', 'convert_chars' );
-add_filter( 'bp_get_the_site_member_profile_data', 'make_clickable' );
-add_filter( 'bp_get_the_site_member_profile_data', 'force_balance_tags' );
-
-add_filter( 'bp_get_the_profile_field_value', 'xprofile_filter_format_field_value', 1, 2 );
-add_filter( 'bp_get_the_site_member_profile_data', 'xprofile_filter_format_field_value', 1, 2 );
-add_filter( 'bp_get_the_profile_field_value', 'xprofile_filter_link_profile_data', 50, 2 );
-
-add_filter( 'bp_get_the_profile_field_edit_value', 'stripslashes' );
-add_filter( 'bp_get_the_profile_field_value', 'stripslashes' );
-add_filter( 'bp_get_the_profile_field_name', 'stripslashes' );
-add_filter( 'xprofile_get_field_data', 'stripslashes' );
-add_filter( 'bp_get_the_profile_field_description', 'stripslashes' );
-add_filter( 'bp_get_the_site_member_profile_data', 'stripslashes' );
+add_filter( 'xprofile_get_field_data',                'stripslashes' );
 
 /* Custom BuddyPress filters */
 
+add_filter( 'bp_get_the_profile_field_value',         'xprofile_filter_format_field_value', 1, 2 );
+add_filter( 'bp_get_the_site_member_profile_data',    'xprofile_filter_format_field_value', 1, 2 );
+add_filter( 'bp_get_the_profile_field_value',         'xprofile_filter_link_profile_data', 50, 2 );
+
 function xprofile_filter_format_field_value( $field_value, $field_type = '' ) {
-	if ( !isset($field_value) || empty( $field_value ) )
+	if ( !isset( $field_value ) || empty( $field_value ) )
 		return false;
 
-	if ( 'datebox' == $field_type ) {
+	if ( 'datebox' == $field_type )
 		$field_value = bp_format_time( $field_value, true );
-	} else {
+	else
 		$field_value = str_replace(']]>', ']]&gt;', $field_value );
-	}
 
-	return stripslashes( stripslashes( $field_value ) );
+	return stripslashes( $field_value );
 }
 
 function xprofile_filter_link_profile_data( $field_value, $field_type = 'textbox' ) {
@@ -67,14 +64,15 @@ function xprofile_filter_link_profile_data( $field_value, $field_type = 'textbox
 		foreach ( (array)$values as $value ) {
 			$value = trim( $value );
 
-			/* If the value is a URL, skip it and just make it clickable. */
+			// If the value is a URL, skip it and just make it clickable.
 			if ( preg_match( '@(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?)@', $value ) ) {
 				$new_values[] = make_clickable( $value );
 			} else {
-				if ( count( explode( ' ', $value ) ) > 5 )
+				if ( count( explode( ' ', $value ) ) > 5 ) {
 					$new_values[] = $value;
-				else
+				} else {
 					$new_values[] = '<a href="' . site_url( BP_MEMBERS_SLUG ) . '/?s=' . strip_tags( $value ) . '">' . $value . '</a>';
+				}
 			}
 		}
 
@@ -86,8 +84,9 @@ function xprofile_filter_link_profile_data( $field_value, $field_type = 'textbox
 
 function xprofile_filter_comments( $comments, $post_id ) {
 	foreach( (array)$comments as $comment ) {
-		if ( $comment->user_id )
+		if ( $comment->user_id ) {
 			$user_ids[] = $comment->user_id;
+		}
 	}
 
 	if ( empty( $user_ids ) )
@@ -101,8 +100,9 @@ function xprofile_filter_comments( $comments, $post_id ) {
 
 	foreach( (array)$comments as $i => $comment ) {
 		if ( !empty( $comment->user_id ) ) {
-			if ( !empty( $users[$comment->user_id] ) )
+			if ( !empty( $users[$comment->user_id] ) ) {
 				$comments[$i]->comment_author = $users[$comment->user_id];
+			}
 		}
 	}
 
