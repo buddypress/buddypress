5a73fcc9d847762bcddc4100c61dd93bae3f7d33
Paul Gibbs <djpaul@gmail.com>
2010-10-25 17:21:25 +0000

    Fixes #2686, props calvin_42 (trunk)
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@3303 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-core.php
+++ bp-core.php
@@ -1137,7 +1137,7 @@ function bp_core_get_user_displayname( $user_id_or_username ) {
 
 	if ( !$fullname = wp_cache_get( 'bp_user_fullname_' . $user_id, 'bp' ) ) {
 		if ( bp_is_active( 'xprofile' ) ) {
-			$fullname = xprofile_get_field_data( 1, $user_id );
+			$fullname = xprofile_get_field_data( BP_XPROFILE_FULLNAME_FIELD_NAME, $user_id );
 
 			if ( empty($fullname) ) {
 				$ud = bp_core_get_core_userdata( $user_id );
