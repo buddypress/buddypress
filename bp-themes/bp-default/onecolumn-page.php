52e310d82db809d40060f29597092470eb898acc
Paul Gibbs <djpaul@gmail.com>
2010-11-14 23:58:27 +0000

    Add single-column page template to BP-Default
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@3428 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

---
+++ bp-themes/bp-default/onecolumn-page.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Template Name: One column, no sidebar
+ *
+ * A custom page template without sidebar.
+ *
+ * @package BuddyPress
+ * @subpackage BP_Default
+ * @since 1.3
+ */
+
+get_header() ?>
+
+	<div id="content">
+		<div class="padder one-column">
+
+		<?php do_action( 'bp_before_blog_page' ) ?>
+
+		<div class="page" id="blog-page">
+
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+				<h2 class="pagetitle"><?php the_title(); ?></h2>
+
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+					<div class="entry">
+
+						<?php the_content( __( '<p class="serif">Read the rest of this page &rarr;</p>', 'buddypress' ) ); ?>
+
+						<?php wp_link_pages( array( 'before' => __( '<p><strong>Pages:</strong> ', 'buddypress' ), 'after' => '</p>', 'next_or_number' => 'number')); ?>
+						<?php edit_post_link( __( 'Edit this entry.', 'buddypress' ), '<p>', '</p>'); ?>
+
+					</div>
+
+				</div>
+
+			<?php comments_template(); ?>
+
+			<?php endwhile; endif; ?>
+
+		</div><!-- .page -->
+
+		<?php do_action( 'bp_after_blog_page' ) ?>
+
+		</div><!-- .padder -->
+	</div><!-- #content -->
+
+<?php get_footer(); ?>
