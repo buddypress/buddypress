900ccbee02172f2ea52ab363be48397d4dd7ac14
Paul Gibbs <djpaul@gmail.com>
2010-12-18 00:45:24 +0000

    Remove unnecessary groups_update_last_activity action as it is already set in groups_join_group(). Fixes #2734, props r-a-y
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@3536 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-groups.php
+++ bp-groups.php
@@ -1331,7 +1331,6 @@ function groups_update_last_activity( $group_id = false ) {
 	
 	groups_update_groupmeta( $group_id, 'last_activity', bp_core_current_time() );
 }
-add_action( 'groups_joined_group', 'groups_update_last_activity' );
 add_action( 'groups_leave_group', 'groups_update_last_activity' );
 add_action( 'groups_created_group', 'groups_update_last_activity' );
 add_action( 'groups_new_forum_topic', 'groups_update_last_activity' );
