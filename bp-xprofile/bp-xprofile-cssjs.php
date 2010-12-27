bf4070ba8c52d98c27d18105baf0cef3ab17b3b2
Paul Gibbs <djpaul@gmail.com>
2010-12-27 20:50:28 +0000

    Only load xprofile admin CSS/JS on the xprofile admin pages. Fixes #2973
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@3591 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-xprofile/bp-xprofile-cssjs.php
+++ bp-xprofile/bp-xprofile-cssjs.php
@@ -1,9 +1,11 @@
 <?php
 function xprofile_add_admin_css() {
-	if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG )
-		wp_enqueue_style( 'xprofile-admin-css', BP_PLUGIN_URL . '/bp-xprofile/admin/css/admin.dev.css' );
-	else
-		wp_enqueue_style( 'xprofile-admin-css', BP_PLUGIN_URL . '/bp-xprofile/admin/css/admin.css' );
+	if ( !empty( $_GET['page'] ) && strpos( $_GET['page'], 'bp-profile-setup' ) !== false ) {
+		if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG )
+			wp_enqueue_style( 'xprofile-admin-css', BP_PLUGIN_URL . '/bp-xprofile/admin/css/admin.dev.css' );
+		else
+			wp_enqueue_style( 'xprofile-admin-css', BP_PLUGIN_URL . '/bp-xprofile/admin/css/admin.css' );
+	}
 }
 add_action( 'admin_menu', 'xprofile_add_admin_css' );
 
