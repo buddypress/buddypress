a419f207bff84354b3c0a205041e2a4b05f688b6
Paul Gibbs <djpaul@gmail.com>
2010-12-28 14:25:51 +0000

    Tidies up default theme's header.php -- removes duplicated generator line and the hardcoded activity feed links and attaches them to wp_head.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@3595 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/header.php
+++ bp-themes/bp-default/header.php
@@ -1,35 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
-
 	<head profile="http://gmpg.org/xfn/11">
-
-		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-
+		<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ) ?>" />
 		<title><?php bp_page_title() ?></title>
-
-		<?php do_action( 'bp_head' ) ?>
-
-		<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats -->
-
-		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-
-		<?php if ( function_exists( 'bp_sitewide_activity_feed_link' ) ) : ?>
-			<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> | <?php _e('Site Wide Activity RSS Feed', 'buddypress' ) ?>" href="<?php bp_sitewide_activity_feed_link() ?>" />
-		<?php endif; ?>
-
-		<?php if ( function_exists( 'bp_member_activity_feed_link' ) && bp_is_member() ) : ?>
-			<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> | <?php bp_displayed_user_fullname() ?> | <?php _e( 'Activity RSS Feed', 'buddypress' ) ?>" href="<?php bp_member_activity_feed_link() ?>" />
-		<?php endif; ?>
-
-		<?php if ( function_exists( 'bp_group_activity_feed_link' ) && bp_is_group() ) : ?>
-			<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> | <?php bp_current_group_name() ?> | <?php _e( 'Group Activity RSS Feed', 'buddypress' ) ?>" href="<?php bp_group_activity_feed_link() ?>" />
-		<?php endif; ?>
-
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+		<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ) ?>" type="text/css" media="screen" />
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ) ?>" />
 
 		<?php wp_head(); ?>
-
 	</head>
 
 	<body <?php body_class() ?> id="bp-default">
@@ -37,7 +15,6 @@
 		<?php do_action( 'bp_before_header' ) ?>
 
 		<div id="header">
-
 			<h1 id="logo" role="banner"><a href="<?php echo home_url() ?>" title="<?php _e( 'Home', 'buddypress' ) ?>"><?php bp_site_name() ?></a></h1>
 
 			<div id="navigation" role="navigation">
@@ -48,7 +25,6 @@
 				<div class="padder">
 
 				<?php if ( bp_search_form_enabled() ) : ?>
-
 					<form action="<?php echo bp_search_form_action() ?>" method="post" id="search-form">
 						<input type="text" id="search-terms" name="search-terms" value="" />
 						<?php echo bp_search_form_type_select() ?>
@@ -56,7 +32,6 @@
 						<input type="submit" name="search-submit" id="search-submit" value="<?php _e( 'Search', 'buddypress' ) ?>" />
 						<?php wp_nonce_field( 'bp_search_form' ) ?>
 					</form><!-- #search-form -->
-
 				<?php endif; ?>
 
 				<?php do_action( 'bp_search_login_bar' ) ?>
