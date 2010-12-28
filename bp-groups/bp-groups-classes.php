55cd3f3741f242b3b4075bbc5b704ba04e851dbc
Boone B Gorges <boonebgorges@gmail.com>
2010-12-28 12:57:24 +0000

    Fixes PHP warning in BP_Group_Extension class
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@3593 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-groups/bp-groups-classes.php
+++ bp-groups/bp-groups-classes.php
@@ -1158,7 +1158,7 @@ class BP_Group_Extension {
 
 		// Construct the admin edit tab for the new group extension
 		if ( $this->enable_edit_item && $bp->is_item_admin ) {
-			add_action( 'groups_admin_tabs', create_function( '$current, $group_slug', 'if ( "' . esc_attr( $this->slug ) . '" == $current ) $selected = " class=\"current\""; echo "<li{$selected}><a href=\"' . $bp->root_domain . '/' . $bp->groups->slug . '/{$group_slug}/admin/' . esc_attr( $this->slug ) . '\">' . esc_attr( $this->name ) . '</a></li>";' ), 10, 2 );
+			add_action( 'groups_admin_tabs', create_function( '$current, $group_slug', '$selected = ""; if ( "' . esc_attr( $this->slug ) . '" == $current ) $selected = " class=\"current\""; echo "<li{$selected}><a href=\"' . $bp->root_domain . '/' . $bp->groups->slug . '/{$group_slug}/admin/' . esc_attr( $this->slug ) . '\">' . esc_attr( $this->name ) . '</a></li>";' ), 10, 2 );
 
 			// Catch the edit screen and forward it to the plugin template
 			if ( $bp->current_component == $bp->groups->slug && 'admin' == $bp->current_action && $this->slug == $bp->action_variables[0] ) {
