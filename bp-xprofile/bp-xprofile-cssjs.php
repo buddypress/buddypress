ce670c1c84b3a36dcd487a7d3357921e31ef29d8
Paul Gibbs <djpaul@gmail.com>
2011-01-21 18:36:37 +0000

    Only load xprofile admin CSS/JS on the xprofile admin pages. Fixes #2973 (branch)
    git-svn-id: https://buddypress.svn.wordpress.org/branches/1.2@3804 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-xprofile/bp-xprofile-cssjs.php
+++ bp-xprofile/bp-xprofile-cssjs.php
@@ -4,8 +4,9 @@ function xprofile_add_admin_css() {
 	// If this is WP 3.1+ and multisite is enabled, only load on the Network Admin
 	if ( is_multisite() && function_exists( 'is_network_admin' ) && ! is_network_admin()  )
 		return false;
-	
-	wp_enqueue_style( 'xprofile-admin-css', BP_PLUGIN_URL . '/bp-xprofile/admin/css/admin.css' );
+
+	if ( !empty( $_GET['page'] ) && strpos( $_GET['page'], 'bp-profile-setup' ) !== false )
+		wp_enqueue_style( 'xprofile-admin-css', BP_PLUGIN_URL . '/bp-xprofile/admin/css/admin.css' );
 }
 add_action( 'admin_menu', 'xprofile_add_admin_css' );
 add_action( 'network_admin_menu', 'xprofile_add_admin_css' );
@@ -15,7 +16,7 @@ function xprofile_add_admin_js() {
 	if ( is_multisite() && function_exists( 'is_network_admin' ) && ! is_network_admin()  )
 		return false;
 
-	if ( strpos( $_GET['page'], 'bp-profile-setup' ) !== false ) {
+	if ( !empty( $_GET['page'] ) && strpos( $_GET['page'], 'bp-profile-setup' ) !== false ) {
 		wp_enqueue_script( array( "jquery-ui-sortable" ) );
 		wp_enqueue_script( 'xprofile-admin-js', BP_PLUGIN_URL . '/bp-xprofile/admin/js/admin.js', array( 'jquery' ) );
 	}
