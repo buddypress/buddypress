43bf36d93f282864c4d3dc3b52392f0204fb5592
John James Jacoby <johnjamesjacoby@me.com>
2011-01-22 14:03:17 +0000

    Update group member list to match other member lists. Add ordering to group member list.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@3819 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/groups/single/members.php
+++ bp-themes/bp-default/groups/single/members.php
@@ -1,39 +1,75 @@
 <?php if ( bp_group_has_members( 'exclude_admins_mods=0' ) ) : ?>
 
-	<?php do_action( 'bp_before_group_members_content' ) ?>
+	<?php do_action( 'bp_before_group_members_content' ); ?>
 
-	<div class="pagination no-ajax">
+	<div class="item-list-tabs" id="subnav" role="navigation">
+		<ul>
 
-		<div id="member-count" class="pag-count">
-			<?php bp_group_member_pagination_count() ?>
+			<?php do_action( 'bp_members_directory_member_sub_types' ); ?>
+
+			<li id="members-order-select" class="last filter">
+
+				<?php _e( 'Order By:', 'buddypress' ); ?>
+
+				<select>
+					<option value="active"><?php _e( 'Last Active', 'buddypress' ); ?></option>
+					<option value="newest"><?php _e( 'Newest Registered', 'buddypress' ); ?></option>
+
+					<?php if ( bp_is_active( 'profile' ) ) : ?>
+
+						<option value="alphabetical"><?php _e( 'Alphabetical', 'buddypress' ); ?></option>
+
+					<?php endif; ?>
+
+					<?php do_action( 'bp_members_directory_order_options' ); ?>
+
+				</select>
+			</li>
+		</ul>
+	</div>
+
+	<div id="pag-top" class="pagination">
+
+		<div class="pag-count" id="member-count-top">
+
+			<?php bp_members_pagination_count(); ?>
+
 		</div>
 
-		<div id="member-pagination" class="pagination-links">
-			<?php bp_group_member_pagination() ?>
+		<div class="pagination-links" id="member-pag-top">
+
+			<?php bp_members_pagination_links(); ?>
+
 		</div>
 
 	</div>
 
-	<?php do_action( 'bp_before_group_members_list' ) ?>
+	<?php do_action( 'bp_before_group_members_list' ); ?>
 
 	<ul id="member-list" class="item-list" role="main">
+
 		<?php while ( bp_group_members() ) : bp_group_the_member(); ?>
 
 			<li>
-				<a href="<?php bp_group_member_domain() ?>">
-					<?php bp_group_member_avatar_thumb() ?>
-				</a>
-				<h5><?php bp_group_member_link() ?></h5>
-				<span class="activity"><?php bp_group_member_joined_since() ?></span>
+				<a href="<?php bp_group_member_domain(); ?>">
 
-				<?php do_action( 'bp_group_members_list_item' ) ?>
+					<?php bp_group_member_avatar_thumb(); ?>
+
+				</a>
+
+				<h5><?php bp_group_member_link(); ?></h5>
+				<span class="activity"><?php bp_group_member_joined_since(); ?></span>
+
+				<?php do_action( 'bp_group_members_list_item' ); ?>
 
 				<?php if ( bp_is_active( 'friends' ) ) : ?>
 
 					<div class="action">
-						<?php bp_add_friend_button( bp_get_group_member_id(), bp_get_group_member_is_friend() ) ?>
 
-						<?php do_action( 'bp_group_members_list_item_action' ) ?>
+						<?php bp_add_friend_button( bp_get_group_member_id(), bp_get_group_member_is_friend() ); ?>
+
+						<?php do_action( 'bp_group_members_list_item_action' ); ?>
+
 					</div>
 
 				<?php endif; ?>
@@ -43,7 +79,25 @@
 
 	</ul>
 
-	<?php do_action( 'bp_after_group_members_content' ) ?>
+	<?php do_action( 'bp_after_group_members_list' ); ?>
+
+	<div id="pag-bottom" class="pagination">
+
+		<div class="pag-count" id="member-count-bottom">
+
+			<?php bp_members_pagination_count(); ?>
+
+		</div>
+
+		<div class="pagination-links" id="member-pag-bottom">
+
+			<?php bp_members_pagination_links(); ?>
+
+		</div>
+
+	</div>
+
+	<?php do_action( 'bp_after_group_members_content' ); ?>
 
 <?php else: ?>
 
