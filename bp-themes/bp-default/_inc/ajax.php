83524b2dd936246e0985b20033c95d24f956012d
John James Jacoby <johnjamesjacoby@me.com>
2011-01-22 16:56:07 +0000

    Bump the activity comment avatar size to 30px on the ajax return.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@3847 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/_inc/ajax.php
+++ bp-themes/bp-default/_inc/ajax.php
@@ -204,7 +204,7 @@ function bp_dtheme_new_activity_comment() {
 		<?php while ( bp_activities() ) : bp_the_activity(); ?>
 			<li id="acomment-<?php bp_activity_id() ?>">
 				<div class="acomment-avatar">
-					<?php bp_activity_avatar() ?>
+					<?php bp_activity_avatar( array( 'height' => 30, 'width' => 30 ) ); ?>
 				</div>
 
 				<div class="acomment-meta">
