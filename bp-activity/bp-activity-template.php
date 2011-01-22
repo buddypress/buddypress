69073b0f5a617dd0903cb4a65c003a3b7fcb3f17
John James Jacoby <johnjamesjacoby@me.com>
2011-01-22 16:53:08 +0000

    Bump activity comment avatar size up from 20px to 30px.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@3846 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-activity/bp-activity-template.php
+++ bp-activity/bp-activity-template.php
@@ -798,7 +798,7 @@ function bp_activity_comments( $args = '' ) {
 					$comment->user_fullname = $comment->display_name;
 
 				$content .= '<li id="acomment-' . $comment->id . '">';
-				$content .= '<div class="acomment-avatar"><a href="' . bp_core_get_user_domain( $comment->user_id, $comment->user_nicename, $comment->user_login ) . '">' . bp_core_fetch_avatar( array( 'item_id' => $comment->user_id, 'width' => 20, 'height' => 20, 'email' => $comment->user_email ) ) . '</a></div>';
+				$content .= '<div class="acomment-avatar"><a href="' . bp_core_get_user_domain( $comment->user_id, $comment->user_nicename, $comment->user_login ) . '">' . bp_core_fetch_avatar( array( 'item_id' => $comment->user_id, 'width' => 30, 'height' => 30, 'email' => $comment->user_email ) ) . '</a></div>';
 				$content .= '<div class="acomment-meta"><a href="' . bp_core_get_user_domain( $comment->user_id, $comment->user_nicename, $comment->user_login ) . '">' . apply_filters( 'bp_acomment_name', $comment->user_fullname, $comment ) . '</a> &middot; ' . sprintf( __( '%s ago', 'buddypress' ), bp_core_time_since( $comment->date_recorded ) );
 
 				// Reply link - the span is so that threaded reply links can be
