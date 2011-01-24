a8022396aa03262f9e94df01af3bc3b717266a1b
John James Jacoby <johnjamesjacoby@me.com>
2011-01-24 10:27:09 +0000

    Edit groups_add_forum_fields_sql to include all topic data, which allows information like the topic_slug to be readily available. Also some code clean-up in bp-groups-filters.php.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@3885 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-groups/bp-groups-filters.php
+++ bp-groups/bp-groups-filters.php
@@ -1,59 +1,59 @@
 <?php
 
 /* Apply WordPress defined filters */
-add_filter( 'bp_get_group_description', 'wptexturize' );
+add_filter( 'bp_get_group_description',         'wptexturize' );
 add_filter( 'bp_get_group_description_excerpt', 'wptexturize' );
-add_filter( 'bp_get_group_name', 'wptexturize' );
+add_filter( 'bp_get_group_name',                'wptexturize' );
 
-add_filter( 'bp_get_group_description', 'convert_smilies' );
+add_filter( 'bp_get_group_description',         'convert_smilies' );
 add_filter( 'bp_get_group_description_excerpt', 'convert_smilies' );
 
-add_filter( 'bp_get_group_description', 'convert_chars' );
+add_filter( 'bp_get_group_description',         'convert_chars' );
 add_filter( 'bp_get_group_description_excerpt', 'convert_chars' );
-add_filter( 'bp_get_group_name', 'convert_chars' );
+add_filter( 'bp_get_group_name',                'convert_chars' );
 
-add_filter( 'bp_get_group_description', 'wpautop' );
+add_filter( 'bp_get_group_description',         'wpautop' );
 add_filter( 'bp_get_group_description_excerpt', 'wpautop' );
 
-add_filter( 'bp_get_group_description', 'make_clickable' );
+add_filter( 'bp_get_group_description',         'make_clickable' );
 add_filter( 'bp_get_group_description_excerpt', 'make_clickable' );
 
-add_filter( 'bp_get_group_name', 'wp_filter_kses', 1 );
-add_filter( 'bp_get_group_permalink', 'wp_filter_kses', 1 );
-add_filter( 'bp_get_group_description', 'bp_groups_filter_kses', 1 );
-add_filter( 'bp_get_group_description_excerpt', 'wp_filter_kses', 1 );
-add_filter( 'groups_group_name_before_save', 'wp_filter_kses', 1 );
+add_filter( 'bp_get_group_name',                    'wp_filter_kses', 1 );
+add_filter( 'bp_get_group_permalink',               'wp_filter_kses', 1 );
+add_filter( 'bp_get_group_description',             'bp_groups_filter_kses', 1 );
+add_filter( 'bp_get_group_description_excerpt',     'wp_filter_kses', 1 );
+add_filter( 'groups_group_name_before_save',        'wp_filter_kses', 1 );
 add_filter( 'groups_group_description_before_save', 'wp_filter_kses', 1 );
 
-add_filter( 'bp_get_group_description', 'stripslashes' );
+add_filter( 'bp_get_group_description',         'stripslashes' );
 add_filter( 'bp_get_group_description_excerpt', 'stripslashes' );
-add_filter( 'bp_get_group_name', 'stripslashes' );
-add_filter( 'bp_get_group_member_name', 'stripslashes' );
-add_filter( 'bp_get_group_member_link', 'stripslashes' );
+add_filter( 'bp_get_group_name',                'stripslashes' );
+add_filter( 'bp_get_group_member_name',         'stripslashes' );
+add_filter( 'bp_get_group_member_link',         'stripslashes' );
 
 add_filter( 'groups_new_group_forum_desc', 'bp_create_excerpt' );
 
-add_filter( 'groups_group_name_before_save', 'force_balance_tags' );
+add_filter( 'groups_group_name_before_save',        'force_balance_tags' );
 add_filter( 'groups_group_description_before_save', 'force_balance_tags' );
 
-add_filter( 'bp_get_total_group_count', 'bp_core_number_format' );
+add_filter( 'bp_get_total_group_count',      'bp_core_number_format' );
 add_filter( 'bp_get_group_total_for_member', 'bp_core_number_format' );
-add_filter( 'bp_get_group_total_members', 'bp_core_number_format' );
+add_filter( 'bp_get_group_total_members',    'bp_core_number_format' );
 
 function bp_groups_filter_kses( $content ) {
 	global $allowedtags;
 
-	$groups_allowedtags = $allowedtags;
-	$groups_allowedtags['a']['class'] = array();
-	$groups_allowedtags['img'] = array();
-	$groups_allowedtags['img']['src'] = array();
-	$groups_allowedtags['img']['alt'] = array();
-	$groups_allowedtags['img']['class'] = array();
-	$groups_allowedtags['img']['width'] = array();
+	$groups_allowedtags                  = $allowedtags;
+	$groups_allowedtags['a']['class']    = array();
+	$groups_allowedtags['img']           = array();
+	$groups_allowedtags['img']['src']    = array();
+	$groups_allowedtags['img']['alt']    = array();
+	$groups_allowedtags['img']['class']  = array();
+	$groups_allowedtags['img']['width']  = array();
 	$groups_allowedtags['img']['height'] = array();
-	$groups_allowedtags['img']['class'] = array();
-	$groups_allowedtags['img']['id'] = array();
-	$groups_allowedtags['code'] = array();
+	$groups_allowedtags['img']['class']  = array();
+	$groups_allowedtags['img']['id']     = array();
+	$groups_allowedtags['code']          = array();
 
 	$groups_allowedtags = apply_filters( 'bp_groups_filter_kses', $groups_allowedtags );
 	return wp_kses( $content, $groups_allowedtags );
@@ -65,16 +65,16 @@ function groups_add_forum_privacy_sql() {
 	global $bp;
 
 	/* Only filter the forum SQL on group pages or on the forums directory */
-	if ( ( !empty( $bp->groups->current_group ) && 'public' == $bp->groups->current_group->status ) ) {
-		add_filter( 'get_topics_fields', 'groups_add_forum_fields_sql' );
+	if ( bp_is_forums_component() || ( !empty( $bp->groups->current_group ) && 'public' == $bp->groups->current_group->status ) ) {
+		add_filter( 'get_topics_fields',     'groups_add_forum_fields_sql' );
 		add_filter( 'get_topics_index_hint', 'groups_add_forum_tables_sql' );
-		add_filter( 'get_topics_where', 'groups_add_forum_where_sql' );
+		add_filter( 'get_topics_where',      'groups_add_forum_where_sql' );
 	}
 }
 add_filter( 'bbpress_init', 'groups_add_forum_privacy_sql' );
 
 function groups_add_forum_fields_sql( $sql ) {
-	return $sql . ', g.id as object_id, g.name as object_name, g.slug as object_slug';
+	return 't.*, g.id as object_id, g.name as object_name, g.slug as object_slug';
 }
 
 function groups_add_forum_tables_sql( $sql ) {
