buddypress/buddypress

commit 4d258ee7cf8541b92b261bfe7449ef87a7c55d03

Rename new 'bp-users' component to 'bp-members' for consistency through-out project. Core, Messages, and Friends Components now use the BP_Component class. Split Friends and Messages components into smaller files. Change references to 'profile' to 'xprofile' through-out project for consistency. Introduce 'bp_actions' and 'bp_screens' standard hooks to replace the usage of 'wp' through-out project. Move component loader sequence into bp-core-bootstrap.php, Move old root_component action into 1.3 deprecated file.
git-svn-id: https://buddypress.svn.wordpress.org/trunk@3917 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-core/bp-core-component.php
@@ -33,6 +33,11 @@ class BP_Component {
 	var $slug;
 
 	/**
+	 * @var string The path to the plugins files
+	 */
+	var $path;
+
+	/**
 	 * @var WP_Query The loop for this component
 	 */
 	var $query;
@@ -62,14 +67,18 @@ class BP_Component {
 	 * @uses bp_Component::_includes() Include the required files
 	 * @uses bp_Component::_setup_actions() Setup the hooks and actions
 	 */
-	function start( $id, $name ) {
+	function start( $id, $name, $path ) {
 		// Internal identifier of component
 		$this->id   = $id;
 
 		// Internal component name
 		$this->name = $name;
 
-		$this->_setup_actions ();
+		// Path for includes
+		$this->path = $path;
+
+		// Move on to the next step
+		$this->_setup_actions();
 	}
 
 	/**
@@ -89,8 +98,10 @@ class BP_Component {
 		/** Slugs *************************************************************/
 
 		$defaults = array(
-			'slug'      => '',
-			'root_slug' => ''
+			'slug'                  => '',
+			'root_slug'             => '',
+			'notification_callback' => '',
+			'global_tables'         => ''
 		);
 		$r = wp_parse_args( $args, $defaults );
 
@@ -100,16 +111,21 @@ class BP_Component {
 		// Slug used for root directory
 		$this->root_slug = apply_filters( 'bp_' . $this->id . '_root_slug', $r['root_slug'] );
 
+		// Notifications callback
+		$this->notification_callback = 'bp_' . $this->id . '_notification_callback';
+
+		// Setup global table names
+		if ( !empty( $r['global_tables'] ) )
+			foreach ( $r['global_tables'] as $global_name => $table_name )
+				$this->$global_name = $table_name;
+
 		/** BuddyPress ********************************************************/
 
-		// Avoid syntactical errors
-		$component_id = $this->id;
-
 		// Register this component in the active components array
-		$bp->active_components[$bp->$component_id->slug] = $this->id;
+		$bp->active_components[$this->slug] = $this->id;
 
-		// Notifications callback
-		$bp->$component_id->notification_callback = apply_filters( 'bp_' . $this->id . '_format_notifications', 'bp_' . $this->id . '_format_notifications' );
+		// Call action
+		do_action( 'bp_' . $this->id . '_setup_globals' );
 	}
 
 	/**
@@ -120,7 +136,28 @@ class BP_Component {
 	 *
 	 * @uses do_action() Calls 'bp_{@link bp_Component::name}_includes'
 	 */
-	function _includes() {
+	function _includes( $includes = '' ) {
+		if ( empty( $includes ) )
+			return;
+
+		// Loop through files to be included
+		foreach ( $includes as $file ) {
+
+			// Check path + file
+			if ( file_exists( $this->path . '/' . $file ) )
+				require_once( $this->path . '/' . $file );
+
+			// Check path + /bp-component/ + file
+			elseif ( file_exists( $this->path . '/bp-' . $this->id . '/' . $file ) )
+				require_once( $this->$path . '/bp-' . $this->id . '/' . $file );
+
+			// Check buddypress/bp-component/bp-component-$file.php
+			elseif ( file_exists( $this->path . '/bp-' . $this->id . '/bp-' . $this->id . '-' . $file  . '.php' ) )
+				require_once( $this->path . '/bp-' . $this->id . '/bp-' . $this->id . '-' . $file . '.php' );
+
+		}
+
+		// Call action
 		do_action( 'bp_' . $this->id . '_includes' );
 	}
 
@@ -144,6 +181,9 @@ class BP_Component {
 		add_action( 'bp_setup_nav',                array ( $this, '_setup_nav'               ), 10 );
 
 		// Register post types
+		add_action( 'bp_setup_title',              array ( $this, '_setup_title'             ), 10 );
+
+		// Register post types
 		add_action( 'bp_register_post_types',      array ( $this, 'register_post_types'      ), 10 );
 
 		// Register taxonomies
@@ -157,6 +197,27 @@ class BP_Component {
 
 		// Additional actions can be attached here
 		do_action( 'bp_' . $this->id . '_setup_actions' );
+	}
+
+	/**
+	 * Setup the navigation
+	 *
+	 * @param arr $main_nav
+	 * @param arr $sub_nav
+	 */
+	function _setup_nav( $main_nav, $sub_nav ) {
+		bp_core_new_nav_item( $main_nav );
+
+		foreach( $sub_nav as $nav )
+			bp_core_new_subnav_item( $nav );
+
+		// Call action
+		do_action( 'bp_' . $this->id . '_setup_nav' );
+	}
+
+
+	function _setup_title( ) {
+		
 	}
 
 	/**
@@ -203,4 +264,6 @@ class BP_Component {
 		do_action( 'bp_' . $this->id . '_generate_rewrite_rules' );
 	}
 }
-endif; // bp_Component
+endif; // BP_Component
+
+?>

bp-core/bp-core-template.php
@@ -279,7 +279,7 @@ function bp_styles() {
  * @return bool Filterable result
  */
 function bp_search_form_enabled() {
-	if ( bp_is_active( 'profile' )
+	if ( bp_is_active( 'xprofile' )
 		 || bp_is_active( 'groups' )
 		 || ( bp_is_active( 'blogs' ) && is_multisite() )
 		 || ( bp_is_active( 'forums' ) && !bp_forum_directory_is_disabled() )
@@ -310,7 +310,7 @@ function bp_search_form_type_select() {
 
 	$options = array();
 
-	if ( bp_is_active( 'profile' ) )
+	if ( bp_is_active( 'xprofile' ) )
 		$options['members'] = __( 'Members', 'buddypress' );
 
 	if ( bp_is_active( 'groups' ) )
@@ -823,7 +823,7 @@ function bp_is_active( $component ) {
 }
 
 function bp_is_profile_component() {
-	if ( bp_is_current_component( 'profile' ) )
+	if ( bp_is_current_component( 'xprofile' ) )
 		return true;
 
 	return false;
@@ -933,7 +933,7 @@ function bp_is_user_friends_activity() {
 function bp_is_user_profile() {
 	global $bp;
 
-	if ( bp_is_current_component( 'profile' ) )
+	if ( bp_is_current_component( 'xprofile' ) )
 		return true;
 
 	return false;
@@ -942,7 +942,7 @@ function bp_is_user_profile() {
 function bp_is_user_profile_edit() {
 	global $bp;
 
-	if ( bp_is_current_component( 'profile' ) && bp_is_current_action( 'edit' ) )
+	if ( bp_is_current_component( 'xprofile' ) && bp_is_current_action( 'edit' ) )
 		return true;
 
 	return false;
@@ -951,7 +951,7 @@ function bp_is_user_profile_edit() {
 function bp_is_user_change_avatar() {
 	global $bp;
 
-	if ( bp_is_current_component( 'profile' ) && bp_is_current_action( 'change-avatar' ) )
+	if ( bp_is_current_component( 'xprofile' ) && bp_is_current_action( 'change-avatar' ) )
 		return true;
 
 	return false;
@@ -1239,7 +1239,7 @@ function bp_the_body_class() {
 
 		if ( !bp_is_blog_page() ) :
 			if ( bp_is_user_profile() )
-				$bp_classes[] = 'profile';
+				$bp_classes[] = 'xprofile';
 
 			if ( bp_is_activity_component() )
 				$bp_classes[] = 'activity';

commit 0de69b449c47b70fc8cefa53204d6820447aa276

Fix default search string regression from r3917
git-svn-id: https://buddypress.svn.wordpress.org/trunk@3918 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-core/bp-core-component.php
@@ -101,15 +101,19 @@ class BP_Component {
 			'slug'                  => '',
 			'root_slug'             => '',
 			'notification_callback' => '',
-			'global_tables'         => ''
+			'search_string'         => '',
+			'global_tables'         => '',
 		);
 		$r = wp_parse_args( $args, $defaults );
 
 		// Slug used for permalinks
-		$this->slug      = apply_filters( 'bp_' . $this->id . '_slug',      $r['slug']      );
+		$this->slug          = apply_filters( 'bp_' . $this->id . '_slug',          $r['slug']          );
 
 		// Slug used for root directory
-		$this->root_slug = apply_filters( 'bp_' . $this->id . '_root_slug', $r['root_slug'] );
+		$this->root_slug     = apply_filters( 'bp_' . $this->id . '_root_slug',     $r['root_slug']     );
+
+		// Search string
+		$this->search_string = apply_filters( 'bp_' . $this->id . '_search_string', $r['search_string'] );
 
 		// Notifications callback
 		$this->notification_callback = 'bp_' . $this->id . '_notification_callback';

bp-core/bp-core-template.php
@@ -353,15 +353,15 @@ function bp_search_default_text( $component = '' ) {
 		$default_text = __( 'Search anything...', 'buddypress' );
 
 		// Most of the time, $component will be the actual component ID
-		if ( !empty( $bp->{$component}->default_search_string ) ) {
-			$default_text = $bp->{$component}->default_search_string;
+		if ( !empty( $bp->{$component}->search_string ) ) {
+			$default_text = $bp->{$component}->search_string;
 		} else {
 			// When the request comes through AJAX, we need to get the component
 			// name out of $bp->pages
 			if ( !empty( $bp->pages->{$component}->slug ) ) {
 				$key = $bp->pages->{$component}->slug;
-				if ( !empty( $bp->{$key}->default_search_string ) )
-					$default_text = $bp->{$key}->default_search_string;
+				if ( !empty( $bp->{$key}->search_string ) )
+					$default_text = $bp->{$key}->search_string;
 			}
 		}
 