ed2b2bb70a154cc7f56bad2d377d226f33835aad
John James Jacoby <johnjamesjacoby@me.com>
2011-01-25 22:28:27 +0000

    Move New Topic creation form to bottom of forums directory to match UX in group forum. (This may not make it for final release, but worth testing.)
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@3924 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/forums/index.php
+++ bp-themes/bp-default/forums/index.php
@@ -16,140 +16,139 @@
 	<div id="content">
 		<div class="padder">
 
-		<?php do_action( 'bp_before_directory_forums' ); ?>
+			<?php do_action( 'bp_before_directory_forums' ); ?>
 
-		<form action="" method="post" id="forums-search-form" class="dir-form">
+			<form action="" method="post" id="forums-search-form" class="dir-form">
 
-			<h3><?php _e( 'Group Forums Directory', 'buddypress' ); ?><?php if ( is_user_logged_in() ) : ?> &nbsp;<a class="button" href="#new-topic" id="new-topic-button"><?php _e( 'New Topic', 'buddypress' ); ?></a><?php endif; ?></h3>
+				<h3><?php _e( 'Group Forums Directory', 'buddypress' ); ?><?php if ( is_user_logged_in() ) : ?> &nbsp;<a class="button" href="#new-topic" id="new-topic-button"><?php _e( 'New Topic', 'buddypress' ); ?></a><?php endif; ?></h3>
 
-			<div id="forums-dir-search" class="dir-search" role="search">
+				<div id="forums-dir-search" class="dir-search" role="search">
 
-				<?php bp_directory_forums_search_form(); ?>
+					<?php bp_directory_forums_search_form(); ?>
 
-			</div>
-		</form>
+				</div>
+			</form>
 
-		<?php do_action( 'bp_before_new_topic_form' ); ?>
+			<?php do_action( 'bp_before_topics' ); ?>
 
-		<div id="new-topic-post" style="display: none;">
+			<form action="" method="post" id="forums-directory-form" class="dir-form">
 
-			<?php if ( is_user_logged_in() ) : ?>
+				<div class="item-list-tabs" role="navigation">
+					<ul>
+						<li class="selected" id="forums-all"><a href="<?php bp_root_domain(); ?>"><?php printf( __( 'All Topics (%s)', 'buddypress' ), bp_get_forum_topic_count() ); ?></a></li>
 
-				<?php if ( bp_has_groups( 'user_id=' . bp_loggedin_user_id() . '&type=alphabetical&max=100&per_page=100' ) ) : ?>
+						<?php if ( is_user_logged_in() && bp_get_forum_topic_count_for_user( bp_loggedin_user_id() ) ) : ?>
 
-					<form action="" method="post" id="forum-topic-form" class="standard-form">
+							<li id="forums-personal"><a href="<?php echo trailingslashit( bp_loggedin_user_domain() . bp_get_groups_slug() ); ?>"><?php printf( __( 'My Topics (%s)', 'buddypress' ), bp_get_forum_topic_count_for_user( bp_loggedin_user_id() ) ); ?></a></li>
 
-						<?php do_action( 'groups_forum_new_topic_before' ) ?>
+						<?php endif; ?>
 
-						<a name="post-new"></a>
-						<h5><?php _e( 'Post a New Topic:', 'buddypress' ); ?></h5>
+						<?php do_action( 'bp_forums_directory_group_types' ); ?>
 
-						<label><?php _e( 'Title:', 'buddypress' ); ?></label>
-						<input type="text" name="topic_title" id="topic_title" value="" />
+					</ul>
+				</div>
 
-						<label><?php _e( 'Content:', 'buddypress' ); ?></label>
-						<textarea name="topic_text" id="topic_text"></textarea>
+				<div class="item-list-tabs" id="subnav" role="navigation">
+					<ul>
 
-						<label><?php _e( 'Tags (comma separated):', 'buddypress' ); ?></label>
-						<input type="text" name="topic_tags" id="topic_tags" value="" />
+						<?php do_action( 'bp_forums_directory_group_sub_types' ); ?>
 
-						<label><?php _e( 'Post In Group Forum:', 'buddypress' ); ?></label>
-						<select id="topic_group_id" name="topic_group_id">
+						<li id="forums-order-select" class="last filter">
 
-							<option value="">----</option>
+							<?php _e( 'Order By:', 'buddypress' ); ?>
 
-							<?php while ( bp_groups() ) : bp_the_group(); ?>
+							<select>
+								<option value="active"><?php _e( 'Last Active', 'buddypress' ); ?></option>
+								<option value="popular"><?php _e( 'Most Posts', 'buddypress' ); ?></option>
+								<option value="unreplied"><?php _e( 'Unreplied', 'buddypress' ); ?></option>
 
-								<?php if ( bp_group_is_forum_enabled() && 'public' == bp_get_group_status() ) : ?>
+								<?php do_action( 'bp_forums_directory_order_options' ); ?>
 
-									<option value="<?php bp_group_id(); ?>"><?php bp_group_name(); ?></option>
+							</select>
+						</li>
+					</ul>
+				</div>
 
-								<?php endif; ?>
+				<?php do_action( 'bp_before_directory_forums_list' ); ?>
 
-							<?php endwhile; ?>
+				<div id="forums-dir-list" class="forums dir-list" role="main">
 
-						</select><!-- #topic_group_id -->
+					<?php locate_template( array( 'forums/forums-loop.php' ), true ); ?>
 
-						<?php do_action( 'groups_forum_new_topic_after' ); ?>
+				</div>
 
-						<div class="submit">
-							<input type="submit" name="submit_topic" id="submit" value="<?php _e( 'Post Topic', 'buddypress' ); ?>" />
-							<input type="button" name="submit_topic_cancel" id="submit_topic_cancel" value="<?php _e( 'Cancel', 'buddypress' ); ?>" />
+				<?php do_action( 'bp_directory_forums_content' ); ?>
+
+				<?php wp_nonce_field( 'directory_forums', '_wpnonce-forums-filter' ); ?>
+
+			</form>
+
+			<?php do_action( 'bp_after_directory_forums' ); ?>
+
+			<?php do_action( 'bp_before_new_topic_form' ); ?>
+
+			<div id="new-topic-post" style="display: none;">
+
+				<?php if ( is_user_logged_in() ) : ?>
+
+					<?php if ( bp_has_groups( 'user_id=' . bp_loggedin_user_id() . '&type=alphabetical&max=100&per_page=100' ) ) : ?>
+
+						<form action="" method="post" id="forum-topic-form" class="standard-form">
+
+							<?php do_action( 'groups_forum_new_topic_before' ) ?>
+
+							<a name="post-new"></a>
+							<h5><?php _e( 'Post a New Topic:', 'buddypress' ); ?></h5>
+
+							<label><?php _e( 'Title:', 'buddypress' ); ?></label>
+							<input type="text" name="topic_title" id="topic_title" value="" />
+
+							<label><?php _e( 'Content:', 'buddypress' ); ?></label>
+							<textarea name="topic_text" id="topic_text"></textarea>
+
+							<label><?php _e( 'Tags (comma separated):', 'buddypress' ); ?></label>
+							<input type="text" name="topic_tags" id="topic_tags" value="" />
+
+							<label><?php _e( 'Post In Group Forum:', 'buddypress' ); ?></label>
+							<select id="topic_group_id" name="topic_group_id">
+
+								<option value="">----</option>
+
+								<?php while ( bp_groups() ) : bp_the_group(); ?>
+
+									<?php if ( bp_group_is_forum_enabled() && 'public' == bp_get_group_status() ) : ?>
+
+										<option value="<?php bp_group_id(); ?>"><?php bp_group_name(); ?></option>
+
+									<?php endif; ?>
+
+								<?php endwhile; ?>
+
+							</select><!-- #topic_group_id -->
+
+							<?php do_action( 'groups_forum_new_topic_after' ); ?>
+
+							<div class="submit">
+								<input type="submit" name="submit_topic" id="submit" value="<?php _e( 'Post Topic', 'buddypress' ); ?>" />
+								<input type="button" name="submit_topic_cancel" id="submit_topic_cancel" value="<?php _e( 'Cancel', 'buddypress' ); ?>" />
+							</div>
+
+							<?php wp_nonce_field( 'bp_forums_new_topic' ); ?>
+
+						</form><!-- #forum-topic-form -->
+
+					<?php else : ?>
+
+						<div id="message" class="info">
+
+							<p><?php printf( __( "You are not a member of any groups so you don't have any group forums you can post in. To start posting, first find a group that matches the topic subject you'd like to start. If this group does not exist, why not <a href='%s'>create a new group</a>? Once you have joined or created the group you can post your topic in that group's forum.", 'buddypress' ), site_url( BP_GROUPS_SLUG . '/create/' ) ) ?></p>
+
 						</div>
-
-						<?php wp_nonce_field( 'bp_forums_new_topic' ); ?>
-
-					</form><!-- #forum-topic-form -->
-
-				<?php else : ?>
-
-					<div id="message" class="info">
-
-						<p><?php printf( __( "You are not a member of any groups so you don't have any group forums you can post in. To start posting, first find a group that matches the topic subject you'd like to start. If this group does not exist, why not <a href='%s'>create a new group</a>? Once you have joined or created the group you can post your topic in that group's forum.", 'buddypress' ), site_url( BP_GROUPS_SLUG . '/create/' ) ) ?></p>
-
-					</div>
-
-				<?php endif; ?>
-
-			<?php endif; ?>
-		</div><!-- #new-topic-post -->
-
-		<?php do_action( 'bp_before_topics' ); ?>
-
-		<form action="" method="post" id="forums-directory-form" class="dir-form">
-
-			<div class="item-list-tabs" role="navigation">
-				<ul>
-					<li class="selected" id="forums-all"><a href="<?php bp_root_domain(); ?>"><?php printf( __( 'All Topics (%s)', 'buddypress' ), bp_get_forum_topic_count() ); ?></a></li>
-
-					<?php if ( is_user_logged_in() && bp_get_forum_topic_count_for_user( bp_loggedin_user_id() ) ) : ?>
-
-						<li id="forums-personal"><a href="<?php echo trailingslashit( bp_loggedin_user_domain() . bp_get_groups_slug() ); ?>"><?php printf( __( 'My Topics (%s)', 'buddypress' ), bp_get_forum_topic_count_for_user( bp_loggedin_user_id() ) ); ?></a></li>
 
 					<?php endif; ?>
 
-					<?php do_action( 'bp_forums_directory_group_types' ); ?>
-
-				</ul>
-			</div>
-
-			<div class="item-list-tabs" id="subnav" role="navigation">
-				<ul>
-
-					<?php do_action( 'bp_forums_directory_group_sub_types' ); ?>
-
-					<li id="forums-order-select" class="last filter">
-
-						<?php _e( 'Order By:', 'buddypress' ); ?>
-
-						<select>
-							<option value="active"><?php _e( 'Last Active', 'buddypress' ); ?></option>
-							<option value="popular"><?php _e( 'Most Posts', 'buddypress' ); ?></option>
-							<option value="unreplied"><?php _e( 'Unreplied', 'buddypress' ); ?></option>
-
-							<?php do_action( 'bp_forums_directory_order_options' ); ?>
-
-						</select>
-					</li>
-				</ul>
-			</div>
-
-			<?php do_action( 'bp_before_directory_forums_list' ); ?>
-
-			<div id="forums-dir-list" class="forums dir-list" role="main">
-
-				<?php locate_template( array( 'forums/forums-loop.php' ), true ); ?>
-
-			</div>
-
-			<?php do_action( 'bp_directory_forums_content' ); ?>
-
-			<?php wp_nonce_field( 'directory_forums', '_wpnonce-forums-filter' ); ?>
-
-		</form>
-
-		<?php do_action( 'bp_after_directory_forums' ); ?>
-
+				<?php endif; ?>
+			</div><!-- #new-topic-post -->
 		</div><!-- .padder -->
 	</div><!-- #content -->
 
