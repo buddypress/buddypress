5c009c85a7bd431b4896e5758428a2c135e6abf2
John James Jacoby <johnjamesjacoby@me.com>
2011-01-27 19:29:27 +0000

    Fix by-ref typo in bp_register_group_extension
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@3954 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-groups/bp-groups-classes.php
+++ bp-groups/bp-groups-classes.php
@@ -1197,7 +1197,7 @@ function bp_register_group_extension( $group_extension_class ) {
 		return false;
 
 	/* Register the group extension on the bp_init action so we have access to all plugins */
-	add_action( 'bp_init', create_function( '', '$extension = new ' . $group_extension_class . '; add_action( "wp", array( $&extension, "_register" ), 2 );' ), 11 );
+	add_action( 'bp_init', create_function( '', '$extension = new ' . $group_extension_class . '; add_action( "wp", array( &$extension, "_register" ), 2 );' ), 11 );
 }
 
 ?>
