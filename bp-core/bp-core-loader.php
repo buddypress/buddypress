b53f2b909af486efa519c3c805d68f0fad762eaa
John James Jacoby <johnjamesjacoby@me.com>
2011-02-02 05:09:49 +0000

    Do the leg-work and set $bp->deactivated_components based on active, optional, and required components arrays
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@3995 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-core/bp-core-loader.php
+++ bp-core/bp-core-loader.php
@@ -51,13 +51,16 @@ class BP_Core extends BP_Component {
 		/** Components ********************************************************/
 
 		// Get a list of activated components
-		$bp->active_components = apply_filters( 'bp_active_components', get_site_option( 'bp-active-components' ) );
+		$bp->active_components      = apply_filters( 'bp_active_components',      get_site_option( 'bp-active-components' ) );
 
 		// Set the included and optional components
-		$bp->optional_components = apply_filters( 'bp_optional_components', array( 'activity', 'blogs', 'forums', 'friends', 'groups', 'messages', 'settings', 'xprofile', ) );
+		$bp->optional_components    = apply_filters( 'bp_optional_components',    array( 'activity', 'blogs', 'forums', 'friends', 'groups', 'messages', 'settings', 'xprofile', ) );
 
 		// Set the required components
-		$bp->required_components = apply_filters( 'bp_required_components', array( 'members', ) );
+		$bp->required_components    = apply_filters( 'bp_required_components',    array( 'members', ) );
+
+		// Set the inactive components for backpat
+		$bp->deactivated_components = apply_filters( 'bp_deactivated_components', array_diff( array_values( array_merge( $bp->optional_components, $bp->required_components ) ), array_keys( $bp->active_components ) ) );
 
 		// Loop through optional components
 		foreach( $bp->optional_components as $component )
