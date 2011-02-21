88817d8b18495f87e9391299640dbbb0ef79d645
John James Jacoby <johnjamesjacoby@me.com>
2011-02-21 14:22:31 +0000

    Prevent errors when an xprofile field group does not have any fields.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@4060 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-xprofile/bp-xprofile-template.php
+++ bp-xprofile/bp-xprofile-template.php
@@ -624,7 +624,7 @@ function bp_profile_group_tabs() {
 		else
 			$selected = '';
 
-		if ( $groups[$i]->fields ) {
+		if ( !empty( $groups[$i]->fields ) ) {
 			$link = $bp->displayed_user->domain . $bp->profile->slug . '/edit/group/' . $groups[$i]->id;
 			$tabs[] = sprintf( '<li %1$s><a href="%2$s">%3$s</a><li>', $selected, $link, esc_html( $groups[$i]->name ) );
 		}
