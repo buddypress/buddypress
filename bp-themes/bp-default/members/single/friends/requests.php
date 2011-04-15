26b9fa20edbd72ab7a8ba7957cf71a9ee19d8412
John James Jacoby <johnjamesjacoby@me.com>
2011-04-15 06:33:02 +0000

    Fix issue where friend requests page was only showing one request at a time. Also add pagination to friend requests page.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@4205 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/members/single/friends/requests.php
+++ bp-themes/bp-default/members/single/friends/requests.php
@@ -1,6 +1,22 @@
 <?php do_action( 'bp_before_member_friend_requests_content' ) ?>
 
-<?php if ( bp_has_members( 'include=' . bp_get_friendship_requests() . '&per_page=0' ) ) : ?>
+<?php if ( bp_has_members( 'include=' . bp_get_friendship_requests() ) ) : ?>
+
+	<div id="pag-top" class="pagination no-ajax">
+
+		<div class="pag-count" id="member-dir-count-top">
+
+			<?php bp_members_pagination_count(); ?>
+
+		</div>
+
+		<div class="pagination-links" id="member-dir-pag-top">
+
+			<?php bp_members_pagination_links(); ?>
+
+		</div>
+
+	</div>
 
 	<ul id="friend-list" class="item-list" role="main">
 		<?php while ( bp_members() ) : bp_the_member(); ?>
@@ -29,6 +45,22 @@
 	</ul>
 
 	<?php do_action( 'bp_friend_requests_content' ) ?>
+
+	<div id="pag-bottom" class="pagination no-ajax">
+
+		<div class="pag-count" id="member-dir-count-bottom">
+
+			<?php bp_members_pagination_count(); ?>
+
+		</div>
+
+		<div class="pagination-links" id="member-dir-pag-bottom">
+
+			<?php bp_members_pagination_links(); ?>
+
+		</div>
+
+	</div>
 
 <?php else: ?>
 
