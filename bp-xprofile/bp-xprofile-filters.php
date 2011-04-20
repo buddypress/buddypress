d770def1ddb1b9f00a47dae64fe049e4f4e95f35
Paul Gibbs <djpaul@gmail.com>
2011-04-20 21:39:42 +0000

    Trim profile data on save to avoid trailing spaces. Fixes #3162
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@4221 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-xprofile/bp-xprofile-filters.php
+++ bp-xprofile/bp-xprofile-filters.php
@@ -38,7 +38,8 @@ add_filter( 'bp_get_the_profile_field_value',         'xprofile_filter_format_fi
 add_filter( 'bp_get_the_site_member_profile_data',    'xprofile_filter_format_field_value', 1, 2 );
 add_filter( 'bp_get_the_profile_field_value',         'xprofile_filter_link_profile_data', 50, 2 );
 
-add_filter( 'xprofile_data_value_before_save',        'xprofile_sanitize_data_value_before_save', 1, 2 );		
+add_filter( 'xprofile_data_value_before_save',        'xprofile_sanitize_data_value_before_save', 1, 2 );
+add_filter( 'xprofile_filtered_data_value_before_save', 'trim', 2 );
 
 /**
  * xprofile_filter_kses ( $content )
