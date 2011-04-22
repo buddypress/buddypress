0b62f0b9e40cab08e8e3279e4c1b29ee2e1bb061
Paul Gibbs <djpaul@gmail.com>
2011-04-22 11:24:11 +0000

    Update styling of attachment template to better match rest of theme, and removes a PHP notice. Fixes #3164
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@4226 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/attachment.php
+++ bp-themes/bp-default/attachment.php
@@ -1,47 +1,75 @@
 <?php get_header(); ?>
 
 	<div id="content">
+		<div class="padder">
 
-		<?php do_action( 'bp_before_attachment' ) ?>
+			<?php do_action( 'bp_before_attachment' ); ?>
 
-		<div class="page" id="attachments-page">
-
-			<h2 class="pagetitle"><?php _e( 'Blog', 'buddypress' ) ?></h2>
+			<div class="page" id="attachments-page" role="main">
 
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 					<?php do_action( 'bp_before_blog_post' ) ?>
 
-					<?php $attachment_link = wp_get_attachment_link($post->ID, array(450, 800)); // This also populates the iconsize for the next line ?>
-					<?php $_post = &get_post($post->ID); $classname = ($_post->iconsize[0] <= 128 ? 'small' : '') . 'attachment'; // This lets us style narrow icons specially ?>
-
 					<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-						<h2 class="posttitle"><a href="<?php echo get_permalink($post->post_parent); ?>" rev="attachment"><?php echo get_the_title($post->post_parent); ?></a> &rarr; <a href="<?php echo get_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>"><?php the_title(); ?></a></h2>
+						<div class="author-box">
+							<?php echo get_avatar( get_the_author_meta( 'user_email' ), '50' ); ?>
+							<p><?php printf( _x( 'by %s', 'Post written by...', 'buddypress' ), bp_core_get_userlink( $post->post_author ) ) ?></p>
+						</div>
 
-						<div class="entry">
-							<p class="<?php echo $classname; ?>"><?php echo $attachment_link; ?><br /><?php echo basename($post->guid); ?></p>
+						<div class="post-content">
+							<h2 class="posttitle"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'buddypress' ) ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
-							<?php the_content( __('<p class="serif">Read the rest of this entry &rarr;</p>', 'buddypress' ) ); ?>
+							<p class="date">
+								<?php printf( __( '%1$s <span>by %2$s</span>', 'buddypress' ), get_the_date(), bp_core_get_userlink( $post->post_author ) ); ?>
+								<span class="post-utility alignright"><?php edit_post_link( __( 'Edit this entry', 'buddypress' ) ); ?></span>
+							</p>
 
-							<?php wp_link_pages( array( 'before' => '<div class="page-link"><p>' . __( '<strong>Pages:</strong> ', 'buddypress' ), 'after' => '</p></div>', 'next_or_number' => 'number')); ?>
+							<div class="entry">
+								<a href="#"><?php echo wp_get_attachment_image( $post->ID, 'large', false, array( 'class' => 'size-large aligncenter' ) ); ?></a>
+
+								<div class="entry-caption"><?php if ( !empty( $post->post_excerpt ) ) the_excerpt(); ?></div>
+								<?php the_content(); ?>
+							</div>
+
+							<p class="postmetadata">
+								<?php
+									if ( wp_attachment_is_image() ) :
+										$metadata = wp_get_attachment_metadata();
+										printf( __( 'Full size is %s pixels', 'buddypress' ),
+											sprintf( '<a href="%1$s" title="%2$s">%3$s &times; %4$s</a>',
+												wp_get_attachment_url(),
+												esc_attr( __( 'Link to full size image', 'buddypress' ) ),
+												$metadata['width'],
+												$metadata['height']
+											)
+										);
+									endif;
+								?>
+								<span class="comments"><?php comments_popup_link( __( 'No Comments &#187;', 'buddypress' ), __( '1 Comment &#187;', 'buddypress' ), __( '% Comments &#187;', 'buddypress' ) ); ?></span>
+							</p>
 						</div>
 
 					</div>
 
 					<?php do_action( 'bp_after_blog_post' ) ?>
 
-				<?php comments_template(); ?>
+					<?php comments_template(); ?>
 
 				<?php endwhile; else: ?>
 
 					<p><?php _e( 'Sorry, no attachments matched your criteria.', 'buddypress' ) ?></p>
 
 				<?php endif; ?>
-		</div>
+
+			</div>
 
 		<?php do_action( 'bp_after_attachment' ) ?>
 
-	</div>
+		</div><!-- .padder -->
+	</div><!-- #content -->
+
+	<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
