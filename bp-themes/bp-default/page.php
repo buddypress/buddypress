72670cfec60ab9fa323d75b2cf8a3161bdde0ea8
Paul Gibbs <djpaul@gmail.com>
2011-05-25 19:23:10 +0000

    Don't say "edit this post" on a page
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@4426 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/page.php
+++ bp-themes/bp-default/page.php
@@ -18,7 +18,7 @@
 						<?php the_content( __( '<p class="serif">Read the rest of this page &rarr;</p>', 'buddypress' ) ); ?>
 
 						<?php wp_link_pages( array( 'before' => '<div class="page-link"><p>' . __( 'Pages: ', 'buddypress' ), 'after' => '</p></div>', 'next_or_number' => 'number' ) ); ?>
-						<?php edit_post_link( __( 'Edit this entry.', 'buddypress' ), '<p class="edit-link">', '</p>'); ?>
+						<?php edit_post_link( __( 'Edit this page.', 'buddypress' ), '<p class="edit-link">', '</p>'); ?>
 
 					</div>
 
