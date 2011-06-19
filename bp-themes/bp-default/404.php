25631350662f84f9701b4961b56cf405b872ffc9
Paul Gibbs <djpaul@gmail.com>
2011-06-19 15:41:38 +0000

    Add search box to 404 template, tweak text, and remove sidebar. Fixes #3236, props bowromir for initial patch
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@4534 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/404.php
+++ bp-themes/bp-default/404.php
@@ -1,29 +1,19 @@
-<?php get_header(); ?>
+<?php get_header() ?>
 
 	<div id="content">
-		<div class="padder">
+		<div class="padder one-column">
+			<?php do_action( 'bp_before_404' ); ?>
+			<div id="post-0" class="post page-404 error404 not-found" role="main">
+				<h2 class="posttitle"><?php _e( "Page not found", 'buddypress' ); ?></h2>
 
-		<?php do_action( 'bp_before_404' ) ?>
+				<p><?php _e( "We're sorry, but we can't find the page that you're looking for. Perhaps searching will help.", 'buddypress' ); ?></p>
+				<?php get_search_form(); ?>
 
-		<div class="page-404">
-
-			<h2 class="pagetitle"><?php _e( 'Page Not Found', 'buddypress' ) ?></h2>
-
-			<div id="message" class="info">
-
-				<p><?php _e( 'The page you were looking for was not found.', 'buddypress' ) ?>
-
+				<?php do_action( 'bp_404' ); ?>
 			</div>
 
-			<?php do_action( 'bp_404' ) ?>
-
-		</div>
-
-		<?php do_action( 'bp_after_404' ) ?>
-
+			<?php do_action( 'bp_after_404' ) ?>
 		</div><!-- .padder -->
 	</div><!-- #content -->
 
-	<?php get_sidebar() ?>
-
-<?php get_footer(); ?>
+<?php get_footer() ?>
