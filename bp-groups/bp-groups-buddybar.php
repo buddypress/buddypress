998b51da32f6269a0f33ddd2c384193988347610
Boone B Gorges <boonebgorges@gmail.com>
2011-06-27 12:12:12 +0000

    Reverts advanced buddybar admin links for groups. See #3302
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@4566 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-groups/bp-groups-buddybar.php
+++ bp-groups/bp-groups-buddybar.php
@@ -11,9 +11,33 @@ function bp_groups_adminbar_admin_menu() {
 		return false; ?>
 
 	<li id="bp-adminbar-adminoptions-menu">
-		<a href="<?php bp_groups_action_link( 'admin' ); ?>"><?php _e( 'Group Admin', 'buddypress' ); ?></a>
+		<a href="<?php bp_groups_action_link( 'admin' ); ?>"><?php _e( 'Admin Options', 'buddypress' ); ?></a>
 
 		<ul>
+			<li><a href="<?php bp_groups_action_link( 'admin/edit-details' ); ?>"><?php _e( 'Edit Details', 'buddypress' ); ?></a></li>
+			
+			<li><a href="<?php bp_groups_action_link( 'admin/group-settings' );  ?>"><?php _e( 'Group Settings', 'buddypress' ); ?></a></li>
+			
+			<li><a href="<?php bp_groups_action_link( 'admin/group-avatar' ); ?>"><?php _e( 'Group Avatar', 'buddypress' ); ?></a></li>
+
+			<?php if ( bp_is_active( 'friends' ) ) : ?>
+
+				<li><a href="<?php bp_groups_action_link( 'send-invites' ); ?>"><?php _e( 'Manage Invitations', 'buddypress' ); ?></a></li>
+
+			<?php endif; ?>
+
+			<li><a href="<?php bp_groups_action_link( 'admin/manage-members' ); ?>"><?php _e( 'Manage Members', 'buddypress' ); ?></a></li>
+
+			<?php if ( $bp->groups->current_group->status == 'private' ) : ?>
+
+				<li><a href="<?php bp_groups_action_link( 'admin/membership-requests' ); ?>"><?php _e( 'Membership Requests', 'buddypress' ); ?></a></li>
+
+			<?php endif; ?>
+
+			<li><a class="confirm" href="<?php echo wp_nonce_url( bp_get_group_permalink( $bp->groups->current_group ) . 'admin/delete-group/', 'groups_delete_group' ); ?>&amp;delete-group-button=1&amp;delete-group-understand=1"><?php _e( "Delete Group", 'buddypress' ) ?></a></li>
+			
+			<?php /* These advanced admin items have been removed for the 1.3 release */ ?>
+			<?php /*
 			<li>
 				<li><a href="<?php bp_groups_action_link( 'admin/edit-details' ); ?>"><?php _e( 'Admin', 'buddypress' ); ?></a>
 
@@ -61,6 +85,8 @@ function bp_groups_adminbar_admin_menu() {
 				</li>
 
 			<?php endif; ?>
+
+			*/ ?>
 
 			<?php do_action( 'bp_groups_adminbar_admin_menu' ) ?>
 
