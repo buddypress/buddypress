8c39cba240fb4ff6bd894c6c990f9bfabcfbf5ed
Boone B Gorges <boonebgorges@gmail.com>
2011-06-27 12:36:51 +0000

    Migrates User Admin moderation menu to the WP Admin Bar. References #3294
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@4569 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-members/bp-members-adminbar.php
+++ bp-members/bp-members-adminbar.php
@@ -95,6 +95,78 @@ function bp_members_admin_bar_my_account_menu() {
 }
 add_action( 'bp_setup_admin_bar', 'bp_members_admin_bar_my_account_menu', 4 );
 
+function bp_members_user_admin_menu() {
+	global $wp_admin_bar;
+
+	// Only show if viewing a user
+	if ( !bp_is_user() )
+		return false;
+
+	// Don't show this menu to non site admins or if you're viewing your own profile
+	if ( !current_user_can( 'edit_users' ) || bp_is_my_profile() )
+		return false;
+
+	// Add the top-level User Admin button
+	$wp_admin_bar->add_menu( array(
+		'id'    => 'user-admin',
+		'title' => __( 'User Admin', 'buddypress' ),
+		'href'  => bp_displayed_user_domain()
+	) );
+
+	// User Admin > Edit this user's profile
+	$wp_admin_bar->add_menu( array(
+		'parent' => 'user-admin',
+		'id'     => 'edit-profile',
+		'title'  => sprintf( __( "Edit %s's Profile", 'buddypress' ), bp_get_displayed_user_fullname() ),
+		'href'   => bp_get_members_component_link( 'profile', 'edit' )
+	) );
+	
+	// User Admin > Edit this user's avatar
+	$wp_admin_bar->add_menu( array(
+		'parent' => 'user-admin',
+		'id'     => 'change-avatar',
+		'title'  => sprintf( __( "Edit %s's Avatar", 'buddypress' ), bp_get_displayed_user_fullname() ),
+		'href'   => bp_get_members_component_link( 'profile', 'change-avatar' )
+	) );
+	
+	// User Admin > Edit this user's avatar
+	$wp_admin_bar->add_menu( array(
+		'parent' => 'user-admin',
+		'id'     => 'change-avatar',
+		'title'  => sprintf( __( "Edit %s's Avatar", 'buddypress' ), bp_get_displayed_user_fullname() ),
+		'href'   => bp_get_members_component_link( 'profile', 'change-avatar' )
+	) );
+	
+	// User Admin > Spam/unspam
+	if ( !bp_core_is_user_spammer( bp_displayed_user_id() ) ) {
+		$wp_admin_bar->add_menu( array(
+			'parent' => 'user-admin',
+			'id'     => 'spam-user',
+			'title'  => __( "Mark as Spammer", 'buddypress' ),
+			'href'   => wp_nonce_url( bp_displayed_user_domain() . 'admin/mark-spammer/', 'mark-unmark-spammer' ),
+			'meta'   => array( 'onclick' => 'confirm(" ' . __( 'Are you sure you want to mark this user as a spammer?', 'buddypress' ) . '");' ) 
+		) );	
+	} else {
+		$wp_admin_bar->add_menu( array(
+			'parent' => 'user-admin',
+			'id'     => 'unspam-user',
+			'title'  => __( "Not a Spammer", 'buddypress' ),
+			'href'   => wp_nonce_url( bp_displayed_user_domain() . 'admin/unmark-spammer/', 'mark-unmark-spammer' ),
+			'meta'   => array( 'onclick' => 'confirm(" ' . __( 'Are you sure you want to mark this user as not a spammer?', 'buddypress' ) . '");' ) 
+		) );
+	}
+	
+	// User Admin > Delete Account
+	$wp_admin_bar->add_menu( array(
+		'parent' => 'user-admin',
+		'id'     => 'delete-user',
+		'title'  => sprintf( __( "Delete %s's Account", 'buddypress' ), bp_get_displayed_user_fullname() ),
+		'href'   => wp_nonce_url( bp_displayed_user_domain() . 'admin/delete-user/', 'delete-user' ),
+		'meta'   => array( 'onclick' => 'confirm(" ' . __( "Are you sure you want to delete this user's account?", 'buddypress' ) . '");' ) 
+	) );
+}
+add_action( 'bp_setup_admin_bar', 'bp_members_user_admin_menu', 99 );
+
 /**
  * Make sure the logout link is at the bottom of the "My Account" menu
  *
