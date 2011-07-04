4053a40d4d96bee2bbe668eef64de9cd6d6a1014
John James Jacoby <johnjamesjacoby@me.com>
2011-07-04 06:08:54 +0000

    Do not show last activity stream items for spam or deleted users.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@4589 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-activity/bp-activity-template.php
+++ bp-activity/bp-activity-template.php
@@ -1229,6 +1229,9 @@ function bp_activity_latest_update( $user_id = 0 ) {
 		if ( !$user_id )
 			$user_id = $bp->displayed_user->id;
 
+		if ( bp_core_is_user_spammer( $user_id ) || bp_core_is_user_deleted( $user_id ) )
+			return false;
+
 		if ( !$update = bp_get_user_meta( $user_id, 'bp_latest_update', true ) )
 			return false;
 
