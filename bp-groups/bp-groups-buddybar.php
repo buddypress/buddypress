e3e0396f2b9693b0602996c2785a377487caddfe
John James Jacoby <johnjamesjacoby@me.com>
2011-07-21 10:12:40 +0000

    Remove commented out moderation menu items. Fixes #3067.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@4761 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-groups/bp-groups-buddybar.php
+++ bp-groups/bp-groups-buddybar.php
@@ -35,58 +35,6 @@ function bp_groups_adminbar_admin_menu() {
 			<?php endif; ?>
 
 			<li><a class="confirm" href="<?php echo wp_nonce_url( bp_get_group_permalink( $bp->groups->current_group ) . 'admin/delete-group/', 'groups_delete_group' ); ?>&amp;delete-group-button=1&amp;delete-group-understand=1"><?php _e( "Delete Group", 'buddypress' ) ?></a></li>
-			
-			<?php /* These advanced admin items have been removed for the 1.3 release */ ?>
-			<?php /*
-			<li>
-				<li><a href="<?php bp_groups_action_link( 'admin/edit-details' ); ?>"><?php _e( 'Admin', 'buddypress' ); ?></a>
-
-				<ul>
-					<li><a href="<?php bp_groups_action_link( 'admin/edit-details' ); ?>"><?php _e( 'Edit Details', 'buddypress' ); ?></a></li>
-					<li><a href="<?php bp_groups_action_link( 'admin/group-settings' );  ?>"><?php _e( 'Group Settings', 'buddypress' ); ?></a></li>
-					<li><a href="<?php bp_groups_action_link( 'admin/group-avatar' ); ?>"><?php _e( 'Group Avatar', 'buddypress' ); ?></a></li>
-
-					<?php if ( bp_is_active( 'friends' ) ) : ?>
-
-						<li><a href="<?php bp_groups_action_link( 'send-invites' ); ?>"><?php _e( 'Manage Invitations', 'buddypress' ); ?></a></li>
-
-					<?php endif; ?>
-
-					<li><a href="<?php bp_groups_action_link( 'admin/manage-members' ); ?>"><?php _e( 'Manage Members', 'buddypress' ); ?></a></li>
-
-					<?php if ( $bp->groups->current_group->status == 'private' ) : ?>
-
-						<li><a href="<?php bp_groups_action_link( 'admin/membership-requests' ); ?>"><?php _e( 'Membership Requests', 'buddypress' ); ?></a></li>
-
-					<?php endif; ?>
-
-					<li><a class="confirm" href="<?php echo wp_nonce_url( bp_get_group_permalink( $bp->groups->current_group ) . 'admin/delete-group/', 'groups_delete_group' ); ?>&amp;delete-group-button=1&amp;delete-group-understand=1"><?php _e( "Delete Group", 'buddypress' ) ?></a></li>
-				</ul>
-			</li>
-
-			<?php if ( bp_is_active( 'activity' ) ) : ?>
-
-				<li>
-					<a href="<?php bp_groups_action_link( 'activity' ); ?>"><?php _e( 'Activity', 'buddypress' ); ?></a>
-					<ul>
-						<li><a href="<?php bp_groups_action_link( 'activity/admin', array( 'clear' => 'all' ), true ); ?>" class="confirm"><?php _e( "Delete All Activity", 'buddypress' ); ?></a></li>
-					</ul>
-				</li>
-
-			<?php endif; ?>
-
-			<?php if ( bp_is_active( 'forums' ) ) : ?>
-
-				<li>
-					<a href="<?php bp_groups_action_link( 'forums' ); ?>"><?php _e( 'Forums', 'buddypress' ); ?></a>
-					<ul>
-						<li><a href="<?php bp_groups_action_link( 'forums/admin', array( 'clear' => 'all' ), true ); ?>" class="confirm"><?php _e( "Delete Forum Contents", 'buddypress' ); ?></a></li>
-					</ul>
-				</li>
-
-			<?php endif; ?>
-
-			*/ ?>
 
 			<?php do_action( 'bp_groups_adminbar_admin_menu' ) ?>
 
