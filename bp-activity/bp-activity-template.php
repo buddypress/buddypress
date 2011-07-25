72d11c3b19fdf404ddaa3549c7004aa67302227c
John James Jacoby <johnjamesjacoby@me.com>
2011-07-25 23:15:31 +0000

    Remove hard-coded quotes from bp_get_activity_latest_update()
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@4863 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-activity/bp-activity-template.php
+++ bp-activity/bp-activity-template.php
@@ -1285,7 +1285,7 @@ function bp_activity_latest_update( $user_id = 0 ) {
 		if ( !$update = bp_get_user_meta( $user_id, 'bp_latest_update', true ) )
 			return false;
 
-		$latest_update = apply_filters( 'bp_get_activity_latest_update_excerpt', '&quot;' . trim( strip_tags( bp_create_excerpt( $update['content'], 358 ) ) ) . '&quot;' );
+		$latest_update = apply_filters( 'bp_get_activity_latest_update_excerpt', trim( strip_tags( bp_create_excerpt( $update['content'], 358 ) ) ) );
 		$latest_update .= ' <a href="' . bp_get_root_domain() . '/' . bp_get_activity_root_slug() . '/p/' . $update['id'] . '/"> ' . __( 'View', 'buddypress' ) . '</a>';
 
 		return apply_filters( 'bp_get_activity_latest_update', $latest_update  );
