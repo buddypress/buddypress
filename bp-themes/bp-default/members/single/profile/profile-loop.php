11ba6dc08c6820e039d21f1a247ce50c1741cb2a
John James Jacoby <johnjamesjacoby@me.com>
2011-07-28 02:33:05 +0000

    Remove title restriction on primary XProfile group in profile-loop.php.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@4883 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/members/single/profile/profile-loop.php
+++ bp-themes/bp-default/members/single/profile/profile-loop.php
@@ -1,4 +1,4 @@
-<?php do_action( 'bp_before_profile_loop_content' ) ?>
+<?php do_action( 'bp_before_profile_loop_content' ); ?>
 
 <?php if ( bp_has_profile() ) : ?>
 
@@ -6,44 +6,43 @@
 
 		<?php if ( bp_profile_group_has_fields() ) : ?>
 
-			<?php do_action( 'bp_before_profile_field_content' ) ?>
+			<?php do_action( 'bp_before_profile_field_content' ); ?>
 
-			<div class="bp-widget <?php bp_the_profile_group_slug() ?>">
-				<?php if ( 1 != bp_get_the_profile_group_id() ) : ?>
-					<h4><?php bp_the_profile_group_name() ?></h4>
-				<?php endif; ?>
+			<div class="bp-widget <?php bp_the_profile_group_slug(); ?>">
+
+				<h4><?php bp_the_profile_group_name(); ?></h4>
 
 				<table class="profile-fields">
+
 					<?php while ( bp_profile_fields() ) : bp_the_profile_field(); ?>
 
 						<?php if ( bp_field_has_data() ) : ?>
-							<tr<?php bp_field_css_class() ?>>
 
-								<td class="label">
-									<?php bp_the_profile_field_name() ?>
-								</td>
+							<tr<?php bp_field_css_class(); ?>>
 
-								<td class="data">
-									<?php bp_the_profile_field_value() ?>
-								</td>
+								<td class="label"><?php bp_the_profile_field_name(); ?></td>
+
+								<td class="data"><?php bp_the_profile_field_value(); ?></td>
 
 							</tr>
+
 						<?php endif; ?>
 
-						<?php do_action( 'bp_profile_field_item' ) ?>
+						<?php do_action( 'bp_profile_field_item' ); ?>
 
 					<?php endwhile; ?>
+
 				</table>
 			</div>
 
-			<?php do_action( 'bp_after_profile_field_content' ) ?>
+			<?php do_action( 'bp_after_profile_field_content' ); ?>
 
 		<?php endif; ?>
 
 	<?php endwhile; ?>
 
-	<?php do_action( 'bp_profile_field_buttons' ) ?>
+	<?php do_action( 'bp_profile_field_buttons' ); ?>
 
 <?php endif; ?>
 
-<?php do_action( 'bp_after_profile_loop_content' ) ?>
+<?php do_action( 'bp_after_profile_loop_content' ); ?>
