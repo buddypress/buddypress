20c4318f72fe1b5560e06b0a582097abd8012a61
Boone B Gorges <boonebgorges@gmail.com>
2011-08-25 20:19:26 +0000

    Escape sprintf() swappable arguments in bp_core_activation_signup_blog_notification(). Fixes #3514
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@5033 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-core/bp-core-filters.php
+++ bp-core/bp-core-filters.php
@@ -201,8 +201,8 @@ function bp_core_activation_signup_blog_notification( $domain, $path, $title, $u
 
 	$from_name       = ( '' == get_site_option( 'site_name' ) ) ? 'WordPress' : esc_html( get_site_option( 'site_name' ) );
 	$message_headers = "MIME-Version: 1.0\n" . "From: \"{$from_name}\" <{$admin_email}>\n" . "Content-Type: text/plain; charset=\"" . get_option( 'blog_charset' ) . "\"\n";
-	$message         = sprintf( __( "Thanks for registering! To complete the activation of your account and blog, please click the following link:\n\n%1$s\n\n\n\nAfter you activate, you can visit your blog here:\n\n%2$s", 'buddypress' ), $activate_url, esc_url( "http://{$domain}{$path}" ) );
-	$subject         = '[' . $from_name . '] ' . sprintf(__('Activate %s', 'buddypress' ), esc_url( 'http://' . $domain . $path ) );
+	$message         = sprintf( __( "Thanks for registering! To complete the activation of your account and blog, please click the following link:\n\n%1\$s\n\n\n\nAfter you activate, you can visit your blog here:\n\n%2\$s", 'buddypress' ), $activate_url, esc_url( "http://{$domain}{$path}" ) );
+	$subject         = '[' . $from_name . '] ' . sprintf(__( 'Activate %s', 'buddypress' ), esc_url( 'http://' . $domain . $path ) );
 
 	// Send the message
 	$to              = apply_filters( 'bp_core_activation_signup_blog_notification_to',   $user_email, $domain, $path, $title, $user, $user_email, $key, $meta );
