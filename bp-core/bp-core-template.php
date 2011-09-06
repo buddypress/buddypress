0b8d348a0e500cba2c17c80e06082ef6c677b381
Boone B Gorges <boonebgorges@gmail.com>
2011-09-06 12:49:12 +0000

    Changes CSS class on group tabs so that AJAX is run correctly. References #3539.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@5111 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-core/bp-core-template.php
+++ bp-core/bp-core-template.php
@@ -49,8 +49,11 @@ function bp_get_options_nav() {
 			$selected = '';
 		}
 
+		// List type depends on our current component
+		$list_type = bp_is_group() ? 'groups' : 'personal';
+
 		// echo out the final list item
-		echo apply_filters( 'bp_get_options_nav_' . $subnav_item['css_id'], '<li id="' . $subnav_item['css_id'] . '-personal-li" ' . $selected . '><a id="' . $subnav_item['css_id'] . '" href="' . $subnav_item['link'] . '">' . $subnav_item['name'] . '</a></li>', $subnav_item );
+		echo apply_filters( 'bp_get_options_nav_' . $subnav_item['css_id'], '<li id="' . $subnav_item['css_id'] . '-' . $list_type . '-li" ' . $selected . '><a id="' . $subnav_item['css_id'] . '" href="' . $subnav_item['link'] . '">' . $subnav_item['name'] . '</a></li>', $subnav_item );
 	}
 }
 
