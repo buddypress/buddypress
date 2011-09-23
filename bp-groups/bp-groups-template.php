1cd69956bbed2ad2990bfadd5b826e4c0be849fd
Boone B Gorges <boonebgorges@gmail.com>
2011-09-23 21:40:29 +0000

    Use groups root slug when constructing Previous Step button during group creation process. Fixes #3607. Props wdfee
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@5180 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-groups/bp-groups-template.php
+++ bp-groups/bp-groups-template.php
@@ -2184,7 +2184,7 @@ function bp_group_creation_previous_link() {
 			$previous_steps[] = $slug;
 		}
 
-		return apply_filters( 'bp_get_group_creation_previous_link', trailingslashit( bp_get_root_domain() ) . bp_get_groups_slug() . '/create/step/' . array_pop( $previous_steps ) );
+		return apply_filters( 'bp_get_group_creation_previous_link', trailingslashit( bp_get_root_domain() ) . bp_get_groups_root_slug() . '/create/step/' . array_pop( $previous_steps ) );
 	}
 
 function bp_is_last_group_creation_step() {
