9cbd74768aa38573e35028e7202ec0c4b6331ad6
Boone B Gorges <boonebgorges@gmail.com>
2011-11-27 16:16:12 +0000

    Rearrange groups/single/forum/topic.php template so that admin links and reply markup appears even when, because of a malfunction, there are no posts in the topic. Fixes #3675
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@5388 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/groups/single/forum/topic.php
+++ bp-themes/bp-default/groups/single/forum/topic.php
@@ -1,56 +1,56 @@
 <?php do_action( 'bp_before_group_forum_topic' ); ?>
 
-<?php if ( bp_has_forum_topic_posts() ) : ?>
-
-	<form action="<?php bp_forum_topic_action() ?>" method="post" id="forum-topic-form" class="standard-form">
-
-		<div class="item-list-tabs no-ajax" id="subnav" role="navigation">
-			<ul>
-				<?php if ( is_user_logged_in() ) : ?>
-
-					<li>
-						<a href="<?php bp_forum_topic_new_reply_link() ?>" class="new-reply-link"><?php _e( 'New Reply', 'buddypress' ) ?></a>
-					</li>
-
-				<?php endif; ?>
-
-				<?php if ( bp_forums_has_directory() ) : ?>
-
-					<li>
-						<a href="<?php bp_forums_directory_permalink() ?>"><?php _e( 'Forum Directory', 'buddypress') ?></a>
-					</li>
-
-				<?php endif; ?>
-
-			</ul>
-		</div>
-
-		<div id="topic-meta">
-			<h3><?php bp_the_topic_title() ?> (<?php bp_the_topic_total_post_count() ?>)</h3>
-
-			<?php if ( bp_forum_topic_has_tags() ) : ?>
-
-				<div class="topic-tags">
-
-					<?php _e( 'Topic tags:', 'buddypress' ) ?> <?php bp_forum_topic_tag_list() ?>
-
-				</div>
-
+<form action="<?php bp_forum_topic_action() ?>" method="post" id="forum-topic-form" class="standard-form">
+	<div class="item-list-tabs no-ajax" id="subnav" role="navigation">
+		<ul>
+			<?php if ( is_user_logged_in() ) : ?>
+	
+				<li>
+					<a href="<?php bp_forum_topic_new_reply_link() ?>" class="new-reply-link"><?php _e( 'New Reply', 'buddypress' ) ?></a>
+				</li>
+	
 			<?php endif; ?>
-
-			<?php if ( bp_group_is_admin() || bp_group_is_mod() || bp_get_the_topic_is_mine() ) : ?>
-
-				<div class="last admin-links">
-
-					<?php bp_the_topic_admin_links() ?>
-
-				</div>
-
+	
+			<?php if ( bp_forums_has_directory() ) : ?>
+	
+				<li>
+					<a href="<?php bp_forums_directory_permalink() ?>"><?php _e( 'Forum Directory', 'buddypress') ?></a>
+				</li>
+	
 			<?php endif; ?>
+	
+		</ul>
+	</div>
+	
+	<div id="topic-meta">
+		<h3><?php bp_the_topic_title() ?> (<?php bp_the_topic_total_post_count() ?>)</h3>
+	
+		<?php if ( bp_forum_topic_has_tags() ) : ?>
+	
+			<div class="topic-tags">
+	
+				<?php _e( 'Topic tags:', 'buddypress' ) ?> <?php bp_forum_topic_tag_list() ?>
+	
+			</div>
+	
+		<?php endif; ?>
+	
+		<?php if ( bp_group_is_admin() || bp_group_is_mod() || bp_get_the_topic_is_mine() ) : ?>
+	
+			<div class="last admin-links">
+	
+				<?php bp_the_topic_admin_links() ?>
+	
+			</div>
+	
+		<?php endif; ?>
+	
+		<?php do_action( 'bp_group_forum_topic_meta' ); ?>
+	
+	</div>
 
-			<?php do_action( 'bp_group_forum_topic_meta' ); ?>
 
-		</div>
+	<?php if ( bp_has_forum_topic_posts() ) : ?>
 
 		<div class="pagination no-ajax">
 
@@ -113,54 +113,54 @@
 
 		</div>
 
-		<?php if ( ( is_user_logged_in() && 'public' == bp_get_group_status() ) || bp_group_is_member() ) : ?>
+	<?php else: ?>
+	
+		<div id="message" class="info">
+			<p><?php _e( 'There are no posts for this topic.', 'buddypress' ) ?></p>
+		</div>
+	
+	<?php endif;?>
 
-			<?php if ( bp_get_the_topic_is_last_page() ) : ?>
+	<?php if ( ( is_user_logged_in() && 'public' == bp_get_group_status() ) || bp_group_is_member() ) : ?>
 
-				<?php if ( bp_get_the_topic_is_topic_open() && !bp_group_is_user_banned() ) : ?>
+		<?php if ( bp_get_the_topic_is_last_page() ) : ?>
 
-					<div id="post-topic-reply">
-						<p id="post-reply"></p>
+			<?php if ( bp_get_the_topic_is_topic_open() && !bp_group_is_user_banned() ) : ?>
 
-						<?php if ( bp_groups_auto_join() && !bp_group_is_member() ) : ?>
-							<p><?php _e( 'You will auto join this group when you reply to this topic.', 'buddypress' ) ?></p>
-						<?php endif; ?>
+				<div id="post-topic-reply">
+					<p id="post-reply"></p>
 
-						<?php do_action( 'groups_forum_new_reply_before' ) ?>
+					<?php if ( bp_groups_auto_join() && !bp_group_is_member() ) : ?>
+						<p><?php _e( 'You will auto join this group when you reply to this topic.', 'buddypress' ) ?></p>
+					<?php endif; ?>
 
-						<h4><?php _e( 'Add a reply:', 'buddypress' ) ?></h4>
+					<?php do_action( 'groups_forum_new_reply_before' ) ?>
 
-						<textarea name="reply_text" id="reply_text"></textarea>
+					<h4><?php _e( 'Add a reply:', 'buddypress' ) ?></h4>
 
-						<div class="submit">
-							<input type="submit" name="submit_reply" id="submit" value="<?php _e( 'Post Reply', 'buddypress' ) ?>" />
-						</div>
+					<textarea name="reply_text" id="reply_text"></textarea>
 
-						<?php do_action( 'groups_forum_new_reply_after' ) ?>
-
-						<?php wp_nonce_field( 'bp_forums_new_reply' ) ?>
+					<div class="submit">
+						<input type="submit" name="submit_reply" id="submit" value="<?php _e( 'Post Reply', 'buddypress' ) ?>" />
 					</div>
 
-				<?php elseif ( !bp_group_is_user_banned() ) : ?>
+					<?php do_action( 'groups_forum_new_reply_after' ) ?>
 
-					<div id="message" class="info">
-						<p><?php _e( 'This topic is closed, replies are no longer accepted.', 'buddypress' ) ?></p>
-					</div>
+					<?php wp_nonce_field( 'bp_forums_new_reply' ) ?>
+				</div>
 
-				<?php endif; ?>
+			<?php elseif ( !bp_group_is_user_banned() ) : ?>
+
+				<div id="message" class="info">
+					<p><?php _e( 'This topic is closed, replies are no longer accepted.', 'buddypress' ) ?></p>
+				</div>
 
 			<?php endif; ?>
 
 		<?php endif; ?>
 
-	</form><!-- #forum-topic-form -->
+	<?php endif; ?>
 
-<?php else: ?>
-
-	<div id="message" class="info">
-		<p><?php _e( 'There are no posts for this topic.', 'buddypress' ) ?></p>
-	</div>
-
-<?php endif;?>
+</form><!-- #forum-topic-form -->
 
 <?php do_action( 'bp_after_group_forum_topic' ) ?>
