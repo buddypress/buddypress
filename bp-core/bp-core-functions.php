b7966eac7de0df016b0f474f0c196172036714e7
Boone B Gorges <boonebgorges@gmail.com>
2011-11-28 15:13:04 +0000

    Switch maintenance_mode check to empty() so that it's not thrown off by an empty string
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@5404 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-core/bp-core-functions.php
+++ bp-core/bp-core-functions.php
@@ -400,7 +400,7 @@ function bp_core_activation_notice() {
 		return;
 
 	// Don't show these messages during setup or upgrade
-	if ( isset( $bp->maintenance_mode ) )
+	if ( !empty( $bp->maintenance_mode ) )
 		return;
 
 	/**
