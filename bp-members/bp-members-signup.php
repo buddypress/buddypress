b3d513a218180db80ffc3e633dd338387c7b44bf
Paul Gibbs <djpaul@gmail.com>
2011-12-05 19:12:44 +0000

    Make sure that all emails' contents have argument swapping for i18n placeholder variables. Fixes #3795
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@5445 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-members/bp-members-signup.php
+++ bp-members/bp-members-signup.php
@@ -568,7 +568,7 @@ function bp_core_signup_send_validation_email( $user_id, $user_email, $key ) {
 
 	$from_name = ( '' == get_option( 'blogname' ) ) ? __( 'BuddyPress', 'buddypress' ) : esc_html( get_option( 'blogname' ) );
 
-	$message = sprintf( __( "Thanks for registering! To complete the activation of your account please click the following link:\n\n%s\n\n", 'buddypress' ), $activate_url );
+	$message = sprintf( __( "Thanks for registering! To complete the activation of your account please click the following link:\n\n%1\$s\n\n", 'buddypress' ), $activate_url );
 	$subject = '[' . $from_name . '] ' . __( 'Activate Your Account', 'buddypress' );
 
 	// Send the message
