3706029dbdfb5dfef3c52497c919024c3f069af6
John James Jacoby <johnjamesjacoby@me.com>
2011-12-11 22:01:08 +0000

    Remove the Edit Page menu when viewing a group. Remove the group name and avatar from Edit Group menu. Fixes #3838.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@5519 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-groups/bp-groups-adminbar.php
+++ bp-groups/bp-groups-adminbar.php
@@ -30,24 +30,13 @@ function bp_groups_group_admin_menu() {
 	if ( !bp_current_user_can( 'bp_moderate' ) && !bp_group_is_admin() )
 		return false;
 
-	// Group avatar
-	$avatar = bp_core_fetch_avatar( array(
-		'object'     => 'group',
-		'type'       => 'thumb',
-		'avatar_dir' => 'group-avatars',
-		'item_id'    => $bp->groups->current_group->id,
-		'width'      => 16,
-		'height'     => 16,
-		'alt'	     => sprintf( __( 'Group logo of %s', 'buddypress' ), $bp->groups->current_group->name )
-	) );
-
-	// Unique ID for the 'My Account' menu
-	$bp->group_admin_menu_id = ( ! empty( $avatar ) ) ? 'group-admin-with-avatar' : 'group-admin';
+	// Unique ID for the 'Edit Group' menu
+	$bp->group_admin_menu_id = 'group-admin';
 
 	// Add the top-level Group Admin button
 	$wp_admin_bar->add_menu( array(
 		'id'    => $bp->group_admin_menu_id,
-		'title' => $avatar . bp_get_current_group_name(),
+		'title' => __( 'Edit Group', 'buddypress' ),
 		'href'  => bp_get_group_permalink( $bp->groups->current_group )
 	) );
 
@@ -114,5 +103,17 @@ function bp_groups_group_admin_menu() {
 	) );
 }
 add_action( 'admin_bar_menu', 'bp_groups_group_admin_menu', 99 );
+
+/**
+ * Remove rogue WP core edit menu when viewing a group
+ *
+ * @since BuddyPress (1.6)
+ */
+function bp_groups_remove_edit_page_menu() {
+	if ( bp_is_group() ) {
+		remove_action( 'admin_bar_menu', 'wp_admin_bar_edit_menu', 80 );
+	}
+}
+add_action( 'bp_init', 'bp_groups_remove_edit_page_menu', 99 );
 
 ?>
