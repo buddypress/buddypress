7e6db45822ab7c93d8194f748eeda00f9968dccc
Boone B Gorges <boonebgorges@gmail.com>
2011-12-11 16:32:45 +0000

    Pass concatenated alt parameter to bp_core_fetch_avatar() throughout bp-default. References #3806
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@5503 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/groups/single/admin.php
+++ bp-themes/bp-default/groups/single/admin.php
@@ -182,7 +182,7 @@
 			
 			<?php while ( bp_members() ) : bp_the_member(); ?>
 			<li>
-				<?php echo bp_core_fetch_avatar( array( 'item_id' => bp_get_member_user_id(), 'type' => 'thumb', 'width' => 30, 'height' => 30, 'alt' => __( 'Profile picture of %s', 'buddypress' ) ) ) ?>
+				<?php echo bp_core_fetch_avatar( array( 'item_id' => bp_get_member_user_id(), 'type' => 'thumb', 'width' => 30, 'height' => 30, 'alt' => sprintf( __( 'Profile picture of %s', 'buddypress' ), bp_get_member_name() ) ) ) ?>
 				<h5>
 					<a href="<?php bp_member_permalink(); ?>"> <?php bp_member_name(); ?></a>
 					<span class="small">
@@ -207,7 +207,7 @@
 				
 					<?php while ( bp_members() ) : bp_the_member(); ?>					
 					<li>
-						<?php echo bp_core_fetch_avatar( array( 'item_id' => bp_get_member_user_id(), 'type' => 'thumb', 'width' => 30, 'height' => 30, 'alt' => __( 'Profile picture of %s', 'buddypress' ) ) ) ?>
+						<?php echo bp_core_fetch_avatar( array( 'item_id' => bp_get_member_user_id(), 'type' => 'thumb', 'width' => 30, 'height' => 30, 'alt' => sprintf( __( 'Profile picture of %s', 'buddypress' ), bp_get_member_name() ) ) ) ?>
 						<h5>
 							<a href="<?php bp_member_permalink(); ?>"> <?php bp_member_name(); ?></a>
 							<span class="small">
