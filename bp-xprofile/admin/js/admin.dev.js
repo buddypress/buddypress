7ca92889d1ad5e44bbad1c182299b7c07833f29f
John James Jacoby <johnjamesjacoby@me.com>
2011-12-24 18:11:28 +0000

    Tweak XProfile CSS and JS. See #3801.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@5599 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-xprofile/admin/js/admin.dev.js
+++ bp-xprofile/admin/js/admin.dev.js
@@ -1,106 +1,82 @@
 function add_option(forWhat) {
-	var holder = document.getElementById(forWhat + "_more");
-	var theId = document.getElementById(forWhat + '_option_number').value;
-
-	var newDiv = document.createElement('p');
-	newDiv.setAttribute('id', forWhat + '_div' + theId);
-
+	var holder    = document.getElementById(forWhat + "_more");
+	var theId     = document.getElementById(forWhat + '_option_number').value;
+	var newDiv    = document.createElement('p');
 	var newOption = document.createElement('input');
-	newOption.setAttribute('type', 'text');
-	newOption.setAttribute('name', forWhat + '_option[' + theId + ']');
-	newOption.setAttribute('id', forWhat + '_option' + theId);
+	var label     = document.createElement( 'label' );
+	var txt       = document.createTextNode( "Option " + theId + ": " );
+	var isDefault = document.createElement( 'input' );
+	var label1    = document.createElement( 'label' );
+	var txt1      = document.createTextNode( " Default Value " );
 
-	var label = document.createElement('label');
-	label.setAttribute('for', forWhat + '_option' + theId);
+	newDiv.setAttribute('id', forWhat + '_div' + theId);
+	
+	newOption.setAttribute( 'type', 'text' );
+	newOption.setAttribute( 'name', forWhat + '_option[' + theId + ']' );
+	newOption.setAttribute( 'id', forWhat + '_option' + theId );
 
-	var txt = document.createTextNode("Option " + theId + ": ");
-	label.appendChild(txt);
+	label.setAttribute( 'for', forWhat + '_option' + theId );
+	label.appendChild( txt );
 
-	var isDefault = document.createElement('input');
-
-	if(forWhat == 'checkbox' || forWhat == 'multiselectbox') {
-		isDefault.setAttribute('type', 'checkbox');
-		isDefault.setAttribute('name', 'isDefault_' + forWhat + '_option[' + theId + ']');
+	if ( forWhat == 'checkbox' || forWhat == 'multiselectbox' ) {
+		isDefault.setAttribute( 'type', 'checkbox' );
+		isDefault.setAttribute( 'name', 'isDefault_' + forWhat + '_option[' + theId + ']' );
 	} else {
-		isDefault.setAttribute('type', 'radio');
-		isDefault.setAttribute('name', 'isDefault_' + forWhat + '_option');
+		isDefault.setAttribute( 'type', 'radio' );
+		isDefault.setAttribute( 'name', 'isDefault_' + forWhat + '_option' );
 	}
 
-	isDefault.setAttribute('value', theId);
+	isDefault.setAttribute( 'value', theId );
 
-	var label1 = document.createElement('label');
-	var txt1 = document.createTextNode(" Default Value ");
+	label1.appendChild( txt1 );
+	label1.setAttribute( 'for', 'isDefault_' + forWhat + '_option[]' );
 
-	label1.appendChild(txt1);
-	label1.setAttribute('for', 'isDefault_' + forWhat + '_option[]');
-	toDelete = document.createElement('a');
+	toDelete     = document.createElement( 'a' );
+	toDeleteText = document.createTextNode( '[x]' );
 
-	toDeleteText = document.createTextNode('[x]');
-	toDelete.setAttribute('href',"javascript:hide('" + forWhat + '_div' + theId + "')");
+	toDelete.setAttribute( 'href', "javascript:hide('" + forWhat + '_div' + theId + "')" );
+	toDelete.setAttribute( 'class', 'delete' );
+	toDelete.appendChild( toDeleteText );
 
-	toDelete.setAttribute('class','delete');
-
-	toDelete.appendChild(toDeleteText);
-
-	newDiv.appendChild(label);
-	newDiv.appendChild(newOption);
-	newDiv.appendChild(document.createTextNode(" "));
-	newDiv.appendChild(isDefault);
-	newDiv.appendChild(label1);
-	newDiv.appendChild(toDelete);
-	holder.appendChild(newDiv);
+	newDiv.appendChild( label );
+	newDiv.appendChild( newOption );
+	newDiv.appendChild( document.createTextNode( " " ) );
+	newDiv.appendChild( isDefault );
+	newDiv.appendChild( label1 );
+	newDiv.appendChild( toDelete );
+	holder.appendChild( newDiv );
 
 
 	theId++
+
 	document.getElementById(forWhat + "_option_number").value = theId;
 }
 
 function show_options(forWhat) {
-	document.getElementById("radio").style.display = "none";
-	document.getElementById("selectbox").style.display = "none";
-	document.getElementById("multiselectbox").style.display = "none";
-	document.getElementById("checkbox").style.display = "none";
+	document.getElementById( 'radio'          ).style.display = 'none';
+	document.getElementById( 'selectbox'      ).style.display = 'none';
+	document.getElementById( 'multiselectbox' ).style.display = 'none';
+	document.getElementById( 'checkbox'       ).style.display = 'none';
 
-	if(forWhat == "radio")
-		document.getElementById("radio").style.display = "";
+	if ( forWhat == 'radio' )
+		document.getElementById( 'radio' ).style.display = "";
 
-	if(forWhat == "selectbox")
-		document.getElementById("selectbox").style.display = "";
+	if ( forWhat == 'selectbox' )
+		document.getElementById( 'selectbox' ).style.display = "";
 
-	if(forWhat == "multiselectbox")
-		document.getElementById("multiselectbox").style.display = "";
+	if ( forWhat == 'multiselectbox' )
+		document.getElementById( 'multiselectbox' ).style.display = "";
 
-	if(forWhat == "checkbox")
-		document.getElementById("checkbox").style.display = "";
+	if ( forWhat == 'checkbox' )
+		document.getElementById( 'checkbox' ).style.display = "";
 }
 
-function hide(id) {
-	if ( !document.getElementById(id) ) return false;
+function hide( id ) {
+	if ( !document.getElementById( id ) ) return false;
 
-	document.getElementById(id).style.display = "none";
-	document.getElementById(id).value = '';
+	document.getElementById( id ).style.display = "none";
+	document.getElementById( id ).value = '';
 }
-
-// Set up deleting options ajax
-jQuery(document).ready( function() {
-
-	jQuery("a.ajax-option-delete").click(
-		function() {
-			var theId = this.id.split('-');
-			theId = theId[1];
-
-			jQuery.post( ajaxurl, {
-				action: 'xprofile_delete_option',
-				'cookie': encodeURIComponent(document.cookie),
-				'_wpnonce': jQuery("input#_wpnonce").val(),
-
-				'option_id': theId
-			},
-			function(response)
-			{});
-		}
-	);
-});
 
 var fixHelper = function(e, ui) {
 	ui.children().each(function() {
@@ -109,13 +85,29 @@ var fixHelper = function(e, ui) {
 	return ui;
 };
 
-// Main XProfile behavior layer
-jQuery(document).ready( function() {
-	/* Show object if JS is enabled */
-	jQuery( "ul#field-group-tabs" ).show();
+// Set up deleting options ajax
+jQuery( document ).ready( function() {
 
-	/* Allow reordering of field group tabs */
-	jQuery( "ul#field-group-tabs" ).sortable( {
+	jQuery( 'a.ajax-option-delete' ).click(
+		function() {
+			var theId = this.id.split( '-' );
+			theId = theId[1];
+
+			jQuery.post( ajaxurl, {
+				action: 'xprofile_delete_option',
+				'cookie': encodeURIComponent( document.cookie ),
+				'_wpnonce': jQuery('input#_wpnonce').val(),
+				'option_id': theId
+			},
+			function( response ) {} );
+		}
+	);
+
+	// Show object if JS is enabled
+	jQuery( 'ul#field-group-tabs' ).show();
+
+	// Allow reordering of field group tabs
+	jQuery( 'ul#field-group-tabs' ).sortable( {
 		cursor: 'move',
 		axis: 'x',
 		opacity: 0.6,
@@ -125,16 +117,16 @@ jQuery(document).ready( function() {
 		update: function() {
 			jQuery.post( ajaxurl, {
 				action: 'xprofile_reorder_groups',
-				'cookie': encodeURIComponent(document.cookie),
-				'_wpnonce_reorder_groups': jQuery( "input#_wpnonce_reorder_groups" ).val(),
-				'group_order': jQuery(this).sortable( 'serialize' )
+				'cookie': encodeURIComponent( document.cookie ),
+				'_wpnonce_reorder_groups': jQuery( 'input#_wpnonce_reorder_groups' ).val(),
+				'group_order': jQuery( this ).sortable( 'serialize' )
 			},
-			function(response){});
+			function( response ) {} );
 		}
 	}).disableSelection();
 
-	/* Allow reordering of fields within groups */
-	jQuery( "fieldset.field-group" ).sortable({
+	// Allow reordering of fields within groups
+	jQuery( 'fieldset.field-group' ).sortable({
 		cursor: 'move',
 		opacity: 0.3,
 		items: 'fieldset',
@@ -144,59 +136,58 @@ jQuery(document).ready( function() {
 			jQuery.post( ajaxurl, {
 				action: 'xprofile_reorder_fields',
 				'cookie': encodeURIComponent(document.cookie),
-				'_wpnonce_reorder_fields': jQuery( "input#_wpnonce_reorder_fields" ).val(),
+				'_wpnonce_reorder_fields': jQuery( 'input#_wpnonce_reorder_fields' ).val(),
 				'field_order': jQuery(this).sortable( 'serialize' ),
 				'field_group_id': jQuery(this).attr( 'id' )
 			},
-			function(response)
-			{});
+			function( response ) {} );
 		}
 	})
 
-	/* Disallow text selection */
+	// Disallow text selection
 	.disableSelection()
 
-	/* Change cursor to move if JS is enabled */
+	// Change cursor to move if JS is enabled
 	.css( 'cursor', 'move' );
 
-	/* tabs init with a custom tab template and an "add" callback filling in the content */
+	// tabs init with a custom tab template and an "add" callback filling in the content
 	var $tab_items;
-	var $tabs = jQuery( "#tabs" ).tabs();
+	var $tabs = jQuery( '#tabs' ).tabs();
 	set_tab_items( $tabs );
 
 	function set_tab_items( $tabs ) {
-		$tab_items = jQuery( "ul:first li", $tabs ).droppable({
-			accept: ".connectedSortable fieldset",
-			hoverClass: "ui-state-hover",
-			activeClass: "ui-state-acceptable",
-			touch: "pointer",
-			tolerance: "pointer",
+		$tab_items = jQuery( 'ul:first li', $tabs ).droppable({
+			accept: '.connectedSortable fieldset',
+			hoverClass: 'ui-state-hover',
+			activeClass: 'ui-state-acceptable',
+			touch: 'pointer',
+			tolerance: 'pointer',
 
-			/* When field is dropped on tab */
+			// When field is dropped on tab
 			drop: function( ev, ui ) {
-				/* The tab */
+				// The tab
 				var $item = jQuery(this);
 
-				/* The tab body */
+				// The tab body
 				var $list = jQuery( $item.find( 'a' ).attr( 'href' ) ).find( '.connectedSortable' );
 
-				/* Remove helper class */
+				// Remove helper class
 				jQuery($item).removeClass( 'drop-candidate' );
 
-				/* Hide field, change selected tab, and show new placement */
+				// Hide field, change selected tab, and show new placement
 				ui.draggable.hide( 'slow', function() {
 
-					/* Select new tab as current */
+					// Select new tab as current
 					$tabs.tabs( 'select', $tab_items.index( $item ) );
 
-					/* Show new placement */
+					// Show new placement
 					jQuery(this).appendTo($list).show( 'slow' ).animate( {opacity: "1"}, 500 );
 
-					/* Refresh $list variable */
+					// Refresh $list variable
 					$list = jQuery( $item.find( 'a' ).attr( 'href' ) ).find( '.connectedSortable' );
 					jQuery($list).find( 'p.nofields' ).hide( 'slow' );
 
-					/* Ajax update field locations and orders */
+					// Ajax update field locations and orders
 					jQuery.post( ajaxurl, {
 						action: 'xprofile_reorder_fields',
 						'cookie': encodeURIComponent(document.cookie),
@@ -204,7 +195,7 @@ jQuery(document).ready( function() {
 						'field_order': jQuery( $list ).sortable( 'serialize' ),
 						'field_group_id': jQuery( $list ).attr( 'id' )
 					},
-					function(response){});
+					function( response ) {} );
 				});
 			},
 			over: function( event, ui ) {
--- bp-xprofile/bp-xprofile-cssjs.php
+++ bp-xprofile/bp-xprofile-cssjs.php
@@ -1,18 +1,31 @@
 <?php
+
 // Exit if accessed directly
 if ( !defined( 'ABSPATH' ) ) exit;
 
+/**
+ * Enqueue the CSS for XProfile admin styling
+ *
+ * @since BuddyPress (1.1)
+ */
 function xprofile_add_admin_css() {
-	$version = '20111212';
+	$version = '20111224';
+
 	if ( !empty( $_GET['page'] ) && strpos( $_GET['page'], 'bp-profile-setup' ) !== false ) {
-		if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG )
+		if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
 			wp_enqueue_style( 'xprofile-admin-css', BP_PLUGIN_URL . 'bp-xprofile/admin/css/admin.dev.css', array(), $version );
-		else
+		} else {
 			wp_enqueue_style( 'xprofile-admin-css', BP_PLUGIN_URL . 'bp-xprofile/admin/css/admin.css',     array(), $version );
+		}
 	}
 }
 add_action( bp_core_admin_hook(), 'xprofile_add_admin_css' );
 
+/**
+ * Enqueue the jQuery libraries for handling drag/drop/sort
+ *
+ * @since BuddyPres (1.5)
+ */
 function xprofile_add_admin_js() {
 	if ( !empty( $_GET['page'] ) && strpos( $_GET['page'], 'bp-profile-setup' ) !== false ) {
 		wp_enqueue_script( 'jquery-ui-core'      );
@@ -22,12 +35,15 @@ function xprofile_add_admin_js() {
 		wp_enqueue_script( 'jquery-ui-droppable' );
 		wp_enqueue_script( 'jquery-ui-sortable'  );
 
-		$version = '20111212';
-		if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG )
+		$version = '20111224';
+
+		if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
 			wp_enqueue_script( 'xprofile-admin-js', BP_PLUGIN_URL . 'bp-xprofile/admin/js/admin.dev.js', array( 'jquery', 'jquery-ui-sortable' ), $version );
-		else
+		} else {
 			wp_enqueue_script( 'xprofile-admin-js', BP_PLUGIN_URL . 'bp-xprofile/admin/js/admin.js',     array( 'jquery', 'jquery-ui-sortable' ), $version );
+		}
 	}
 }
 add_action( bp_core_admin_hook(), 'xprofile_add_admin_js', 1 );
+
 ?>
