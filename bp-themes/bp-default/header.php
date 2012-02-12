198d93996db18fe9fe1482f390b99b60ddae8534
John James Jacoby <johnjamesjacoby@me.com>
2012-02-12 00:34:04 +0000

    Tweak responsive styling of bp-default. Fixes #3606. Props kamatosed.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@5733 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/header.php
+++ bp-themes/bp-default/header.php
@@ -3,6 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 	<head profile="http://gmpg.org/xfn/11">
 		<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ) ?>; charset=<?php bloginfo( 'charset' ) ?>" />
+		<?php if ( current_theme_supports( 'bp-default-responsive' ) ) : ?><meta name="viewport" content="width=device-width, initial-scale=1.0"><?php endif; ?>
 		<title><?php wp_title( '|', true, 'right' ); bloginfo( 'name' ); ?></title>
 
 		<?php do_action( 'bp_head' ) ?>
