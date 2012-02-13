8693c4207519ec5d15ae4e6860d3ee5321619420
John James Jacoby <johnjamesjacoby@me.com>
2012-02-13 07:28:31 +0000

    Use get_page_by_path() in bp_core_load_template() to properly check if page exists at uri offset. Fix #4000.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@5760 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-core/bp-core-catchuri.php
+++ bp-core/bp-core-catchuri.php
@@ -342,16 +342,15 @@ function bp_core_enable_root_profiles() {
  * @return false|int The user ID of the matched user, or false.
  */
 function bp_core_load_template( $templates ) {
-	global $post, $bp, $wpdb, $wp_query;
+	global $post, $bp, $wp_query;
 
-	// Bail if there is no offset
-	if ( empty( $bp->unfiltered_uri[$bp->unfiltered_uri_offset] ) )
-		return false;
-
-	// Determine if the root object WP page exists for this request
-	// (@todo: use reliable API function (get_page_by_path() doesn't work?)
-	if ( !$wpdb->get_var( $wpdb->prepare( "SELECT ID FROM {$wpdb->posts} WHERE post_name = %s", $bp->unfiltered_uri[$bp->unfiltered_uri_offset] ) ) )
-		return false;
+	// Bail if there is a uri offset but not a page in its place.
+	// * Possibly root profiles are enabled
+ 	if ( !empty( $bp->unfiltered_uri_offset ) ) {		
+		if ( ! get_page_by_path( $bp->unfiltered_uri[$bp->unfiltered_uri_offset] ) ) {
+			return false;
+		}
+	}
 
 	// Set the root object as the current wp_query-ied item
 	$object_id = 0;
