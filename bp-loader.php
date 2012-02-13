8453b9bc817dd988c23ae0f55eb6023a41660c17
John James Jacoby <johnjamesjacoby@me.com>
2012-02-13 08:47:36 +0000

    Bump bump bump bump it up!
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@5766 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-loader.php
+++ bp-loader.php
@@ -52,7 +52,7 @@ class BuddyPress {
 	/**
 	 * @var string BuddyPress version
 	 */
-	public $version = '1.6-alpha-5752';
+	public $version = '1.6-alpha-5766';
 
 	/**
 	 * @var int Database version of current BuddyPress files
