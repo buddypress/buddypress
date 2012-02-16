0ac9176b500072ec4f44f6c489637922319d62cc
Paul Gibbs <djpaul@gmail.com>
2012-02-16 22:36:14 +0000

    Don't allow direct url access to new activity caps file. See #4015
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@5799 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-xprofile/bp-xprofile-caps.php
+++ bp-xprofile/bp-xprofile-caps.php
@@ -7,6 +7,9 @@
  * @since 1.6
  */
 
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
 /**
  * Maps XProfile caps to built in WordPress caps
  *
@@ -26,17 +29,17 @@ function bp_xprofile_map_meta_caps( $caps, $cap, $user_id, $args ) {
 	switch ( $cap ) {
 		case 'bp_xprofile_change_field_visibility' :
 			$caps = array( 'read' );
-			
+
 			// You may pass args manually: $field_id, $profile_user_id
-			$field_id 	 = isset( $args[0] ) ? (int)$args[0] : bp_get_the_profile_field_id();
+			$field_id        = isset( $args[0] ) ? (int)$args[0] : bp_get_the_profile_field_id();
 			$profile_user_id = isset( $args[1] ) ? (int)$args[1] : bp_displayed_user_id();
-			
+
 			// Visibility on the fullname field is not editable
 			if ( 1 == $field_id ) {
 				$caps[] = 'do_not_allow';
 				break;
 			}
-			
+
 			// Has the admin disabled visibility modification for this field?
 			if ( 'disabled' == bp_xprofile_get_meta( $field_id, 'field', 'allow_custom_visibility' ) ) {
 				$caps[] = 'do_not_allow';
@@ -52,9 +55,8 @@ function bp_xprofile_map_meta_caps( $caps, $cap, $user_id, $args ) {
 
 			break;
 	}
-	
+
 	return apply_filters( 'bp_xprofile_map_meta_caps', $caps, $cap, $user_id, $args );
 }
 add_filter( 'bp_map_meta_caps', 'bp_xprofile_map_meta_caps', 10, 4 );
-
 ?>
