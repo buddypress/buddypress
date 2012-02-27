b9ddabcf530608bcbd778571c02b9f6c9c08c125
Bot (Assets) <nobody@localhost>
2012-02-27 07:31:10 +0000

    Compress scripts/styles: 1.6-alpha-5854.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@5854 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-loader.php
+++ bp-loader.php
@@ -52,7 +52,7 @@ class BuddyPress {
 	/**
 	 * @var string BuddyPress version
 	 */
-	public $version = '1.6-alpha-5784';
+	public $version = '1.6-alpha-5854';
 
 	/**
 	 * @var int Database version of current BuddyPress files
