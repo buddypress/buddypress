a230f711d6d8d48c67ad7ac8ee88b160796cd190
Paul Gibbs <djpaul@gmail.com>
2012-05-15 09:20:53 +0000

    Correct AJAX Friends pagination on the member profile page; don't return all members, only show your friends. Fixes #4165
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@6040 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/members/single/friends.php
+++ bp-themes/bp-default/members/single/friends.php
@@ -17,8 +17,8 @@
 
 			<li id="members-order-select" class="last filter">
 
-				<label for="members-all"><?php _e( 'Order By:', 'buddypress' ); ?></label>
-				<select id="members-all">
+				<label for="members-friends"><?php _e( 'Order By:', 'buddypress' ); ?></label>
+				<select id="members-friends">
 					<option value="active"><?php _e( 'Last Active', 'buddypress' ); ?></option>
 					<option value="newest"><?php _e( 'Newest Registered', 'buddypress' ); ?></option>
 					<option value="alphabetical"><?php _e( 'Alphabetical', 'buddypress' ); ?></option>
