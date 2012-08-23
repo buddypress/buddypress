45a6e42e2c53fe2faedab4fa160c6356979eeca5
Boone B Gorges <boonebgorges@gmail.com>
2012-08-23 17:17:21 +0000

    Fire widget AJAX handlers on wp_ajax_nopriv_ actions, in addition to wp_ajax_
    This ensures that the group and member widget AJAX continues to work for
    logged-out users when ajaxurl points to admin-ajax.php.
    
    Fixes #4464
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@6262 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-groups/bp-groups-widgets.php
+++ bp-groups/bp-groups-widgets.php
@@ -205,5 +205,6 @@ function groups_ajax_widget_groups_list() {
 
 }
 add_action( 'wp_ajax_widget_groups_list', 'groups_ajax_widget_groups_list' );
+add_action( 'wp_ajax_nopriv_widget_groups_list', 'groups_ajax_widget_groups_list' );
 
 ?>
