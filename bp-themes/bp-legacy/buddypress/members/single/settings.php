58b4c6d593df9846443feeb277e278ca6df3a304
John James Jacoby <johnjamesjacoby@me.com>
2012-09-07 14:20:06 +0000

    Legacy theme Settings:
    * Fix for capabilities screen.
    * Put template part loader in a switch, to prevent calling bp_is_current_action() repeatedly.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@6316 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-legacy/buddypress/members/single/settings.php
+++ bp-themes/bp-legacy/buddypress/members/single/settings.php
@@ -21,18 +21,20 @@
 
 <?php
 
-if ( bp_is_current_action( 'notifications' ) ) :
-	 bp_get_template_part( 'members/single/settings/notifications' );
-
-elseif ( bp_is_current_action( 'delete-account' ) ) :
-	 bp_get_template_part( 'members/single/settings/delete-account' );
-
-elseif ( bp_is_current_action( 'general' ) ) :
-	bp_get_template_part( 'members/single/settings/general' );
-
-else :
-	bp_get_template_part( 'members/single/plugins' );
-
-endif;
-
-?>
+switch ( bp_current_action() ) :
+	case 'notifications'  :
+		bp_get_template_part( 'members/single/settings/notifications'  );
+		break;
+	case 'capabilities'   :
+		bp_get_template_part( 'members/single/settings/capabilities'   );
+		break;
+	case 'delete-account' :
+		bp_get_template_part( 'members/single/settings/delete-account' );
+		break;
+	case 'general'        :
+		bp_get_template_part( 'members/single/settings/general'        );
+		break;
+	default:
+		bp_get_template_part( 'members/single/plugins'                 );
+		break;
+endswitch;;
