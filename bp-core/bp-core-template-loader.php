0dac12c1886b5c0b78d64d5a336e179e1a7ee276
John James Jacoby <johnjamesjacoby@me.com>
2012-09-11 18:45:56 +0000

    Theme Compat:
    * Use theme directory functions instead of constants in bp_locate_template().
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@6329 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-core/bp-core-template-loader.php
+++ bp-core/bp-core-template-loader.php
@@ -72,20 +72,23 @@ function bp_locate_template( $template_names, $load = false, $require_once = tru
 
 		// Trim off any slashes from the template name
 		$template_name = ltrim( $template_name, '/' );
+		$child_theme    = get_stylesheet_directory();
+		$parent_theme   = get_template_directory();
+		$fallback_theme = bp_get_theme_compat_dir();
 
 		// Check child theme first
-		if ( file_exists( trailingslashit( STYLESHEETPATH ) . $template_name ) ) {
-			$located = trailingslashit( STYLESHEETPATH ) . $template_name;
+		if ( file_exists( trailingslashit( $child_theme ) . $template_name ) ) {
+			$located = trailingslashit( $child_theme ) . $template_name;
 			break;
 
 		// Check parent theme next
-		} elseif ( file_exists( trailingslashit( TEMPLATEPATH ) . $template_name ) ) {
-			$located = trailingslashit( TEMPLATEPATH ) . $template_name;
+		} elseif ( file_exists( trailingslashit( $parent_theme ) . $template_name ) ) {
+			$located = trailingslashit( $child_theme ) . $template_name;
 			break;
 
 		// Check theme compatibility last
-		} elseif ( file_exists( trailingslashit( bp_get_theme_compat_dir() ) . $template_name ) ) {
-			$located = trailingslashit( bp_get_theme_compat_dir() ) . $template_name;
+		} elseif ( file_exists( trailingslashit( $fallback_theme ) . $template_name ) ) {
+			$located = trailingslashit( $fallback_theme ) . $template_name;
 			break;
 		}
 	}
