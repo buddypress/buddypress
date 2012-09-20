102044841ca9343e6936d9ecc2339d99ed691bf5
r-a-y <r-a-y@gmx.com>
2012-09-20 23:01:52 +0000

    Theme Compat:
    * Remove duplicate member header in /members/single/plugins.php
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@6346 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-legacy/buddypress/members/single/plugins.php
+++ bp-themes/bp-legacy/buddypress/members/single/plugins.php
@@ -1,36 +1,22 @@
-<div id="buddypress">
+<?php
+/**
+ * BuddyPress - Users Plugins Template
+ *
+ * 3rd-party plugins should use this template to easily add template
+ * support to their plugins for the members component.
+ *
+ * @package BuddyPress
+ * @subpackage bp-legacy
+ */
+?>
 
-	<?php do_action( 'bp_before_member_plugin_template' ); ?>
-
-	<div id="item-header">
-
-		<?php bp_get_template_part( 'members/single/member-header' ) ?>
-
-	</div><!-- #item-header -->
-
-	<div id="item-nav">
-		<div class="item-list-tabs no-ajax" id="object-nav" role="navigation">
-			<ul>
-
-				<?php bp_get_displayed_user_nav(); ?>
-
-				<?php do_action( 'bp_member_options_nav' ); ?>
-
-			</ul>
-		</div>
-	</div><!-- #item-nav -->
-
-	<div id="item-body" role="main">
-
-		<?php do_action( 'bp_before_member_body' ); ?>
+		<?php do_action( 'bp_before_member_plugin_template' ); ?>
 
 		<div class="item-list-tabs no-ajax" id="subnav">
 			<ul>
-
 				<?php bp_get_options_nav(); ?>
 
 				<?php do_action( 'bp_member_plugin_options_nav' ); ?>
-
 			</ul>
 		</div><!-- .item-list-tabs -->
 
@@ -38,10 +24,4 @@
 
 		<?php do_action( 'bp_template_content' ); ?>
 
-		<?php do_action( 'bp_after_member_body' ); ?>
-
-	</div><!-- #item-body -->
-
-	<?php do_action( 'bp_after_member_plugin_template' ); ?>
-
-</div><!-- #buddypress -->
+		<?php do_action( 'bp_after_member_plugin_template' ); ?>
