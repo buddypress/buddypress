0ca9ef926b1b99f67c4fd56eeb4ee67c190d696b
John James Jacoby <johnjamesjacoby@me.com>
2012-10-22 08:03:18 +0000

    Use bp_locate_template() in 1.7 deprecated functions.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@6450 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-core/deprecated/1.7.php
+++ bp-core/deprecated/1.7.php
@@ -13,12 +13,12 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 function xprofile_get_profile() {
 	_deprecated_function( __FUNCTION__, '1.7' );
-	locate_template( array( 'profile/profile-loop.php'), true );
+	bp_locate_template( array( 'profile/profile-loop.php' ), true );
 }
 
 function bp_get_profile_header() {
 	_deprecated_function( __FUNCTION__, '1.7' );
-	locate_template( array( '/profile/profile-header.php' ), true );
+	bp_locate_template( array( 'profile/profile-header.php' ), true );
 }
 
 function bp_exists( $component_name ) {
@@ -31,5 +31,5 @@ function bp_exists( $component_name ) {
 
 function bp_get_plugin_sidebar() {
 	_deprecated_function( __FUNCTION__, '1.7' );
-	locate_template( array( 'plugin-sidebar.php' ), true );
+	bp_locate_template( array( 'plugin-sidebar.php' ), true );
 }
