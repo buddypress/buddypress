64460c17bece3b8a6bc37e72818897820bee0fb0
John James Jacoby <johnjamesjacoby@me.com>
2012-11-13 06:38:14 +0000

    Theme Compat:
    * Remove duplicate code from bp-legacy/buddypress/groups/single/plugins.php.
    * Improves theme compat with group components.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@6513 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-templates/bp-legacy/buddypress/groups/single/plugins.php
+++ bp-templates/bp-legacy/buddypress/groups/single/plugins.php
@@ -1,35 +1,13 @@
-<div id="buddypress">
+<?php do_action( 'bp_before_group_plugin_template' ); ?>
 
-	<?php do_action( 'bp_before_group_plugin_template' ); ?>
+<div id="item-body">
 
-	<div id="item-header">
+	<?php do_action( 'bp_before_group_body' ); ?>
 
-		<?php bp_get_template_part( 'groups/single/group-header' ); ?>
+	<?php do_action( 'bp_template_content' ); ?>
 
-	</div><!-- #item-header -->
+	<?php do_action( 'bp_after_group_body' ); ?>
 
-	<div id="item-nav">
-		<div class="item-list-tabs no-ajax" id="object-nav" role="navigation">
-			<ul>
+</div><!-- #item-body -->
 
-				<?php bp_get_options_nav(); ?>
-
-				<?php do_action( 'bp_group_plugin_options_nav' ); ?>
-
-			</ul>
-		</div>
-	</div><!-- #item-nav -->
-
-	<div id="item-body">
-
-		<?php do_action( 'bp_before_group_body' ); ?>
-
-		<?php do_action( 'bp_template_content' ); ?>
-
-		<?php do_action( 'bp_after_group_body' ); ?>
-
-	</div><!-- #item-body -->
-
-	<?php do_action( 'bp_after_group_plugin_template' ); ?>
-
-</div><!-- #buddypress -->
+<?php do_action( 'bp_after_group_plugin_template' ); ?>
