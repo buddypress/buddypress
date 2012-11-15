1df20e1d47fb5533ef78ea1ea29314a2b498e882
Paul Gibbs <djpaul@gmail.com>
2012-11-15 13:39:14 +0000

    Add new Activity template loop function to get the parent activity's user ID. Fixes #4663
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@6523 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-activity/bp-activity-template.php
+++ bp-activity/bp-activity-template.php
@@ -1380,6 +1380,35 @@ function bp_activity_parent_content( $args = '' ) {
 	}
 
 /**
+ * Output the parent activity's user ID
+ *
+ * @since BuddyPress (1.7)
+ */
+function bp_activity_parent_user_id() {
+	echo bp_get_activity_parent_user_id();
+}
+
+	/**
+	 * Return the parent activity's user ID
+	 *
+	 * @global BP_Activity_Template $activities_template
+	 * @return bool|int False if parent activity can't be found, otherwise returns the parent activity's user ID
+	 * @since BuddyPress (1.7)
+	 */
+	function bp_get_activity_parent_user_id() {
+		global $activities_template;
+
+		$retval = false;
+
+		// Get the user ID of the parent activity
+		$parent_id = $activities_template->activity->item_id;
+		if ( $parent_id && ! empty( $activities_template->activity_parents[$parent_id] ) )
+			$retval = $activities_template->activity_parents[$parent_id]->user_id;
+
+		return apply_filters( 'bp_get_activity_parent_user_id', $retval );
+	}
+
+/**
  * Output whether or not the current activity is in a current user's favorites
  *
  * @since BuddyPress (1.2)
