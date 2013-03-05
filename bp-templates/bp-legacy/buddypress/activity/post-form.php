93970bcdcefee15483aa3bb4b84b5b7dccfaa9fe
Boone B Gorges <boonebgorges@gmail.com>
2013-03-05 14:13:30 +0000

    Adds "What's New" text to activity posting interface in theme compat
    Fixes #4864
    
    Props mercime
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@6840 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-templates/bp-legacy/buddypress/activity/post-form.php
+++ bp-templates/bp-legacy/buddypress/activity/post-form.php
@@ -18,6 +18,12 @@
 			<?php bp_loggedin_user_avatar( 'width=' . bp_core_avatar_thumb_width() . '&height=' . bp_core_avatar_thumb_height() ); ?>
 		</a>
 	</div>
+	
+	<p class="activity-greeting"><?php if ( bp_is_group() )
+		printf( __( "What's new in %s, %s?", 'buddypress' ), bp_get_group_name(), bp_get_user_firstname() );
+	else
+		printf( __( "What's new, %s?", 'buddypress' ), bp_get_user_firstname() );
+	?></p>
 
 	<div id="whats-new-content">
 		<div id="whats-new-textarea">
