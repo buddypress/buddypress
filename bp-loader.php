79109b93dcfeb4dadef63a993e97e5072257fdd3
Boone B Gorges <boonebgorges@gmail.com>
2013-04-08 19:19:44 +0000

    Bumping trunk version to 1.8-bleeding
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@6902 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-loader.php
+++ bp-loader.php
@@ -16,7 +16,7 @@
  * Description: Social networking in a box. Build a social network for your company, school, sports team or niche community all based on the power and flexibility of WordPress.
  * Author:      The BuddyPress Community
  * Author URI:  http://buddypress.org/community/members/
- * Version:     1.7
+ * Version:     1.8-bleeding
  * Text Domain: buddypress
  * Domain Path: /bp-languages/
  * License:     GPLv2 or later (license.txt)
@@ -207,7 +207,7 @@ class BuddyPress {
 
 	/**
 	 * Bootstrap constants
-	 * 
+	 *
 	 * @since BuddyPress (1.6)
 	 *
 	 * @uses is_multisite()
@@ -278,7 +278,7 @@ class BuddyPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '1.7';
+		$this->version    = '1.8-bleeding';
 		$this->db_version = 6080;
 
 		/** Loading ***********************************************************/
