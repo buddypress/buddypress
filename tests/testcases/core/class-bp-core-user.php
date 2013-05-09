a39cc8ab1749a47d1992c86daaa5948e4f334236
Boone B Gorges <boonebgorges@gmail.com>
2013-05-09 13:19:25 +0000

    More int-casting in test results
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@7035 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- tests/testcases/core/class-bp-core-user.php
+++ tests/testcases/core/class-bp-core-user.php
@@ -26,7 +26,7 @@ class BP_Tests_BP_Core_User_TestCases extends BP_UnitTestCase {
 		$exclude_qs = $u1 . ',junkstring,' . $u3;
 
 		$users = BP_Core_User::get_users( 'active', 0, 1, 0, false, false, true, $exclude_qs );
-		$user_ids = wp_list_pluck( $users['users'], 'id' );
+		$user_ids = wp_parse_id_list( wp_list_pluck( $users['users'], 'id' ) );
 
 		$this->assertEquals( array( $u2 ), $user_ids );
 	}
