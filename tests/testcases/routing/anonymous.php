f77e26450a0395697fb225aa8a2cf6987139116d
Boone B Gorges <boonebgorges@gmail.com>
2013-05-09 13:19:35 +0000

    In anonymous routing tests, ensure that user is logged out
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@7037 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- tests/testcases/routing/anonymous.php
+++ tests/testcases/routing/anonymous.php
@@ -3,6 +3,19 @@
  * @group routing
  */
 class BP_Tests_Routing_Anonymous extends BP_UnitTestCase {
+	protected $old_current_user = 0;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->old_current_user = get_current_user_id();
+		$this->set_current_user( 0 );
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+		$this->set_current_user( $this->old_current_user );
+	}
 
 	function test_wordpress_page() {
 		$this->go_to( '/' );
