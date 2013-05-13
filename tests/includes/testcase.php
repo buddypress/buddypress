929b1f3fcc1216d2cab5d8da44ba8b9ad1a302dd
Boone B Gorges <boonebgorges@gmail.com>
2013-05-13 14:25:16 +0000

    Adds missing declaration from previous commit
    See #4948
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@7061 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- tests/includes/testcase.php
+++ tests/includes/testcase.php
@@ -32,6 +32,7 @@ class BP_UnitTestCase extends WP_UnitTestCase {
 		buddypress()->current_user          = new stdClass();
 		buddypress()->displayed_user        = new stdClass();
 		buddypress()->loggedin_user         = new stdClass();
+		buddypress()->avatar                = new stdClass();
 
 		parent::clean_up_global_scope();
 	}
