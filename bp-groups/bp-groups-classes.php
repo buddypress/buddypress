d2644efecd39e43335d993f61facc8bf1a2abfe5
Boone B Gorges <boonebgorges@gmail.com>
2013-05-15 12:03:21 +0000

    Fixes abandoned extract()ed variable in BP_Groups_Group::get()
    Fixes #5002
    
    Props ericlewis
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@7072 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-groups/bp-groups-classes.php
+++ bp-groups/bp-groups-classes.php
@@ -442,7 +442,7 @@ class BP_Groups_Group {
 		}
 
 		if ( ! empty( $r['user_id'] ) ) {
-			$total_sql['where'][] = "m.group_id = g.id AND m.user_id = {$user_id} AND m.is_confirmed = 1 AND m.is_banned = 0";
+			$total_sql['where'][] = "m.group_id = g.id AND m.user_id = {$r['user_id']} AND m.is_confirmed = 1 AND m.is_banned = 0";
 		}
 
 		// Already escaped in the paginated results block
