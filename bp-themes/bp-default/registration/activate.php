52c09bc966c81069152d98e687f235e30c5ef585
Boone B Gorges <boonebgorges@gmail.com>
2013-06-06 23:58:39 +0000

    Add a "log in" link to Activation page
    Fixes #4963
    
    Props themightymo, merty
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@7176 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/registration/activate.php
+++ bp-themes/bp-default/registration/activate.php
@@ -22,7 +22,7 @@
 				<?php if ( isset( $_GET['e'] ) ) : ?>
 					<p><?php _e( 'Your account was activated successfully! Your account details have been sent to you in a separate email.', 'buddypress' ); ?></p>
 				<?php else : ?>
-					<p><?php _e( 'Your account was activated successfully! You can now log in with the username and password you provided when you signed up.', 'buddypress' ); ?></p>
+					<p><?php printf( __( 'Your account was activated successfully! You can now <a href="%s">log in</a> with the username and password you provided when you signed up.', 'buddypress' ), wp_login_url( bp_get_root_domain() ) ); ?></p>
 				<?php endif; ?>
 
 			<?php else : ?>
