7d60efce070af8103ba25386faf2430f11be62fe
Boone B Gorges <boonebgorges@gmail.com>
2013-06-21 12:31:55 +0000

    Adds missing .php extension when including request-membership template in bp-default
    This extension had been accidentally removed in r6517, with the result that
    the request-membership template was not loaded in bp-default.
    
    Fixes #5062
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@7230 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-themes/bp-default/groups/single/home.php
+++ bp-themes/bp-default/groups/single/home.php
@@ -80,10 +80,9 @@
 
 				// Group is not visible
 				elseif ( ! bp_group_is_visible() ) :
-
 					// Membership request
 					if ( bp_is_group_membership_request() ) :
-						locate_template( array( 'groups/single/request-membership' ), true );
+						locate_template( array( 'groups/single/request-membership.php' ), true );
 
 					// The group is not visible, show the status message
 					else :
