fb089fe0925b5774a686f8149ac8ca37c28b966b
Boone B Gorges <boonebgorges@gmail.com>
2013-07-17 16:15:05 +0000

    Bumps 1.8 branch to 1.8.1-alpha
    git-svn-id: https://buddypress.svn.wordpress.org/branches/1.8@7284 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-loader.php
+++ bp-loader.php
@@ -16,7 +16,7 @@
  * Description: Social networking in a box. Build a social network for your company, school, sports team or niche community all based on the power and flexibility of WordPress.
  * Author:      The BuddyPress Community
  * Author URI:  http://buddypress.org/community/members/
- * Version:     1.8
+ * Version:     1.8.1-alpha
  * Text Domain: buddypress
  * Domain Path: /bp-languages/
  * License:     GPLv2 or later (license.txt)
@@ -294,7 +294,7 @@ class BuddyPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '1.8';
+		$this->version    = '1.8.1-alpha';
 		$this->db_version = 6080;
 
 		/** Loading ***********************************************************/
