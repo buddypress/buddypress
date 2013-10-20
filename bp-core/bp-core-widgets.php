e038045fdc2d444db0d83ca4f19c4588a4e2ffeb
Boone B Gorges <boonebgorges@gmail.com>
2013-10-20 18:28:48 +0000

    Introduce BuddyPress Login Widget
    This sidebar widget shows a set of login fields to logged-out users, along with
    a link to the Register page (if registration is enabled). Logged-in users will
    see their avatar, a link to their profile, and a Log Out link (as in the old
    bp-default sidebar). This is particularly important now that BP can be used
    with any theme, even themes that do not have obvious login mechanisms on the
    front end.
    
    Borrowed in part from bbPress, and in part from the BuddyPress Default theme.
    
    Props modemlooper for an initial patch.
    
    Fixes #1474
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@7450 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-core/bp-core-widgets.php
+++ bp-core/bp-core-widgets.php
@@ -11,11 +11,133 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 /* Register widgets for the core component */
 function bp_core_register_widgets() {
+	add_action('widgets_init', create_function('', 'return register_widget("BP_Core_Login_Widget");') );
 	add_action('widgets_init', create_function('', 'return register_widget("BP_Core_Members_Widget");') );
 	add_action('widgets_init', create_function('', 'return register_widget("BP_Core_Whos_Online_Widget");') );
 	add_action('widgets_init', create_function('', 'return register_widget("BP_Core_Recently_Active_Widget");') );
 }
 add_action( 'bp_register_widgets', 'bp_core_register_widgets' );
+
+/**
+ * BuddyPress Login Widget
+ *
+ * @since BuddyPress (1.9.0)
+ */
+class BP_Core_Login_Widget extends WP_Widget {
+
+	/**
+	 * Constructor method.
+	 */
+	public function __construct() {
+		parent::__construct(
+			false,
+			_x( '(BuddyPress) Log In', 'Title of the login widget', 'buddypress' ),
+			array(
+				'description' => __( 'Show a Log In form to logged-out visitors, and a Log Out link to those who are logged in.', 'buddypress' ),
+				'classname' => 'widget_bp_core_login_widget buddypress widget',
+			)
+		);
+	}
+
+	/**
+	 * Display the login widget.
+	 *
+	 * @see WP_Widget::widget() for description of parameters.
+	 *
+	 * @param array $args Widget arguments.
+	 * @param array $instance Widget settings, as saved by the user.
+	 */
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
+
+		echo $args['before_widget'];
+
+		echo $args['before_title'] . esc_html( $title ) . $args['after_title']; ?>
+
+		<?php if ( is_user_logged_in() ) : ?>
+
+			<?php do_action( 'bp_before_login_widget_loggedin' ); ?>
+
+			<div class="bp-login-widget-user-avatar">
+				<a href="<?php echo bp_loggedin_user_domain(); ?>">
+					<?php bp_loggedin_user_avatar( 'type=thumb&width=50&height=50' ); ?>
+				</a>
+			</div>
+
+			<div class="bp-login-widget-user-links">
+				<div class="bp-login-widget-user-link"><?php echo bp_core_get_userlink( bp_loggedin_user_id() ); ?></div>
+				<div class="bp-login-widget-user-logout"><a class="logout" href="<?php echo wp_logout_url( wp_guess_url() ); ?>"><?php _e( 'Log Out', 'buddypress' ); ?></a></div>
+			</div>
+
+			<?php do_action( 'bp_after_login_widget_loggedin' ); ?>
+
+		<?php else : ?>
+
+			<?php do_action( 'bp_before_login_widget_loggedout' ); ?>
+
+			<form name="bp-login-form" id="bp-login-widget-form" class="standard-form" action="<?php echo site_url( 'wp-login.php', 'login_post' ); ?>" method="post">
+				<label for="bp-login-widget-user-login"><?php _e( 'Username', 'buddypress' ); ?></label>
+				<input type="text" name="log" id="bp-login-widget-user-login" class="input" value="" />
+
+				<label for="bp-login-widget-user-pass"><?php _e( 'Password', 'buddypress' ); ?></label>
+				<input type="password" name="pwd" id="bp-login-widget-user-pass" class="input" value=""  />
+
+				<div class="forgetmenot"><label><input name="rememberme" type="checkbox" id="bp-login-widget-rememberme" value="forever" /> <?php _e( 'Remember Me', 'buddypress' ); ?></label></div>
+
+				<input type="submit" name="wp-submit" id="bp-login-widget-submit" value="<?php _e( 'Log In', 'buddypress' ); ?>" />
+
+				<?php if ( bp_get_signup_allowed() ) : ?>
+
+					<span class="bp-login-widget-register-link"><?php printf( __( '<a href="%s" title="Register for a new account">Register</a>', 'buddypress' ), bp_get_signup_page() ); ?></span>
+
+				<?php endif; ?>
+
+				<input type="hidden" name="testcookie" value="1" />
+			</form>
+
+
+			<?php do_action( 'bp_after_login_widget_loggedout' ); ?>
+
+		<?php endif;
+
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Update the login widget options.
+	 *
+	 * @param array $new_instance The new instance options.
+	 * @param array $old_instance The old instance options.
+	 * @return array $instance The parsed options to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance             = $old_instance;
+		$instance['title']    = strip_tags( $new_instance['title'] );
+		$instance['register'] = esc_url_raw( $new_instance['register'] );
+		$instance['lostpass'] = esc_url_raw( $new_instance['lostpass'] );
+
+		return $instance;
+	}
+
+	/**
+	 * Output the login widget options form.
+	 *
+	 * @param $instance Settings for this widget.
+	 */
+	public function form( $instance = array() ) {
+
+		$settings = wp_parse_args( $instance, array(
+			'title' => '',
+		) ); ?>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'buddypress' ); ?>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $settings['title'] ); ?>" /></label>
+		</p>
+
+		<?php
+	}
+}
 
 /*** MEMBERS WIDGET *****************/
 
