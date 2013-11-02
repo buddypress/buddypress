cd02347f8c090a22652fe1905f72f2efab5ce0ce
Boone B Gorges <boonebgorges@gmail.com>
2013-11-02 18:38:39 +0000

    Declare test stub methods as static to avoid E_STRICT notices
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@7504 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- tests/testcases/groups/class-bp-groups-group.php
+++ tests/testcases/groups/class-bp-groups-group.php
@@ -643,11 +643,11 @@ class BP_Tests_BP_Groups_Group_TestCases extends BP_UnitTestCase {
  * Stub class for accessing protected methods
  */
 class _BP_Groups_Group extends BP_Groups_Group {
-	public function _convert_type_to_order_orderby( $type ) {
+	static public function _convert_type_to_order_orderby( $type ) {
 		return self::convert_type_to_order_orderby( $type );
 	}
 
-	public function _convert_orderby_to_order_by_term( $term ) {
+	static public function _convert_orderby_to_order_by_term( $term ) {
 		return self::convert_orderby_to_order_by_term( $term );
 	}
 }
