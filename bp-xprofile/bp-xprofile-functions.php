b0ef465953bea97e1cf975099f8718b8112bdb31
Boone B Gorges <boonebgorges@gmail.com>
2013-11-29 18:50:43 +0000

    Allow admin-set field visibility default to override when user hasn't set a value yet
    Fixes #5262
    
    Props dcavins
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@7609 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-xprofile/bp-xprofile-functions.php
+++ bp-xprofile/bp-xprofile-functions.php
@@ -752,7 +752,7 @@ function bp_xprofile_get_fields_by_visibility_levels( $user_id, $levels = array(
 	foreach( (array) $default_visibility_levels as $d_field_id => $defaults ) {
 		// If the admin has forbidden custom visibility levels for this field, replace
 		// the user-provided setting with the default specified by the admin
-		if ( isset( $defaults['allow_custom'] ) && isset( $defaults['default'] ) && 'disabled' == $defaults['allow_custom'] && isset( $user_visibility_levels[$d_field_id] ) ) {
+		if ( isset( $defaults['allow_custom'] ) && isset( $defaults['default'] ) && 'disabled' == $defaults['allow_custom'] ) {
 			$user_visibility_levels[$d_field_id] = $defaults['default'];
 		}
 	}
