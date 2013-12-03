7113db596b8284f483d32c714ab03dbcca75e8dc
Boone B Gorges <boonebgorges@gmail.com>
2013-12-03 00:59:36 +0000

    Bump version numbers to 1.9-beta2
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@7640 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-loader.php
+++ bp-loader.php
@@ -16,7 +16,7 @@
  * Description: Social networking in a box. Build a social network for your company, school, sports team or niche community all based on the power and flexibility of WordPress.
  * Author:      The BuddyPress Community
  * Author URI:  http://buddypress.org/community/members/
- * Version:     1.9-beta1
+ * Version:     1.9-beta2
  * Text Domain: buddypress
  * Domain Path: /bp-languages/
  * License:     GPLv2 or later (license.txt)
@@ -297,7 +297,7 @@ class BuddyPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '1.9-beta1';
+		$this->version    = '1.9-beta2';
 		$this->db_version = 7553;
 
 		/** Loading ***********************************************************/
