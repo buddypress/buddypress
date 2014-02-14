dc10da3119b81efb2002e7bac13c3f3563764bab
Boone B Gorges <boonebgorges@gmail.com>
2014-02-14 01:28:47 +0000

    Add tests for blogmeta functions.
    See #4551
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@7869 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

---
+++ tests/testcases/blogs/functions.php
@@ -0,0 +1,158 @@
+<?php
+
+/**
+ * @group blogs
+ */
+class BP_Tests_Blogs_Functions extends BP_UnitTestCase {
+	/**
+	 * @group blogmeta
+	 * @group bp_blogs_delete_blogmeta
+	 */
+	public function test_bp_blogs_delete_blogmeta_non_numeric_blog_id() {
+		$this->assertFalse( bp_blogs_delete_blogmeta( 'foo' ) );
+	}
+
+	/**
+	 * @group blogmeta
+	 * @group bp_blogs_delete_blogmeta
+	 */
+	public function test_bp_blogs_delete_blogmeta_illegal_characters() {
+		$this->assertTrue( bp_blogs_update_blogmeta( 1, 'foo', 'bar' ) );
+		$this->assertSame( 'bar', bp_blogs_get_blogmeta( 1, 'foo' ) );
+		$krazy_key = ' f!@#$%^o *(){}o?+';
+		$this->assertTrue( bp_blogs_delete_blogmeta( 1, $krazy_key ) );
+		$this->assertSame( '', bp_blogs_get_blogmeta( 1, 'foo' ) );
+	}
+
+	/**
+	 * @group blogmeta
+	 * @group bp_blogs_delete_blogmeta
+	 */
+	public function test_bp_blogs_delete_blogmeta_trim_meta_value() {
+		$this->assertTrue( bp_blogs_update_blogmeta( 1, 'foo', 'bar' ) );
+		$this->assertSame( 'bar', bp_blogs_get_blogmeta( 1, 'foo' ) );
+		$this->assertTrue( bp_blogs_delete_blogmeta( 1, 'foo', '   bar  ') );
+		$this->assertSame( '', bp_blogs_get_blogmeta( 1, 'foo' ) );
+	}
+
+	/**
+	 * @group blogmeta
+	 * @group bp_blogs_delete_blogmeta
+	 */
+	public function test_bp_blogs_delete_blogmeta_no_meta_key() {
+		bp_blogs_update_blogmeta( 1, 'foo', 'bar' );
+		bp_blogs_update_blogmeta( 1, 'foo2', 'bar2' );
+		$this->assertNotEmpty( bp_blogs_get_blogmeta( 1 ) );
+		$this->assertTrue( bp_blogs_delete_blogmeta( 1 ) );
+		$this->assertSame( array(), bp_blogs_get_blogmeta( 1 ) );
+	}
+
+	/**
+	 * @group blogmeta
+	 * @group bp_blogs_delete_blogmeta
+	 */
+	public function test_bp_blogs_delete_blogmeta_with_meta_value() {
+		bp_blogs_update_blogmeta( 1, 'foo', 'bar' );
+		$this->assertSame( 'bar', bp_blogs_get_blogmeta( 1, 'foo' ) );
+		bp_blogs_delete_blogmeta( 1, 'foo', 'baz' );
+		$this->assertSame( 'bar', bp_blogs_get_blogmeta( 1, 'foo' ) );
+		$this->assertTrue( bp_blogs_delete_blogmeta( 1, 'foo', 'bar' ) );
+		$this->assertSame( '', bp_blogs_get_blogmeta( 1, 'foo' ) );
+	}
+
+	/**
+	 * @group blogmeta
+	 * @group bp_blogs_get_blogmeta
+	 */
+	public function test_bp_blogs_get_blogmeta_empty_blog_id() {
+		$this->assertFalse( bp_blogs_get_blogmeta( 0 ) );
+		$this->assertFalse( bp_blogs_get_blogmeta( '' ) );
+	}
+
+	/**
+	 * @group blogmeta
+	 * @group bp_blogs_get_blogmeta
+	 */
+	public function test_bp_blogs_get_blogmeta_illegal_characters() {
+		bp_blogs_update_blogmeta( 1, 'foo', 'bar' );
+		$krazy_key = ' f!@#$%^o *(){}o?+';
+		$this->assertSame( 'bar', bp_blogs_get_blogmeta( 1, $krazy_key ) );
+	}
+
+	/**
+	 * @group blogmeta
+	 * @group bp_blogs_get_blogmeta
+	 */
+	public function test_bp_blogs_get_blogmeta_no_meta_key() {
+		bp_blogs_update_blogmeta( 1, 'foo', 'bar' );
+		bp_blogs_update_blogmeta( 1, 'foo2', 'bar2' );
+
+		$this->assertSame( array( 'bar', 'bar2', ), bp_blogs_get_blogmeta( 1 ) );
+	}
+
+	/**
+	 * @group blogmeta
+	 * @group bp_blogs_get_blogmeta
+	 */
+	public function test_bp_blogs_get_blogmeta_no_meta_key_empty() {
+		$this->assertSame( array(), bp_blogs_get_blogmeta( 1 ) );
+	}
+
+	/**
+	 * @group blogmeta
+	 * @group bp_blogs_update_blogmeta
+	 */
+	public function test_bp_blogs_update_blogmeta_non_numeric_blog_id() {
+		$this->assertFalse( bp_blogs_update_blogmeta( 'foo', 'foo', 'bar' ) );
+	}
+
+	/**
+	 * @group blogmeta
+	 * @group bp_blogs_update_blogmeta
+	 */
+	public function test_bp_blogs_update_blogmeta_illegal_characters() {
+		$krazy_key = ' f!@#$%^o *(){}o?+';
+		bp_blogs_update_blogmeta( 1, $krazy_key, 'bar' );
+		$this->assertSame( 'bar', bp_blogs_get_blogmeta( 1, 'foo' ) );
+	}
+
+	/**
+	 * @group blogmeta
+	 * @group bp_blogs_update_blogmeta
+	 */
+	public function test_bp_blogs_update_blogmeta_stripslashes() {
+		$slashed = 'This \"string\" is cool';
+		bp_blogs_update_blogmeta( 1, 'foo', $slashed );
+		$this->assertSame( 'This "string" is cool', bp_blogs_get_blogmeta( 1, 'foo' ) );
+	}
+
+	/**
+	 * @group blogmeta
+	 * @group bp_blogs_update_groupmeta
+	 */
+	public function test_bp_blogs_update_blogmeta_new() {
+		$this->assertTrue( bp_blogs_update_blogmeta( 1, 'foo', 'bar' ) );
+		$this->assertSame( 'bar', bp_blogs_get_blogmeta( 1, 'foo' ) );
+	}
+
+	/**
+	 * @group blogmeta
+	 * @group bp_blogs_update_groupmeta
+	 */
+	public function test_bp_blogs_update_blogmeta_existing() {
+		bp_blogs_update_blogmeta( 1, 'foo', 'bar' );
+		$this->assertSame( 'bar', bp_blogs_get_blogmeta( 1, 'foo' ) );
+		$this->assertTrue( bp_blogs_update_blogmeta( 1, 'foo', 'baz' ) );
+		$this->assertSame( 'baz', bp_blogs_get_blogmeta( 1, 'foo' ) );
+	}
+
+	/**
+	 * @group blogmeta
+	 * @group bp_blogs_update_groupmeta
+	 */
+	public function test_bp_blogs_update_blogmeta_existing_no_change() {
+		bp_blogs_update_blogmeta( 1, 'foo', 'bar' );
+		$this->assertSame( 'bar', bp_blogs_get_blogmeta( 1, 'foo' ) );
+		$this->assertFalse( bp_blogs_update_blogmeta( 1, 'foo', 'bar' ) );
+	}
+}
