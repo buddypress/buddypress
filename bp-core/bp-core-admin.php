cdde9edfc8a567d7b8f19bbad257441867eca623
Paul Gibbs <djpaul@gmail.com>
2014-04-04 11:48:29 +0000

    Update contributors list for 2.0 (to date)
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@8235 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-core/bp-core-admin.php
+++ bp-core/bp-core-admin.php
@@ -721,7 +721,8 @@ class BP_Admin {
 				<a href="https://profiles.wordpress.org/SlothLoveChunk/">SlothLoveChunk</a>,
 				<a href="https://profiles.wordpress.org/StijnDeWitt/">StijnDeWitt</a>,
 				<a href="https://profiles.wordpress.org/terraling/">terraling</a>,
-				<a href="https://profiles.wordpress.org/tw2113/">tw2113</a>
+				<a href="https://profiles.wordpress.org/tw2113/">tw2113</a>,
+				<a href="https://profiles.wordpress.org/vanillalounge/">vanillalounge</a>.
 			</p>
 
 			<?php if ( current_user_can( $this->capability ) ) :?>
