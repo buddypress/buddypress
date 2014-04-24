53cab594a3e6d88f1cfbc021097c4636a13dfa02
Boone B Gorges <boonebgorges@gmail.com>
2014-04-24 00:22:29 +0000

    Improve the migration tool that converts old non-multisite signups to post-2.0 system
    This refactoring involves a number of improvements:
    
    - Old signups are identified as those users with user_status=2, instead of
      those users with an 'activation_key' usermeta value. Use of the latter
      technique caused problems with upgrades when plugins had interfered in the
      normal BuddyPress registration flow in such a way as to retain the activation
      key on activated accounts. user_status=2 is a more reliable technique.
    - Querying by user_status=2 means that the get_users() query does not have to
      include all usermeta, which makes for a more performant query on large sites.
    - The migration routine has been broken out into a separate function
      bp_members_migrate_signups(), and unit tests have been written for it.
    - Activation keys are now regenerated for migrated users who did not have an
      existing key in usermeta for some reason.
    
    Fixes #5553
    
    Props imath
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@8315 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-members/bp-members-functions.php
+++ bp-members/bp-members-functions.php
@@ -1637,6 +1637,87 @@ function bp_core_activate_signup( $key ) {
 	return $user_id;
 }
 
+/**
+ * Migrate signups from pre-2.0 configuration to wp_signups.
+ *
+ * @since BuddyPress (2.0.1)
+ */
+function bp_members_migrate_signups() {
+	global $wpdb;
+
+	$status_2_ids = $wpdb->get_col( "SELECT ID FROM {$wpdb->users} WHERE user_status = '2'" );
+
+	if ( ! empty( $status_2_ids ) ) {
+		$signups = get_users( array(
+			'fields'  => array(
+				'ID',
+				'user_login',
+				'user_pass',
+				'user_registered',
+				'user_email',
+				'display_name',
+			),
+			'include' => $status_2_ids,
+		) );
+
+		// Fetch activation keys separately, to avoid the all_with_meta
+		// overhead
+		$status_2_ids_sql = implode( ',', $status_2_ids );
+		$ak_data = $wpdb->get_results( "SELECT user_id, meta_value FROM {$wpdb->usermeta} WHERE meta_key = 'activation_key' AND user_id IN ({$status_2_ids_sql})" );
+
+		// Rekey
+		$activation_keys = array();
+		foreach ( $ak_data as $ak_datum ) {
+			$activation_keys[ intval( $ak_datum->user_id ) ] = $ak_datum->meta_value;
+		}
+
+		unset( $status_2_ids_sql, $status_2_ids, $ak_data );
+
+		// Merge
+		foreach ( $signups as &$signup ) {
+			if ( isset( $activation_keys[ $signup->ID ] ) ) {
+				$signup->activation_key = $activation_keys[ $signup->ID ];
+			}
+		}
+
+		// Reset the signup var as we're using it to process the migration
+		unset( $signup );
+
+	} else {
+		return;
+	}
+
+	foreach ( $signups as $signup ) {
+		$meta = array();
+
+		// Rebuild the activation key, if missing
+		if ( empty( $signup->activation_key ) ) {
+			$signup->activation_key = wp_hash( $signup->ID );
+		}
+
+		if ( bp_is_active( 'xprofile' ) ) {
+			$meta['field_1'] = $signup->display_name;
+		}
+
+		$meta['password'] = $signup->user_pass;
+
+		$user_login = preg_replace( '/\s+/', '', sanitize_user( $signup->user_login, true ) );
+		$user_email = sanitize_email( $signup->user_email );
+
+		BP_Signup::add( array(
+			'user_login'     => $user_login,
+			'user_email'     => $user_email,
+			'registered'     => $signup->user_registered,
+			'activation_key' => $signup->activation_key,
+			'meta'           => $meta
+		) );
+
+		// Deleting these options will remove signups from users count
+		delete_user_option( $signup->ID, 'capabilities' );
+		delete_user_option( $signup->ID, 'user_level'   );
+	}
+}
+
 function bp_core_new_user_activity( $user ) {
 	if ( empty( $user ) || !bp_is_active( 'activity' ) )
 		return false;
