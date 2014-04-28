a7f92d8f966e482b99e2ea7d549cef73718b20f6
John James Jacoby <johnjamesjacoby@me.com>
2014-04-28 23:11:38 +0000

    Add unit test for comparing subsequent calls to `BP_Core_User::get_last_activity()` to compare cached results to uncached results. See #5590, #5597, to test logic introduced in r8047. (2.0 branch)
    git-svn-id: https://buddypress.svn.wordpress.org/branches/2.0@8327 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- tests/testcases/core/class-bp-core-user.php
+++ tests/testcases/core/class-bp-core-user.php
@@ -159,6 +159,27 @@ class BP_Tests_BP_Core_User_TestCases extends BP_UnitTestCase {
 	/**
 	 * @group last_activity
 	 */
+	public function test_get_last_activity_in_cache_single_user() {
+		$u    = $this->create_user();
+		$time = bp_core_current_time();
+
+		BP_Core_User::update_last_activity( $u, $time );
+
+		// Cache is set during user creation. Clear to reflect actual pageload
+		wp_cache_delete( $u, 'bp_last_activity' );
+
+		// prime cache & get uncached results
+		$a = BP_Core_User::get_last_activity( $u );
+
+		// get cached results
+		$b = BP_Core_User::get_last_activity( $u );
+
+		$this->assertSame( $a, $b );
+	}
+
+	/**
+	 * @group last_activity
+	 */
 	public function test_update_last_activity() {
 		$u = $this->create_user();
 		$time = bp_core_current_time();
