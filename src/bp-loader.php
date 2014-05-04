e78470d6a4219b7ab70f97c0f4f8b5581677bdda
Paul Gibbs <djpaul@gmail.com>
2014-05-04 14:33:16 +0000

    Core: fix unnecessary forward-slash in include path
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@8369 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-loader.php
+++ src/bp-loader.php
@@ -413,7 +413,7 @@ class BuddyPress {
 	private function includes() {
 
 		// Load the WP abstraction file so BuddyPress can run on all WordPress setups.
-		require( $this->plugin_dir . '/bp-core/bp-core-wpabstraction.php' );
+		require( $this->plugin_dir . 'bp-core/bp-core-wpabstraction.php' );
 
 		// Setup the versions (after we include multisite abstraction above)
 		$this->versions();
