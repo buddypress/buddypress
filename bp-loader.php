801a9a407c4828c1ab909cdcabe3b9680d8dd83b
Paul Gibbs <djpaul@gmail.com>
2014-05-04 13:33:51 +0000

    Grunt: add first pass at loader file for sites running `trunk`.
    This commit is part of a series that will re-organise BuddyPress trunk to use a Grunt build process. See #5160
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@8363 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

---
+++ bp-loader.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The BuddyPress Plugin
+ *
+ * BuddyPress is social networking software with a twist from the creators of WordPress.
+ *
+ * @package BuddyPress
+ * @subpackage Main
+ */
+
+/**
+ * Plugin Name: BuddyPress
+ * Plugin URI:  http://buddypress.org
+ * Description: Social networking in a box. Build a social network for your company, school, sports team or niche community all based on the power and flexibility of WordPress.
+ * Author:      The BuddyPress Community
+ * Author URI:  http://buddypress.org/community/members/
+ * Version:     2.1-alpha
+ * Text Domain: buddypress
+ * Domain Path: /bp-languages/
+ * License:     GPLv2 or later (license.txt)
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Assume you want to load from build
+$bp_loader = __DIR__ . '/build/bp-loader.php';
+
+// Load from source if no build exists
+if ( ! file_exists( $bp_loader ) || defined( 'BP_LOAD_SAUCE' ) ) {
+	$bp_loader = __DIR__ . '/src/bp-loader.php';
+}
+
+include( $bp_loader );
+unset( $bp_loader );
