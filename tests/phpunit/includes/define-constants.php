b9869fe5781df0942ad7e0ec540aff8b29b4b657
Boone B Gorges <boonebgorges@gmail.com>
2014-05-12 20:02:21 +0000

    Rework the way that the WP tests directory is inferred during test bootstrap, to support WP_DEVELOP_DIR
    This provides greater flexibility, and better compatibility with other projects.
    
    Fixes #5376
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@8415 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- tests/phpunit/includes/define-constants.php
+++ tests/phpunit/includes/define-constants.php
@@ -11,16 +11,26 @@ if ( ! defined( 'BP_TESTS_DIR' ) ) {
 }
 
 /**
- * In the pre-develop.svn WP development environment, an environmental bash
- * variable would be set to run PHP Unit tests. However, this has been done
- * away with in a post-develop.svn world. We'll still check if this variable
- * is set for backwards compat.
+ * Determine where the WP test suite lives. Three options are supported:
+ *
+ * - Define a WP_DEVELOP_DIR environment variable, which points to a checkout
+ *   of the develop.svn.wordpress.org repository (this is recommended)
+ * - Define a WP_TESTS_DIR environment variable, which points to a checkout of
+ *   WordPress test suite
+ * - Assume that we are inside of a develop.svn.wordpress.org setup, and walk
+ *   up the directory tree
  */
-if ( getenv( 'WP_TESTS_DIR' ) ) {
+if ( false !== getenv( 'WP_TESTS_DIR' ) ) {
 	define( 'WP_TESTS_DIR', getenv( 'WP_TESTS_DIR' ) );
 	define( 'WP_ROOT_DIR', WP_TESTS_DIR );
 } else {
-	define( 'WP_ROOT_DIR', dirname( dirname( dirname( dirname( dirname( dirname( dirname( __DIR__ ) ) ) ) ) ) ) );
+	// Support WP_DEVELOP_DIR, as used by some plugins
+	if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
+		define( 'WP_TESTS_DIR', getenv( 'WP_DEVELOP_DIR' ) );
+	} else {
+		define( 'WP_ROOT_DIR', dirname( dirname( dirname( dirname( dirname( dirname( dirname( __DIR__ ) ) ) ) ) ) ) );
+	}
+
 	define( 'WP_TESTS_DIR', WP_ROOT_DIR . '/tests/phpunit' );
 }
 
