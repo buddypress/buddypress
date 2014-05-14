ca846cc778271f5de80f6034ad7e5af40891448a
Boone B Gorges <boonebgorges@gmail.com>
2014-05-14 12:32:15 +0000

    Fix incorrect constant name when setting up phpunit constants
    Fixes #5376
    
    Props netweb
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@8418 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- tests/phpunit/includes/define-constants.php
+++ tests/phpunit/includes/define-constants.php
@@ -26,7 +26,7 @@ if ( false !== getenv( 'WP_TESTS_DIR' ) ) {
 } else {
 	// Support WP_DEVELOP_DIR, as used by some plugins
 	if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-		define( 'WP_TESTS_DIR', getenv( 'WP_DEVELOP_DIR' ) );
+		define( 'WP_ROOT_DIR', getenv( 'WP_DEVELOP_DIR' ) );
 	} else {
 		define( 'WP_ROOT_DIR', dirname( dirname( dirname( dirname( dirname( dirname( dirname( __DIR__ ) ) ) ) ) ) ) );
 	}
