e4d75f599dba098c4d51137ba74b4d92fd41aff7
Boone B Gorges <boonebgorges@gmail.com>
2014-06-04 18:24:12 +0000

    Clean up parsed meta_query clauses before performing preg_replaces() in BP_Groups_Group::get_meta_query_sql()
    Some weaknesses in parsing were exposed by upstream changes. See
    https://core.trac.wordpress.org/ticket/25538#comment:23.
    
    This changeset makes the parsing less brittle by relying less on the details of
    the line breaks introduced by WP into the query string.
    
    See #5682
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@8476 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-groups/bp-groups-classes.php
+++ src/bp-groups/bp-groups-classes.php
@@ -896,6 +896,10 @@ class BP_Groups_Group {
 				$sql_array['where'] = '';
 
 				$meta_query_where_clauses = explode( "\n", $meta_sql['where'] );
+
+				// Trim empties
+				$meta_query_where_clauses = array_values( array_filter( $meta_query_where_clauses ) );
+
 				foreach( $matches_b[1] as $key => $group_id_clause ) {
 					$sql_array['where'] .= ' ' . preg_replace( '/^(AND\s+[\(\s]+)/', '$1' . $group_id_clause . ' AND ', ltrim( $meta_query_where_clauses[ $key ] ) );
 				}
