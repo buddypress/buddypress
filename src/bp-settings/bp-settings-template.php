ba1b4d3dcad368375313bafa36eaab3c1aea6da1
Boone B Gorges <boonebgorges@gmail.com>
2014-07-05 19:01:09 +0000

    When a user changes her email address, require email verification of the new address
    This feature, built into WordPress MS by default (when updating one's profile
    via the Dashboard), ensures that new email addresses are valid and not
    mistyped, helping to avoid unintentionally locked-out accounts.
    
    Fixes #2265
    
    Props j.conti for an initial patch
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@8560 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-settings/bp-settings-template.php
+++ src/bp-settings/bp-settings-template.php
@@ -55,3 +55,29 @@ function bp_settings_root_slug() {
 	function bp_get_settings_root_slug() {
 		return apply_filters( 'bp_get_settings_root_slug', buddypress()->settings->root_slug );
 	}
+
+/**
+ * Add the 'pending email change' message to the settings page.
+ *
+ * @since BuddyPress (2.1.0)
+ */
+function bp_settings_pending_email_notice() {
+	$pending_email = bp_get_user_meta( bp_displayed_user_id(), 'pending_email_change', true );
+
+	if ( empty( $pending_email['newemail'] ) ) {
+		return;
+	}
+
+	if ( bp_get_displayed_user_email() == $pending_email['newemail'] ) {
+		return;
+	}
+
+	?>
+
+	<div id="message" class="bp-template-notice error">
+		<p><?php printf( __( 'There is a pending change of your email address to <code>%1$s</code>.<br />Check your email (<code>%2$s</code>) for the verification link. <a href="%3$s">Cancel</a>', 'buddypress' ), $pending_email['newemail'], bp_get_displayed_user_email(), esc_url( bp_displayed_user_domain() . bp_get_settings_slug() . '/?dismiss_email_change=1' ) ) ?></p>
+	</div>
+
+	<?php
+}
+add_action( 'bp_before_member_settings_template', 'bp_settings_pending_email_notice' );
