3381c6e18936680d378debf36d2963e5c9b47b66
John James Jacoby <johnjamesjacoby@me.com>
2014-07-15 22:57:17 +0000

    Add brackets to conditions and do some general code clean up in `bp-messages-actions.php`.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@8621 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-messages/bp-messages-actions.php
+++ src/bp-messages/bp-messages-actions.php
@@ -19,13 +19,17 @@ if ( !defined( 'ABSPATH' ) ) exit;
  */
 function messages_action_conversation() {
 
-	if ( !bp_is_messages_component() || !bp_is_current_action( 'view' ) )
+	// Bail if not viewing a single conversation
+	if ( ! bp_is_messages_component() || ! bp_is_current_action( 'view' ) ) {
 		return false;
+	}
 
-	$thread_id = (int)bp_action_variable( 0 );
+	// Get the thread ID from the action variable
+	$thread_id = (int) bp_action_variable( 0 );
 
-	if ( !$thread_id || !messages_is_valid_thread( $thread_id ) || ( !messages_check_thread_access( $thread_id ) && !bp_current_user_can( 'bp_moderate' ) ) )
+	if ( ! messages_is_valid_thread( $thread_id ) || ( ! messages_check_thread_access( $thread_id ) && ! bp_current_user_can( 'bp_moderate' ) ) ) {
 		bp_core_redirect( trailingslashit( bp_displayed_user_domain() . bp_get_messages_slug() ) );
+	}
 
 	// Check if a new reply has been submitted
 	if ( isset( $_POST['send'] ) ) {
@@ -33,8 +37,14 @@ function messages_action_conversation() {
 		// Check the nonce
 		check_admin_referer( 'messages_send_message', 'send_message_nonce' );
 
+		$new_reply = messages_new_message( array(
+			'thread_id' => $thread_id,
+			'subject'   => ! empty( $_POST['subject'] ) ? $_POST['subject'] : false,
+			'content'   => $_POST['content']
+		) );
+
 		// Send the reply
-		if ( messages_new_message( array( 'thread_id' => $thread_id, 'subject' => ! empty( $_POST['subject'] ) ? $_POST['subject'] : false, 'content' => $_POST['content'] ) ) ) {
+		if ( ! empty( $new_reply ) ) {
 			bp_core_add_message( __( 'Your reply was sent successfully', 'buddypress' ) );
 		} else {
 			bp_core_add_message( __( 'There was a problem sending your reply, please try again', 'buddypress' ), 'error' );
@@ -57,16 +67,18 @@ add_action( 'bp_actions', 'messages_action_conversation' );
  */
 function messages_action_delete_message() {
 
-	if ( !bp_is_messages_component() || bp_is_current_action( 'notices' ) || !bp_is_action_variable( 'delete', 0 ) )
+	if ( ! bp_is_messages_component() || bp_is_current_action( 'notices' ) || ! bp_is_action_variable( 'delete', 0 ) ) {
 		return false;
+	}
 
 	$thread_id = bp_action_variable( 1 );
 
 	if ( !$thread_id || !is_numeric( $thread_id ) || !messages_check_thread_access( $thread_id ) ) {
 		bp_core_redirect( trailingslashit( bp_displayed_user_domain() . bp_get_messages_slug() . '/' . bp_current_action() ) );
 	} else {
-		if ( !check_admin_referer( 'messages_delete_thread' ) )
+		if ( ! check_admin_referer( 'messages_delete_thread' ) ) {
 			return false;
+		}
 
 		// Delete message
 		if ( !messages_delete_thread( $thread_id ) ) {
@@ -86,8 +98,9 @@ add_action( 'bp_actions', 'messages_action_delete_message' );
  */
 function messages_action_bulk_delete() {
 
-	if ( !bp_is_messages_component() || !bp_is_action_variable( 'bulk-delete', 0 ) )
+	if ( ! bp_is_messages_component() || ! bp_is_action_variable( 'bulk-delete', 0 ) ) {
 		return false;
+	}
 
 	$thread_ids = $_POST['thread_ids'];
 
