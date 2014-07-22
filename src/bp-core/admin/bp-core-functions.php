68b01af44521f2c5118728000dfa85577000474d
Paul Gibbs <djpaul@gmail.com>
2014-07-22 19:49:35 +0000

    Core: adjust contextual help in dashboard Pages screen.
    Props mercime, fixes #5538
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@8664 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-core/admin/bp-core-functions.php
+++ src/bp-core/admin/bp-core-functions.php
@@ -509,15 +509,15 @@ function bp_core_add_contextual_help_content( $tab = '' ) {
 
 	switch ( $tab ) {
 		case 'bp-comp-overview' :
-			$retval = __( 'By default, all BuddyPress components are enabled. You can selectively disable any of the components by using the form. Your BuddyPress installation will continue to function. However, the features of the disabled components will no longer be accessible to anyone using the site.', 'buddypress' );
+			$retval = __( 'By default, all but four of the BuddyPress components are enabled. You can selectively enable or disable any of the components by using the form below. Your BuddyPress installation will continue to function. However, the features of the disabled components will no longer be accessible to anyone using the site.', 'buddypress' );
 			break;
 
 		case 'bp-page-overview' :
-			$retval = __( 'BuddyPress Components use WordPress Pages for their root directory/archive pages. Here you can change the page associations for each active component.', 'buddypress' );
+			$retval = __( 'BuddyPress Components use WordPress Pages for their root directory/archive pages. You can change the page associations for each active component by using the form below.', 'buddypress' );
 			break;
 
 		case 'bp-settings-overview' :
-			$retval = __( 'Extra configuration settings.', 'buddypress' );
+			$retval = __( 'Extra configuration settings are provided and activated. You can selectively enable or disable any setting by using the form on this screen.', 'buddypress' );
 			break;
 
 		case 'bp-profile-overview' :
