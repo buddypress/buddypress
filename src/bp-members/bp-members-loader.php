buddypress/buddypress

commit d317b1bc49eaa7b83bcfa92d28541709593d3267

Untangle some load order and mutual dependency issues between component loaders
BuddyPress's components are dependent upon each other in numerous ways. All
components depend in one way or another on bp-core. Many components reference
bp-members functions and globals. References to bp-xprofile can be found in
a number of components. And so on. We have managed to make these dependencies
work on a case-by-case basis, though the system becomes less stable as it
becomes more complex. The fact that all components are loaded at
bp_setup_components introduces the possibliity of numerous race conditions that
may not cause problems on vanilla installations of BuddyPress, but can cause
fatal errors when trying to customize in various ways.

As part of an ongoing project to disentangle and regularize these dependencies
(see eg #5750), this changeset introduces a few enhancements that make the
dependencies between components more predictable, more serialized, and more
stable.

- In bp-members and bp-groups, move canonical_stack determination out of the
  main setup_globals() method and into its own setup_canonical_stack() method.
  This new method runs after all components have had a chance to run
  setup_globals(). As a result, the components are able to reference each other
  safely when determining default and fallback components for redirects (for
  example, the members component has access to the activity component's slug
  and other global data when determining whether
  example.com/members/boone/activity/ should resolve to example.com/members/boone/)
- In bp-members, use only WP core data to set up the loggedin_user and
  displayed_user global objects. Previously, these objects were configured by
  referenced to xprofile, which required that xprofile be loaded before members.
  Now, users' display names are first loaded from wp_users, and are overridden
  by the xprofile component via filter, only after xprofile has fully
  initialized.

src/bp-members/bp-members-loader.php
@@ -96,8 +96,11 @@ class BP_Members_Component extends BP_Component {
 
 		/** Logged in user ****************************************************/
 
+		// The core userdata of the user who is currently logged in.
+		$bp->loggedin_user->userdata       = bp_core_get_core_userdata( bp_loggedin_user_id() );
+
 		// Fetch the full name for the logged in user
-		$bp->loggedin_user->fullname       = bp_core_get_user_displayname( bp_loggedin_user_id() );
+		$bp->loggedin_user->fullname       = isset( $bp->loggedin_user->userdata->display_name ) ? $bp->loggedin_user->userdata->display_name : '';
 
 		// Hits the DB on single WP installs so get this separately
 		$bp->loggedin_user->is_super_admin = $bp->loggedin_user->is_site_admin = is_super_admin( bp_loggedin_user_id() );
@@ -105,19 +108,16 @@ class BP_Members_Component extends BP_Component {
 		// The domain for the user currently logged in. eg: http://domain.com/members/andy
 		$bp->loggedin_user->domain         = bp_core_get_user_domain( bp_loggedin_user_id() );
 
-		// The core userdata of the user who is currently logged in.
-		$bp->loggedin_user->userdata       = bp_core_get_core_userdata( bp_loggedin_user_id() );
-
 		/** Displayed user ****************************************************/
-
-		// The domain for the user currently being displayed
-		$bp->displayed_user->domain   = bp_core_get_user_domain( bp_displayed_user_id() );
 
 		// The core userdata of the user who is currently being displayed
 		$bp->displayed_user->userdata = bp_core_get_core_userdata( bp_displayed_user_id() );
 
 		// Fetch the full name displayed user
-		$bp->displayed_user->fullname = bp_core_get_user_displayname( bp_displayed_user_id() );
+		$bp->displayed_user->fullname = isset( $bp->displayed_user->userdata->fullname ) ? $bp->displayed_user->userdata->fullname : '';
+
+		// The domain for the user currently being displayed
+		$bp->displayed_user->domain   = bp_core_get_user_domain( bp_displayed_user_id() );
 
 		/** Signup ************************************************************/
 
@@ -130,6 +130,15 @@ class BP_Members_Component extends BP_Component {
 			$bp->profile->slug = 'profile';
 			$bp->profile->id   = 'profile';
 		}
+	}
+
+	/**
+	 * Set up canonical stack for this component.
+	 *
+	 * @since BuddyPress (2.1.0)
+	 */
+	public function setup_canonical_stack() {
+		$bp = buddypress();
 
 		/** Default Profile Component *****************************************/
 

src/bp-core/bp-core-actions.php
@@ -66,6 +66,7 @@ add_action( 'bp_loaded', 'bp_register_theme_directory', 14 );
  */
 add_action( 'bp_init', 'bp_core_set_uri_globals',    2  );
 add_action( 'bp_init', 'bp_setup_globals',           4  );
+add_action( 'bp_init', 'bp_setup_canonical_stack',   5  );
 add_action( 'bp_init', 'bp_setup_nav',               6  );
 add_action( 'bp_init', 'bp_setup_title',             8  );
 add_action( 'bp_init', 'bp_core_load_admin_bar_css', 12 );

src/bp-core/bp-core-component.php
@@ -337,6 +337,9 @@ class BP_Component {
 		// Setup globals
 		add_action( 'bp_setup_globals',          array( $this, 'setup_globals'          ), 10 );
 
+		// Set up canonical stack
+		add_action( 'bp_setup_canonical_stack',  array( $this, 'setup_canonical_stack'  ), 10 );
+
 		// Include required files. Called early to ensure that BP core
 		// components are loaded before plugins that hook their loader functions
 		// to bp_include with the default priority of 10. This is for backwards
@@ -377,6 +380,13 @@ class BP_Component {
 		// Additional actions can be attached here
 		do_action( 'bp_' . $this->id . '_setup_actions' );
 	}
+
+	/**
+	 * Set up the canonical URL stack for this component.
+	 *
+	 * @since BuddyPress (2.1.0)
+	 */
+	public function setup_canonical_stack() {}
 
 	/**
 	 * Set up component navigation.

src/bp-core/bp-core-dependency.php
@@ -32,6 +32,13 @@ function bp_setup_components() {
 }
 
 /**
+ * Fire the 'bp_setup_canonical_stack' action, where plugins should set up their canonical URL.
+ */
+function bp_setup_canonical_stack() {
+	do_action( 'bp_setup_canonical_stack' );
+}
+
+/**
  * Fire the 'bp_setup_globals' action, where plugins should initialize global settings.
  */
 function bp_setup_globals() {

src/bp-core/bp-core-loader.php
@@ -263,4 +263,4 @@ class BP_Core extends BP_Component {
 function bp_setup_core() {
 	buddypress()->core = new BP_Core();
 }
-add_action( 'bp_setup_components', 'bp_setup_core', 2 );
+add_action( 'bp_loaded', 'bp_setup_core', 0 );

src/bp-groups/bp-groups-loader.php
@@ -240,34 +240,6 @@ class BP_Groups_Component extends BP_Component {
 			return;
 		}
 
-		if ( bp_is_groups_component() && !empty( $this->current_group ) ) {
-
-			$this->default_extension = apply_filters( 'bp_groups_default_extension', defined( 'BP_GROUPS_DEFAULT_EXTENSION' ) ? BP_GROUPS_DEFAULT_EXTENSION : 'home' );
-
-			if ( !bp_current_action() ) {
-				$bp->current_action = $this->default_extension;
-			}
-
-			// Prepare for a redirect to the canonical URL
-			$bp->canonical_stack['base_url'] = bp_get_group_permalink( $this->current_group );
-
-			if ( bp_current_action() ) {
-				$bp->canonical_stack['action'] = bp_current_action();
-			}
-
-			if ( !empty( $bp->action_variables ) ) {
-				$bp->canonical_stack['action_variables'] = bp_action_variables();
-			}
-
-			// When viewing the default extension, the canonical URL should not have
-			// that extension's slug, unless more has been tacked onto the URL via
-			// action variables
-			if ( bp_is_current_action( $this->default_extension ) && empty( $bp->action_variables ) )  {
-				unset( $bp->canonical_stack['action'] );
-			}
-
-		}
-
 		// Preconfigured group creation steps
 		$this->group_creation_steps = apply_filters( 'groups_create_group_steps', array(
 			'group-details'  => array(
@@ -305,6 +277,46 @@ class BP_Groups_Component extends BP_Component {
 
 		// Auto join group when non group member performs group activity
 		$this->auto_join = defined( 'BP_DISABLE_AUTO_GROUP_JOIN' ) && BP_DISABLE_AUTO_GROUP_JOIN ? false : true;
+	}
+
+	/**
+	 * Set up canonical stack for this component.
+	 *
+	 * @since BuddyPress (2.1.0)
+	 */
+	public function setup_canonical_stack() {
+		if ( ! bp_is_groups_component() ) {
+			return;
+		}
+
+		if ( empty( $this->current_group ) ) {
+			return;
+		}
+
+
+		$this->default_extension = apply_filters( 'bp_groups_default_extension', defined( 'BP_GROUPS_DEFAULT_EXTENSION' ) ? BP_GROUPS_DEFAULT_EXTENSION : 'home' );
+
+		if ( !bp_current_action() ) {
+			buddypress()->current_action = $this->default_extension;
+		}
+
+		// Prepare for a redirect to the canonical URL
+		buddypress()->canonical_stack['base_url'] = bp_get_group_permalink( $this->current_group );
+
+		if ( bp_current_action() ) {
+			buddypress()->canonical_stack['action'] = bp_current_action();
+		}
+
+		if ( !empty( buddypress()->action_variables ) ) {
+			buddypress()->canonical_stack['action_variables'] = bp_action_variables();
+		}
+
+		// When viewing the default extension, the canonical URL should not have
+		// that extension's slug, unless more has been tacked onto the URL via
+		// action variables
+		if ( bp_is_current_action( $this->default_extension ) && empty( buddypress()->action_variables ) )  {
+			unset( buddypress()->canonical_stack['action'] );
+		}
 	}
 
 	/**

src/bp-xprofile/bp-xprofile-functions.php
@@ -497,6 +497,35 @@ function xprofile_update_field_position( $field_id, $position, $field_group_id )
 }
 
 /**
+ * Replace the displayed and logged-in userss fullnames with the xprofile name, if required.
+ *
+ * The Members component uses the logged-in user's display_name to set the
+ * value of buddypress()->loggedin_user->fullname. However, in cases where
+ * profile sync is disabled, display_name may diverge from the xprofile
+ * fullname field value, and the xprofile field should take precedence.
+ *
+ * Runs at bp_setup_globals:100 to ensure that all components have loaded their
+ * globals before attempting any overrides.
+ *
+ * @since BuddyPress (2.0.0)
+ */
+function xprofile_override_user_fullnames() {
+	// If sync is enabled, the two names will match. No need to continue.
+	if ( ! bp_disable_profile_sync() ) {
+		return;
+	}
+
+	if ( bp_loggedin_user_id() ) {
+		buddypress()->loggedin_user->fullname = bp_core_get_user_displayname( bp_loggedin_user_id() );
+	}
+
+	if ( bp_displayed_user_id() ) {
+		buddypress()->displayed_user->fullname = bp_core_get_user_displayname( bp_displayed_user_id() );
+	}
+}
+add_action( 'bp_setup_globals', 'xprofile_override_user_fullnames', 100 );
+
+/**
  * Setup the avatar upload directory for a user.
  *
  * @package BuddyPress Core

src/bp-xprofile/bp-xprofile-loader.php
@@ -218,25 +218,47 @@ class BP_XProfile_Component extends BP_Component {
 			);
 		}
 
-		// Privacy Settings
-		if ( bp_is_active( 'settings' ) ) {
-
-			// Get the settings slug
-			$settings_slug = bp_get_settings_slug();
-
-			// Add the sub-navigation
-			$sub_nav[] = array(
-				'name'            => __( 'Profile', 'buddypress' ),
-				'slug'            => 'profile',
-				'parent_url'      => trailingslashit( $user_domain . $settings_slug ),
-				'parent_slug'     => $settings_slug,
-				'screen_function' => 'bp_xprofile_screen_settings',
-				'position'        => 30,
-				'user_has_access' => bp_core_can_edit_settings()
-			);
-		}
+		// The Settings > Profile nav item can only be set up after
+		// the Settings component has run its own nav routine
+		add_action( 'bp_settings_setup_nav', array( $this, 'setup_settings_nav' ) );
 
 		parent::setup_nav( $main_nav, $sub_nav );
+	}
+
+	/**
+	 * Set up the Settings > Profile nav item.
+	 *
+	 * Loaded in a separate method because the Settings component may not
+	 * be loaded in time for BP_XProfile_Component::setup_nav().
+	 *
+	 * @since BuddyPress (2.1.0)
+	 */
+	public function setup_settings_nav() {
+		if ( ! bp_is_active( 'settings' ) ) {
+			return;
+		}
+
+		// Determine user to use
+		if ( bp_displayed_user_domain() ) {
+			$user_domain = bp_displayed_user_domain();
+		} elseif ( bp_loggedin_user_domain() ) {
+			$user_domain = bp_loggedin_user_domain();
+		} else {
+			return;
+		}
+
+		// Get the settings slug
+		$settings_slug = bp_get_settings_slug();
+
+		bp_core_new_subnav_item( array(
+			'name'            => __( 'Profile', 'buddypress' ),
+			'slug'            => 'profile',
+			'parent_url'      => trailingslashit( $user_domain . $settings_slug ),
+			'parent_slug'     => $settings_slug,
+			'screen_function' => 'bp_xprofile_screen_settings',
+			'position'        => 30,
+			'user_has_access' => bp_core_can_edit_settings()
+		) );
 	}
 
 	/**
@@ -354,4 +376,4 @@ function bp_setup_xprofile() {
 	if ( !isset( $bp->profile->id ) )
 		$bp->profile = new BP_XProfile_Component();
 }
-add_action( 'bp_setup_components', 'bp_setup_xprofile', 6 );
+add_action( 'bp_setup_components', 'bp_setup_xprofile', 2 );

commit 29ef91d34d687d20e7bd0ebc4fc8b75303cc501f

Members: set the displayed user's `fullname` to their display name
This was broken in r8610 because it was trying to access a property in the WP_User object that doesn't exist.

Fixes #5764, props imath

git-svn-id: https://buddypress.svn.wordpress.org/trunk@8660 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-members/bp-members-loader.php
@@ -113,7 +113,7 @@ class BP_Members_Component extends BP_Component {
 		$bp->displayed_user->userdata = bp_core_get_core_userdata( bp_displayed_user_id() );
 
 		// Fetch the full name displayed user
-		$bp->displayed_user->fullname = isset( $bp->displayed_user->userdata->fullname ) ? $bp->displayed_user->userdata->fullname : '';
+		$bp->displayed_user->fullname = isset( $bp->displayed_user->userdata->display_name ) ? $bp->displayed_user->userdata->display_name : '';
 
 		// The domain for the user currently being displayed
 		$bp->displayed_user->domain   = bp_core_get_user_domain( bp_displayed_user_id() );