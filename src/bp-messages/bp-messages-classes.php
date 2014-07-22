bdeae9f111aa10b14e98277c865f862c8b03824d
John James Jacoby <johnjamesjacoby@me.com>
2014-07-22 18:09:59 +0000

    Remove incorrect `prepare()` usage in `get_current_threads_for_user()`. See #5388.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@8661 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-messages/bp-messages-classes.php
+++ src/bp-messages/bp-messages-classes.php
@@ -270,7 +270,7 @@ class BP_Messages_Thread {
 		if ( 'sentbox' == $box ) {
 			$user_id_sql = $wpdb->prepare( 'm.sender_id = %d', $user_id );
 			$thread_ids  = $wpdb->get_results( "SELECT m.thread_id, MAX(m.date_sent) AS date_sent FROM {$bp->messages->table_name_recipients} r, {$bp->messages->table_name_messages} m WHERE m.thread_id = r.thread_id AND m.sender_id = r.user_id AND {$user_id_sql} AND r.is_deleted = 0 {$search_sql} GROUP BY m.thread_id ORDER BY date_sent DESC {$pag_sql}" );
-			$total_threads = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT( DISTINCT m.thread_id ) FROM {$bp->messages->table_name_recipients} r, {$bp->messages->table_name_messages} m WHERE m.thread_id = r.thread_id AND m.sender_id = r.user_id AND {$user_id_sql} AND r.is_deleted = 0 {$search_sql} ", $user_id ) );
+			$total_threads = $wpdb->get_var( "SELECT COUNT( DISTINCT m.thread_id ) FROM {$bp->messages->table_name_recipients} r, {$bp->messages->table_name_messages} m WHERE m.thread_id = r.thread_id AND m.sender_id = r.user_id AND {$user_id_sql} AND r.is_deleted = 0 {$search_sql} " );
 		} else {
 			$user_id_sql = $wpdb->prepare( 'r.user_id = %d', $user_id );
 			$thread_ids = $wpdb->get_results( "SELECT m.thread_id, MAX(m.date_sent) AS date_sent FROM {$bp->messages->table_name_recipients} r, {$bp->messages->table_name_messages} m WHERE m.thread_id = r.thread_id AND r.is_deleted = 0 AND {$user_id_sql} AND r.sender_only = 0 {$type_sql} {$search_sql} GROUP BY m.thread_id ORDER BY date_sent DESC {$pag_sql}" );
