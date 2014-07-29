bfa721b8df338e1c957c604d90caf49e29fd7f72
John James Jacoby <johnjamesjacoby@me.com>
2014-07-29 13:57:09 +0000

    Column alignment tweaks in `bp_activity_thumbnail_content_images()`.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@8716 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-activity/bp-activity-functions.php
+++ src/bp-activity/bp-activity-functions.php
@@ -1733,16 +1733,16 @@ function bp_activity_thumbnail_content_images( $content, $link = false, $args = 
 	if ( !empty( $matches ) && !empty( $matches[0] ) ) {
 
 		// Get the SRC value
-		preg_match( '/<img.*?(src\=[\'|"]{0,1}.*?[\'|"]{0,1})[\s|>]{1}/i', $matches[0][0], $src );
+		preg_match( '/<img.*?(src\=[\'|"]{0,1}.*?[\'|"]{0,1})[\s|>]{1}/i',    $matches[0][0], $src    );
 
 		// Get the width and height
 		preg_match( '/<img.*?(height\=[\'|"]{0,1}.*?[\'|"]{0,1})[\s|>]{1}/i', $matches[0][0], $height );
 		preg_match( '/<img.*?(width\=[\'|"]{0,1}.*?[\'|"]{0,1})[\s|>]{1}/i',  $matches[0][0], $width  );
 
 		if ( !empty( $src ) ) {
-			$src    = substr( substr( str_replace( 'src=', '', $src[1] ), 0, -1 ), 1 );
+			$src    = substr( substr( str_replace( 'src=',    '', $src[1]    ), 0, -1 ), 1 );
 			$height = substr( substr( str_replace( 'height=', '', $height[1] ), 0, -1 ), 1 );
-			$width  = substr( substr( str_replace( 'width=', '', $width[1] ), 0, -1 ), 1 );
+			$width  = substr( substr( str_replace( 'width=',  '', $width[1]  ), 0, -1 ), 1 );
 
 			if ( empty( $width ) || empty( $height ) ) {
 				$width  = 100;
