340903beb99e165bfb6929ab9c2750c2cb2cc4a6
Paul Gibbs <djpaul@gmail.com>
2014-08-09 16:30:44 +0000

    Activity: Combine the @mentions enqueuing function.
    Previously, there was a function for loading in both admin and frontend contexts.
    I've combined the loading into one function which removes a bunch of duplicated logic, probably left over from an early approach at building the suggestions feature.
    
    See #3278
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@8773 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-activity/bp-activity-cssjs.php
+++ src/bp-activity/bp-activity-cssjs.php
@@ -20,6 +20,17 @@ function bp_activity_mentions_script() {
 		return;
 	}
 
+	// Special handling for New/Edit screens in wp-admin
+	if ( is_admin() ) {
+		if (
+			! get_current_screen() ||
+			! in_array( get_current_screen()->base, array( 'page', 'post' ) ) ||
+			! post_type_supports( get_current_screen()->post_type, 'editor' ) ) {
+			return;
+		}
+	}
+
+
 	$min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 
 	wp_enqueue_script( 'bp-mentions', buddypress()->plugin_url . "bp-activity/js/mentions{$min}.js", array( 'jquery', 'jquery-atwho' ), bp_get_version(), true );
@@ -34,36 +45,4 @@ function bp_activity_mentions_script() {
 	do_action( 'bp_activity_mentions_prime_results' );
 }
 add_action( 'bp_enqueue_scripts', 'bp_activity_mentions_script' );
-
-/**
- * Enqueue @mentions JS in wp-admin.
- *
- * @since BuddyPress (2.1)
- */
-function bp_activity_mentions_dashboard_script() {
-	if ( ! bp_activity_maybe_load_mentions_scripts() ) {
-		return;
-	}
-
-	// Special handling for New/Edit screens in wp-admin
-	if (
-		! get_current_screen() ||
-		! in_array( get_current_screen()->base, array( 'page', 'post' ) ) || 
-		! post_type_supports( get_current_screen()->post_type, 'editor' ) ) {
-		return;
-	}
-
-	$min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
-
-	wp_enqueue_script( 'bp-mentions', buddypress()->plugin_url . "bp-activity/js/mentions{$min}.js", array( 'jquery', 'jquery-atwho' ), bp_get_version(), true );
-	wp_enqueue_style( 'bp-mentions-css', buddypress()->plugin_url . "bp-activity/css/mentions{$min}.css", array(), bp_get_version() );
-
-	wp_style_add_data( 'bp-mentions-css', 'rtl', true );
-	if ( $min ) {
-		wp_style_add_data( 'bp-mentions-css', 'suffix', $min );
-	}
-
-	// Print a list of the current user's friends to the page for quicker @mentions lookups.
-	do_action( 'bp_activity_mentions_prime_results' );
-}
-add_action( 'bp_admin_enqueue_scripts', 'bp_activity_mentions_dashboard_script' );
+add_action( 'bp_admin_enqueue_scripts', 'bp_activity_mentions_script' );
