e2202580669a345adcac157be813f7339ba4a8ee
Paul Gibbs <djpaul@gmail.com>
2014-08-10 16:16:35 +0000

    Welcome screen: add strings for maintenance/security release notes
    These have been copied from WordPress core, which uses them to update
    their welcome screen for minor releases without causing string changes
    (or tries to).
    
    Since the layout of our welcome screen is not that dissimilar from what
    WordPress’, we should see if we are able to make use of these in any
    future 2.1.x minor releases.
    
    If we can, this will help our welcome screen be more relevant and
    informative.
    
    See #5805
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@8779 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-core/bp-core-admin.php
+++ src/bp-core/bp-core-admin.php
@@ -781,5 +781,35 @@ endif; // class_exists check
  * @uses BP_Admin
  */
 function bp_admin() {
-       buddypress()->admin = new BP_Admin();
+	buddypress()->admin = new BP_Admin();
+	return;
+
+
+	// These are strings we may use to describe maintenance/security releases, where we aim for no new strings.
+
+	_n_noop( 'Maintenance Release', 'Maintenance Releases', 'buddypress' );
+	_n_noop( 'Security Release', 'Security Releases', 'buddypress' );
+	_n_noop( 'Maintenance and Security Release', 'Maintenance and Security Releases', 'buddypress' );
+
+	/* translators: 1: WordPress version number. */
+	_n_noop( '<strong>Version %1$s</strong> addressed a security issue.',
+	         '<strong>Version %1$s</strong> addressed some security issues.',
+	         'buddypress' );
+
+	/* translators: 1: WordPress version number, 2: plural number of bugs. */
+	_n_noop( '<strong>Version %1$s</strong> addressed %2$s bug.',
+	         '<strong>Version %1$s</strong> addressed %2$s bugs.',
+	         'buddypress' );
+
+	/* translators: 1: WordPress version number, 2: plural number of bugs. Singular security issue. */
+	_n_noop( '<strong>Version %1$s</strong> addressed a security issue and fixed %2$s bug.',
+	         '<strong>Version %1$s</strong> addressed a security issue and fixed %2$s bugs.',
+	         'buddypress' );
+
+	/* translators: 1: WordPress version number, 2: plural number of bugs. More than one security issue. */
+	_n_noop( '<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bug.',
+	         '<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bugs.',
+	         'buddypress' );
+
+	__( 'For more information, see <a href="%s">the release notes</a>.', 'buddypress' );
 }
