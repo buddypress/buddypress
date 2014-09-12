301e94dc77252b1a9635a1955d706ad7efdf2e55
Paul Gibbs <djpaul@gmail.com>
2014-09-12 21:46:08 +0000

    Activity: @mentions Fix search logic for RTL.
    I'd previously made a faulty assumption regarding letter order in the database, but my testing reveals the letter order stays intact, it is the word order that is reversed. Pending feedback from anyone who uses a RTL language, I'm modifying the search logic so that it's identical in both LTR and RTL modes.
    
    Fixes #5855
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@9009 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-core/bp-core-classes.php
+++ src/bp-core/bp-core-classes.php
@@ -2669,7 +2669,7 @@ class BP_Members_Suggestions extends BP_Suggestions {
 			'page'            => 1,
 			'per_page'        => $this->args['limit'],
 			'search_terms'    => $this->args['term'],
-			'search_wildcard' => is_rtl() ? 'left' : 'right',
+			'search_wildcard' => 'right',
 		);
 
 		// Only return matches of friends of this user.
