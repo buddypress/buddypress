c6cf4186c91d555e9cfdfd880ef8b0cf9a6de8cf
Mathieu Viet <mathchristo@yahoo.fr>
2014-12-20 16:29:56 +0000

    Make sure activities about a friendship are removed when this friendship is cancelled
    Fixes #6055
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@9241 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- tests/phpunit/testcases/friends/activity.php
+++ tests/phpunit/testcases/friends/activity.php
@@ -48,5 +48,52 @@ class BP_Tests_Friends_Activity extends BP_UnitTestCase {
 
 		$this->assertSame( $expected, $a_obj->action );
 	}
+
+	/**
+	 * @group friends_delete_activity
+	 */
+	public function test_friends_delete_activity() {
+		$old_user = get_current_user_id();
+
+		$u1 = $this->factory->user->create();
+		$u2 = $this->factory->user->create();
+
+		friends_add_friend( $u2, $u1 );
+		$friendship_id = friends_get_friendship_id( $u2, $u1 );
+
+		// Set current user to u1 to accepte the friendship
+		$this->set_current_user( $u1 );
+		friends_accept_friendship( $friendship_id );
+
+		// Reset the current user
+		$this->set_current_user( $old_user );
+
+		// Random activities
+		$au1 = $this->factory->activity->create( array( 'user_id' => $u1 ) );
+		$au2 = $this->factory->activity->create( array( 'user_id' => $u2 ) );
+
+		$fc_act = bp_activity_get( array(
+			'component'   => buddypress()->friends->id,
+			'item_id'     => $friendship_id,
+			'filter'      => array( 'action' => array( 'friendship_created' ) ),
+			'show_hidden' => false
+		) );
+
+		$this->assertTrue( count( $fc_act['activities'] ) == 1, '1 public activity should be created when a friendship is confirmed' );
+
+		// Remove the friendship
+		friends_remove_friend( $u2, $u1 );
+
+		$this->assertFalse( friends_check_friendship( $u2, $u1 ), '2 users should not be friend once the friendship is removed' );
+
+		$fd_act = bp_activity_get( array(
+			'component'   => buddypress()->friends->id,
+			'item_id'     => $friendship_id,
+			'filter'      => array( 'action' => array( 'friendship_created' ) ),
+			'show_hidden' => true
+		) );
+
+		$this->assertTrue( count( $fd_act['activities'] ) == 0, 'friends_delete_activity() should remove "friendship_created" activities about a deleted friendship' );
+	}
 }
 
