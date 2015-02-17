7c642383074655144e4476e43619a88b0f41af6e
Michael Beckwith <tw2113@gmail.com>
2015-02-17 05:31:15 +0000

    Adds hooks documentation for bp-groups-forums.php.
    See #5943.
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@9488 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-groups/bp-groups-forums.php
+++ src/bp-groups/bp-groups-forums.php
@@ -49,6 +49,14 @@ function groups_new_group_forum( $group_id = 0, $group_name = '', $group_desc = 
 
 	groups_update_groupmeta( $group_id, 'forum_id', $forum_id );
 
+	/**
+	 * Fires after the creation of a new forum inside a specific BuddyPress group.
+	 *
+	 * @since BuddyPress (1.0.0)
+	 *
+	 * @param int $forum_id ID of the newly created forum.
+	 * @param int $group_id ID of the associated group.
+	 */
 	do_action( 'groups_new_group_forum', $forum_id, $group_id );
 }
 
@@ -74,6 +82,13 @@ function groups_update_group_forum( $group_id ) {
 		return false;
 	}
 
+	/**
+	 * Filters the group forum metadata value argument.
+	 *
+	 * @since BuddyPress (1.2.5)
+	 *
+	 * @param array $value Array of metadata values to update the group forum with.
+	 */
 	bp_forums_update_forum( apply_filters( 'groups_update_group_forum', array(
 		'forum_id'   => groups_get_groupmeta( $group_id, 'forum_id' ),
 		'forum_name' => $group->name,
@@ -102,7 +117,22 @@ function groups_new_group_forum_post( $post_text, $topic_id, $page = false ) {
 		return false;
 	}
 
+	/**
+	 * Filters the text for the forum post before save.
+	 *
+	 * @since BuddyPress (1.2.0)
+	 *
+	 * @param string $post_text Text for the forum post.
+	 */
 	$post_text = apply_filters( 'group_forum_post_text_before_save',     $post_text );
+
+	/**
+	 * Filters the ID for the forum post before save.
+	 *
+	 * @since BuddyPress (1.2.0)
+	 *
+	 * @param int $topic_id ID of the topic the post will be associated with.
+	 */
 	$topic_id  = apply_filters( 'group_forum_post_topic_id_before_save', $topic_id  );
 	$post_id   = bp_forums_insert_post( array(
 		'post_text' => $post_text,
@@ -122,16 +152,56 @@ function groups_new_group_forum_post( $post_text, $topic_id, $page = false ) {
 		$primary_link .= "?topic_page=" . $page;
 	}
 
-	// Record this in activity streams
+	/**
+	 * Filters the new groups activity forum post action.
+	 *
+	 * @since BuddyPress (1.2.0)
+	 *
+	 * @param string $activity_action Formatted action related to group activity posting.
+	 * @param int    $post_id         ID of the newly created group forum post.
+	 * @param string $post_text       The text for the forum post.
+	 * @param object $topic           Object holding current topic details. Passed by reference.
+	 */
+	$action = apply_filters_ref_array( 'groups_activity_new_forum_post_action',  array( $activity_action,  $post_id, $post_text, &$topic ) );
+
+	/**
+	 * Filters the new groups activity forum post content.
+	 *
+	 * @since BuddyPress (1.2.0)
+	 *
+	 * @param string $activity_content Excerpt-length activity content to be posted.
+	 * @param int    $post_id          ID of the newly created group forum post.
+	 * @param string $post_text        The text for the forum post.
+	 * @param object $topic            Object holding current topic details. Passed by reference.
+	 */
+	$content = apply_filters_ref_array( 'groups_activity_new_forum_post_content', array( $activity_content, $post_id, $post_text, &$topic ) );
+
+	/**
+	 * Filters the new groups activity forum post primary link.
+	 *
+	 * @since BuddyPress (1.1.0)
+	 *
+	 * @param string $value URL to the newly posted forum post.
+	 */
+	$filtered_primary_link = apply_filters( 'groups_activity_new_forum_post_primary_link', "{$primary_link}#post-{$post_id}" );
+
 	groups_record_activity( array(
-		'action'            => apply_filters_ref_array( 'groups_activity_new_forum_post_action',  array( $activity_action,  $post_id, $post_text, &$topic ) ),
-		'content'           => apply_filters_ref_array( 'groups_activity_new_forum_post_content', array( $activity_content, $post_id, $post_text, &$topic ) ),
-		'primary_link'      => apply_filters( 'groups_activity_new_forum_post_primary_link', "{$primary_link}#post-{$post_id}" ),
+		'action'            => $action,
+		'content'           => $content,
+		'primary_link'      => $filtered_primary_link,
 		'type'              => 'new_forum_post',
 		'item_id'           => bp_get_current_group_id(),
 		'secondary_item_id' => $post_id
 	) );
 
+	/**
+	 * Fires after the creation of a new group forum topic post.
+	 *
+	 * @since BuddyPress (1.0.0)
+	 *
+	 * @param int $value   ID of the current group.
+	 * @param int $post_id ID of the new created forum topic post.
+	 */
 	do_action( 'groups_new_forum_topic_post', bp_get_current_group_id(), $post_id );
 
 	return $post_id;
@@ -154,9 +224,40 @@ function groups_new_group_forum_topic( $topic_title, $topic_text, $topic_tags, $
 	if ( empty( $topic_title ) || empty( $topic_text ) )
 		return false;
 
+	/**
+	 * Filters the new groups forum topic title before saving.
+	 *
+	 * @since BuddyPress (1.2.0)
+	 *
+	 * @param string $topic_title The title for the forum topic.
+	 */
 	$topic_title = apply_filters( 'group_forum_topic_title_before_save',    $topic_title );
+
+	/**
+	 * Filters the new groups forum topic text before saving.
+	 *
+	 * @since BuddyPress (1.2.0)
+	 *
+	 * @param string $topic_text The text for the forum topic.
+	 */
 	$topic_text  = apply_filters( 'group_forum_topic_text_before_save',     $topic_text  );
+
+	/**
+	 * Filters the new groups forum topic tags before saving.
+	 *
+	 * @since BuddyPress (1.2.0)
+	 *
+	 * @param string $topic_tags A comma-delimited string of topic tags.
+	 */
 	$topic_tags  = apply_filters( 'group_forum_topic_tags_before_save',     $topic_tags  );
+
+	/**
+	 * Filters the forum ID this forum topic resides in.
+	 *
+	 * @since BuddyPress (1.2.0)
+	 *
+	 * @param string $forum_id The forum ID this forum topic resides in.
+	 */
 	$forum_id    = apply_filters( 'group_forum_topic_forum_id_before_save', $forum_id    );
 	$topic_id    = bp_forums_new_topic( array(
 		'topic_title' => $topic_title,
@@ -173,16 +274,54 @@ function groups_new_group_forum_topic( $topic_title, $topic_text, $topic_tags, $
 	$activity_action  = sprintf( __( '%1$s started the forum topic %2$s in the group %3$s', 'buddypress'), bp_core_get_userlink( bp_loggedin_user_id() ), '<a href="' . bp_get_group_permalink( groups_get_current_group() ) . 'forum/topic/' . $topic->topic_slug .'/">' . esc_attr( $topic->topic_title ) . '</a>', '<a href="' . bp_get_group_permalink( groups_get_current_group() ) . '">' . esc_attr( bp_get_current_group_name() ) . '</a>' );
 	$activity_content = bp_create_excerpt( $topic_text );
 
-	// Record this in activity streams
+	/**
+	 * Filters the new groups activity forum topic action.
+	 *
+	 * @since BuddyPress (1.2.0)
+	 *
+	 * @param string $activity_action Formatted action related to forum topic.
+	 * @param string $topic_text      New topic text.
+	 * @param object $topic           Object holding current topic details. Passed by reference.
+	 */
+	$action = apply_filters_ref_array( 'groups_activity_new_forum_topic_action',  array( $activity_action,  $topic_text, &$topic ) );
+
+	/**
+	 * Filters the new groups activity forum topic content.
+	 *
+	 * @since BuddyPress (1.2.0)
+	 *
+	 * @param string $activity_content Excerpt-length activity content to be posted.
+	 * @param string $topic_text       New topic text.
+	 * @param object $topic            Object holding current topic details. Passed by reference.
+	 */
+	$content = apply_filters_ref_array( 'groups_activity_new_forum_topic_content', array( $activity_content, $topic_text, &$topic ) );
+
+	/**
+	 * Filters the new groups activity forum topic primary link.
+	 *
+	 * @since BuddyPress (1.1.0)
+	 *
+	 * @param string $value Concatenated primary link.
+	 */
+	$primary_link = apply_filters( 'groups_activity_new_forum_topic_primary_link', bp_get_group_permalink( groups_get_current_group() ) . 'forum/topic/' . $topic->topic_slug . '/' );
+
 	groups_record_activity( array(
-		'action'            => apply_filters_ref_array( 'groups_activity_new_forum_topic_action',  array( $activity_action,  $topic_text, &$topic ) ),
-		'content'           => apply_filters_ref_array( 'groups_activity_new_forum_topic_content', array( $activity_content, $topic_text, &$topic ) ),
-		'primary_link'      => apply_filters( 'groups_activity_new_forum_topic_primary_link', bp_get_group_permalink( groups_get_current_group() ) . 'forum/topic/' . $topic->topic_slug . '/' ),
+		'action'            => $action,
+		'content'           => $content,
+		'primary_link'      => $primary_link,
 		'type'              => 'new_forum_topic',
 		'item_id'           => bp_get_current_group_id(),
 		'secondary_item_id' => $topic->topic_id
 	) );
 
+	/**
+	 * Fires after the creation of a new group forum topic.
+	 *
+	 * @since BuddyPress (1.0.0)
+	 *
+	 * @param int    $value ID of the current group.
+	 * @param object $topic Object holding current topic details. Passed by reference.
+	 */
 	do_action_ref_array( 'groups_new_forum_topic', array( bp_get_current_group_id(), &$topic ) );
 
 	return $topic;
@@ -204,7 +343,10 @@ function groups_new_group_forum_topic( $topic_title, $topic_text, $topic_tags, $
 function groups_update_group_forum_topic( $topic_id, $topic_title, $topic_text, $topic_tags = false ) {
 	$bp = buddypress();
 
+	/** This filter is documented in bp-groups/bp-groups-forums.php */
 	$topic_title = apply_filters( 'group_forum_topic_title_before_save', $topic_title );
+
+	/** This filter is documented in bp-groups/bp-groups-forums.php */
 	$topic_text  = apply_filters( 'group_forum_topic_text_before_save',  $topic_text  );
 	$topic       = bp_forums_update_topic( array(
 		'topic_title' => $topic_title,
@@ -230,12 +372,20 @@ function groups_update_group_forum_topic( $topic_id, $topic_title, $topic_text, 
 	$activity_action  = sprintf( __( '%1$s edited the forum topic %2$s in the group %3$s', 'buddypress'), bp_core_get_userlink( $topic->topic_poster ), '<a href="' . bp_get_group_permalink( groups_get_current_group() ) . 'forum/topic/' . $topic->topic_slug .'/">' . esc_attr( $topic->topic_title ) . '</a>', '<a href="' . bp_get_group_permalink( groups_get_current_group() ) . '">' . esc_attr( bp_get_current_group_name() ) . '</a>' );
 	$activity_content = bp_create_excerpt( $topic_text );
 
-	// Record this in activity streams
+	/** This filter is documented in bp-groups/bp-groups-forums.php */
+	$action = apply_filters_ref_array( 'groups_activity_new_forum_topic_action',  array( $activity_action,  $topic_text, &$topic ) );
+
+	/** This filter is documented in bp-groups/bp-groups-forums.php */
+	$content = apply_filters_ref_array( 'groups_activity_new_forum_topic_content', array( $activity_content, $topic_text, &$topic ) );
+
+	/** This filter is documented in bp-groups/bp-groups-forums.php */
+	$primary_link = apply_filters( 'groups_activity_new_forum_topic_primary_link', bp_get_group_permalink( groups_get_current_group() ) . 'forum/topic/' . $topic->topic_slug . '/' );
+
 	groups_record_activity( array(
 		'id'                => $id,
-		'action'            => apply_filters_ref_array( 'groups_activity_new_forum_topic_action',  array( $activity_action,  $topic_text, &$topic ) ),
-		'content'           => apply_filters_ref_array( 'groups_activity_new_forum_topic_content', array( $activity_content, $topic_text, &$topic ) ),
-		'primary_link'      => apply_filters( 'groups_activity_new_forum_topic_primary_link', bp_get_group_permalink( groups_get_current_group() ) . 'forum/topic/' . $topic->topic_slug . '/' ),
+		'action'            => $action,
+		'content'           => $content,
+		'primary_link'      => $primary_link,
 		'type'              => 'new_forum_topic',
 		'item_id'           => (int) bp_get_current_group_id(),
 		'user_id'           => (int) $topic->topic_poster,
@@ -243,6 +393,13 @@ function groups_update_group_forum_topic( $topic_id, $topic_title, $topic_text, 
 		'recorded_time '    => $topic->topic_time
 	) );
 
+	/**
+	 * Fires after the update of a group forum topic.
+	 *
+	 * @since BuddyPress (1.1.0)
+	 *
+	 * @param object $topic Object holding current topic being updated. Passed by reference.
+	 */
 	do_action_ref_array( 'groups_update_group_forum_topic', array( &$topic ) );
 
 	return $topic;
@@ -265,7 +422,10 @@ function groups_update_group_forum_topic( $topic_id, $topic_title, $topic_text, 
 function groups_update_group_forum_post( $post_id, $post_text, $topic_id, $page = false ) {
 	$bp = buddypress();
 
+	/** This filter is documented in bp-groups/bp-groups-forums.php */
 	$post_text = apply_filters( 'group_forum_post_text_before_save', $post_text );
+
+	/** This filter is documented in bp-groups/bp-groups-forums.php */
 	$topic_id  = apply_filters( 'group_forum_post_topic_id_before_save', $topic_id );
 	$post      = bp_forums_get_post( $post_id );
 	$post_id   = bp_forums_insert_post( array(
@@ -300,12 +460,20 @@ function groups_update_group_forum_post( $post_id, $post_text, $topic_id, $page 
 		 ) );
 	}
 
-	// Update the entry in activity streams
+	/** This filter is documented in bp-groups/bp-groups-forums.php */
+	$action = apply_filters_ref_array( 'groups_activity_new_forum_post_action',  array( $activity_action,  $post_text, &$topic, &$topic ) );
+
+	/** This filter is documented in bp-groups/bp-groups-forums.php */
+	$content = apply_filters_ref_array( 'groups_activity_new_forum_post_content', array( $activity_content, $post_text, &$topic, &$topic ) );
+
+	/** This filter is documented in bp-groups/bp-groups-forums.php */
+	$filtered_primary_link = apply_filters( 'groups_activity_new_forum_post_primary_link', $primary_link . "#post-" . $post_id );
+
 	groups_record_activity( array(
 		'id'                => $id,
-		'action'            => apply_filters_ref_array( 'groups_activity_new_forum_post_action',  array( $activity_action,  $post_text, &$topic, &$topic ) ),
-		'content'           => apply_filters_ref_array( 'groups_activity_new_forum_post_content', array( $activity_content, $post_text, &$topic, &$topic ) ),
-		'primary_link'      => apply_filters( 'groups_activity_new_forum_post_primary_link', $primary_link . "#post-" . $post_id ),
+		'action'            => $action,
+		'content'           => $content,
+		'primary_link'      => $filtered_primary_link,
 		'type'              => 'new_forum_post',
 		'item_id'           => (int) bp_get_current_group_id(),
 		'user_id'           => (int) $post->poster_id,
@@ -313,6 +481,14 @@ function groups_update_group_forum_post( $post_id, $post_text, $topic_id, $page 
 		'recorded_time'     => $post->post_time
 	) );
 
+	/**
+	 * Fires after the update of a group forum post.
+	 *
+	 * @since BuddyPress (1.1.0)
+	 *
+	 * @param object $post  Object holding current post being updated.
+	 * @param object $topic Object holding current topic details. Passed by reference.
+	 */
 	do_action_ref_array( 'groups_update_group_forum_post', array( $post, &$topic ) );
 
 	return $post_id;
@@ -336,6 +512,14 @@ function groups_delete_group_forum_topic( $topic_id ) {
 	$action = bp_forums_delete_topic( array( 'topic_id' => $topic_id ) );
 
 	if ( !empty( $action ) ) {
+
+		/**
+		 * Fires before the deletion of a group forum topic.
+		 *
+		 * @since BuddyPress (1.2.9)
+		 *
+		 * @param int $topic_id ID of the topic to be deleted.
+		 */
 		do_action( 'groups_before_delete_group_forum_topic', $topic_id );
 
 		// Delete the corresponding activity stream items
@@ -360,6 +544,13 @@ function groups_delete_group_forum_topic( $topic_id ) {
 			}
 		}
 
+		/**
+		 * Fires after the deletion of a group forum topic.
+		 *
+		 * @since BuddyPress (1.1.0)
+		 *
+		 * @param int $topic_id ID of the topic that was deleted.
+		 */
 		do_action( 'groups_delete_group_forum_topic', $topic_id );
 	}
 
@@ -384,6 +575,15 @@ function groups_delete_group_forum_post( $post_id, $topic_id = false ) {
 	$action = bp_forums_delete_post( array( 'post_id' => $post_id ) );
 
 	if ( !empty( $action ) ) {
+
+		/**
+		 * Fires before the deletion of a group forum post.
+		 *
+		 * @since BuddyPress (1.5.0)
+		 *
+		 * @param int $post_id  ID of the post to be deleted.
+		 * @param int $topic_id ID of the associated topic.
+		 */
 		do_action( 'groups_before_delete_group_forum_post', $post_id, $topic_id );
 
 		// Delete the corresponding activity stream item
@@ -396,6 +596,14 @@ function groups_delete_group_forum_post( $post_id, $topic_id = false ) {
 			) );
 		}
 
+		/**
+		 * Fires after the deletion of a group forum post.
+		 *
+		 * @since BuddyPress (1.1.0)
+		 *
+		 * @param int $post_id  ID of the post that was deleted.
+		 * @param int $topic_id ID of the associated topic.
+		 */
 		do_action( 'groups_delete_group_forum_post', $post_id, $topic_id );
 	}
 
@@ -412,6 +620,14 @@ function groups_delete_group_forum_post( $post_id, $topic_id = false ) {
  * @return int The topic count.
  */
 function groups_total_public_forum_topic_count( $type = 'newest' ) {
+
+	/**
+	 * Filters the total count of all public topics of a given type, across groups/forums.
+	 *
+	 * @since BuddyPress (1.1.0)
+	 *
+	 * @param int $value Total count of all public topics.
+	 */
 	return apply_filters( 'groups_total_public_forum_topic_count', BP_Groups_Group::get_global_forum_topic_count( $type ) );
 }
 
@@ -426,5 +642,13 @@ function groups_total_public_forum_topic_count( $type = 'newest' ) {
  * @return int The topic count.
  */
 function groups_total_forum_topic_count( $status = 'public', $search_terms = false ) {
+
+	/**
+	 * Filters the total count of all topics of a given status, across groups/forums.
+	 *
+	 * @since BuddyPress (1.5.0)
+	 *
+	 * @param int $value Total count of all topics.
+	 */
 	return apply_filters( 'groups_total_forum_topic_count', BP_Groups_Group::get_global_topic_count( $status, $search_terms ) );
 }
