a45c0acb901c733b0b90845a716d871b85c056e5
Boone B Gorges <boonebgorges@gmail.com>
2015-04-01 01:23:37 +0000

    Improve localizability of screen reader text on bp-members list tables.
    See #6149.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@9670 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-members/admin/bp-members-admin-classes.php
+++ src/bp-members/admin/bp-members-admin-classes.php
@@ -231,7 +231,7 @@ class BP_Members_List_Table extends WP_Users_List_Table {
 	 */
 	public function column_cb( $signup_object = null ) {
 	?>
-		<label class="screen-reader-text" for="signup_<?php echo intval( $signup_object->id ); ?>"><?php printf( esc_html__( 'Select %s', 'buddypress' ), $signup_object->user_login ); ?></label>
+		<label class="screen-reader-text" for="signup_<?php echo intval( $signup_object->id ); ?>"><?php printf( esc_html__( 'Select user: %s', 'buddypress' ), $signup_object->user_login ); ?></label>
 		<input type="checkbox" id="signup_<?php echo intval( $signup_object->id ) ?>" name="allsignups[]" value="<?php echo esc_attr( $signup_object->id ) ?>" />
 		<?php
 	}
@@ -581,7 +581,7 @@ class BP_Members_MS_List_Table extends WP_MS_Users_List_Table {
 	 */
 	public function column_cb( $signup_object = null ) {
 	?>
-		<label class="screen-reader-text" for="signup_<?php echo intval( $signup_object->id ); ?>"><?php printf( esc_html__( 'Select %s', 'buddypress' ), $signup_object->user_login ); ?></label>
+		<label class="screen-reader-text" for="signup_<?php echo intval( $signup_object->id ); ?>"><?php printf( esc_html__( 'Select user: %s', 'buddypress' ), $signup_object->user_login ); ?></label>
 		<input type="checkbox" id="signup_<?php echo intval( $signup_object->id ) ?>" name="allsignups[]" value="<?php echo esc_attr( $signup_object->id ) ?>" />
 		<?php
 	}
