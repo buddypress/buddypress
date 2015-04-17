08f8fb6d941a5f70a6cce97414bc6991abc93ba3
Michael Beckwith <tw2113@gmail.com>
2015-04-17 03:19:17 +0000

    Adds hooks documentation for the blogs template files.
    See #5948.
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@9761 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-templates/bp-legacy/buddypress/blogs/blogs-loop.php
+++ src/bp-templates/bp-legacy/buddypress/blogs/blogs-loop.php
@@ -11,7 +11,14 @@
 
 ?>
 
-<?php do_action( 'bp_before_blogs_loop' ); ?>
+<?php
+
+/**
+ * Fires before the start of the blogs loop.
+ *
+ * @since BuddyPress (1.2.0)
+ */
+do_action( 'bp_before_blogs_loop' ); ?>
 
 <?php if ( bp_has_blogs( bp_ajax_querystring( 'blogs' ) ) ) : ?>
 
@@ -27,7 +34,14 @@
 
 	</div>
 
-	<?php do_action( 'bp_before_directory_blogs_list' ); ?>
+	<?php
+
+	/**
+	 * Fires before the blogs directory list.
+	 *
+	 * @since BuddyPress (1.1.0)
+	 */
+	do_action( 'bp_before_directory_blogs_list' ); ?>
 
 	<ul id="blogs-list" class="item-list" role="main">
 
@@ -42,12 +56,26 @@
 				<div class="item-title"><a href="<?php bp_blog_permalink(); ?>"><?php bp_blog_name(); ?></a></div>
 				<div class="item-meta"><span class="activity"><?php bp_blog_last_active(); ?></span></div>
 
-				<?php do_action( 'bp_directory_blogs_item' ); ?>
+				<?php
+
+				/**
+				 * Fires after the listing of a blog item in the blogs loop.
+				 *
+				 * @since BuddyPress (1.2.0)
+				 */
+				do_action( 'bp_directory_blogs_item' ); ?>
 			</div>
 
 			<div class="action">
 
-				<?php do_action( 'bp_directory_blogs_actions' ); ?>
+				<?php
+
+				/**
+				 * Fires inside the blogs action listing area.
+				 *
+				 * @since BuddyPress (1.1.0)
+				 */
+				do_action( 'bp_directory_blogs_actions' ); ?>
 
 				<div class="meta">
 
@@ -64,7 +92,14 @@
 
 	</ul>
 
-	<?php do_action( 'bp_after_directory_blogs_list' ); ?>
+	<?php
+
+	/**
+	 * Fires after the blogs directory list.
+	 *
+	 * @since BuddyPress (1.1.0)
+	 */
+	do_action( 'bp_after_directory_blogs_list' ); ?>
 
 	<?php bp_blog_hidden_fields(); ?>
 
@@ -92,4 +127,11 @@
 
 <?php endif; ?>
 
-<?php do_action( 'bp_after_blogs_loop' ); ?>
+<?php
+
+/**
+ * Fires after the display of the blogs loop.
+ *
+ * @since BuddyPress (1.2.0)
+ */
+do_action( 'bp_after_blogs_loop' ); ?>
--- src/bp-templates/bp-legacy/buddypress/blogs/index.php
+++ src/bp-templates/bp-legacy/buddypress/blogs/index.php
@@ -1,9 +1,23 @@
-<?php do_action( 'bp_before_directory_blogs' ); ?>
+<?php
+
+/**
+ * Fires at the top of the blogs directory template file.
+ *
+ * @since BuddyPress (1.5.0)
+ */
+do_action( 'bp_before_directory_blogs' ); ?>
 
 <div id="buddypress">
 	<form action="" method="post" id="blogs-directory-form" class="dir-form">
 
-		<?php do_action( 'bp_before_directory_blogs_content' ); ?>
+		<?php
+
+		/**
+		 * Fires before the display of the blogs listing content.
+		 *
+		 * @since BuddyPress (1.1.0)
+		 */
+		do_action( 'bp_before_directory_blogs_content' ); ?>
 
 		<div id="blog-dir-search" class="dir-search" role="search">
 
@@ -21,7 +35,14 @@
 
 				<?php endif; ?>
 
-				<?php do_action( 'bp_blogs_directory_blog_types' ); ?>
+				<?php
+
+				/**
+				 * Fires inside the unordered list displaying blog types.
+				 *
+				 * @since BuddyPress (1.2.0)
+				 */
+				do_action( 'bp_blogs_directory_blog_types' ); ?>
 
 			</ul>
 		</div><!-- .item-list-tabs -->
@@ -29,7 +50,14 @@
 		<div class="item-list-tabs" id="subnav" role="navigation">
 			<ul>
 
-				<?php do_action( 'bp_blogs_directory_blog_sub_types' ); ?>
+				<?php
+
+				/**
+				 * Fires inside the unordered list displaying blog sub-types.
+				 *
+				 * @since BuddyPress (1.5.0)
+				 */
+				do_action( 'bp_blogs_directory_blog_sub_types' ); ?>
 
 				<li id="blogs-order-select" class="last filter">
 
@@ -39,7 +67,14 @@
 						<option value="newest"><?php _e( 'Newest', 'buddypress' ); ?></option>
 						<option value="alphabetical"><?php _e( 'Alphabetical', 'buddypress' ); ?></option>
 
-						<?php do_action( 'bp_blogs_directory_order_options' ); ?>
+						<?php
+
+						/**
+						 * Fires inside the select input listing blogs orderby options.
+						 *
+						 * @since BuddyPress (1.2.0)
+						 */
+						do_action( 'bp_blogs_directory_order_options' ); ?>
 
 					</select>
 				</li>
@@ -52,14 +87,35 @@
 
 		</div><!-- #blogs-dir-list -->
 
-		<?php do_action( 'bp_directory_blogs_content' ); ?>
+		<?php
+
+		/**
+		 * Fires inside and displays the blogs content.
+		 *
+		 * @since BuddyPress (1.1.0)
+		 */
+		do_action( 'bp_directory_blogs_content' ); ?>
 
 		<?php wp_nonce_field( 'directory_blogs', '_wpnonce-blogs-filter' ); ?>
 
-		<?php do_action( 'bp_after_directory_blogs_content' ); ?>
+		<?php
+
+		/**
+		 * Fires after the display of the blogs listing content.
+		 *
+		 * @since BuddyPress (1.1.0)
+		 */
+		do_action( 'bp_after_directory_blogs_content' ); ?>
 
 	</form><!-- #blogs-directory-form -->
 
-	<?php do_action( 'bp_after_directory_blogs' ); ?>
+	<?php
+
+	/**
+	 * Fires at the top of the blogs directory template file.
+	 *
+	 * @since BuddyPress (1.5.0)
+	 */
+	do_action( 'bp_after_directory_blogs' ); ?>
 
 </div>
