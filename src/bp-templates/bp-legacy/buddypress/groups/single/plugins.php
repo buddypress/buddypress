112898135db3a1e1e55acc2b6003c9c5b7e59104
Michael Beckwith <tw2113@gmail.com>
2015-04-28 01:03:58 +0000

    Adds hooks documentation for the groups templates plugins.php file.
    See #5948
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@9812 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-templates/bp-legacy/buddypress/groups/single/plugins.php
+++ src/bp-templates/bp-legacy/buddypress/groups/single/plugins.php
@@ -1,5 +1,26 @@
-<?php do_action( 'bp_before_group_plugin_template' ); ?>
+<?php
 
-<?php do_action( 'bp_template_content' ); ?>
+/**
+ * Fires before the display of content for plugins using the BP_Group_Extension.
+ *
+ * @since BuddyPress (1.2.0)
+ */
+do_action( 'bp_before_group_plugin_template' ); ?>
 
-<?php do_action( 'bp_after_group_plugin_template' );
+<?php
+
+/**
+ * Fires and displays content for plugins using the BP_Group_Extension.
+ *
+ * @since BuddyPress (1.0.0)
+ */
+do_action( 'bp_template_content' ); ?>
+
+<?php
+
+/**
+ * Fires after the display of content for plugins using the BP_Group_Extension.
+ *
+ * @since BuddyPress (1.2.0)
+ */
+do_action( 'bp_after_group_plugin_template' );
