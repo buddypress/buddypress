a836c3fd392f24d3fce88f4fd96d52cac0ba9f95
Mathieu Viet <mathchristo@yahoo.fr>
2015-04-29 17:19:33 +0000

    Add unit tests for BuddyPress navigation functions.
    See #6026
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@9818 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- tests/phpunit/testcases/core/nav.php
+++ tests/phpunit/testcases/core/nav.php
@@ -5,6 +5,151 @@
  * @group nav
  */
 class BP_Tests_Core_Nav extends BP_UnitTestCase {
+
+	/**
+	 * @group bp_core_new_nav_item
+	 */
+	public function test_bp_core_new_nav_item_user_nav() {
+		$bp_nav = buddypress()->bp_nav;
+
+		$u = $this->factory->user->create();
+		$old_current_user = get_current_user_id();
+		$this->set_current_user( $u );
+
+		$this->go_to( bp_core_get_user_domain( $u ) );
+
+		bp_core_new_nav_item( array(
+			'name'                    => 'Foo',
+			'slug'                    => 'foo',
+			'position'                => 25,
+			'screen_function'         => 'foo_screen_function',
+			'default_subnav_slug'     => 'foo-sub'
+		) );
+
+		$expected = array(
+			'name'                    => 'Foo',
+			'slug'                    => 'foo',
+			'link'                    => trailingslashit( bp_core_get_user_domain( $u ) . 'foo' ),
+			'css_id'                  => 'foo',
+			'show_for_displayed_user' => true,
+			'position'                => 25,
+			'screen_function'         => 'foo_screen_function',
+			'default_subnav_slug'     => 'foo-sub'
+		);
+
+		$this->assertSame( buddypress()->bp_nav['foo'], $expected );
+
+		// Clean up
+		buddypress()->bp_nav = $bp_nav;
+		$this->set_current_user( $old_current_user );
+	}
+
+	/**
+	 * @group bp_core_new_nav_item
+	 */
+	public function test_bp_core_new_nav_item_group_nav() {
+		$bp_nav = buddypress()->bp_nav;
+
+		$u = $this->factory->user->create();
+		$g = $this->factory->group->create();
+		$old_current_user = get_current_user_id();
+		$this->set_current_user( $u );
+
+		$group = groups_get_group( array(
+			'group_id' => $g,
+		) );
+
+		$this->go_to( bp_get_group_permalink( $group ) );
+
+		$this->assertTrue( buddypress()->bp_nav[ $group->slug ]['position'] === -1 );
+
+		// Clean up
+		buddypress()->bp_nav = $bp_nav;
+		$this->set_current_user( $old_current_user );
+	}
+
+	/**
+	 * @group bp_core_sort_nav_items
+	 */
+	public function test_bp_core_sort_nav_items() {
+		$bp_nav = buddypress()->bp_nav;
+
+		$u = $this->factory->user->create();
+		$old_current_user = get_current_user_id();
+		$this->set_current_user( $u );
+
+		$this->go_to( bp_core_get_user_domain( $u ) );
+
+		bp_core_new_nav_item( array(
+			'name'                    => 'Foo',
+			'slug'                    => 'foo',
+			'position'                => 25,
+			'screen_function'         => 'foo_screen_function',
+			'default_subnav_slug'     => 'foo-sub'
+		) );
+
+		$expected = array(
+			'name'                    => 'Foo',
+			'slug'                    => 'foo',
+			'link'                    => trailingslashit( bp_core_get_user_domain( $u ) . 'foo' ),
+			'css_id'                  => 'foo',
+			'show_for_displayed_user' => true,
+			'position'                => 25,
+			'screen_function'         => 'foo_screen_function',
+			'default_subnav_slug'     => 'foo-sub'
+		);
+
+		bp_core_sort_nav_items();
+
+		$this->assertSame( buddypress()->bp_nav[25], $expected );
+
+		// Clean up
+		buddypress()->bp_nav = $bp_nav;
+		$this->set_current_user( $old_current_user );
+	}
+
+	/**
+	 * @group bp_core_new_subnav_item
+	 */
+	public function test_bp_core_new_subnav_item_user_subnav() {
+		$bp_options_nav = buddypress()->bp_options_nav;
+
+		$u = $this->factory->user->create();
+		$old_current_user = get_current_user_id();
+		$this->set_current_user( $u );
+
+		$user_domain = bp_core_get_user_domain( $u );
+
+		$this->go_to( $user_domain );
+
+		bp_core_new_subnav_item( array(
+			'name'            => 'Foo',
+			'slug'            => 'foo',
+			'parent_url'      => trailingslashit( $user_domain . 'foo' ),
+			'parent_slug'     => 'foo',
+			'screen_function' => 'foo_screen_function',
+			'position'        => 10
+		) );
+
+		$expected = array(
+			'name'              => 'Foo',
+			'link'              => trailingslashit( $user_domain . 'foo/foo' ),
+			'slug'              => 'foo',
+			'css_id'            => 'foo',
+			'position'          => 10,
+			'user_has_access'   => true,
+			'no_access_url'     => '',
+			'screen_function'   => 'foo_screen_function',
+			'show_in_admin_bar' => false,
+		);
+
+		$this->assertSame( buddypress()->bp_options_nav['foo']['foo'], $expected );
+
+		// Clean up
+		buddypress()->bp_options_nav = $bp_options_nav;
+		$this->set_current_user( $old_current_user );
+	}
+
 	/**
 	 * @group bp_core_new_subnav_item
 	 */
@@ -399,5 +544,49 @@ class BP_Tests_Core_Nav extends BP_UnitTestCase {
 		) );
 
 		$this->assertSame( $link, buddypress()->bp_options_nav['foo']['bar']['link'] );
+	}
+
+	/**
+	 * @group bp_core_sort_subnav_items
+	 */
+	public function test_bp_core_sort_subnav_items() {
+		$bp_options_nav = buddypress()->bp_options_nav;
+
+		$u = $this->factory->user->create();
+		$old_current_user = get_current_user_id();
+		$this->set_current_user( $u );
+
+		$user_domain = bp_core_get_user_domain( $u );
+
+		$this->go_to( $user_domain );
+
+		bp_core_new_subnav_item( array(
+			'name'            => 'Foo',
+			'slug'            => 'foo',
+			'parent_url'      => trailingslashit( $user_domain . 'foo' ),
+			'parent_slug'     => 'foo',
+			'screen_function' => 'foo_screen_function',
+			'position'        => 10
+		) );
+
+		bp_core_sort_subnav_items();
+
+		$expected = array(
+			'name'              => 'Foo',
+			'link'              => trailingslashit( $user_domain . 'foo/foo' ),
+			'slug'              => 'foo',
+			'css_id'            => 'foo',
+			'position'          => 10,
+			'user_has_access'   => true,
+			'no_access_url'     => '',
+			'screen_function'   => 'foo_screen_function',
+			'show_in_admin_bar' => false,
+		);
+
+		$this->assertSame( buddypress()->bp_options_nav['foo'][10], $expected );
+
+		// Clean up
+		buddypress()->bp_options_nav = $bp_options_nav;
+		$this->set_current_user( $old_current_user );
 	}
 }
