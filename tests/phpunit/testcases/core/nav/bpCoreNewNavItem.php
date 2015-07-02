676a7379ea593eaae276f45fec7cb6c5d85d7cda
Boone B Gorges <boonebgorges@gmail.com>
2015-07-02 20:01:20 +0000

    Improved unit tests for `bp_core_new_nav_item()`.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@9990 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- tests/phpunit/testcases/core/nav/bpCoreNewNavItem.php
+++ tests/phpunit/testcases/core/nav/bpCoreNewNavItem.php
@@ -60,4 +60,68 @@ class BP_Tests_Core_Nav_BpCoreNewNavItem extends BP_UnitTestCase {
 		buddypress()->bp_nav = $bp_nav;
 		$this->set_current_user( $old_current_user );
 	}
+
+	public function test_should_return_false_if_name_is_not_provided() {
+		$args = array(
+			'slug' => 'foo',
+		);
+
+		$this->assertFalse( bp_core_new_nav_item( $args ) );
+	}
+
+	public function test_should_return_false_if_slug_is_not_provided() {
+		$args = array(
+			'name' => 'foo',
+		);
+
+		$this->assertFalse( bp_core_new_nav_item( $args ) );
+	}
+
+	public function test_should_return_false_if_site_admin_only_and_current_user_cannot_bp_moderate() {
+		// Should already be set to a 0 user.
+		$this->assertFalse( bp_current_user_can( 'bp_moderate' ) );
+		$args = array(
+			'name' => 'Foo',
+			'slug' => 'foo',
+			'site_admin_only' => true,
+		);
+
+		$this->assertFalse( bp_core_new_nav_item( $args ) );
+	}
+
+	public function test_css_id_should_fall_back_on_slug() {
+		$args = array(
+			'name' => 'Foo',
+			'slug' => 'foo',
+		);
+		bp_core_new_nav_item( $args );
+
+		$this->assertSame( 'foo', buddypress()->bp_nav['foo']['css_id'] );
+	}
+
+	public function test_css_id_should_be_respected() {
+		$args = array(
+			'name' => 'Foo',
+			'slug' => 'foo',
+			'item_css_id' => 'bar',
+		);
+		bp_core_new_nav_item( $args );
+
+		$this->assertSame( 'bar', buddypress()->bp_nav['foo']['css_id'] );
+	}
+
+	public function test_show_for_displayed_user_false_should_force_function_to_return_false_when_bp_user_has_access_is_also_false() {
+		$args = array(
+			'name' => 'Foo',
+			'slug' => 'foo',
+			'show_for_displayed_user' => false,
+		);
+
+		add_filter( 'bp_user_has_access', '__return_false' );
+		$retval = bp_core_new_nav_item( $args );
+		remove_filter( 'bp_user_has_access', '__return_false' );
+
+		$this->assertFalse( $retval );
+	}
+
 }
