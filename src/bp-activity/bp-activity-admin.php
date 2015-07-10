54299e8d3bb24be2bb76fdc2cc5f351834851821
John James Jacoby <johnjamesjacoby@me.com>
2015-07-10 21:55:53 +0000

    Admin: Add new `wp-list-table` class to Activity & Groups list tables.
    The commit ensures BuddyPress's list-table implementation integrates nicely wih WordPress's default administration styling. It also addresses a WordPress 4.3 compatibilty issue introduced in [WP33016] that left the new `.toggle-row` button visible on our rows.
    
    See #6465. For 2.4 (trunk)
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@10005 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-activity/bp-activity-admin.php
+++ src/bp-activity/bp-activity-admin.php
@@ -1219,7 +1219,7 @@ class BP_Activity_List_Table extends WP_List_Table {
 	function display() {
 		$this->display_tablenav( 'top' ); ?>
 
-		<table class="<?php echo implode( ' ', $this->get_table_classes() ); ?>" cellspacing="0">
+		<table class="wp-list-table <?php echo implode( ' ', $this->get_table_classes() ); ?>" cellspacing="0">
 			<thead>
 				<tr>
 					<?php $this->print_column_headers(); ?>
