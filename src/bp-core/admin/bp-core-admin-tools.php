563b363408a3e6e2843f108756e902b64a3b52de
Boone B Gorges <boonebgorges@gmail.com>
2015-07-21 22:02:47 +0000

    Available Tools selectors should match WP 4.2+.
    Merges [10024] to the 2.3 branch.
    
    Props anthonyvalera.
    Fixes #6561.
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/branches/2.3@10025 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-core/admin/bp-core-admin-tools.php
+++ src/bp-core/admin/bp-core-admin-tools.php
@@ -397,8 +397,8 @@ function bp_core_admin_available_tools_intro() {
 	$page = bp_core_do_network_admin() ? 'admin.php' : 'tools.php' ;
 	$url  = add_query_arg( $query_arg, bp_get_admin_url( $page ) );
 	?>
-	<div class="tool-box">
-		<h3 class="title"><?php esc_html_e( 'BuddyPress Tools', 'buddypress' ) ?></h3>
+	<div class="card tool-box">
+		<h3><?php esc_html_e( 'BuddyPress Tools', 'buddypress' ) ?></h3>
 		<p>
 			<?php esc_html_e( 'BuddyPress keeps track of various relationships between users, groups, and activity items. Occasionally these relationships become out of sync, most often after an import, update, or migration.', 'buddypress' ); ?>
 			<?php printf( esc_html_x( 'Use the %s to repair these relationships.', 'buddypress tools intro', 'buddypress' ), '<a href="' . esc_url( $url ) . '">' . esc_html__( 'BuddyPress Tools', 'buddypress' ) . '</a>' ); ?>
