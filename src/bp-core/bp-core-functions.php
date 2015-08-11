48e77d721d8a9f2a68c7d04a83adea1f1623b188
r-a-y <r-a-y@gmx.com>
2015-08-11 17:03:48 +0000

    Core: Fix issue with `bp_verify_nonce_request()` when a port is used in the home URL.
    When a site is using a port for their home URL, `bp_verify_nonce_request()`
    can sometimes fail.  This is because the port is duplicated twice due to
    `$_SERVER['HTTP_HOST']` including the port number and we're appending the
    port number again with `$_SERVER['SERVER_PORT']`.
    
    This commit appends `$_SERVER['SERVER_PORT']` if `$_SERVER['HTTP_HOST']`
    does not already include the port number and includes a unit test.
    
    Fixes #6528.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@10044 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-core/bp-core-functions.php
+++ src/bp-core/bp-core-functions.php
@@ -2055,7 +2055,7 @@ function bp_verify_nonce_request( $action = '', $query_arg = '_wpnonce' ) {
 	/** Requested URL *********************************************************/
 
 	// Maybe include the port, if it's included in home_url()
-	if ( isset( $parsed_home['port'] ) ) {
+	if ( isset( $parsed_home['port'] ) && false === strpos( $_SERVER['HTTP_HOST'], ':' ) ) {
 		$request_host = $_SERVER['HTTP_HOST'] . ':' . $_SERVER['SERVER_PORT'];
 	} else {
 		$request_host = $_SERVER['HTTP_HOST'];
---
+++ tests/phpunit/testcases/core/functions/bpVerifyNonceRequest.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * @group core
+ * @group functions
+ * @group bp_verify_nonce_request
+ */
+class BP_Tests_Core_Functions_BPVerifyNonceRequest extends BP_UnitTestCase {
+	private $http_host = '';
+	private $server_port = '';
+	private $request_uri = '';
+
+	public function setUp() {
+		parent::setUp();
+
+		if ( isset( $_SERVER['HTTP_HOST'] ) ) {
+			$this->http_host = $_SERVER['HTTP_HOST'];
+		}
+
+		if ( isset( $_SERVER['SERVER_PORT'] ) ) {
+			$this->server_port = $_SERVER['SERVER_PORT'];
+		}
+
+		if ( isset( $_SERVER['REQUEST_URI'] ) ) {
+			$this->request_uri = $_SERVER['REQUEST_URI'];
+		}
+	}
+
+	public function tearDown() {
+		if ( '' !== $this->http_host ) {
+			$_SERVER['HTTP_HOST'] = $this->http_host;
+		}
+
+		if ( '' !== $this->server_port ) {
+			$_SERVER['SERVER_PORT'] = $this->server_port;
+		}
+
+		if ( '' !== $this->request_uri ) {
+			$_SERVER['REQUEST_URI'] = $this->request_uri;
+		}
+
+		parent::tearDown();
+	}
+
+	public function test_bp_verify_nonce_request_with_port_in_home_url_and_wordpress_installed_in_subdirectory() {
+		// fake various $_SERVER parameters
+		$host = explode( ':', $_SERVER['HTTP_HOST'] );
+		$_SERVER['HTTP_HOST'] = $host[0] . ':80';
+		$_SERVER['SERVER_PORT'] = 80;
+		$_SERVER['REQUEST_URI'] = '/wordpress/';
+
+		// add port number and subdirecotry to home URL for testing
+		add_filter( 'home_url', array( $this, 'add_port_and_subdirectory_to_home_url' ), 10, 3 );
+
+		// test bp_verify_nonce_request()
+		$action = 'verify-this';
+		$_REQUEST[$action] = wp_create_nonce( $action );
+		$test = bp_verify_nonce_request( $action, $action );
+
+		// clean up!
+		remove_filter( 'home_url', array( $this, 'add_port_and_subdirectory_to_home_url' ), 10, 3 );
+		unset( $_REQUEST[$action] );
+
+		// assert!
+		$this->assertSame( 1, $test );
+	}
+
+	/**
+	 * Add port 80 and /wordpress/ subdirectory to home URL.
+	 *
+	 * @param string      $url         The complete home URL including scheme and path.
+	 * @param string      $path        Path relative to the home URL. Blank string if no path is specified.
+	 * @param string|null $orig_scheme Scheme to give the home URL context. Accepts 'http', 'https', 'relative' or null.
+	 * @return string
+	 */
+	public function add_port_and_subdirectory_to_home_url( $url, $path, $scheme ) {
+		$home = parse_url( get_option( 'home' ) );
+		$home_path = isset( $home['path'] ) ? $home['path'] : '';
+		return $scheme . $home['host'] . ':80' . $home_path . '/wordpress' . $path;
+	}
+}
