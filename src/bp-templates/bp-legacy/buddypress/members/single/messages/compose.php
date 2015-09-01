f499e0869c6a94ff2606f15ba79a5f2b4ea17288
Mercime <mercijavier@gmail.com>
2015-09-01 21:06:39 +0000

    Fix 'for' value of `label` in Messages > Compose.
    Accessibility fix to bind `label` and `textarea` by matching the 'for' 
    value of the `label` tag with the 'id' value of the `textarea` input 
    field in the Message > Compose template.
    
    See #6531
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@10078 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-templates/bp-legacy/buddypress/members/single/messages/compose.php
+++ src/bp-templates/bp-legacy/buddypress/members/single/messages/compose.php
@@ -24,7 +24,7 @@
 	<label for="subject"><?php _e( 'Subject', 'buddypress' ); ?></label>
 	<input type="text" name="subject" id="subject" value="<?php bp_messages_subject_value(); ?>" />
 
-	<label for="content"><?php _e( 'Message', 'buddypress' ); ?></label>
+	<label for="message_content"><?php _e( 'Message', 'buddypress' ); ?></label>
 	<textarea name="content" id="message_content" rows="15" cols="40"><?php bp_messages_content_value(); ?></textarea>
 
 	<input type="hidden" name="send_to_usernames" id="send-to-usernames" value="<?php bp_message_get_recipient_usernames(); ?>" class="<?php bp_message_get_recipient_usernames(); ?>" />
