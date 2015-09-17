31729b5b24b9ad14130e25b94a7720ae7a085d1c
Mercime <mercijavier@gmail.com>
2015-09-17 16:00:26 +0000

    Members Widget: Replace the 'for' value with 'id' value of `input`.
    Accessibility fix which replaces the static 'for' value with the 
    dynamic 'id' value of the `input` field.
    
    See #6624.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@10118 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-members/bp-members-widgets.php
+++ src/bp-members/bp-members-widgets.php
@@ -195,7 +195,7 @@ class BP_Core_Members_Widget extends WP_Widget {
 		$link_title     = (bool) $settings['link_title']; ?>
 
 		<p>
-			<label for="bp-core-widget-title">
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>">
 				<?php esc_html_e( 'Title:', 'buddypress' ); ?>
 				<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" style="width: 100%" />
 			</label>
