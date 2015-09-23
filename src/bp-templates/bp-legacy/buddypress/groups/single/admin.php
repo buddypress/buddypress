8e8f0fd5b03e8dc0495d92b7ba16ac820a4011f3
Mercime <mercijavier@gmail.com>
2015-09-23 01:01:42 +0000

    Accessibility fix for Group Manage > Settings page.
    This changeset:
    * unwrap `labels` from `uls` for valid markups
    * add 'id' attributes to `radio inputs`
    * add 'aria-describedby' attributes to `radio inputs`
    * add 'id' attributes to `uls` to bind to 'aria-describedby' 
      in `radio inputs`
    * add 'for' attributes to `labels`
    * remove `<strong>` tags surrounding `label` text
    
    See #6618.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@10128 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-templates/bp-legacy/buddypress/groups/single/admin.php
+++ src/bp-templates/bp-legacy/buddypress/groups/single/admin.php
@@ -91,35 +91,31 @@ do_action( 'bp_before_group_admin_content' ); ?>
 	<h4><?php _e( 'Privacy Options', 'buddypress' ); ?></h4>
 
 	<div class="radio">
-		<label>
-			<input type="radio" name="group-status" value="public"<?php bp_group_show_status_setting( 'public' ); ?> />
-			<strong><?php _e( 'This is a public group', 'buddypress' ); ?></strong>
-			<ul>
-				<li><?php _e( 'Any site member can join this group.', 'buddypress' ); ?></li>
-				<li><?php _e( 'This group will be listed in the groups directory and in search results.', 'buddypress' ); ?></li>
-				<li><?php _e( 'Group content and activity will be visible to any site member.', 'buddypress' ); ?></li>
-			</ul>
-		</label>
 
-		<label>
-			<input type="radio" name="group-status" value="private"<?php bp_group_show_status_setting( 'private' ); ?> />
-			<strong><?php _e( 'This is a private group', 'buddypress' ); ?></strong>
-			<ul>
-				<li><?php _e( 'Only users who request membership and are accepted can join the group.', 'buddypress' ); ?></li>
-				<li><?php _e( 'This group will be listed in the groups directory and in search results.', 'buddypress' ); ?></li>
-				<li><?php _e( 'Group content and activity will only be visible to members of the group.', 'buddypress' ); ?></li>
-			</ul>
-		</label>
+		<label for="group-status-public"><input type="radio" name="group-status" id="group-status-public" value="public"<?php if ( 'public' == bp_get_new_group_status() || !bp_get_new_group_status() ) { ?> checked="checked"<?php } ?> aria-describedby="public-group-description" /> <?php _e( 'This is a public group', 'buddypress' ); ?></label>
 
-		<label>
-			<input type="radio" name="group-status" value="hidden"<?php bp_group_show_status_setting( 'hidden' ); ?> />
-			<strong><?php _e( 'This is a hidden group', 'buddypress' ); ?></strong>
-			<ul>
-				<li><?php _e( 'Only users who are invited can join the group.', 'buddypress' ); ?></li>
-				<li><?php _e( 'This group will not be listed in the groups directory or search results.', 'buddypress' ); ?></li>
-				<li><?php _e( 'Group content and activity will only be visible to members of the group.', 'buddypress' ); ?></li>
-			</ul>
-		</label>
+		<ul id="public-group-description">
+			<li><?php _e( 'Any site member can join this group.', 'buddypress' ); ?></li>
+			<li><?php _e( 'This group will be listed in the groups directory and in search results.', 'buddypress' ); ?></li>
+			<li><?php _e( 'Group content and activity will be visible to any site member.', 'buddypress' ); ?></li>
+		</ul>
+
+		<label for="group-status-private"><input type="radio" name="group-status" id="group-status-private" value="private"<?php if ( 'private' == bp_get_new_group_status() ) { ?> checked="checked"<?php } ?> aria-describedby="private-group-description" /> <?php _e( 'This is a private group', 'buddypress' ); ?></label>
+
+		<ul id="private-group-description">
+			<li><?php _e( 'Only users who request membership and are accepted can join the group.', 'buddypress' ); ?></li>
+			<li><?php _e( 'This group will be listed in the groups directory and in search results.', 'buddypress' ); ?></li>
+			<li><?php _e( 'Group content and activity will only be visible to members of the group.', 'buddypress' ); ?></li>
+		</ul>
+
+		<label for="group-status-hidden"><input type="radio" name="group-status" id="group-status-hidden" value="hidden"<?php if ( 'hidden' == bp_get_new_group_status() ) { ?> checked="checked"<?php } ?> aria-describedby="hidden-group-description" /> <?php _e('This is a hidden group', 'buddypress' ); ?></label>
+
+		<ul id="hidden-group-description">
+			<li><?php _e( 'Only users who are invited can join the group.', 'buddypress' ); ?></li>
+			<li><?php _e( 'This group will not be listed in the groups directory or search results.', 'buddypress' ); ?></li>
+			<li><?php _e( 'Group content and activity will only be visible to members of the group.', 'buddypress' ); ?></li>
+		</ul>
+
 	</div>
 
 	<hr />
@@ -129,20 +125,13 @@ do_action( 'bp_before_group_admin_content' ); ?>
 	<p><?php _e( 'Which members of this group are allowed to invite others?', 'buddypress' ); ?></p>
 
 	<div class="radio">
-		<label>
-			<input type="radio" name="group-invite-status" value="members"<?php bp_group_show_invite_status_setting( 'members' ); ?> />
-			<strong><?php _e( 'All group members', 'buddypress' ); ?></strong>
-		</label>
 
-		<label>
-			<input type="radio" name="group-invite-status" value="mods"<?php bp_group_show_invite_status_setting( 'mods' ); ?> />
-			<strong><?php _e( 'Group admins and mods only', 'buddypress' ); ?></strong>
-		</label>
+		<label for="group-invite-status-members"><input type="radio" name="group-invite-status" id="group-invite-status-members" value="members"<?php bp_group_show_invite_status_setting( 'members' ); ?> /> <?php _e( 'All group members', 'buddypress' ); ?></label>
 
-		<label>
-			<input type="radio" name="group-invite-status" value="admins"<?php bp_group_show_invite_status_setting( 'admins' ); ?> />
-			<strong><?php _e( 'Group admins only', 'buddypress' ); ?></strong>
-		</label>
+		<label for="group-invite-status-mods"><input type="radio" name="group-invite-status" id="group-invite-status-mods" value="mods"<?php bp_group_show_invite_status_setting( 'mods' ); ?> /> <?php _e( 'Group admins and mods only', 'buddypress' ); ?></label>
+
+		<label for="group-invite-status-admins"><input type="radio" name="group-invite-status" id="group-invite-status-admins" value="admins"<?php bp_group_show_invite_status_setting( 'admins' ); ?> /> <?php _e( 'Group admins only', 'buddypress' ); ?></label>
+
  	</div>
 
 	<hr />
