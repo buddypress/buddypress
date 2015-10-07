2aa5bba0cd0c4e0e76421e679a4b068494c2d537
Paul Gibbs <djpaul@gmail.com>
2015-10-07 14:54:44 +0000

    Bump 2.3 branch version to 2.3.4
    git-svn-id: https://buddypress.svn.wordpress.org/branches/2.3@10199 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-loader.php
+++ src/bp-loader.php
@@ -14,7 +14,7 @@
  * Description: BuddyPress helps you run any kind of social network on your WordPress, with member profiles, activity streams, user groups, messaging, and more.
  * Author:      The BuddyPress Community
  * Author URI:  https://buddypress.org/
- * Version:     2.3.3
+ * Version:     2.3.4
  * Text Domain: buddypress
  * Domain Path: /bp-languages/
  * License:     GPLv2 or later (license.txt)
@@ -309,7 +309,7 @@ class BuddyPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '2.3.3';
+		$this->version    = '2.3.4';
 		$this->db_version = 10071;
 
 		/** Loading ***********************************************************/
