192ae103329088348b3d40acecc4faf5259debbd
Boone B Gorges <boonebgorges@gmail.com>
2015-10-15 02:27:16 +0000

    Support for `WP_Hook` in `bp_get_template_stack()`.
    The proposed `WP_Hook` #WP17817 breaks backward compatibility for plugins that
    attempt to manipulate `$wp_filter` in certain ways. For BuddyPress, it means
    that we cannot directly modify the callbacks that are currently registered as
    an array at `$wp_filter[ $tag ]`, in `bp_get_template_stack()`. Instead, when
    `WP_Hook` is not available, we assign the callbacks to a variable (leveraging
    `WP_Hook`'s `ArrayAccess`) and manipulate the variable.
    
    Props jbrinley.
    Fixes #6649.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@10279 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-core/bp-core-template-loader.php
+++ src/bp-core/bp-core-template-loader.php
@@ -212,23 +212,29 @@ function bp_get_template_stack() {
 	$wp_current_filter[] = $tag;
 
 	// Sort
-	if ( ! isset( $merged_filters[ $tag ] ) ) {
-		ksort( $wp_filter[$tag] );
-		$merged_filters[ $tag ] = true;
+	if ( class_exists( 'WP_Hook' ) ) {
+		$filter = $wp_filter[ $tag ]->callbacks;
+	} else {
+		$filter = &$wp_filter[ $tag ];
+
+		if ( ! isset( $merged_filters[ $tag ] ) ) {
+			ksort( $filter );
+			$merged_filters[ $tag ] = true;
+		}
 	}
 
 	// Ensure we're always at the beginning of the filter array
-	reset( $wp_filter[ $tag ] );
+	reset( $filter );
 
 	// Loop through 'bp_template_stack' filters, and call callback functions
 	do {
-		foreach( (array) current( $wp_filter[$tag] ) as $the_ ) {
+		foreach( (array) current( $filter ) as $the_ ) {
 			if ( ! is_null( $the_['function'] ) ) {
 				$args[1] = $stack;
 				$stack[] = call_user_func_array( $the_['function'], array_slice( $args, 1, (int) $the_['accepted_args'] ) );
 			}
 		}
-	} while ( next( $wp_filter[$tag] ) !== false );
+	} while ( next( $filter ) !== false );
 
 	// Remove 'bp_template_stack' from the current filter array
 	array_pop( $wp_current_filter );
