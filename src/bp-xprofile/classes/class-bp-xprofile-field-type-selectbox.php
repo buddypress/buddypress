c7287bfc41399b41eb22eef521248c266fd50af4
Mercime <mercijavier@gmail.com>
2015-10-22 01:42:28 +0000

    xProfile: Add `label` for `selectbox` fields in wp-admin.
    See #6655.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@10300 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-xprofile/classes/class-bp-xprofile-field-type-selectbox.php
+++ src/bp-xprofile/classes/class-bp-xprofile-field-type-selectbox.php
@@ -161,11 +161,21 @@ class BP_XProfile_Field_Type_Selectbox extends BP_XProfile_Field_Type {
 	 * @param array $raw_properties Optional key/value array of permitted attributes that you want to add.
 	 */
 	public function admin_field_html( array $raw_properties = array() ) {
-		?>
 
-		<select <?php echo $this->get_edit_field_html_elements( $raw_properties ); ?>>
-			<?php bp_the_profile_field_options(); ?>
-		</select>
+		if ( is_admin() ) : ?>
+
+			<label for="<?php bp_the_profile_field_input_name(); ?>" class="screen-reader-text"><?php esc_html_e( 'Select', 'buddypress' ); ?></label>
+			<select <?php echo $this->get_edit_field_html_elements( $raw_properties ); ?>>
+				<?php bp_the_profile_field_options(); ?>
+			</select>
+
+		<?php else : ?>
+
+			<select <?php echo $this->get_edit_field_html_elements( $raw_properties ); ?>>
+				<?php bp_the_profile_field_options(); ?>
+			</select>
+
+		<?php endif; ?>
 
 		<?php
 	}
