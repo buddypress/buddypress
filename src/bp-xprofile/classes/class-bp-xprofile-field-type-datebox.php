456a4456bcbd2f83b47dc2388da8a71ccf447cb6
Mercime <mercijavier@gmail.com>
2015-10-27 00:29:16 +0000

    xProfile: Add `labels` for `datebox` field types in wp-admin.
    See #6655.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@10311 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-xprofile/classes/class-bp-xprofile-field-type-datebox.php
+++ src/bp-xprofile/classes/class-bp-xprofile-field-type-datebox.php
@@ -270,14 +270,17 @@ class BP_XProfile_Field_Type_Datebox extends BP_XProfile_Field_Type {
 			'name' => bp_get_the_profile_field_input_name() . '_year'
 		) ); ?>
 
+		<label for="<?php bp_the_profile_field_input_name(); ?>_day" class="screen-reader-text"><?php esc_html_e( 'Select day', 'buddypress' ); ?></label>
 		<select <?php echo $this->get_edit_field_html_elements( $day_r ); ?>>
 			<?php bp_the_profile_field_options( array( 'type' => 'day' ) ); ?>
 		</select>
 
+		<label for="<?php bp_the_profile_field_input_name(); ?>_month" class="screen-reader-text"><?php esc_html_e( 'Select month', 'buddypress' ); ?></label>
 		<select <?php echo $this->get_edit_field_html_elements( $month_r ); ?>>
 			<?php bp_the_profile_field_options( array( 'type' => 'month' ) ); ?>
 		</select>
 
+		<label for="<?php bp_the_profile_field_input_name(); ?>_year" class="screen-reader-text"><?php esc_html_e( 'Select year', 'buddypress' ); ?></label>
 		<select <?php echo $this->get_edit_field_html_elements( $year_r ); ?>>
 			<?php bp_the_profile_field_options( array( 'type' => 'year' ) ); ?>
 		</select>
