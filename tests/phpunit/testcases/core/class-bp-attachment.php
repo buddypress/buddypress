4aedd2d28a4bfd292619bf264bc8146b7bbe04ec
Mathieu Viet <mathchristo@yahoo.fr>
2016-02-03 21:20:30 +0000

    Fix the unit test about `BP_Attachment::get_image_data()`
    WordPress changed the type of the "Orientation" image metadata from an integer to a string. As we were using a strict comparison inside the unit test, it was failing.
    
    Fixes #6868
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@10502 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- tests/phpunit/testcases/core/class-bp-attachment.php
+++ tests/phpunit/testcases/core/class-bp-attachment.php
@@ -461,7 +461,7 @@ class BP_Tests_BP_Attachment_TestCases extends BP_UnitTestCase {
 	public function test_bp_attachment_get_image_data() {
 		$image_data = BP_Attachment::get_image_data( BP_TESTS_DIR . 'assets/upside-down.jpg' );
 
-		$this->assertTrue( 3 === $image_data['meta']['orientation'] );
+		$this->assertTrue( 3 == $image_data['meta']['orientation'] );
 	}
 
 	/**
