413d597d432aa657d63d04625f6b8863312cfcee
Paul Gibbs <djpaul@gmail.com>
2016-02-14 12:34:37 +0000

    Log in widget: avoid using HTML in translation strings, and escaping improvements.
    Fixes #6905, props ramiy
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@10591 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-core/classes/class-bp-core-login-widget.php
+++ src/bp-core/classes/class-bp-core-login-widget.php
@@ -116,7 +116,7 @@ class BP_Core_Login_Widget extends WP_Widget {
 
 				<?php if ( bp_get_signup_allowed() ) : ?>
 
-					<span class="bp-login-widget-register-link"><?php printf( __( '<a href="%s" title="Register for a new account">Register</a>', 'buddypress' ), bp_get_signup_page() ); ?></span>
+					<span class="bp-login-widget-register-link"><a href="<?php echo esc_url( bp_get_signup_page() ); ?>" title="<?php esc_attr_e( 'Register for a new account', 'buddypress' ); ?>"><?php _e( 'Register', 'buddypress' ); ?></a></span>
 
 				<?php endif; ?>
 
