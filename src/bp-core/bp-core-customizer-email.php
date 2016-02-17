cdd5648c4d9066957d614612bff4c59ee0786bfa
r-a-y <r-a-y@gmx.com>
2016-02-17 21:43:56 +0000

    Emails: Change header label for custom post type and customizer panel.
    To be less ambiguous, we're changing the header label from "Emails" to
    "BuddyPress Emails" on the main CPT admin page and in the customizer
    panel.
    
    See #6918.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@10608 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-core/bp-core-customizer-email.php
+++ src/bp-core/bp-core-customizer-email.php
@@ -31,7 +31,7 @@ function bp_email_init_customizer( WP_Customize_Manager $wp_customize ) {
 
 	$wp_customize->add_panel( 'bp_mailtpl', array(
 		'description' => __( 'Customize the appearance of emails sent by BuddyPress.', 'buddypress' ),
-		'title'       => _x( 'Emails', 'screen heading', 'buddypress' ),
+		'title'       => _x( 'BuddyPress Emails', 'screen heading', 'buddypress' ),
 	) );
 
 	$sections = bp_email_get_customizer_sections();
