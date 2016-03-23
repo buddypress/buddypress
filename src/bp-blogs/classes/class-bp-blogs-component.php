36a342d3f013893cd0b7bc40788e791c5133e9a8
Boone B Gorges <boonebgorges@gmail.com>
2016-03-23 14:27:04 +0000

    Avoid markup in translatable string when building Sites navigation item.
    Props ramiy.
    Fixes #6899.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@10674 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-blogs/classes/class-bp-blogs-component.php
+++ src/bp-blogs/classes/class-bp-blogs-component.php
@@ -179,7 +179,15 @@ class BP_Blogs_Component extends BP_Component {
 		// Add 'Sites' to the main navigation.
 		$count    = (int) bp_get_total_blog_count_for_user();
 		$class    = ( 0 === $count ) ? 'no-count' : 'count';
-		$nav_text = sprintf( __( 'Sites <span class="%s">%s</span>', 'buddypress' ), esc_attr( $class ), bp_core_number_format( $count )  );
+		$nav_text = sprintf(
+			/* translators: %s: Site count for the current user */
+			__( 'Sites %s', 'buddypress' ),
+			sprintf(
+				'<span class="%s">%s</span>',
+				esc_attr( $class ),
+				bp_core_number_format( $count )
+			)
+		);
 		$main_nav = array(
 			'name'                => $nav_text,
 			'slug'                => $slug,
