bf6ee8ae6363f6870a77c7d57cddc70a82b85d49
Boone B Gorges <boonebgorges@gmail.com>
2016-05-12 15:40:32 +0000

    Add tests for `bp_core_remove_nav_item()`.
    See #6534.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@10743 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

---
+++ tests/phpunit/testcases/core/nav/bpCoreRemoveNavItem.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @group core
+ * @group nav
+ */
+class BP_Tests_Core_Nav_BpCoreRemoveNavItem extends BP_UnitTestCase {
+	public function test_should_remove_subnav_items() {
+		$bp = buddypress();
+
+		$_bp_nav = $bp->bp_nav;
+		$_bp_options_nav = $bp->bp_options_nav;
+
+		bp_core_new_nav_item( array(
+			'name' => 'Foo',
+			'slug' => 'foo',
+			'screen_function' => 'foo',
+		) );
+
+		bp_core_new_subnav_item( array(
+			'name' => 'Bar',
+			'slug' => 'bar',
+			'parent_slug' => 'foo',
+			'parent_url' => 'foo',
+			'screen_function' => 'bar',
+		) );
+
+		$this->assertTrue( isset( $bp->bp_nav['foo'] ) );
+		$this->assertTrue( isset( $bp->bp_options_nav['foo'] ) );
+		$this->assertTrue( isset( $bp->bp_options_nav['foo']['bar'] ) );
+
+		bp_core_remove_nav_item( 'foo' );
+
+		$this->assertFalse( isset( $bp->bp_options_nav['foo']['bar'] ) );
+
+		$bp->bp_nav = $_bp_nav;
+		$bp->bp_options_nav = $_bp_options_nav;
+	}
+
+	public function test_should_remove_nav_item() {
+		$bp = buddypress();
+
+		$_bp_nav = $bp->bp_nav;
+		$_bp_options_nav = $bp->bp_options_nav;
+
+		bp_core_new_nav_item( array(
+			'name' => 'Foo',
+			'slug' => 'foo',
+			'screen_function' => 'foo',
+		) );
+
+		$this->assertTrue( isset( $bp->bp_nav['foo'] ) );
+
+		bp_core_remove_nav_item( 'foo' );
+
+		$this->assertFalse( isset( $bp->bp_nav['foo'] ) );
+
+		$bp->bp_nav = $_bp_nav;
+		$bp->bp_options_nav = $_bp_options_nav;
+	}
+}
