bf5923711ebe790a42e5ad1c56c496471e4e661c
Boone B Gorges <boonebgorges@gmail.com>
2016-05-24 16:30:06 +0000

    Bumping version numbers to 2.5.3.
    git-svn-id: https://buddypress.svn.wordpress.org/branches/2.5@10802 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-loader.php
+++ src/bp-loader.php
@@ -15,7 +15,7 @@
  * Description: BuddyPress helps you build any type of community website using WordPress, with member profiles, activity streams, user groups, messaging, and more.
  * Author:      The BuddyPress Community
  * Author URI:  https://buddypress.org/
- * Version:     2.5.2
+ * Version:     2.5.3
  * Text Domain: buddypress
  * Domain Path: /bp-languages/
  * License:     GPLv2 or later (license.txt)
@@ -327,7 +327,7 @@ class BuddyPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '2.5.2';
+		$this->version    = '2.5.3';
 		$this->db_version = 10469;
 
 		/** Loading ***********************************************************/
