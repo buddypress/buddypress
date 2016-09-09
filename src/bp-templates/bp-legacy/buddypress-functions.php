9ebaab1e5d62440a7e2dfc753a3b825da58a802a
David Cavins <david.cavins@gmail.com>
2016-09-09 14:40:49 +0000

    Group admin: Add search to manage members screen.
    Adds a search form to a single group’s manage members screen. This is
    very helpful when managing groups with several hundred members.
    
    Props hnla, dcavins.
    
    Fixes #6385.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@11070 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-templates/bp-legacy/buddypress-functions.php
+++ src/bp-templates/bp-legacy/buddypress-functions.php
@@ -105,11 +105,12 @@ class BP_Legacy extends BP_Theme_Compat {
 
 			// Group buttons.
 			if ( bp_is_active( 'groups' ) ) {
-				add_action( 'bp_group_header_actions',          'bp_group_join_button',               5      );
-				add_action( 'bp_group_header_actions',          'bp_group_new_topic_button',         20      );
-				add_action( 'bp_directory_groups_actions',      'bp_group_join_button'                       );
-				add_action( 'bp_groups_directory_group_filter', 'bp_legacy_theme_group_create_nav', 999      );
-				add_action( 'bp_after_group_admin_content',     'bp_legacy_groups_admin_screen_hidden_input' );
+				add_action( 'bp_group_header_actions',          'bp_group_join_button',               5           );
+				add_action( 'bp_group_header_actions',          'bp_group_new_topic_button',         20           );
+				add_action( 'bp_directory_groups_actions',      'bp_group_join_button'                            );
+				add_action( 'bp_groups_directory_group_filter', 'bp_legacy_theme_group_create_nav', 999           );
+				add_action( 'bp_after_group_admin_content',     'bp_legacy_groups_admin_screen_hidden_input'      );
+				add_action( 'bp_before_group_admin_form',       'bp_legacy_theme_group_manage_members_add_search' );
 			}
 
 			// Blog button.
@@ -1912,4 +1913,21 @@ function bp_legacy_theme_cover_image( $params = array() ) {
 			}
 		}
 	';
+}
+
+/**
+ * Add a search box to a single group's manage members screen.
+ *
+ * @since 2.7.0
+ *
+ * @return string HTML for the search form.
+ */
+function bp_legacy_theme_group_manage_members_add_search() {
+	if ( bp_is_action_variable( 'manage-members' ) ) :
+		?>
+		<div id="members-dir-search" class="dir-search no-ajax" role="search">
+			<?php bp_directory_members_search_form(); ?>
+		</div>
+		<?php
+	endif;
 }
