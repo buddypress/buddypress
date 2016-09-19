0cfbff71c1e571887460ab5279d8ad40674b842f
r-a-y <r-a-y@gmx.com>
2016-09-19 02:20:52 +0000

    Core: Introduce `BP_Core_HTML_Element` class.
    This will be used to generate HTML elements when necessary.
    
    See #7226.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@11112 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-core/bp-core-classes.php
+++ src/bp-core/bp-core-classes.php
@@ -33,6 +33,7 @@ require dirname( __FILE__ ) . '/classes/class-bp-phpmailer.php';
 require dirname( __FILE__ ) . '/classes/class-bp-core-nav.php';
 require dirname( __FILE__ ) . '/classes/class-bp-core-nav-item.php';
 require dirname( __FILE__ ) . '/classes/class-bp-core-oembed-extension.php';
+require dirname( __FILE__ ) . '/classes/class-bp-core-html-element.php';
 
 if ( buddypress()->do_nav_backcompat ) {
 	require dirname( __FILE__ ) . '/classes/class-bp-core-bp-nav-backcompat.php';
---
+++ src/bp-core/classes/class-bp-core-html-element.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Core component classes.
+ *
+ * @package BuddyPress
+ * @subpackage Core
+ * @since 2.7.0
+ */
+
+/**
+ * Generate markup for an HTML element.
+ *
+ * @since 2.7.0
+ */
+class BP_Core_HTML_Element {
+	/**
+	 * Open tag for an element.
+	 *
+	 * This would include attributes if applicable. eg. '<a href="" class="">'
+	 *
+	 * @since 2.7.0
+	 *
+	 * @var string
+	 */
+	public $open_tag   = '';
+
+	/**
+	 * Inner HTML for an element.
+	 *
+	 * For example, this could be anchor text within an <a> element.
+	 *
+	 * @since 2.7.0
+	 *
+	 * @var string
+	 */
+	public $inner_html = '';
+
+	/**
+	 * Closing tag for an element.
+	 *
+	 * For example, "</a>".
+	 *
+	 * @since 2.7.0
+	 *
+	 * @var string
+	 */
+	public $close_tag  = '';
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 2.7.0
+	 *
+	 * @param array $r {
+	 *     An array of arguments.
+	 *     @type string $element    The element to render. eg. 'a' for the anchor element.
+	 *     @type array  $attr       Optional. The element's attributes set as key/value pairs. eg.
+	 *                              array( 'href' => 'http://example.com', 'class' => 'my-class' )
+	 *     @type string $inner_html Optional. The inner HTML for the element if applicable. Please note that
+	 *                              this isn't sanitized, so you should use your own sanitization routine
+	 *                              before using this parameter.
+	 * }
+	 */
+	public function __construct( $r = array() ) {
+		$elem = sanitize_html_class( $r['element'] );
+		if ( empty( $elem ) ) {
+			return;
+		}
+
+		// Render attributes.
+		$attributes = '';
+		foreach( (array) $r['attr'] as $attr => $val ) {
+			// If attribute is empty, skip.
+			if ( empty( $val ) ) {
+				continue;
+			}
+
+			if ( 'href' === $attr || 'formaction' === $attr || 'src' === $attr ) {
+				$val = esc_url( $val );
+			} elseif ( 'id' === $attr ) {
+				$val = sanitize_html_class( $val );
+			} else {
+				$val = esc_attr( $val );
+			}
+
+			$attributes .= sprintf( '%s="%s" ', sanitize_html_class( $attr ), $val );
+		}
+
+		// <input> / <img> is self-closing.
+		if ( 'input' === $elem || 'img' === $elem ) {
+			$this->open_tag = sprintf( '<%1$s %2$s />', $elem, $attributes );
+
+		// All other elements.
+		} else {
+			$this->open_tag   = sprintf( '<%1$s %2$s>', $elem, $attributes );
+			$this->inner_html = ! empty( $r['inner_html'] ) ? $r['inner_html'] : '';
+			$this->close_tag  = sprintf( '</%1$s>',	$elem );
+		}
+	}
+
+	/**
+	 * Returns a property from this class.
+	 *
+	 * @since 2.7.0
+	 *
+	 * @param  string $prop Property name. Either 'open_tag', 'inner_html', 'close_tag'.
+	 * @return string
+	 */
+	public function get( $prop = '' ) {
+		if ( ! isset( $this->{$prop} ) ) {
+			return '';
+		}
+
+		return $this->{$prop};
+	}
+
+	/**
+	 * Returns full contents of HTML element.
+	 *
+	 * @since 2.7.0
+	 *
+	 * @return string
+	 */
+	public function contents() {
+		return $this->open_tag . $this->inner_html . $this->close_tag;
+	}
+}
