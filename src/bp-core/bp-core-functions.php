0b930a0e46aade8c395653efd4b9b86d96a000f2
Boone B Gorges <boonebgorges@gmail.com>
2016-12-29 21:13:41 +0000

    Use a closure for `bp_sort_by_key()` callback.
    Previously, `bp_sort_by_key()` was refactored to avoid the use
    of `create_function()`. See #6864. This refactoring needed to be
    PHP 5.2-compatible, which required a workaround for the
    unavailabilty of closures. This workaround can now be removed.
    
    See #7299.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@11362 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-core/bp-core-functions.php
+++ src/bp-core/bp-core-functions.php
@@ -120,10 +120,41 @@ function bp_core_get_table_prefix() {
  * @return array $items The sorted array.
  */
 function bp_sort_by_key( $items, $key, $type = 'alpha', $preserve_keys = false ) {
+	$callback = function( $a, $b ) use ( $key, $type ) {
+		$values = array( 0 => false, 1 => false );
+		$func_args = func_get_args();
+		foreach ( $func_args as $indexi => $index ) {
+			if ( isset( $index->{$key} ) ) {
+				$values[ $indexi ] = $index->{$key};
+			} elseif ( isset( $index[ $key ] ) ) {
+				$values[ $indexi ] = $index[ $key ];
+			}
+		}
+
+		if ( isset( $values[0], $values[1] ) ) {
+			if ( 'num' === $type ) {
+				$cmp = $values[0] - $values[1];
+			} else {
+				$cmp = strcmp( $values[0], $values[1] );
+			}
+
+			if ( 0 > $cmp ) {
+				$retval = -1;
+			} elseif ( 0 < $cmp ) {
+				$retval = 1;
+			} else {
+				$retval = 0;
+			}
+			return $retval;
+		} else {
+			return 0;
+		}
+	};
+
 	if ( true === $preserve_keys ) {
-		uasort( $items, array( new BP_Core_Sort_By_Key_Callback( $key, $type ), 'sort_callback' ) );
+		uasort( $items, $callback );
 	} else {
-		usort( $items, array( new BP_Core_Sort_By_Key_Callback( $key, $type ), 'sort_callback' ) );
+		usort( $items, $callback );
 	}
 
 	return $items;
