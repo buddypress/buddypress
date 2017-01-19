f581e7d77fd83be924606763d79a98d456c78313
Boone B Gorges <boonebgorges@gmail.com>
2017-01-19 20:29:46 +0000

    Fix sentence that are not grammatical in PHP minimum notice.
    Introduced in [11389].
    
    Props dcavins.
    See #7277.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@11394 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-loader.php
+++ src/bp-loader.php
@@ -61,7 +61,7 @@ function bp_php_requirements_notice() {
 	<div id="message" class="error notice">
 		<p><strong><?php esc_html_e( 'Your site does not support BuddyPress.', 'buddypress' ); ?></strong></p>
 		<?php /* translators: 1: current PHP version, 2: required PHP version */ ?>
-		<p><?php printf( esc_html__( 'Your site is currently running PHP version %1$s, while BuddyPress requires version at least version %2$s.', 'buddypress' ), esc_html( phpversion() ), esc_html( BP_REQUIRED_PHP_VERSION ) ); ?> <?php printf( __( 'See <a href="%s">the Codex guide</a> for more information.', 'buddypress' ), 'https://codex.buddypress.org/getting-started/buddypress-2-8-will-require-php-5-3/' ); ?></p>
+		<p><?php printf( esc_html__( 'Your site is currently running PHP version %1$s, while BuddyPress requires version %2$s or greater.', 'buddypress' ), esc_html( phpversion() ), esc_html( BP_REQUIRED_PHP_VERSION ) ); ?> <?php printf( __( 'See <a href="%s">the Codex guide</a> for more information.', 'buddypress' ), 'https://codex.buddypress.org/getting-started/buddypress-2-8-will-require-php-5-3/' ); ?></p>
 		<p><?php esc_html_e( 'Please update your server or deactivate BuddyPress.', 'buddypress' ); ?></p>
 	</div>
 
