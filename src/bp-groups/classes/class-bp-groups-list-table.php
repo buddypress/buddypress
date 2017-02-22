fbd075a77e368d14829e9a115113314c1748192f
Boone B Gorges <boonebgorges@gmail.com>
2017-02-22 17:46:53 +0000

    Fix incorrect `apply_filters_ref_array()` syntax.
    Introduced in [8426].
    
    Props garrett-eclipse, 1naveengiri.
    Fixes #7420.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@11440 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-groups/classes/class-bp-groups-list-table.php
+++ src/bp-groups/classes/class-bp-groups-list-table.php
@@ -520,7 +520,7 @@ class BP_Groups_List_Table extends WP_List_Table {
 		 * @param string $value Name of the group being rendered.
 		 * @param array  $item  Array for the current group item.
 		 */
-		$group_name = apply_filters_ref_array( 'bp_get_group_name', array( $item['name'] ), $item );
+		$group_name = apply_filters_ref_array( 'bp_get_group_name', array( $item['name'], $item ) );
 
 		// Rollover actions.
 		// Edit.
