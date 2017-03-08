6b5dc19bbb20c2083d667d967ac472e069550198
Boone B Gorges <boonebgorges@gmail.com>
2017-03-08 19:47:01 +0000

    Nonce protection for canceling email change.
    Merge of [11459] to the 2.8 branch.
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/branches/2.8@11460 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-settings/bp-settings-actions.php
+++ src/bp-settings/bp-settings-actions.php
@@ -472,8 +472,12 @@ function bp_settings_verify_email_change(){
 
 	// Email change is being dismissed.
 	} elseif ( ! empty( $_GET['dismiss_email_change'] ) ) {
-		bp_delete_user_meta( bp_displayed_user_id(), 'pending_email_change' );
-		bp_core_add_message( __( 'You have successfully dismissed your pending email change.', 'buddypress' ) );
+		$nonce_check = isset( $_GET['_wpnonce'] ) && wp_verify_nonce( wp_unslash( $_GET['_wpnonce'] ), 'bp_dismiss_email_change' );
+
+		if ( $nonce_check ) {
+			bp_delete_user_meta( bp_displayed_user_id(), 'pending_email_change' );
+			bp_core_add_message( __( 'You have successfully dismissed your pending email change.', 'buddypress' ) );
+		}
 
 		bp_core_redirect( $redirect_to );
 		die();
