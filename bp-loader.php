346d17f4a4f3a38d9d52e1b399c36b66010e0228
Hugo Ashmore <hugo@nettropia.co.uk>
2017-06-24 17:23:58 +0000

    Update version numbers to `2.9.0-beta2` from `beta1` hugo messed up!
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@11623 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-loader.php
+++ bp-loader.php
@@ -15,7 +15,7 @@
  * Description: BuddyPress adds community features to WordPress. Member Profiles, Activity Streams, Direct Messaging, Notifications, and more!
  * Author:      The BuddyPress Community
  * Author URI:  https://buddypress.org/
- * Version:     2.9.0-beta1
+ * Version:     2.9.0-beta2
  * Text Domain: buddypress
  * Domain Path: /bp-languages/
  * License:     GPLv2 or later (license.txt)
