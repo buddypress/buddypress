c15b729a71f06975288e6d766f434b862a4ec5bb
Mercime <mercijavier@gmail.com>
2017-07-12 18:04:19 +0000

    Add bp-tooltip class for notification actions.
    Fixes #7188.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@11637 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-notifications/bp-notifications-template.php
+++ src/bp-notifications/bp-notifications-template.php
@@ -647,7 +647,7 @@ function bp_the_notification_mark_unread_link( $user_id = 0 ) {
 		// Set default user ID to use.
 		$user_id = 0 === $user_id ? bp_displayed_user_id() : $user_id;
 
-		$retval = sprintf( '<a href="%1$s" class="mark-unread primary">%2$s</a>', esc_url( bp_get_the_notification_mark_unread_url( $user_id ) ), __( 'Unread', 'buddypress' ) );
+		$retval = sprintf( '<a href="%1$s" class="mark-unread primary bp-tooltip">%2$s</a>', esc_url( bp_get_the_notification_mark_unread_url( $user_id ) ), __( 'Unread', 'buddypress' ) );
 
 		/**
 		 * Filters the link used for marking a single notification as unread.
@@ -781,7 +781,7 @@ function bp_the_notification_delete_link( $user_id = 0 ) {
 		// Set default user ID to use.
 		$user_id = 0 === $user_id ? bp_displayed_user_id() : $user_id;
 
-		$retval = sprintf( '<a href="%1$s" class="delete secondary confirm">%2$s</a>', esc_url( bp_get_the_notification_delete_url( $user_id ) ), __( 'Delete', 'buddypress' ) );
+		$retval = sprintf( '<a href="%1$s" class="delete secondary confirm bp-tooltip">%2$s</a>', esc_url( bp_get_the_notification_delete_url( $user_id ) ), __( 'Delete', 'buddypress' ) );
 
 		/**
 		 * Filters the delete link for the current notification.
