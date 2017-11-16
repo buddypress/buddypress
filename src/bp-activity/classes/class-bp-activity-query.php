b490afa0c19472acc09e7a942f9f4522f8087e95
Boone B Gorges <boonebgorges@gmail.com>
2017-11-16 19:55:13 +0000

    Activity queries should be filterable by 'primary_link'.
    Props mauteri.
    Fixes #7616.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@11746 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-activity/classes/class-bp-activity-query.php
+++ src/bp-activity/classes/class-bp-activity-query.php
@@ -46,7 +46,7 @@ class BP_Activity_Query extends BP_Recursive_Query {
 	 * @var array
 	 */
 	public $db_columns = array(
-		'id', 'user_id', 'component', 'type', 'action', 'content',
+		'id', 'user_id', 'component', 'type', 'action', 'content', 'primary_link',
 		'item_id', 'secondary_item_id', 'hide_sitewide', 'is_spam',
 	);
 
