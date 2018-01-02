480afb266f2274b666e33d283ece602b0bf5269a
Paul Gibbs <djpaul@gmail.com>
2018-01-02 17:12:20 +0000

    Emails: merge in updates from upstream template.
    Fixes #7650
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@11785 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-templates/bp-nouveau/buddypress/assets/emails/single-bp-email.php
+++ src/bp-templates/bp-nouveau/buddypress/assets/emails/single-bp-email.php
@@ -19,7 +19,7 @@ License for the original template:
 
 The MIT License (MIT)
 
-Copyright (c) 2013 Ted Goas
+Copyright (c) 2017 Ted Goas
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -44,12 +44,14 @@ defined( 'ABSPATH' ) || exit;
 
 $settings = bp_email_get_appearance_settings();
 
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+?><!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 <head>
 	<meta charset="<?php echo esc_attr( get_bloginfo( 'charset' ) ); ?>">
 	<meta name="viewport" content="width=device-width"> <!-- Forcing initial-scale shouldn't be necessary -->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- Use the latest (edge) version of IE rendering engine -->
+	<meta name="x-apple-disable-message-reformatting">  <!-- Disable auto-scale in iOS 10 Mail entirely -->
+	<title></title> <!-- The title tag shows in email notifications, like Android 4.4. -->
 
 	<!-- CSS Reset -->
 	<style type="text/css">
@@ -67,11 +69,6 @@ $settings = bp_email_get_appearance_settings();
 		* {
 			-ms-text-size-adjust: 100%;
 			-webkit-text-size-adjust: 100%;
-		}
-
-		/* What it does: Forces Outlook.com to display emails full width. */
-		.ExternalClass {
-			width: 100%;
 		}
 
 		/* What is does: Centers email on Android 4.4 */
@@ -105,22 +102,42 @@ $settings = bp_email_get_appearance_settings();
 			max-width: 100%;
 		}
 
-		/* What it does: Overrides styles added when Yahoo's auto-senses a link. */
-		.yshortcuts a {
-			border-bottom: none !important;
+		/* What it does: A work-around for email clients meddling in triggered links. */
+		*[x-apple-data-detectors],  /* iOS */
+		.x-gmail-data-detectors,    /* Gmail */
+		.x-gmail-data-detectors *,
+		.aBn {
+			border-bottom: 0 !important;
+			cursor: default !important;
+			color: inherit !important;
+			text-decoration: none !important;
+			font-size: inherit !important;
+			font-family: inherit !important;
+			font-weight: inherit !important;
+			line-height: inherit !important;
 		}
 
-		/* What it does: A work-around for iOS meddling in triggered links. */
-		a[x-apple-data-detectors] {
-			color: inherit !important;
-			text-decoration: underline !important;
+		/* What it does: Prevents Gmail from displaying an download button on large, non-linked images. */
+		.a6S {
+			display: none !important;
+			opacity: 0.01 !important;
+		}
+
+		/* If the above doesn't work, add a .g-img class to any image in question. */
+			img.g-img + div {
+			display: none !important;
+		}
+
+		/* What it does: Prevents underlining the button text in Windows 10 */
+		.button-link {
+			text-decoration: none !important;
 		}
 	</style>
 
 </head>
-<body class="email_bg" width="100%" height="100%" bgcolor="<?php echo esc_attr( $settings['email_bg'] ); ?>" style="Margin: 0;">
+<body class="email_bg" width="100%" bgcolor="<?php echo esc_attr( $settings['email_bg'] ); ?>" style="margin: 0; mso-line-height-rule: exactly;">
 <table cellpadding="0" cellspacing="0" border="0" height="100%" width="100%" bgcolor="<?php echo esc_attr( $settings['email_bg'] ); ?>" style="border-collapse:collapse;" class="email_bg"><tr><td valign="top">
-	<center style="width: 100%;">
+	<center style="width: 100%; text-align: left;">
 
 		<!-- Visually Hidden Preheader Text : BEGIN -->
 		<div style="display: none; font-size: 1px; line-height: 1px; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; mso-hide: all; font-family: sans-serif;">
@@ -128,15 +145,15 @@ $settings = bp_email_get_appearance_settings();
 		</div>
 		<!-- Visually Hidden Preheader Text : END -->
 
-		<div style="max-width: 600px;">
-			<!--[if (gte mso 9)|(IE)]>
-			<table cellspacing="0" cellpadding="0" border="0" width="600" align="center">
+		<div style="max-width: 600px; margin: auto;" class="email-container">
+			<!--[if mso]>
+			<table role="presentation" cellspacing="0" cellpadding="0" border="0" width="600" align="center">
 			<tr>
 			<td>
 			<![endif]-->
 
 			<!-- Email Header : BEGIN -->
-			<table cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 600px; border-top: 7px solid <?php echo esc_attr( $settings['highlight_color'] ); ?>" bgcolor="<?php echo esc_attr( $settings['header_bg'] ); ?>" class="header_bg">
+			<table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 600px; border-top: 7px solid <?php echo esc_attr( $settings['highlight_color'] ); ?>" bgcolor="<?php echo esc_attr( $settings['header_bg'] ); ?>" class="header_bg">
 				<tr>
 					<td style="text-align: center; padding: 15px 0; font-family: sans-serif; mso-height-rule: exactly; font-weight: bold; color: <?php echo esc_attr( $settings['header_text_color'] ); ?>; font-size: <?php echo esc_attr( $settings['header_text_size'] . 'px' ); ?>" class="header_text_color header_text_size">
 						<?php
@@ -162,12 +179,12 @@ $settings = bp_email_get_appearance_settings();
 			<!-- Email Header : END -->
 
 			<!-- Email Body : BEGIN -->
-			<table cellspacing="0" cellpadding="0" border="0" align="center" bgcolor="<?php echo esc_attr( $settings['body_bg'] ); ?>" width="100%" style="max-width: 600px; border-radius: 5px;" class="body_bg">
+			<table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" bgcolor="<?php echo esc_attr( $settings['body_bg'] ); ?>" width="100%" style="max-width: 600px; border-radius: 5px;" class="body_bg">
 
 				<!-- 1 Column Text : BEGIN -->
 				<tr>
 					<td>
-						<table cellspacing="0" cellpadding="0" border="0" width="100%">
+						<table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
 							<tr>
 								<td style="padding: 20px; font-family: sans-serif; mso-height-rule: exactly; line-height: <?php echo esc_attr( floor( $settings['body_text_size'] * 1.618 ) . 'px' ) ?>; color: <?php echo esc_attr( $settings['body_text_color'] ); ?>; font-size: <?php echo esc_attr( $settings['body_text_size'] . 'px' ); ?>" class="body_text_color body_text_size">
 									<span style="font-weight: bold; font-size: <?php echo esc_attr( floor( $settings['body_text_size'] * 1.35 ) . 'px' ); ?>" class="welcome"><?php bp_email_the_salutation( $settings ); ?></span>
@@ -185,7 +202,7 @@ $settings = bp_email_get_appearance_settings();
 
 			<!-- Email Footer : BEGIN -->
 			<br>
-			<table cellspacing="0" cellpadding="0" border="0" align="left" width="100%" style="max-width: 600px; border-radius: 5px;" bgcolor="<?php echo esc_attr( $settings['footer_bg'] ); ?>" class="footer_bg">
+			<table role="presentation" cellspacing="0" cellpadding="0" border="0" align="left" width="100%" style="max-width: 600px; border-radius: 5px;" bgcolor="<?php echo esc_attr( $settings['footer_bg'] ); ?>" class="footer_bg">
 				<tr>
 					<td style="padding: 20px; width: 100%; font-size: <?php echo esc_attr( $settings['footer_text_size'] . 'px' ); ?>; font-family: sans-serif; mso-height-rule: exactly; line-height: <?php echo esc_attr( floor( $settings['footer_text_size'] * 1.618 ) . 'px' ) ?>; text-align: left; color: <?php echo esc_attr( $settings['footer_text_color'] ); ?>;" class="footer_text_color footer_text_size">
 						<?php
@@ -214,7 +231,7 @@ $settings = bp_email_get_appearance_settings();
 			</table>
 			<!-- Email Footer : END -->
 
-			<!--[if (gte mso 9)|(IE)]>
+			<!--[if mso]>
 			</td>
 			</tr>
 			</table>
