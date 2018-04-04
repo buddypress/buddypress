buddypress/buddypress

commit c50c95d2f9e632f7ac32e5332e904f413855a0d2

Activity, Akismet: add support for Akismet's "discard worse spam" feature.
Fixes #6594

Thanks to slaFFik, and via bbPress, netweb and Otto42, for historical contributions towards this change.

git-svn-id: https://buddypress.svn.wordpress.org/trunk@11890 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-activity/classes/class-bp-akismet.php
@@ -411,6 +411,19 @@ class BP_Akismet {
 
 			// Mark as spam.
 			bp_activity_mark_as_spam( $activity, 'by_akismet' );
+
+			if (
+				Akismet::allow_discard() &&
+				! empty( $activity_data['bp_as_result_headers']['X-akismet-pro-tip'] ) &&
+				$activity_data['bp_as_result_headers']['X-akismet-pro-tip'] === 'discard'
+			) {
+				// If this is so spammy it's not worth your time, let's just delete it.
+				if ( $activity->type === 'activity_comment' ) {
+					bp_activity_delete_comment( $activity->item_id, $activity->id );
+				} else {
+					bp_activity_delete( array( 'id' => $activity->id ) );
+				}
+			}
 		}
 
 		// Update activity meta after a spam check.
@@ -546,11 +559,14 @@ class BP_Akismet {
 		$response = Akismet::http_post( $query_string, $path );
 		remove_filter( 'akismet_ua', array( $this, 'buddypress_ua' ) );
 
+		$activity_data['bp_as_result_headers'] = array();
+		$activity_data['bp_as_result']         = '';
+
 		// Get the response.
-		if ( ! empty( $response[1] ) && ! is_wp_error( $response[1] ) )
-			$activity_data['bp_as_result'] = $response[1];
-		else
-			$activity_data['bp_as_result'] = false;
+		if ( ! empty( $response[1] ) && ! is_wp_error( $response[1] ) ) {
+			$activity_data['bp_as_result_headers'] = $response[0];
+			$activity_data['bp_as_result']         = $response[1];
+		}
 
 		// Perform a daily tidy up.
 		if ( ! wp_next_scheduled( 'bp_activity_akismet_delete_old_metadata' ) )

commit 1501dec6aa1ce4c457c1d037daac3d1f9bd4a7ce

Activity, Akismet: fix infinite loop introduced in r11890
Props r-a-y

Fixes #6594

git-svn-id: https://buddypress.svn.wordpress.org/trunk@11950 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-activity/classes/class-bp-akismet.php
@@ -414,8 +414,8 @@ class BP_Akismet {
 
 			if (
 				Akismet::allow_discard() &&
-				! empty( $activity_data['bp_as_result_headers']['X-akismet-pro-tip'] ) &&
-				$activity_data['bp_as_result_headers']['X-akismet-pro-tip'] === 'discard'
+				! empty( $activity_data['akismet_pro_tip'] ) &&
+				'discard' === $activity_data['akismet_pro_tip']
 			) {
 				// If this is so spammy it's not worth your time, let's just delete it.
 				if ( $activity->type === 'activity_comment' ) {
@@ -559,13 +559,10 @@ class BP_Akismet {
 		$response = Akismet::http_post( $query_string, $path );
 		remove_filter( 'akismet_ua', array( $this, 'buddypress_ua' ) );
 
-		$activity_data['bp_as_result_headers'] = array();
-		$activity_data['bp_as_result']         = '';
-
-		// Get the response.
-		if ( ! empty( $response[1] ) && ! is_wp_error( $response[1] ) ) {
-			$activity_data['bp_as_result_headers'] = $response[0];
-			$activity_data['bp_as_result']         = $response[1];
+		// Save response data.
+		$activity_data['bp_as_result'] = $response[1];
+		if ( isset( $response[0]['x-akismet-pro-tip'] ) ) {
+			$activity_data['akismet_pro_tip'] = $response[0]['x-akismet-pro-tip'];
 		}
 
 		// Perform a daily tidy up.