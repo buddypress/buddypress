a95cf20ce871c3f4a67ff913547fabda8d13aae2
Hugo Ashmore <hugo@nettropia.co.uk>
2018-04-15 10:09:34 +0000

    Nouveau: Update groups single activity template markup
    Commit updates the template to remove the hardcoded UL construct & moves the data attr & tokens to the parent div element.
    
    This brings template in line with recent changes on main act loop & members act loop to adjust stream element injection.
    
    Props mercime
    
    Fixes #7749
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@11976 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-templates/bp-nouveau/buddypress/groups/single/activity.php
+++ src/bp-templates/bp-nouveau/buddypress/groups/single/activity.php
@@ -10,27 +10,25 @@
 <?php bp_nouveau_groups_activity_post_form(); ?>
 
 
-	<div class="subnav-filters filters clearfix">
+<div class="subnav-filters filters clearfix">
 
-		<ul>
-			<li class="feed"><a href="<?php bp_group_activity_feed_link(); ?>" class="bp-tooltip no-ajax" data-bp-tooltip="<?php esc_attr_e( 'RSS Feed', 'buddypress' ); ?>"><span class="bp-screen-reader-text"><?php _e( 'RSS', 'buddypress' ); ?></span></a></li>
+	<ul>
 
-			<li class="group-act-search"><?php bp_nouveau_search_form(); ?></li>
-		</ul>
+		<li class="feed"><a href="<?php bp_group_activity_feed_link(); ?>" class="bp-tooltip no-ajax" data-bp-tooltip="<?php esc_attr_e( 'RSS Feed', 'buddypress' ); ?>"><span class="bp-screen-reader-text"><?php _e( 'RSS', 'buddypress' ); ?></span></a></li>
+
+		<li class="group-act-search"><?php bp_nouveau_search_form(); ?></li>
+
+	</ul>
 
 		<?php bp_get_template_part( 'common/filters/groups-screens-filters' ); ?>
-	</div><!-- // .subnav-filters -->
+</div><!-- // .subnav-filters -->
 
 
 <?php bp_nouveau_group_hook( 'before', 'activity_content' ); ?>
 
-<div class="activity single-group">
-
-	<ul id="activity-stream" class="activity-list item-list bp-list" data-bp-list="activity">
+<div id="activity-stream" class="activity single-group" data-bp-list="activity">
 
 		<li id="bp-activity-ajax-loader"><?php bp_nouveau_user_feedback( 'group-activity-loading' ); ?></li>
-
-	</ul>
 
 </div><!-- .activity -->
 
