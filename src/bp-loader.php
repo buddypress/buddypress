3dcbbe9f3a4be919ee4b1884a009bff85ee55bed
Boone B Gorges <boonebgorges@gmail.com>
2018-11-15 21:12:40 +0000

    Version bumps for 4.0.0-RC1.
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@12291 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-loader.php
+++ src/bp-loader.php
@@ -15,7 +15,7 @@
  * Description: BuddyPress adds community features to WordPress. Member Profiles, Activity Streams, Direct Messaging, Notifications, and more!
  * Author:      The BuddyPress Community
  * Author URI:  https://buddypress.org/
- * Version:     4.0.0-beta1
+ * Version:     4.0.0-RC1
  * Text Domain: buddypress
  * Domain Path: /bp-languages/
  * License:     GPLv2 or later (license.txt)
