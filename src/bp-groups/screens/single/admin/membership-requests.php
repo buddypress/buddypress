e84941fcf084a40b411abbf5c8258db728134ac0
David Cavins <david.cavins@gmail.com>
2019-08-12 20:44:49 +0000

    Invitations: Update groups_screen_group_admin_requests().
    Update `groups_screen_group_admin_requests()` to  work with new Invitations API.
    
    See #6210.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@12436 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-groups/screens/single/admin/membership-requests.php
+++ src/bp-groups/screens/single/admin/membership-requests.php
@@ -23,33 +23,41 @@ function groups_screen_group_admin_requests() {
 		return false;
 	}
 
-	$request_action = (string) bp_action_variable( 1 );
-	$membership_id  = (int) bp_action_variable( 2 );
+	$request_action = isset( $_GET['action'] ) ? $_GET['action'] : false;
+	$user_id        = isset( $_GET['user_id'] ) ? (int) $_GET['user_id'] : false;
+	$group_id       = bp_get_current_group_id();
 
-	if ( !empty( $request_action ) && !empty( $membership_id ) ) {
-		if ( 'accept' == $request_action && is_numeric( $membership_id ) ) {
+	if ( $request_action && $user_id && $group_id ) {
+		if ( 'accept' === $request_action ) {
 
 			// Check the nonce first.
-			if ( !check_admin_referer( 'groups_accept_membership_request' ) )
+			if ( ! check_admin_referer( 'groups_accept_membership_request' ) ) {
 				return false;
+			}
 
 			// Accept the membership request.
-			if ( !groups_accept_membership_request( $membership_id ) )
+			if ( ! groups_accept_membership_request( false, $user_id, $group_id ) ) {
 				bp_core_add_message( __( 'There was an error accepting the membership request. Please try again.', 'buddypress' ), 'error' );
-			else
+			} else {
 				bp_core_add_message( __( 'Group membership request accepted', 'buddypress' ) );
+			}
 
-		} elseif ( 'reject' == $request_action && is_numeric( $membership_id ) ) {
+		} elseif ( 'reject' === $request_action ) {
 			/* Check the nonce first. */
-			if ( !check_admin_referer( 'groups_reject_membership_request' ) )
+			if ( ! check_admin_referer( 'groups_reject_membership_request' ) ) {
 				return false;
+			}
 
 			// Reject the membership request.
-			if ( !groups_reject_membership_request( $membership_id ) )
+			if ( ! groups_reject_membership_request( false, $user_id, $group_id ) ) {
 				bp_core_add_message( __( 'There was an error rejecting the membership request. Please try again.', 'buddypress' ), 'error' );
-			else
+			} else {
 				bp_core_add_message( __( 'Group membership request rejected', 'buddypress' ) );
+			}
 		}
+
+		// Was the member added to the group?
+		$membership_id = groups_is_user_member( $user_id, $group_id );
 
 		/**
 		 * Fires before the redirect if a group membership request has been handled.
@@ -58,9 +66,11 @@ function groups_screen_group_admin_requests() {
 		 *
 		 * @param int    $id             ID of the group that was edited.
 		 * @param string $request_action Membership request action being performed.
-		 * @param int    $membership_id  The key of the action_variables array that you want.
+		 * @param int    $membership_id  The membership ID of the new user; false if rejected.
+		 * @param int    $user_id        The ID of the requesting user.
+		 * @param int    $group_id       The ID of the requested group.
 		 */
-		do_action( 'groups_group_request_managed', $bp->groups->current_group->id, $request_action, $membership_id );
+		do_action( 'groups_group_request_managed', $bp->groups->current_group->id, $request_action, $membership_id, $user_id, $group_id );
 		bp_core_redirect( bp_get_group_permalink( groups_get_current_group() ) . 'admin/membership-requests/' );
 	}
 
