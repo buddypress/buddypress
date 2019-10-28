d2794efc70f727bd32cece6daab624e029ceed8e
Mathieu Viet <mathchristo@yahoo.fr>
2019-10-28 19:49:19 +0000

    Revert [12483]
    For further details see this [https://wordpress.slack.com/archives/C02RQBYUG/p1572193392178200 discussion on slack]
    
    Props espellcaste, johnjamesjacoby
    
    See #8151 (Branch 5.0)
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/branches/5.0@12484 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-xprofile/classes/class-bp-xprofile-component.php
+++ src/bp-xprofile/classes/class-bp-xprofile-component.php
@@ -499,17 +499,10 @@ class BP_XProfile_Component extends BP_Component {
 	 *                           description.
 	 */
 	public function rest_api_init( $controllers = array() ) {
-		$controllers = array(
+		parent::rest_api_init( array(
 			'BP_REST_XProfile_Fields_Endpoint',
 			'BP_REST_XProfile_Field_Groups_Endpoint',
 			'BP_REST_XProfile_Data_Endpoint',
-		);
-
-		// Only Load the Member's Avatar controller if Avatar uploads are allowed.
-		if ( ! bp_disable_avatar_uploads() ) {
-			$controllers[] = 'BP_REST_Attachments_Member_Avatar_Endpoint';
-		}
-
-		parent::rest_api_init( $controllers );
+		) );
 	}
 }
