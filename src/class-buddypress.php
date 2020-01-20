553d0507f825353392ac90783d825e14b2704790
Mathieu Viet <mathchristo@yahoo.fr>
2020-01-20 18:58:31 +0000

    Core: fix PHP Code standards in inline comments
    props passoniate
    
    Fixes #8217
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@12535 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/class-buddypress.php
+++ src/class-buddypress.php
@@ -113,10 +113,10 @@ class BuddyPress {
 	 */
 	public static function instance() {
 
-		// Store the instance locally to avoid private static replication
+		// Store the instance locally to avoid private static replication.
 		static $instance = null;
 
-		// Only run these methods if they haven't been run previously
+		// Only run these methods if they haven't been run previously.
 		if ( null === $instance ) {
 			$instance = new BuddyPress;
 			$instance->constants();
@@ -126,7 +126,7 @@ class BuddyPress {
 			$instance->setup_actions();
 		}
 
-		// Always return the instance
+		// Always return the instance.
 		return $instance;
 
 		// The last metroid is in captivity. The galaxy is at peace.
@@ -226,7 +226,7 @@ class BuddyPress {
 			require( WP_PLUGIN_DIR . '/bp-custom.php' );
 		}
 
-		// Path and URL
+		// Path and URL.
 		if ( ! defined( 'BP_PLUGIN_DIR' ) ) {
 			define( 'BP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 		}
@@ -245,34 +245,34 @@ class BuddyPress {
 			define( 'BP_FORUMS_SLUG', 'forums' );
 		}
 
-		// Only applicable to those running trunk
+		// Only applicable to those running trunk.
 		if ( ! defined( 'BP_SOURCE_SUBDIRECTORY' ) ) {
 			define( 'BP_SOURCE_SUBDIRECTORY', '' );
 		}
 
-		// Define on which blog ID BuddyPress should run
+		// Define on which blog ID BuddyPress should run.
 		if ( ! defined( 'BP_ROOT_BLOG' ) ) {
 
-			// Default to use current blog ID
-			// Fulfills non-network installs and BP_ENABLE_MULTIBLOG installs
+			// Default to use current blog ID.
+			// Fulfills non-network installs and BP_ENABLE_MULTIBLOG installs.
 			$root_blog_id = get_current_blog_id();
 
-			// Multisite check
+			// Multisite check.
 			if ( is_multisite() ) {
 
-				// Multiblog isn't enabled
+				// Multiblog isn't enabled.
 				if ( ! defined( 'BP_ENABLE_MULTIBLOG' ) || ( defined( 'BP_ENABLE_MULTIBLOG' ) && (int) constant( 'BP_ENABLE_MULTIBLOG' ) === 0 ) ) {
 					// Check to see if BP is network-activated
 					// We're not using is_plugin_active_for_network() b/c you need to include the
 					// /wp-admin/includes/plugin.php file in order to use that function.
 
-					// get network-activated plugins
+					// Get network-activated plugins.
 					$plugins = get_site_option( 'active_sitewide_plugins');
 
-					// basename
+					// Basename.
 					$basename = basename( constant( 'BP_PLUGIN_DIR' ) ) . '/bp-loader.php';
 
-					// plugin is network-activated; use main site ID instead
+					// Plugin is network-activated; use main site ID instead.
 					if ( isset( $plugins[ $basename ] ) ) {
 						$current_site = get_current_site();
 						$root_blog_id = $current_site->blog_id;
@@ -285,9 +285,9 @@ class BuddyPress {
 		}
 
 		// The search slug has to be defined nice and early because of the way
-		// search requests are loaded
+		// search requests are loaded.
 		//
-		// @todo Make this better
+		// @todo Make this better.
 		if ( ! defined( 'BP_SEARCH_SLUG' ) ) {
 			define( 'BP_SEARCH_SLUG', 'search' );
 		}
@@ -371,27 +371,27 @@ class BuddyPress {
 
 		/** Paths**************************************************************/
 
-		// BuddyPress root directory
+		// BuddyPress root directory.
 		$this->file           = constant( 'BP_PLUGIN_DIR' ) . 'bp-loader.php';
 		$this->basename       = basename( constant( 'BP_PLUGIN_DIR' ) ) . '/bp-loader.php';
 		$this->plugin_dir     = trailingslashit( constant( 'BP_PLUGIN_DIR' ) . constant( 'BP_SOURCE_SUBDIRECTORY' ) );
 		$this->plugin_url     = trailingslashit( constant( 'BP_PLUGIN_URL' ) . constant( 'BP_SOURCE_SUBDIRECTORY' ) );
 
-		// Languages
+		// Languages.
 		$this->lang_dir       = $this->plugin_dir . 'bp-languages';
 
-		// Templates (theme compatibility)
+		// Templates (theme compatibility).
 		$this->themes_dir     = $this->plugin_dir . 'bp-templates';
 		$this->themes_url     = $this->plugin_url . 'bp-templates';
 
-		// Themes (for bp-default)
+		// Themes (for bp-default).
 		$this->old_themes_dir = $this->plugin_dir . 'bp-themes';
 		$this->old_themes_url = $this->plugin_url . 'bp-themes';
 
 		/** Theme Compat ******************************************************/
 
-		$this->theme_compat   = new stdClass(); // Base theme compatibility class
-		$this->filters        = new stdClass(); // Used when adding/removing filters
+		$this->theme_compat   = new stdClass(); // Base theme compatibility class.
+		$this->filters        = new stdClass(); // Used when adding/removing filters.
 
 		/** Users *************************************************************/
 
@@ -429,17 +429,17 @@ class BuddyPress {
 	 */
 	private function legacy_constants() {
 
-		// Define the BuddyPress version
+		// Define the BuddyPress version.
 		if ( ! defined( 'BP_VERSION' ) ) {
 			define( 'BP_VERSION', $this->version );
 		}
 
-		// Define the database version
+		// Define the database version.
 		if ( ! defined( 'BP_DB_VERSION' ) ) {
 			define( 'BP_DB_VERSION', $this->db_version );
 		}
 
-		// Define if deprecated functions should be ignored
+		// Define if deprecated functions should be ignored.
 		if ( ! defined( 'BP_IGNORE_DEPRECATED' ) ) {
 			define( 'BP_IGNORE_DEPRECATED', true );
 		}
@@ -457,16 +457,16 @@ class BuddyPress {
 		// Load the WP abstraction file so BuddyPress can run on all WordPress setups.
 		require( $this->plugin_dir . 'bp-core/bp-core-wpabstraction.php' );
 
-		// Setup the versions (after we include multisite abstraction above)
+		// Setup the versions (after we include multisite abstraction above).
 		$this->versions();
 
 		/** Update/Install ****************************************************/
 
-		// Theme compatibility
+		// Theme compatibility.
 		require( $this->plugin_dir . 'bp-core/bp-core-template-loader.php'     );
 		require( $this->plugin_dir . 'bp-core/bp-core-theme-compatibility.php' );
 
-		// Require all of the BuddyPress core libraries
+		// Require all of the BuddyPress core libraries.
 		require( $this->plugin_dir . 'bp-core/bp-core-dependency.php'       );
 		require( $this->plugin_dir . 'bp-core/bp-core-actions.php'          );
 		require( $this->plugin_dir . 'bp-core/bp-core-caps.php'             );
@@ -489,7 +489,7 @@ class BuddyPress {
 		require( $this->plugin_dir . 'bp-core/bp-core-customizer-email.php' );
 		require( $this->plugin_dir . 'bp-core/bp-core-rest-api.php'         );
 
-		// Maybe load deprecated functionality (this double negative is proof positive!)
+		// Maybe load deprecated functionality (this double negative is proof positive!).
 		if ( ! bp_get_option( '_bp_ignore_deprecated_code', ! $this->load_deprecated ) ) {
 			require( $this->plugin_dir . 'bp-core/deprecated/1.2.php' );
 			require( $this->plugin_dir . 'bp-core/deprecated/1.5.php' );
@@ -510,7 +510,7 @@ class BuddyPress {
 			require( $this->plugin_dir . 'bp-core/deprecated/4.0.php' );
 		}
 
-		// Load wp-cli module if PHP 5.4+
+		// Load wp-cli module if PHP 5.4+.
 		if ( defined( 'WP_CLI' ) && file_exists( $this->plugin_dir . 'cli/wp-cli-bp.php' ) && version_compare( phpversion(), '5.4.0', '>=' ) ) {
 			require( $this->plugin_dir . 'cli/wp-cli-bp.php' );
 		}
@@ -658,31 +658,31 @@ class BuddyPress {
 	 */
 	private function setup_actions() {
 
-		// Add actions to plugin activation and deactivation hooks
+		// Add actions to plugin activation and deactivation hooks.
 		add_action( 'activate_'   . $this->basename, 'bp_activation'   );
 		add_action( 'deactivate_' . $this->basename, 'bp_deactivation' );
 
-		// If BuddyPress is being deactivated, do not add any actions
+		// If BuddyPress is being deactivated, do not add any actions.
 		if ( bp_is_deactivation( $this->basename ) ) {
 			return;
 		}
 
 		// Array of BuddyPress core actions
 		$actions = array(
-			'setup_theme',              // Setup the default theme compat
-			'setup_current_user',       // Setup currently logged in user
-			'register_post_types',      // Register post types
-			'register_post_statuses',   // Register post statuses
-			'register_taxonomies',      // Register taxonomies
-			'register_views',           // Register the views
-			'register_theme_directory', // Register the theme directory
-			'register_theme_packages',  // Register bundled theme packages (bp-themes)
-			'load_textdomain',          // Load textdomain
-			'add_rewrite_tags',         // Add rewrite tags
-			'generate_rewrite_rules'    // Generate rewrite rules
+			'setup_theme',              // Setup the default theme compat.
+			'setup_current_user',       // Setup currently logged in user.
+			'register_post_types',      // Register post types.
+			'register_post_statuses',   // Register post statuses.
+			'register_taxonomies',      // Register taxonomies.
+			'register_views',           // Register the views.
+			'register_theme_directory', // Register the theme directory.
+			'register_theme_packages',  // Register bundled theme packages (bp-themes).
+			'load_textdomain',          // Load textdomain.
+			'add_rewrite_tags',         // Add rewrite tags.
+			'generate_rewrite_rules'    // Generate rewrite rules.
 		);
 
-		// Add the actions
+		// Add the actions.
 		foreach( $actions as $class_action ) {
 			if ( method_exists( $this, $class_action ) ) {
 				add_action( 'bp_' . $class_action, array( $this, $class_action ), 5 );
@@ -708,22 +708,22 @@ class BuddyPress {
 	 */
 	private function versions() {
 
-		// Get the possible DB versions (boy is this gross)
+		// Get the possible DB versions (boy is this gross).
 		$versions               = array();
 		$versions['1.6-single'] = get_blog_option( $this->root_blog_id, '_bp_db_version' );
 
-		// 1.6-single exists, so trust it
+		// 1.6-single exists, so trust it.
 		if ( !empty( $versions['1.6-single'] ) ) {
 			$this->db_version_raw = (int) $versions['1.6-single'];
 
-		// If no 1.6-single exists, use the max of the others
+		// If no 1.6-single exists, use the max of the others.
 		} else {
 			$versions['1.2']        = get_site_option(                      'bp-core-db-version' );
 			$versions['1.5-multi']  = get_site_option(                           'bp-db-version' );
 			$versions['1.6-multi']  = get_site_option(                          '_bp_db_version' );
 			$versions['1.5-single'] = get_blog_option( $this->root_blog_id,      'bp-db-version' );
 
-			// Remove empty array items
+			// Remove empty array items.
 			$versions             = array_filter( $versions );
 			$this->db_version_raw = (int) ( !empty( $versions ) ) ? (int) max( $versions ) : 0;
 		}
@@ -764,7 +764,7 @@ class BuddyPress {
 	 */
 	public function register_theme_packages() {
 
-		// Register the default theme compatibility package
+		// Register the default theme compatibility package.
 		bp_register_theme_package( array(
 			'id'      => 'legacy',
 			'name'    => __( 'BuddyPress Legacy', 'buddypress' ),
@@ -794,12 +794,12 @@ class BuddyPress {
 	 */
 	public function setup_theme() {
 
-		// Bail if something already has this under control
+		// Bail if something already has this under control.
 		if ( ! empty( $this->theme_compat->theme ) ) {
 			return;
 		}
 
-		// Setup the theme package to use for compatibility
+		// Setup the theme package to use for compatibility.
 		bp_setup_theme_compat( bp_get_theme_package_id() );
 	}
 }
