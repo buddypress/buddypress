442280144d08723779bb98ecbb3e68cccae1a73f
Mathieu Viet <mathchristo@yahoo.fr>
2020-07-30 21:10:40 +0000

    Core: use the right option name for moderation disallowed keys
    Since WordPress 5.5 the `blacklist_keys` option name has been deprecated in favor of the `disallowed_keys` one. Depending on the installed WordPress version, this commit makes sure we use the right option name.
    
    Props boonebgorges
    
    Fixes #8338 (branch 6.0)
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/branches/6.0@12695 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-core/bp-core-moderation.php
+++ src/bp-core/bp-core-moderation.php
@@ -190,7 +190,7 @@ function bp_core_check_for_moderation( $user_id = 0, $title = '', $content = '',
  * @since 1.6.0
  * @since 2.6.0 Added $error_type parameter.
  *
- * @todo Why don't we use wp_blacklist_check() for this?
+ * @todo Why don't we use wp_check_comment_disallowed_list() for this?
  *
  * @param int    $user_id    User ID.
  * @param string $title      The title of the content.
@@ -224,9 +224,13 @@ function bp_core_check_for_blacklist( $user_id = 0, $title = '', $content = '', 
 
 	/** Blacklist ************************************************************
 	 */
+	$disallowed_keys_option_name = 'blacklist_keys';
+	if ( function_exists( 'wp_check_comment_disallowed_list' ) ) {
+		$disallowed_keys_option_name = 'disallowed_keys';
+	}
 
 	// Get the moderation keys.
-	$blacklist = trim( get_option( 'blacklist_keys' ) );
+	$blacklist = trim( get_option( $disallowed_keys_option_name ) );
 
 	// Bail if blacklist is empty.
 	if ( empty( $blacklist ) ) {
