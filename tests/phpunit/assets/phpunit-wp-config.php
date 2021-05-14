5a612fbf50dd4f86f7f3275ef6802dc76050d116
Mathieu Viet <mathchristo@yahoo.fr>
2021-05-14 05:23:02 +0000

    Build/test tools: use GitHub Actions to run PHP unit tests
    We're starting using GitHub Actions with these 4 jobs to do our PHP Unit tests on this matrix :
    
    - WP Trunk - PHP 7.4
    - WP Trunk - PHP 8.0
    - WP 5.7 - PHP 7.4
    - WP 5.7 - PHP 8.0
    
    As the future of Travis.org is uncertain, we believe it's an important step to secure our testing tool.
    
    Props slaFFik, Huge Props espellcaste 💪
    
    Fixes #8421
    
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@12950 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- tests/phpunit/assets/phpunit-wp-config.php
+++ tests/phpunit/assets/phpunit-wp-config.php
@@ -26,10 +26,10 @@ define( 'DB_NAME', 'tests-wordpress');
 define( 'DB_USER', 'root');
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '');
+define( 'DB_PASSWORD', 'password');
 
 /** MySQL hostname */
-define( 'DB_HOST', 'mysql');
+define( 'DB_HOST', 'tests-mysql');
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8');
