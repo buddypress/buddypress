d034256962a5e8d12492f6567cff1d3a33d03434
Mathieu Viet <mathchristo@yahoo.fr>
2021-06-12 11:26:49 +0000

    BP Nouveau: apply the activity content filter the right way
    Inside the BP Nouveau Template pack, the `bp_nouveau_ajax_get_single_activity_content()` function was applying the `'bp_get_activity_content_body'` filter using the wrong function and wasn't passing the second parameter of the filter:
    
    - Instead of using `apply_filters()` it should use `apply_filters_ref_array()`.
    - The filter parameters should also include the Activity object passed by reference.
    
    Props sbrajesh
    
    See #8485 (trunk)
    
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@12972 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-templates/bp-nouveau/includes/activity/ajax.php
+++ src/bp-templates/bp-nouveau/includes/activity/ajax.php
@@ -3,7 +3,7 @@
  * Activity Ajax functions
  *
  * @since 3.0.0
- * @version 8.0.0
+ * @version 8.1.0
  */
 
 // Exit if accessed directly.
@@ -310,7 +310,13 @@ function bp_nouveau_ajax_get_single_activity_content() {
 	remove_filter( 'bp_get_activity_content_body', 'bp_activity_truncate_entry', 5 );
 
 	/** This filter is documented in bp-activity/bp-activity-template.php */
-	$content = apply_filters( 'bp_get_activity_content_body', $activity->content );
+	$content = apply_filters_ref_array(
+		'bp_get_activity_content_body',
+		array(
+			$activity->content,
+			&$activity
+		)
+	);
 
 	wp_send_json_success( array( 'contents' => $content ) );
 }
