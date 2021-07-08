7aa1c5c75955833695e249c1877e10f5eb926c5a
Mathieu Viet <mathchristo@yahoo.fr>
2021-07-08 13:23:26 +0000

    Introduce a new function to get a list of type labels keyed by names
    `bp_activity_get_types_list()` returns an array of activity type labels keyed by activity type names. It satisfies a need which appeared working on a new Activity BP Block.
    
    Fixes #8504
    
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@12987 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-activity/bp-activity-functions.php
+++ src/bp-activity/bp-activity-functions.php
@@ -843,6 +843,52 @@ function bp_activity_get_types() {
 }
 
 /**
+ * Returns the list of available BuddyPress activity types.
+ *
+ * @since 9.0.0
+ *
+ * @return array An array of activity type labels keyed by type names.
+ */
+function bp_activity_get_types_list() {
+	$actions_object = bp_activity_get_actions();
+	$actions_array  = get_object_vars( $actions_object );
+
+	$types = array();
+	foreach ( $actions_array as $component => $actions ) {
+		$new_types = wp_list_pluck( $actions, 'label', 'key' );
+
+		if ( $types ) {
+			// Makes sure activity types are unique.
+			$new_types = array_diff_key( $new_types, $types );
+
+			if ( 'friends' === $component ) {
+				$new_types = array_diff_key(
+					array(
+						'friendship_accepted'              => false,
+						'friendship_created'               => false,
+						'friends_register_activity_action' => false,
+					),
+					$new_types
+				);
+
+				$new_types['friendship_accepted,friendship_created'] = __( 'Friendships', 'buddypress' );
+			}
+		}
+
+		$types = array_merge( $types, $new_types );
+	}
+
+	/**
+	 * Filter here to edit the activity types list.
+	 *
+	 * @since 9.0.0
+	 *
+	 * @param array $types An array of activity type labels keyed by type names.
+	 */
+	return apply_filters( 'bp_activity_get_types_list', $types );
+}
+
+/**
  * Gets the current activity context.
  *
  * The "context" is the current view type, corresponding roughly to the
