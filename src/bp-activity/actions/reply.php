4df66288f4932fc1fbb9e0ca81358de7999bbfc6
Mathieu Viet <mathchristo@yahoo.fr>
2021-08-13 12:19:58 +0000

    Activity: update the nonce used by the Activity Reply JS Fallback
    If JavaScript is unavailable (whether it was disabled or it errored), posting an activity uses the `bp_activity_action_post_comment()` PHP function after a page load. Unlike the Ajax action, the nonce used into this function has not been updated in [12735]. The main goal of this commit is to fix this oversight. The secondary goal is to improve code formatting.
    
    See #8545 (trunk) 
    
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@13041 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-activity/actions/reply.php
+++ src/bp-activity/actions/reply.php
@@ -15,20 +15,31 @@
  * @return bool False on failure.
  */
 function bp_activity_action_post_comment() {
-	if ( !is_user_logged_in() || !bp_is_activity_component() || !bp_is_current_action( 'reply' ) )
+	if ( ! is_user_logged_in() || ! bp_is_activity_component() || ! bp_is_current_action( 'reply' ) ) {
 		return false;
+	}
+
+	if ( ! isset( $_POST['comment_form_id'] ) ) {
+		return false;
+	}
+
+	$activity_id = absint( wp_unslash( $_POST['comment_form_id'] ) );
+
+	if ( ! isset( $activity_id ) ) {
+		return false;
+	}
 
 	// Check the nonce.
-	check_admin_referer( 'new_activity_comment', '_wpnonce_new_activity_comment' );
+	check_admin_referer( 'new_activity_comment', '_wpnonce_new_activity_comment_' . $activity_id );
 
 	/**
 	 * Filters the activity ID a comment will be in reply to.
 	 *
 	 * @since 1.2.0
 	 *
-	 * @param string $value ID of the activity being replied to.
+	 * @param string $activity_id ID of the activity being replied to.
 	 */
-	$activity_id = apply_filters( 'bp_activity_post_comment_activity_id', $_POST['comment_form_id'] );
+	$activity_id = apply_filters( 'bp_activity_post_comment_activity_id', $activity_id );
 
 	/**
 	 * Filters the comment content for a comment reply.
@@ -50,16 +61,19 @@ function bp_activity_action_post_comment() {
 		bp_core_redirect( wp_get_referer() . '#ac-form-' . $activity_id );
 	}
 
-	$comment_id = bp_activity_new_comment( array(
-		'content'     => $content,
-		'activity_id' => $activity_id,
-		'parent_id'   => false
-	));
+	$comment_id = bp_activity_new_comment(
+		array(
+			'content'     => $content,
+			'activity_id' => $activity_id,
+			'parent_id'   => false
+		)
+	);
 
-	if ( !empty( $comment_id ) )
+	if ( ! empty( $comment_id ) ) {
 		bp_core_add_message( __( 'Reply Posted!', 'buddypress' ) );
-	else
+	} else {
 		bp_core_add_message( __( 'There was an error posting that reply. Please try again.', 'buddypress' ), 'error' );
+	}
 
 	bp_core_redirect( wp_get_referer() . '#ac-form-' . $activity_id );
 }
