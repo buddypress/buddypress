c4903781c09740dfc08418fea3e4f62716067983
Mathieu Viet <mathchristo@yahoo.fr>
2021-09-01 19:00:37 +0000

    Media: improve Cover Image UI feedbacks when an image is uploaded
    Instead of only informing the uploaded image is smaller than the recommended size, the UI will now also inform the image has been successfully uploaded in such a case.
    
    Props mandro
    
    Fixes #8563
    
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@13100 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-core/js/cover-image.js
+++ src/bp-core/js/cover-image.js
@@ -193,25 +193,31 @@ window.bp = window.bp || {};
 		},
 
 		uploadResult: function( model ) {
-			var message, type;
+			var feedbackCode, feedbackViews = [];
 
 			if ( ! _.isUndefined( model.get( 'url' ) ) ) {
+				feedbackCode = model.get( 'feedback_code' );
 
 				// Image is too small.
-				if ( 0 === model.get( 'feedback_code' ) ) {
-					message = BP_Uploader.strings.cover_image_warnings.dimensions;
-					type    = 'warning';
-
-				// Success, Rock n roll!
-				} else {
-					message = BP_Uploader.strings.feedback_messages[ model.get( 'feedback_code' ) ];
-					type = 'success';
+				if ( 0 === feedbackCode ) {
+					feedbackCode = 1;
+					feedbackViews.push(
+						new bp.Views.CoverImageStatus( {
+							value : BP_Uploader.strings.cover_image_warnings.dimensions,
+							type  : 'warning'
+						} )
+					);
 				}
 
-				this.views.set( '.bp-uploader-progress', new bp.Views.CoverImageStatus( {
-					value : message,
-					type  : type
-				} ) );
+				feedbackViews.unshift(
+					new bp.Views.CoverImageStatus( {
+						value : BP_Uploader.strings.feedback_messages[ feedbackCode ],
+						type  : 'success'
+					} )
+				);
+
+				// Success, Rock n roll!
+				this.views.set( '.bp-uploader-progress', feedbackViews );
 
 				// Update the header of the page.
 				$( '#header-cover-image' ).css( {
