4fb6c0125dbd8ff51df7923b53e30169ac2baebb
Dion Hulse <dd32@dd32.id.au>
2022-03-03 13:32:00 +1000

    Add context to the 'bp_is_user_spammer' filter.

--- src/bp-members/bp-members-functions.php
+++ src/bp-members/bp-members-functions.php
@@ -942,9 +942,10 @@ function bp_is_user_spammer( $user_id = 0 ) {
 	 *
 	 * @since 1.6.0
 	 *
-	 * @param bool $is_spammer Whether or not user is marked as spammer.
+	 * @param bool     $is_spammer Whether or not user is marked as spammer.
+	 * @param \WP_User $user       The user to which we are acting on.
 	 */
-	return apply_filters( 'bp_is_user_spammer', (bool) $is_spammer );
+	return apply_filters( 'bp_is_user_spammer', (bool) $is_spammer, $user );
 }
 
 /**
