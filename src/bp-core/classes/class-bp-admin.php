c108af2decae6efb1bb5a842a28e7914e119f0e2
Mathieu Viet <mathchristo@yahoo.fr>
2022-11-24 23:14:42 +0000

    Deprecate `bp-moment` JS dependency in favor of WP's `moment` one
    Doing so is taking care of using a fresher version of moment.js (v2.29.4).
    
    Props thomaslhotta, dcavins
    
    Closes https://github.com/buddypress/buddypress/pull/39
    Fixes #8766
    
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@13373 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-core/classes/class-bp-admin.php
+++ src/bp-core/classes/class-bp-admin.php
@@ -1138,7 +1138,6 @@ class BP_Admin {
 				<a href="https://github.com/carhartl/jquery-cookie">jquery.cookie</a>,
 				<a href="https://mattbradley.github.io/livestampjs/">Livestamp.js</a>,
 				<a href="https://www.mediawiki.org/wiki/MediaWiki">MediaWiki</a>,
-				<a href="https://momentjs.com/">Moment.js</a>,
 				<a href="https://wordpress.org">WordPress</a>.
 			</p>
 
