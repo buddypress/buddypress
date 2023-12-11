0742520f9ac0ec7bdba43e615fce7215a926c63d
Mathieu Viet <mathchristo@yahoo.fr>
2023-12-11 21:07:43 +0000

    12.0.0 version bumps (trunk)
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@13664 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- bp-loader.php
+++ bp-loader.php
@@ -21,7 +21,7 @@
  * Domain Path:       /bp-languages/
  * Requires PHP:      5.6
  * Requires at least: 5.8
- * Version:           12.0.0-RC1
+ * Version:           12.0.0
  */
 
 // Exit if accessed directly
--- src/class-buddypress.php
+++ src/class-buddypress.php
@@ -460,7 +460,7 @@ class BuddyPress {
 
 		/** Versions */
 
-		$this->version    = '12.0.0-RC1';
+		$this->version    = '12.0.0';
 		$this->db_version = 13422;
 
 		/** Loading */
