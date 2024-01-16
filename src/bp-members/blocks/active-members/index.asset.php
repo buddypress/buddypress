a8de710d96bd5be5daec44c36ab511b832813b58
Mathieu Viet <mathchristo@yahoo.fr>
2024-01-16 20:29:07 +0000

    Make Avatar only Block Widgets display consistent in Posts/Sidebars
    Fixes #9037
    
    
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/branches/12.0@13699 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-members/blocks/active-members/index.asset.php
+++ src/bp-members/blocks/active-members/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '5861e64bdbe99a973ca6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '311a697f673ec1c39680');
--- src/bp-members/blocks/online-members/index.asset.php
+++ src/bp-members/blocks/online-members/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'f23f88b15eb2d2c469e3');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '0c583609ef5cb4b7f9db');
