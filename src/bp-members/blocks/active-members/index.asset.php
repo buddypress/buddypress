9d01b03b6c10cf140a73c47deba4e2233cc9ca95
Mathieu Viet <mathchristo@yahoo.fr>
2024-01-16 20:12:53 +0000

    Make Avatar only Block Widgets display consistent in Posts/Sidebars
    See #9037
    Closes https://github.com/buddypress/buddypress/pull/218
    
    
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@13698 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-members/blocks/active-members/index.asset.php
+++ src/bp-members/blocks/active-members/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '2cf313e342f1db29f272');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'ebcabdc452d850bc2b31');
--- src/bp-members/blocks/online-members/index.asset.php
+++ src/bp-members/blocks/online-members/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'a682169cdddb9445585b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '9d7011c1c325cb35815b');
