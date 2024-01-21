93c329b724d112e135f736270b93970486ccac14
imath <imathi.eu@outlook.fr>
2024-01-21 22:22:01 +0100

    Make sure Groups Dynamic Block default title can be translated

--- src/bp-groups/blocks/dynamic-groups/index.asset.php
+++ src/bp-groups/blocks/dynamic-groups/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '63dec15a4161caa1d398');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '3d85c460f52f1dae9d78');
