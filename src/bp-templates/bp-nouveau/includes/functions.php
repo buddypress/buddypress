64edf582d6ac0d51ee18e5c16ac07359b7c6ce21
Mathieu Viet <mathchristo@yahoo.fr>
2024-02-21 17:42:27 +0000

    BP Nouveau: fix a typo and missing ponctuation of a function docblock
    The concerned function is `bp_nouveau_get_component_filters()`.
    
    Props itpathsolutions
    
    Fixes #9102
    
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@13729 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-templates/bp-nouveau/includes/functions.php
+++ src/bp-templates/bp-nouveau/includes/functions.php
@@ -3,7 +3,7 @@
  * Common functions
  *
  * @since 3.0.0
- * @version 12.3.0
+ * @version 14.0.0
  */
 
 // Exit if accessed directly.
@@ -520,14 +520,14 @@ function bp_nouveau_parse_hooked_options( $hook = '', $filters = array() ) {
 }
 
 /**
- * Get Dropdawn filters for the current component of the one passed in params
+ * Get Dropdown filters for the current component of the one passed in params.
  *
  * @since 3.0.0
  *
- * @param string $context   'directory', 'user' or 'group'
- * @param string $component The BuddyPress component ID
+ * @param string $context   'directory', 'user' or 'group'.
+ * @param string $component The BuddyPress component ID.
  *
- * @return array the dropdown filters
+ * @return array the dropdown filters.
  */
 function bp_nouveau_get_component_filters( $context = '', $component = '' ) {
 	$filters = array();
