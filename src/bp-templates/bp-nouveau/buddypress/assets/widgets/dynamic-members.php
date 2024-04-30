4c064948b327c4650a153a17f6a13dec60642457
Mathieu Viet <mathchristo@yahoo.fr>
2024-04-30 22:43:04 +0000

    Dynamic Members/Friends/Groups blocks: improve output escaping
    branch 10.0)
    
    
    
    git-svn-id: https://buddypress.svn.wordpress.org/branches/10.0@13833 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-templates/bp-nouveau/buddypress/assets/widgets/dynamic-members.php
+++ src/bp-templates/bp-nouveau/buddypress/assets/widgets/dynamic-members.php
@@ -13,7 +13,7 @@
 	<li class="vcard">
 		<div class="item-avatar">
 			<a href="{{{data.link}}}" class="bp-tooltip" data-bp-tooltip="{{data.name}}">
-				<img loading="lazy" src="{{{data.avatar_urls.thumb}}}" class="avatar user-{{data.id}}-avatar avatar-50 photo" width="50" height="50" alt="{{{data.avatar_alt}}}">
+				<img loading="lazy" src="{{{data.avatar_urls.thumb}}}" class="avatar user-{{data.id}}-avatar avatar-50 photo" width="50" height="50" alt="{{data.avatar_alt}}">
 			</a>
 		</div>
 
