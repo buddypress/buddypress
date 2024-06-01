7000559b4c0e9ede2a1bd60e7108ee5de433b531
Renato Alves <espellcaste@gmail.com>
2024-06-01 22:19:51 +0000

    WPCS: Part III: miscellaneous fixes for some of the files of the `core` component.
    Follow-up to [13883] and [13886]
    
    See #9164 and #7228
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@13887 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-core/classes/class-bp-theme-compat.php
+++ src/bp-core/classes/class-bp-theme-compat.php
@@ -39,6 +39,7 @@ class BP_Theme_Compat {
 	 * Template package properties, as passed to the constructor.
 	 *
 	 * @since 1.7.0
+	 *
 	 * @var array
 	 */
 	protected $_data = array();
@@ -50,7 +51,7 @@ class BP_Theme_Compat {
 	 *
 	 * @param array $properties Array of properties for BP_Theme_Compat.
 	 */
-	public function __construct( Array $properties = array() ) {
+	public function __construct( array $properties = array() ) {
 		$this->_data = $properties;
 	}
 
@@ -102,7 +103,7 @@ class BP_Theme_Compat {
 	 * @return bool True on success, false on failure.
 	 */
 	public function __set( $property, $value ) {
-		return $this->_data[$property] = $value;
+		return $this->_data[ $property ] = $value;
 	}
 
 	/**
@@ -115,7 +116,7 @@ class BP_Theme_Compat {
 	 *               empty string.
 	 */
 	public function __get( $property ) {
-		return array_key_exists( $property, $this->_data ) ? $this->_data[$property] : '';
+		return array_key_exists( $property, $this->_data ) ? $this->_data[ $property ] : '';
 	}
 
 	/**
