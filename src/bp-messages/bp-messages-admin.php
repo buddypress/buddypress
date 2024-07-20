24e62b11113fbcbd0464bc3c3f4993e226507444
imath <math.viet@gmail.com>
2024-07-20 17:58:45 +0200

    Remaining 14.0.0 references to replace by 15.0.0 ones

--- src/bp-messages/bp-messages-admin.php
+++ src/bp-messages/bp-messages-admin.php
@@ -5,10 +5,10 @@
  * @package BuddyPress
  * @subpackage MessagesAdmin
  * @since 3.0.0
- * @deprecated 14.0.0
+ * @deprecated 15.0.0
  */
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-_deprecated_file( basename( __FILE__ ), '14.0.0', '', esc_html__( 'Notices are now a required feature of the BP Members component.', 'buddypress' ) );
+_deprecated_file( basename( __FILE__ ), '15.0.0', '', esc_html__( 'Notices are now a required feature of the BP Members component.', 'buddypress' ) );
--- src/bp-messages/bp-messages-blocks.php
+++ src/bp-messages/bp-messages-blocks.php
@@ -5,10 +5,10 @@
  * @package BuddyPress
  * @subpackage MessagesBlocks
  * @since 9.0.0
- * @deprecated 14.0.0
+ * @deprecated 15.0.0
  */
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-_deprecated_file( basename( __FILE__ ), '14.0.0', '', esc_html__( 'The SiteWide Notice block has been moved to the BP Members component.', 'buddypress' ) );
+_deprecated_file( basename( __FILE__ ), '15.0.0', '', esc_html__( 'The SiteWide Notice block has been moved to the BP Members component.', 'buddypress' ) );
