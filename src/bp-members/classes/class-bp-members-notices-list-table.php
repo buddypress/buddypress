a9c87bcaf4a5387915bb07a5b4884514a2138d0f
imath <math.viet@gmail.com>
2024-07-30 23:34:51 +0200

    Use priority 255 to deactivate a notice Add a column to display priority into the Notices Admin screen

--- src/bp-members/classes/class-bp-members-notices-list-table.php
+++ src/bp-members/classes/class-bp-members-notices-list-table.php
@@ -64,7 +64,8 @@ class BP_Members_Notices_List_Table extends WP_List_Table {
 		$this->items = BP_Members_Notice::get(
 			array(
 				'pag_num'  => $per_page,
-				'pag_page' => $page
+				'pag_page' => $page,
+				'type'     => 'all',
 			)
 		);
 
@@ -89,6 +90,7 @@ class BP_Members_Notices_List_Table extends WP_List_Table {
 			'bp_notices_list_table_get_columns',
 			array(
 				'subject'   => _x( 'Subject', 'Admin Notices column header', 'buddypress' ),
+				'priority'  => _x( 'Priority', 'Admin Notices column header', 'buddypress' ),
 				'target'    => _x( 'Targeted audience', 'Admin Notices column header', 'buddypress' ),
 				'date_sent' => _x( 'Created', 'Admin Notices column header', 'buddypress' ),
 			)
@@ -124,22 +126,22 @@ class BP_Members_Notices_List_Table extends WP_List_Table {
 	public function column_subject( $item ) {
 		$actions = array(
 			'activate_deactivate' => sprintf(
-				'<a href="%s" data-bp-notice-id="%d" data-bp-action="activate">%s</a>',
+				'<a href="%s" data-bp-notice-id="%d" data-bp-action="deactivate">%s</a>',
 				esc_url(
 					wp_nonce_url(
 						add_query_arg(
 							array(
 								'page'          => 'bp-notices',
-								'notice_action' => 'activate',
+								'notice_action' => 'deactivate',
 								'notice_id'     => $item->id
 							),
 							bp_get_admin_url( 'users.php' )
 						),
-						'messages-activate-notice-' . $item->id
+						'messages-deactivate-notice-' . $item->id
 					)
 				),
 				(int) $item->id,
-				esc_html__( 'Activate Notice', 'buddypress' )
+				esc_html__( 'Deactivate Notice', 'buddypress' )
 			),
 			'delete' => sprintf(
 				'<a href="%s" data-bp-notice-id="%d" data-bp-action="delete">%s</a>',
@@ -161,30 +163,28 @@ class BP_Members_Notices_List_Table extends WP_List_Table {
 			)
 		);
 
-		if ( isset( $item->priority ) && 1 === $item->priority ) {
-			/* translators: %s: notice subject */
-			$item->subject = sprintf( _x( 'Active: %s', 'Tag prepended to active site-wide notice titles on WP Admin notices list table', 'buddypress' ), $item->subject );
+		if ( 255 === (int) $item->priority ) {
 			$actions['activate_deactivate'] = sprintf(
-				'<a href="%s" data-bp-notice-id="%d" data-bp-action="deactivate">%s</a>',
+				'<a href="%s" data-bp-notice-id="%d" data-bp-action="activate">%s</a>',
 				esc_url(
 					wp_nonce_url(
 						add_query_arg(
 							array(
 								'page'          => 'bp-notices',
-								'notice_action' => 'deactivate',
+								'notice_action' => 'activate',
 								'notice_id'     => $item->id
 							),
 							bp_get_admin_url( 'users.php' )
 						),
-						'messages-deactivate-notice-' . $item->id
+						'messages-activate-notice-' . $item->id
 					)
 				),
 				(int) $item->id,
-				esc_html__( 'Deactivate Notice', 'buddypress' )
+				esc_html__( 'Activate Notice', 'buddypress' )
 			);
 		}
 
-		echo '<strong>' . esc_html( apply_filters( 'bp_get_message_notice_subject', $item->subject ) ) . '</strong> ';
+		echo '<strong>' . bp_get_notice_title( $item ) . '</strong> ';
 
 		// BuddyPress relies on WordPress's `WP_List_Table::row_actions()`.
 		// phpcs:ignore WordPress.Security.EscapeOutput
@@ -192,22 +192,25 @@ class BP_Members_Notices_List_Table extends WP_List_Table {
 	}
 
 	/**
-	 * Generates content for the "message" column.
+	 * Generates content for the "priority" column.
+	 *
+	 * @since 15.0.0
+	 *
+	 * @param object $item The current item
+	 */
+	public function column_priority( $item ) {
+		bp_notice_priority( $item );
+	}
+
+	/**
+	 * Generates content for the "target" column.
 	 *
 	 * @since 15.0.0
 	 *
 	 * @param object $item The current item
 	 */
 	public function column_target( $item ) {
-		$target_key = bp_get_notice_target( $item );
-
-		$targets = array(
-			'community'    => __( 'All community members', 'buddypress' ),
-			'admins'       => __( 'All administrators', 'buddypress' ),
-			'contributors' => __( 'All contributors', 'buddypress' ),
-		);
-
-		echo esc_html( $targets[ $target_key ] );
+		bp_notice_target( $item );
 	}
 
 	/**
@@ -218,6 +221,6 @@ class BP_Members_Notices_List_Table extends WP_List_Table {
 	 * @param object $item The current item
 	 */
 	public function column_date_sent( $item ) {
-		echo esc_html( apply_filters( 'bp_get_message_notice_post_date', bp_format_time( strtotime( $item->date_sent ) ) ) );
+		echo esc_html( bp_format_time( strtotime( $item->date_sent ) ) );
 	}
 }
