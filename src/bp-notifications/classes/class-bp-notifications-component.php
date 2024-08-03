8d983bbc94c9a0b01c217cd80b352f42f416e7ee
imath <math.viet@gmail.com>
2024-08-03 13:26:35 +0200

    Make the Notifications component use the Notices center

--- src/bp-notifications/classes/class-bp-notifications-component.php
+++ src/bp-notifications/classes/class-bp-notifications-component.php
@@ -47,7 +47,6 @@ class BP_Notifications_Component extends BP_Component {
 	 */
 	public function includes( $includes = array() ) {
 		$includes = array(
-			'adminbar',
 			'template',
 			'filters',
 			'functions',
