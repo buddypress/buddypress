c645986ed912f9c2e007445a033ab55dfd3e55c2
imath <math.viet@gmail.com>
2024-08-15 10:29:42 +0200

    Remove Target Dashicons in favor or a title prefix

--- src/bp-templates/bp-nouveau/buddypress/members/single/notices/entry.php
+++ src/bp-templates/bp-nouveau/buddypress/members/single/notices/entry.php
@@ -11,12 +11,11 @@ if ( ! isset( $args['context'] ) ) {
 }
 ?>
 
-<article id="notice-<?php bp_notice_id( $args['context'] ); ?>">
+<article id="notice-<?php bp_notice_id( $args['context'] ); ?>" class="notice-item <?php bp_notice_item_class( $args['context'] ); ?>">
 	<header class="bp-notice-header">
 		<h3><?php bp_notice_title( $args['context'] ); ?></h2>
 	</header>
 	<div class="bp-notice-body">
-		<div class="bp-notice-type dashicons <?php echo esc_attr( bp_get_notice_target_icon( $args['context'] ) ); ?>" ></div>
 		<div class="bp-notice-content">
 			<?php bp_notice_content( $args['context'] ); ?>
 		</div>
