9a5acb61635c20949c4b59ce77c4c265032e9996
imath <math.viet@gmail.com>
2024-08-28 01:03:26 +0200

    Improve notice display on front end
    Add pagination and filter

--- src/bp-templates/bp-nouveau/buddypress/common/filters/user-screens-filters.php
+++ src/bp-templates/bp-nouveau/buddypress/common/filters/user-screens-filters.php
@@ -3,21 +3,21 @@
  * BP Nouveau User screens filters
  *
  * @since 3.0.0
- * @version 3.0.0
+ * @version 15.0.0
  */
 ?>
 <div id="comp-filters" class="component-filters clearfix">
-		<div id="<?php bp_nouveau_filter_container_id(); ?>" class="last filter">
-			<label for="<?php bp_nouveau_filter_id(); ?>" class="bp-screen-reader-text">
-				<span ><?php bp_nouveau_filter_label(); ?></span>
-			</label>
-			<div class="select-wrap">
-				<select id="<?php bp_nouveau_filter_id(); ?>" data-bp-filter="<?php bp_nouveau_filter_component(); ?>">
+	<div id="<?php bp_nouveau_filter_container_id(); ?>" class="last filter">
+		<label for="<?php bp_nouveau_filter_id(); ?>" class="bp-screen-reader-text">
+			<span><?php bp_nouveau_filter_label(); ?></span>
+		</label>
+		<div class="select-wrap">
+			<select id="<?php bp_nouveau_filter_id(); ?>" data-bp-filter="<?php bp_nouveau_filter_component(); ?>">
 
-					<?php bp_nouveau_filter_options(); ?>
+				<?php bp_nouveau_filter_options(); ?>
 
-				</select>
-				<span class="select-arrow" aria-hidden="true"></span>
-			</div>
+			</select>
+			<span class="select-arrow" aria-hidden="true"></span>
 		</div>
+	</div>
 </div>
