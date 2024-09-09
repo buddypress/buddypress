f3344e958703454c34906e520bba23f703bca006
narenin <narendra.sisodiya10@gmail.com>
2024-09-09 16:57:18 +0530

    Implemented the Suggestions

--- src/bp-core/bp-core-functions.php
+++ src/bp-core/bp-core-functions.php
@@ -5295,8 +5295,5 @@ function bp_is_admin( $screen_id = '' ) {
  * @return bool True if bp_classic is active, false otherwise.
  */
 function is_bp_classic_active() {
-    if ( function_exists( 'bp_classic' ) || is_plugin_active( 'bp-classic/bp-classic.php' ) ) {
-        return true;
-    }
-    return false;
+	return function_exists( 'bp_classic' ) || is_plugin_active( 'bp-classic/bp-classic.php' );
 }
