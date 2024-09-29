d738a2d70eb0c8afa294f0943bccbc1264004da1
imath <math.viet@gmail.com>
2024-09-29 03:01:05 +0200

    Update BP Blocks to use BP REST API v2

--- src/bp-core/blocks/block-components/index.asset.php
+++ src/bp-core/blocks/block-components/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '087de50ba3f11a777078');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '0bcd1fe8f2746b343048');
