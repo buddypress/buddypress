a7d14bc03de1347bb387c56ad7560331fde47c0e
Renato Alves <19148962+renatonascalves@users.noreply.github.com>
2025-12-14 23:30:48 -0300

    is_home returns false for BP pages

--- tests/phpunit/testcases/routing/core.php
+++ tests/phpunit/testcases/routing/core.php
@@ -22,9 +22,33 @@ class BP_Tests_Routing_Core extends BP_UnitTestCase {
 		parent::tear_down();
 	}
 
-	function test_wordpress_page() {
+	public function test_wordpress_page() {
 		$this->set_permalink_structure( '/%postname%/' );
 		$this->go_to( '/' );
 		$this->assertEmpty( bp_current_component() );
+	}
+
+	/**
+	 * @ticket BP9300
+	 */
+	public function test_buddypress_directory_is_home_false() {
+		$this->set_permalink_structure( '/%postname%/' );
+
+		$is_home_value = null;
+
+		// Capture the is_home value during pre_get_posts for the main query.
+		$callback = function ( $query ) use ( &$is_home_value ) {
+			if ( $query->is_main_query() ) {
+				$is_home_value = $query->is_home;
+			}
+		};
+
+		add_action( 'pre_get_posts', $callback );
+
+		$this->go_to( bp_get_members_directory_permalink() );
+
+		remove_action( 'pre_get_posts', $callback );
+
+		$this->assertFalse( $is_home_value, 'is_home should be false for a BuddyPress directory page on the main query.' );
 	}
 }
