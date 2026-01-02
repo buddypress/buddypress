18f29dbfe8f324fe7e070e82b1cf443f0a6f4437
Renato Alves <espellcaste@gmail.com>
2026-01-02 19:10:26 +0000

    Switch the function that updates the spam status of the member on multisite configs and deprecate `bp_core_update_member_status`.
    Since WordPress 5.3, the user status is now updated using the globally available `wp_update_user()` function. So let us use it instead and deprecate `bp_core_update_member_status()`.
    
    It includes some minor Docblock changes.
    
    Props imath.
    
    Closes https://github.com/buddypress/buddypress/pull/434/
    See #9173
    Fixes #9167
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@14195 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-core/deprecated/15.0.php
+++ src/bp-core/deprecated/15.0.php
@@ -106,3 +106,47 @@ function bp_activity_delete_by_activity_id( $activity_id ) {
 
 	return bp_activity_delete( array( 'id' => $activity_id ) );
 }
+
+/**
+ * Update the spam status of the member on multisite configs.
+ *
+ * @since 5.0.0
+ * @deprecated 15.0.0
+ *
+ * @param int    $user_id The user ID to spam or ham.
+ * @param string $value   '0' to mark the user as `ham`, '1' to mark as `spam`.
+ * @return bool
+ */
+function bp_core_update_member_status( $user_id = 0, $value = 0 ) {
+	_deprecated_function( __FUNCTION__, '15.0.0' );
+
+	if ( ! is_multisite() || ! $user_id ) {
+		return false;
+	}
+
+	/**
+	 * The `update_user_status()` function is deprecated since WordPress 5.3.0.
+	 * Continue to use it if WordPress current major version is lower than 5.3.
+	 */
+	if ( bp_get_major_wp_version() < 5.3 ) {
+		return update_user_status( $user_id, 'spam', $value );
+	}
+
+	if ( $value ) {
+		$value = '1';
+	}
+
+	// Otherwise use the replacement function.
+	$user = wp_update_user(
+		array(
+			'ID'   => $user_id,
+			'spam' => $value,
+		)
+	);
+
+	if ( is_wp_error( $user ) ) {
+		return false;
+	}
+
+	return true;
+}
