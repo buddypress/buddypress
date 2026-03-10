cc787bcaa70702e49181c81fdc2a950b4e2fcc46
Developer Ravi <khadkaravi170@gmail.com>
2026-03-10 17:47:26 +0545

    Pass post ID to 'the_title' filter
    Adds the latest post ID as the second parameter when applying the_title. This ensures better compatibility with plugins and themes that rely on the post ID for context-aware title filtering.

--- src/bp-blogs/bp-blogs-template.php
+++ src/bp-blogs/bp-blogs-template.php
@@ -786,12 +786,12 @@ function bp_blog_latest_post( $args = array() ) {
 				$retval = sprintf(
 					/* translators: %s: the title of the latest post */
 					__( 'Latest Post: %s', 'buddypress' ),
-					'<a href="' . $blogs_template->blog->latest_post->guid . '">' . apply_filters( 'the_title', $retval, $post_id ) . '</a>'
+					'<a href="' . $blogs_template->blog->latest_post->guid . '">' . apply_filters( 'the_title', $retval, $blogs_template->blog->latest_post->ID ) . '</a>'
 				);
 			} else {
 
 				/** This filter is documented in bp-blogs/bp-blogs-template.php */
-				$retval = '<a href="' . $blogs_template->blog->latest_post->guid . '">' . apply_filters( 'the_title', $retval, $post_id ) . '</a>';
+				$retval = '<a href="' . $blogs_template->blog->latest_post->guid . '">' . apply_filters( 'the_title', $retval, $blogs_template->blog->latest_post->ID ) . '</a>';
 			}
 		}
 
