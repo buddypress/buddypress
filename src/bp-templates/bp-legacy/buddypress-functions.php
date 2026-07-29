68648809690c12bc55ca48b8d44e34f439c11894
David Cavins <david.cavins@gmail.com>
2026-07-29 15:34:43 +0000

    Activity: Ensure user can view item (12.0 branch).
    Before returning an activity item, be sure that the requesting user should be allowed to view the item.
    
    Special thanks to  boonsuan1 (ho boon suan) who first reported this issue responsibly.
    
    Props dcavins, emaralive, johnjamesjacoby, espellcaste, boonsuan1 (ho boon suan), winstoncrooker (Winston Crooker), fran_hack_hu (Fran_hack_hu), waynezinn (wayne g), vvh1te3zz (Wao0o0o), niv09 (Niv), obsidiancladlabs (Charles), enable7_, hillng (Hill N).
    
    git-svn-id: https://buddypress.svn.wordpress.org/branches/12.0@14219 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-templates/bp-legacy/buddypress-functions.php
+++ src/bp-templates/bp-legacy/buddypress-functions.php
@@ -845,6 +845,10 @@ function bp_legacy_theme_object_template_loader() {
 		return;
 	}
 
+	if ( ! bp_current_user_can( 'bp_view', array( 'bp_component' => $object ) ) ) {
+		return;
+	}
+
 	/**
 	 * AJAX requests happen too early to be seen by bp_update_is_directory()
 	 * so we do it manually here to ensure templates load with the correct
@@ -911,6 +915,10 @@ function bp_legacy_theme_requests_template_loader() {
  */
 function bp_legacy_theme_activity_template_loader() {
 	if ( ! bp_is_post_request() ) {
+		return;
+	}
+
+	if ( ! bp_current_user_can( 'bp_view', array( 'bp_component' => 'activity' ) ) ) {
 		return;
 	}
 
@@ -1359,6 +1367,11 @@ function bp_legacy_theme_get_single_activity_content() {
 
 	if ( empty( $activity ) ) {
 		exit; // @todo: error?
+	}
+
+	// Ensure that the user is allowed to read the activity item.
+	if ( ! bp_activity_user_can_read( $activity ) ) {
+		exit;
 	}
 
 	/**
