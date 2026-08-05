8ed3269cd6a3acaecade8272c4a6be122ac9acdd
David Cavins <david.cavins@gmail.com>
2026-08-05 21:50:37 +0000

    Improve security of status update messages.
    • Only allow logged-in users to see status messages. (These messages are like "Group successfully created," so only apply to logged-in users.)
    • When re-loading message content from the user's cookies, don't allow shortcodes to be included.
    
    Many thanks to mikemyers for responsibly reporting the issue.
    
    Props emaralive.
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@14265 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-core/bp-core-functions.php
+++ src/bp-core/bp-core-functions.php
@@ -1669,13 +1669,16 @@ function bp_core_add_message( $message, $type = '' ) {
  * @since 1.1.0
  */
 function bp_core_setup_message() {
+	if ( ! is_user_logged_in() ) {
+		return;
+	}
 
 	// Get BuddyPress.
 	$bp = buddypress();
 
 	// For a mysterious reason WP Playground seems to add a 'deleted' value to `$_COOKIE['bp-message']` when not set.
 	if ( empty( $bp->template_message ) && isset( $_COOKIE['bp-message'] ) && 'deleted' !== $_COOKIE['bp-message'] ) {
-		$bp->template_message = stripslashes( $_COOKIE['bp-message'] );
+		$bp->template_message = strip_shortcodes( stripslashes( $_COOKIE['bp-message'] ) );
 	}
 
 	if ( empty( $bp->template_message_type ) && isset( $_COOKIE['bp-message-type'] ) ) {
