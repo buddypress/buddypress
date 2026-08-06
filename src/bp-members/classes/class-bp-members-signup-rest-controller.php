6676e7cb80c15b4a53890b05d654c6a7e1cda9bf
David Cavins <david.cavins@gmail.com>
2026-08-06 21:25:29 +0000

    Improve security of signup/activate REST endpoint.
    Special thanks to  hilln (Hill N) who first reported this isssue responsibly.
    
    Props hilln (Hill N), 0xdoubled (David Demchak), dcavins, emaralive, vapvarun, espellcaste
    
    git-svn-id: https://buddypress.svn.wordpress.org/trunk@14267 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

--- src/bp-members/classes/class-bp-members-signup-rest-controller.php
+++ src/bp-members/classes/class-bp-members-signup-rest-controller.php
@@ -689,10 +689,10 @@ class BP_Members_Signup_REST_Controller extends WP_REST_Controller {
 		$activation_key = $request->get_param( 'activation_key' );
 
 		// Get the signup to activate thanks to the activation key.
-		$signup    = $this->get_signup_object( $activation_key );
+		$signup    = $this->get_signup_object_by_field( $activation_key, 'activation_key' );
 		$activated = bp_core_activate_signup( $activation_key );
 
-		if ( ! $activated ) {
+		if ( is_wp_error( $activated ) ) {
 			return new WP_Error(
 				'bp_rest_signup_activate_fail',
 				__( 'Fail to activate the signup.', 'buddypress' ),
@@ -739,8 +739,19 @@ class BP_Members_Signup_REST_Controller extends WP_REST_Controller {
 		// Get the activation key.
 		$activation_key = $request->get_param( 'activation_key' );
 
+		// Block numeric IDs to prevent enumeration attacks.
+		if ( is_numeric( $activation_key ) ) {
+			return new WP_Error(
+				'bp_rest_invalid_activation_key_format',
+				__( 'Invalid activation key format.', 'buddypress' ),
+				array(
+					'status' => 400,
+				)
+			);
+		}
+
 		// Check the activation key is valid.
-		if ( $this->get_signup_object( $activation_key ) ) {
+		if ( $this->get_signup_object_by_field( $activation_key, 'activation_key' ) ) {
 			$retval = true;
 		}
 
@@ -971,6 +982,49 @@ class BP_Members_Signup_REST_Controller extends WP_REST_Controller {
 
 		if ( ! empty( $signups['signups'] ) ) {
 			return reset( $signups['signups'] );
+		}
+
+		return false;
+	}
+
+	/**
+	 * Get signup object by specific field with security validation.
+	 *
+	 * @since 15.0.0
+	 *
+	 * @param int|string $identifier Signup identifier.
+	 * @param string $field Signup lookup field ('id', 'email', or 'activation_key').
+	 * @return BP_Signup|false
+	 */
+	public function get_signup_object_by_field( $identifier, $field ) {
+		$signup_args = array();
+
+		if ( 'id' === $field && is_numeric( $identifier ) ) {
+			$signup_args['include'] = array( intval( $identifier ) );
+		} else if ( 'email' === $field && is_email( $identifier ) ) {
+			$signup_args['usersearch'] = $identifier;
+		} else if ( 'activation_key' === $field ) {
+			// The activation key is used when activating a signup.
+
+			// Block numeric IDs to prevent enumeration attacks.
+			if ( is_numeric( $identifier ) ) {
+				return false;
+			}
+
+			// Basic validation: minimum length check.
+			if ( empty( $identifier ) || strlen( $identifier ) < 10 ) {
+				return false;
+			}
+			$signup_args['activation_key'] = $identifier;
+		}
+
+		if ( ! empty( $signup_args ) ) {
+			// Get signups.
+			$signups = \BP_Signup::get( $signup_args );
+
+			if ( ! empty( $signups['signups'] ) ) {
+				return reset( $signups['signups'] );
+			}
 		}
 
 		return false;
